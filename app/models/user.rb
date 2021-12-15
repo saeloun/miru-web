d61231e5719cde477809ae0dad43e90a562f488b
JUDIS <61043700+judis007@users.noreply.github.com>
2021-12-15 12:23:27 +0530

    Added Devise and the User model (#21)
    * added devise and validations for user model
    
    * added RSpec tests for the User model
    
    * added RSpec as the github action
    
    * added the 'test' folder to gitignore
    
    * removed the user_test file from the PR
    
    * removed user test file

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  validates :first_name, :last_name, :email, :encrypted_password, presence: true
+  validates :first_name, :last_name, length: { maximum: 50 }
+  validates :email, format: { with: /^([^\s]+)((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, multiline: true }
+  validates :encrypted_password, length: { minimum: 6 }
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
