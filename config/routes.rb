1a1a7a4fcc4d523f4a100ec680d41ec50a41d326
JUDIS <61043700+judis007@users.noreply.github.com>
2021-12-15 12:23:27 +0530

    Added Devise and the User model (#21)
    * added devise and validations for user model
    
    * added RSpec tests for the User model
    
    * added RSpec as the github action
    
    * added the 'test' folder to gitignore
    
    * removed the user_test file from the PR
    
    * removed user test file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   get "home/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
