7f940dde539d052feebecf272ebffcc589f86df6
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2021-12-17 17:18:44 +0530

    React login structure added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,12 @@
 import "../stylesheets/application.scss";
 
-import("@rails/ujs").start();
-import("@rails/activestorage").start();
+import * as ActiveStorage from "@rails/activestorage";
+import Rails from "@rails/ujs";
+import * as ReactRailsUJS from "react_ujs";
+
+Rails.start();
+ActiveStorage.start();
 
 // Support component names relative to this directory:
 const componentRequireContext = require.context("components", true);
-import * as ReactRailsUJS from "react_ujs";
 ReactRailsUJS.useContext(componentRequireContext);
--- postcss.config.js
+++ postcss.config.js
@@ -7,6 +7,8 @@ module.exports = {
         flexbox: 'no-2009'
       },
       stage: 3
-    })
+    }),
+    require("tailwindcss")("./tailwind.config.js"),
+    require("autoprefixer"),
   ]
 }
