fe735a2877c4c059ebb6ef46174ee53d2495be0c
Aniket Kaushik <aniket@saeloun.com>
2021-12-19 03:01:31 +0530

    LogIn page design with responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'xsm': '380px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+    },
     extend: {
       colors: {
         miru: {
@@ -15,7 +23,7 @@ module.exports = {
       },
       spacing: {
         112: "28rem",
-      }
+      },
     },
   },
   variants: {
