188f13f6f5ed9fcb08aad9dd589090c2db7b4daa
Aniket Kaushik <aniket@saeloun.com>
2021-12-23 01:03:03 +0530

    added container tag on Login Page

--- app/javascript/components/Account/LoginPage/index.tsx
+++ app/javascript/components/Account/LoginPage/index.tsx
@@ -2,6 +2,9 @@ import * as React from "react";
 
 import authApi from "../../apis/auth";
 import { setAuthHeaders } from "../../apis/axios";
+
+import Container from "../../Container";
+
 import { setToLocalStorage } from "../../helpers/storage";
 
 const LoginPage = () => {
@@ -24,75 +27,70 @@ const LoginPage = () => {
     }
   };
   return (
-    <div className="pt-10/100 px-10 xsm:px-20/100 sm:px-26/100 md:px-30/100 lg:px-32/100 xl:px-36/100">
-      <div className="font-jakartasans">
-        <h2 className="font-medium text-center text-3xl lg:text-4xl text-mirublack-1000 mb-5">
-          Miru
+    <Container>
+      <h2 className="font-medium text-center text-3xl lg:text-4xl text-mirublack-1000 mb-5">
+        Miru
+      </h2>
+      <div className="p-5 2xl:p-10 shadow-xl rounded-lg">
+        <h2 className="text-miru-1000 lg:mb-3 text-base font-medium">
+          Sign In
         </h2>
-        <div className="p-5 2xl:p-10 shadow-xl rounded-lg">
-          <h2 className="text-miru-1000 lg:mb-3 text-base font-medium">
-            Sign In
-          </h2>
 
-          <form>
+        <form>
+          <div className="field">
+            <label className="text-xs text-mirublack-1000" htmlFor="email">
+              Email
+            </label>
+            <br />
+            <input
+              className="h-8 w-full focus:outline-none bg-mirugrey-200 text-sm"
+              id="email"
+              type="text"
+              placeholder="Enter Email"
+              onChange={e => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="mt-3">
             <div className="field">
-              <label className="text-xs text-mirublack-1000" htmlFor="email">
-                Email
+              <label className="text-xs text-mirublack-1000" htmlFor="password">
+                Password
               </label>
               <br />
               <input
                 className="h-8 w-full focus:outline-none bg-mirugrey-200 text-sm"
-                id="email"
-                type="text"
-                placeholder="Enter Email"
-                onChange={e => setEmail(e.target.value)}
+                id="password"
+                type="password"
+                placeholder="Enter Password"
+                onChange={e => setPassword(e.target.value)}
               />
             </div>
+          </div>
 
-            <div className="mt-3">
-              <div className="field">
-                <label
-                  className="text-xs text-mirublack-1000"
-                  htmlFor="password"
-                >
-                  Password
-                </label>
-                <br />
-                <input
-                  className="h-8 w-full focus:outline-none bg-mirugrey-200 text-sm"
-                  id="password"
-                  type="password"
-                  placeholder="Enter Password"
-                  onChange={e => setPassword(e.target.value)}
-                />
-              </div>
-            </div>
-
-            <div className="actions mt-3">
-              <button
-                className="h-8 w-full bg-mirugrey-400 font-bold text-white text-base hover:bg-mirublack-1000"
-                onClick={handleSubmit}
-              >
-                SIGN IN
-              </button>
-            </div>
-          </form>
-          <div className="mt-3 text-center">
-            <a
-              className="text-center text-mirugrey-400 text-xs hover:text-mirublack-1000"
-              href="#"
+          <div className="actions">
+            <button
+              className="h-8 mt-6 w-full bg-mirugrey-400 font-bold text-white text-base hover:bg-mirublack-1000"
+              onClick={handleSubmit}
             >
-              Forgot Password?
-            </a>
+              SIGN IN
+            </button>
           </div>
-          <div className="text-center">
-            <p className="text-center text-mirugrey-400 text-xs hover:text-mirublack-1000">
-              Don't have an account? <a href="/signup">Sign Up</a>
-            </p>
-          </div>
+        </form>
+        <div className="mt-3 text-center">
+          <a
+            className="text-center text-mirugrey-400 text-xs hover:text-mirublack-1000"
+            href="#"
+          >
+            Forgot Password?
+          </a>
+        </div>
+        <div className="text-center">
+          <p className="text-center text-mirugrey-400 text-xs hover:text-mirublack-1000">
+            Don't have an account? <a href="/signup">Sign Up</a>
+          </p>
         </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
