e0b5b0161dc9e71a21278462af37054a81c2eeed
Judis <judismjeevan@gmail.com>
2021-12-22 20:42:54 +0530

    added user creation endpoint

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,23 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  protect_from_forgery with: :null_session
   respond_to :json
+
+  def create
+    @user = User.new
+
+    @user.first_name = params[:first_name]
+    @user.last_name = params[:last_name]
+    @user.email = params[:email]
+    @user.password = params[:password]
+
+    if @user.save
+      render json: @user
+    else
+      head(:unprocessable_entity)
+    end
+  end
 
   private
     def respond_with(resource, _opts = {})
--- app/models/user.rb
+++ app/models/user.rb
@@ -35,5 +35,6 @@ class User < ApplicationRecord
   devise :database_authenticatable,
         :jwt_authenticatable,
         :registerable,
+        :recoverable, :rememberable, :validatable,
         jwt_revocation_strategy: JwtDenylist
 end
