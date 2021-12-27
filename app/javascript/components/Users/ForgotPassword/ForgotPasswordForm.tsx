c57706dea804838786cbe14a33d8e5604a09f390
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2021-12-27 13:22:20 +0530

    React based user auth setup added

---
+++ app/javascript/components/Users/ForgotPassword/ForgotPasswordForm.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+
+interface ForgotPasswordFormProps {
+  setEmail: (email: string) => void;
+  handleSubmit: (e) => void;
+}
+
+const ForgotPasswordForm = ({
+  setEmail,
+  handleSubmit
+}: ForgotPasswordFormProps) => (
+  <form>
+    <div className="field">
+      <label htmlFor="email" className="text-xs text-miru-1000">
+          Email
+      </label>
+      <br />
+      <input
+        type="email"
+        className="h-8 w-full focus:outline-none bg-miru-200 text-sm"
+        onChange={e => setEmail(e.target.value)}
+      />
+    </div>
+
+    <div className="actions">
+      <button
+        type="submit"
+        className="mt-5 h-8 w-full bg-miru-400 font-bold text-white text-base hover:bg-miru-1000"
+        onClick={handleSubmit}
+      >
+          RESET PASSWORD
+      </button>
+    </div>
+  </form>
+);
+
+export default ForgotPasswordForm;
---
+++ app/javascript/components/Users/Login/LoginForm.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+
+interface LoginFormProps {
+  setEmail: (email: string) => void;
+  setPassword: (password: string) => void;
+  handleSubmit: (e) => void;
+}
+
+const LoginForm = ({ setEmail, setPassword, handleSubmit }: LoginFormProps) => (
+  <form>
+    <div className="field">
+      <label className="text-xs text-miru-1000" htmlFor="email">
+          Email
+      </label>
+      <br />
+      <input
+        className="h-8 w-full focus:outline-none bg-miru-200 text-sm"
+        type="text"
+        placeholder="Enter Email"
+        onChange={e => setEmail(e.target.value)}
+      />
+    </div>
+
+    <div className="mt-3">
+      <div className="field">
+        <label className="text-xs text-miru-1000" htmlFor="password">
+            Password
+        </label>
+        <br />
+        <input
+          className="h-8 w-full focus:outline-none bg-miru-200 text-sm"
+          type="password"
+          placeholder="Enter Password"
+          onChange={e => setPassword(e.target.value)}
+        />
+      </div>
+    </div>
+
+    <div className="actions mt-3">
+      <button
+        className="h-8 w-full bg-miru-400 font-bold text-white text-base hover:bg-miru-1000"
+        onClick={handleSubmit}
+      >
+          SIGN IN
+      </button>
+    </div>
+  </form>
+);
+
+export default LoginForm;
