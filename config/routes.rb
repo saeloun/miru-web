d945fe3ccc950738848fb23c7b3d8835a8016d41
Aniket Kaushik <aniket@saeloun.com>
2022-01-03 17:16:31 +0530

    added admin fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get "time_trackings/index"
   devise_for :users, controllers: { registrations: "users/registrations" }
   root "time_trackings#index"
   resources :dashboard, only: [:index]
