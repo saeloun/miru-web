b2357b51001b4746320e51033722094ceda172e8
Judis <judismjeevan@gmail.com>
2022-01-03 20:16:41 +0530

    adding the org setup

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,10 @@
 
 class DashboardController < ApplicationController
   def index
+    if current_user.owner?
+      redirect_to new_company_path
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
