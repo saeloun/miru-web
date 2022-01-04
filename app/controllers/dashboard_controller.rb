ffd87c096528f0770de5b11a6a6a2b587d1ef06c
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2022-01-04 13:47:13 +0530

    minor fix

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController
-  before_action :validate_company!
-
   def index
   end
-
-  private
-    def validate_company!
-      redirect_to new_company_path if current_user.company_id.nil? && current_user.owner?
-    end
 end
