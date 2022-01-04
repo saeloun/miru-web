ed4125ea721a5869d249a9e887546bc66fdd8e0a
Judis <judismjeevan@gmail.com>
2022-01-04 12:04:42 +0530

    redirecting after org setup to the root page

---
+++ db/migrate/20220104045826_create_company.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class CreateCompany < ActiveRecord::Migration[7.0]
+  def change
+    create_table :companies do |t|
+      t.string "company"
+      t.text "address"
+      t.integer "business_phone"
+      t.decimal "base_currency"
+      t.decimal "standard_price"
+      t.string "fiscal_year_end"
+      t.date "date"
+
+      t.timestamps
+    end
+  end
+end
