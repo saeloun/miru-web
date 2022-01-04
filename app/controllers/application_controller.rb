1f5d2a0c5549d707bc3370665fe2000879665215
Aniket K <94526737+aniket-k-kaushik@users.noreply.github.com>
2022-01-04 12:42:00 +0530

    Fixes#54 Fixes#33 build the root pages & Logout  (#56)
    * login page small fix
    
    * added partial for navbar
    
    * added miru custom colours
    
    * added logo position
    
    * added dropdown function
    
    * added time_tracking controller removed home controller
    
    * added admin fields
    
    * added if condition for admin navbar fields
    
    * admin condition fix
    
    * removed owner condition
    
    * added color fix, removed user_signin, added newline
    
    * added owner in user model
    
    * added condition for owner in navbar
    
    * env.example fix
    
    * function for defautt pages, dashboard for admin owner, timetracking for employee
    
    * rspec fix
    
    * minor fix
    
    * logo image fix
    
    * Added couple of functionality changes
    
    * added hover to profile dropdown
    
    * minor fix
    
    * favicon fix
    
    * avtar ring hover design
    
    Co-authored-by: Aniket Kaushik <aniket@saeloun.com>
    Co-authored-by: Akhil G Krishnan <akhilgkrishnan4u@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def redirect_path
+    path = case current_user.role
+           when "owner" || "admin"
+             dashboard_index_path
+           else
+             time_trackings_path
+    end
+    redirect_to path
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,9 @@ import * as ActiveStorage from "@rails/activestorage";
 import Rails from "@rails/ujs";
 import * as ReactRailsUJS from "react_ujs";
 
+import "alpine-turbo-drive-adapter";
+require("alpinejs");
+
 import "@fontsource/plus-jakarta-sans";
 
 global.toastr = require("toastr");
