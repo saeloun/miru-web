9c61289a5313809e3cfaac2adc9a1f9ca64b15b9
Judis <judismjeevan@gmail.com>
2022-01-04 12:04:42 +0530

    redirecting after org setup to the root page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController
+  before_action :validate_company!
+
   def index
-    if current_user.owner?
-      redirect_to new_company_path
-    else
-      redirect_to new_user_session_path
-    end
   end
+
+  private
+    def validate_company!
+      redirect_to new_company_path if current_user.company_id.nil? && current_user.owner?
+    end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: companies
+#
+#  id              :integer          not null, primary key
+#  company         :string
+#  address         :text
+#  business_phone  :integer
+#  base_currency   :decimal(, )
+#  standard_price  :decimal(, )
+#  fiscal_year_end :string
+#  date            :date
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 # frozen_string_literal: true
 
 class Company < ApplicationRecord
