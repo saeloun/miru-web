da09df2178876ff08b147f62b2d19323c5c27928
Judis <judismjeevan@gmail.com>
2022-01-05 13:18:09 +0530

    deleted unwanted migrations

--- db/migrate/20211214133434_devise_create_users.rb
+++ db/migrate/20211214133434_devise_create_users.rb
@@ -9,6 +9,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       t.string :last_name,          null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.integer :role,              null: false
+      t.integer :company_id,        null: false
 
       ## Recoverable
       t.string   :reset_password_token
