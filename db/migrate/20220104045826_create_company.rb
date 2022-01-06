60b3d5a9281ae2f3fc8609e868215d9d5f38f355
Abinash Panda <abinashp@protonmail.ch>
2022-01-06 13:25:31 +0530

    seed added and typo fixed

--- db/migrate/20220104045826_create_company.rb
+++ db/migrate/20220104045826_create_company.rb
@@ -6,8 +6,8 @@ class CreateCompany < ActiveRecord::Migration[7.0]
       t.string "name", null: false
       t.text "address", null: false
       t.string "business_phone"
-      t.string "base_currency"
-      t.decimal "standard_price", null: false, precision: 6, scale: 2
+      t.string "base_currency", null: false, default: "USD"
+      t.decimal "standard_price", null: false, default: 0
       t.string "fiscal_year_end"
       t.string "date_format"
       t.string "country", null: false
