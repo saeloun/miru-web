af854e041331c8c78110374c402926b02bba7c69
Abinash Panda <abinashp@protonmail.ch>
2022-01-06 20:03:41 +0530

    typo fixed in AddCompanyIdToUser

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,37 @@
 # frozen_string_literal: true
 
 # == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  first_name             :string           default(""), not null
+#  last_name              :string           default(""), not null
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default("0"), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string
+#  last_sign_in_ip        :string
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
+#  unconfirmed_email      :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  role                   :integer
+#  company_id             :integer          not null
+#
+# Indexes
+#
+#  index_users_on_company_id            (company_id)
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
 
 # Table name: users
 #
