0a9272f6eb751c58e56bf757e0405db56b74a4db
Keshav Biswa <keshavbiswa21@gmail.com>
2022-01-10 19:46:09 +0530

    code review changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, :validate_company!
 
-  def redirect_path
-    path = if current_user.has_any_role?(:owner, :admin)
-      dashboard_index_path
-    else
-      time_trackings_path
-    end
-  end
-
   private
     def validate_company!
       return if current_user.nil?
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,31 @@
 # frozen_string_literal: true
 
 # Create company
+puts "Creating company"
 company = Company.create!(
   name: "Saeloun Inc",
   address: "31R Providence Rd Westford MA, 01886",
   country: "US",
   timezone: "EST"
 )
+puts "Company Created"
 
-# Create Owner user
-company.users.create!(
-  first_name: "Vipul",
-  last_name: "A M",
-  email: "vipul@example.com",
-  password: "password",
-  password_confirmation: "password",
-  confirmed_at: DateTime.now
-)
+users = [
+          { first_name: "Vipul", last_name: "A M", email: "vipul@example.com", role: :owner },
+          { first_name: "Supriya", last_name: "Agarwal", email: "supriya@example.com", role: :admin },
+          { first_name: "Akhil", last_name: "G Krishnan", email: "akhil@example.com", role: :employee }
+        ]
 
-# Create Admin user
-company.users.create!(
-  first_name: "Supriya",
-  last_name: "Agarwal",
-  email: "supriya@example.com",
-  password: "password",
-  password_confirmation: "password",
-  confirmed_at: DateTime.now
-)
-
-# Create Employee User
-company.users.create!(
-  first_name: "Akhil",
-  last_name: "G Krishnan",
-  email: "akhil@example.com",
-  password: "password",
-  password_confirmation: "password",
-  confirmed_at: DateTime.now
-)
+puts "Creating users for #{company.name}"
+users.each do |user|
+  company_user = company.users.create!(
+    first_name: user[:first_name],
+    last_name: user[:last_name],
+    email: user[:email],
+    password: "password",
+    password_confirmation: "password",
+    confirmed_at: Time.current
+  )
+  company_user.add_role(user[:role])
+end
+puts "#{users.size} Users created"
