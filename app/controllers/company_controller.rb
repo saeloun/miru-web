c1e3d4a3be1b395861e1a883b47df8e35ea0eade
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2022-01-10 16:54:57 +0530

    minor fix

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -28,5 +28,6 @@ class CompanyController < ApplicationController
       if current_user.company.present?
         flash[:error] = "You already have a company"
         redirect_to root_path
-=======
+      end
+    end
 end
