b90c57cce0664d69f8d7d4c948b78aa4a759f8b6
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2022-01-10 15:53:49 +0530

    Add minor changes

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -2,6 +2,7 @@
 
 class CompanyController < ApplicationController
   skip_before_action :validate_company!, only: [:create, :new]
+  before_action :company_validation, only: [:new, :create]
 
   def new
     render
@@ -14,11 +15,19 @@ class CompanyController < ApplicationController
 
     redirect_to root_path
   rescue
+    flash[:error] = "Company creation failed"
     Rails.logger.errors "DEBUG::COMPANY_CONTROLLER::CREATE"
   end
 
   private
     def company_params
       params.permit(:name, :address, :business_phone, :country, :timezone, :base_currency, :standard_price, :fiscal_year_end, :date_format)
+    end
+
+    def company_validation
+      if current_user.company.present?
+        flash[:error] = "You already have a company"
+        redirect_to root_path
+      end
     end
 end
