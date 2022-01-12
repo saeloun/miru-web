a647fc8a5fce8d8049c86f43f906d488889184e3
Aniket Kaushik <aniket@saeloun.com>
2022-01-12 13:16:55 +0530

    added user profile

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,6 +5,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :avatar])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :avatar])
+    end
+
+    def update_resource(resource, params)
+      if params[:current_password].blank?
+        resource.update_without_password(params.except(:current_password))
+      else
+        resource.update_with_password(params)
+      end
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def user_avatar(user)
+    if user.avatar.attached?
+      user.avatar
+    else
+      image_url "avtar.svg"
+    end
+  end
 end
