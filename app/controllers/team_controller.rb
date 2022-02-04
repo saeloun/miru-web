f265625147c50d97d4f2bd1757af21543f82577a
Aniket Kaushik <aniket.k.kaushik@outlook.com>
2022-02-04 13:31:24 +0530

    role update feature added

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,30 +1,30 @@
 # frozen_string_literal: true
 
 class TeamController < ApplicationController
-  # after_action :assign_role, only: [:update]
-  before_action :set_user, only: %i[show update]
+  after_action :assign_role, only: [:update]
 
   def index
     @teams = current_company.users
   end
 
   def show
+    @user = user
   end
 
   def update
-    if @user.invitation_accepted?
-      if @user.update(user_params)
+    if user.invitation_accepted?
+      if user.update(user_params)
         redirect_to team_index_path
       end
     else
-      @user.skip_reconfirmation!
-      if @user.email != (user_params[:email])
-        if @user.update(user_params)
-          @user.invite!
+      user.skip_reconfirmation!
+      if user.email != (user_params[:email])
+        if user.update(user_params)
+          user.invite!
           redirect_to team_index_path
         end
       else
-        if @user.update(user_params)
+        if user.update(user_params)
           redirect_to team_index_path
         end
       end
@@ -32,9 +32,9 @@ class TeamController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
-    @user.discard
-    redirect_to "/team"
+    user = User.find(params[:id])
+    user.discard
+    redirect_to team_index_path
   end
 
   private
@@ -42,17 +42,14 @@ class TeamController < ApplicationController
       params.require(:user).permit(:first_name, :last_name, :email, :confirmation_token)
     end
 
-    # def user
-    #   @_user ||= User.find(params[:id])
-    # end
-    # def assign_role
-    #   @user.remove_role
-    #   if @user.errors.empty?
-    #     @user.add_role(params[:user][:roles].downcase.to_sym)
-    #   end
-    # end
-    #
-    def set_user
-      @user = User.find(params[:id])
+    def user
+      @_user ||= User.find(params[:id])
+    end
+
+    def assign_role
+      user.remove_role(user.roles.first.name)
+      if user.errors.empty?
+        user.add_role(params[:user][:roles].downcase.to_sym)
+      end
     end
 end
