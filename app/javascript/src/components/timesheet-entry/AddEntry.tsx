47e714c760d1ac440ba28a62ec6c4e2bb3b3f265
Abinash Panda <abinashp@protonmail.ch>
2022-02-07 21:34:26 +0530

    eslint added

--- app/javascript/src/components/timesheet-entry/AddEntry.tsx
+++ app/javascript/src/components/timesheet-entry/AddEntry.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import timesheetEntryApi from "../../apis/timesheet-entry";
-import { getMonthFromString } from "../../helpers/date-parser";
 import { minutesFromHHMM, minutesToHHMM } from "../../helpers/hhmm-parser";
 import { getNumberWithOrdinal } from "../../helpers/ordinal";
 
