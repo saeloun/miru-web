1c2d80932b092020ac94a2530d34c8031c4b341f
Abinash Panda <abinashp@protonmail.ch>
2022-02-08 17:13:41 +0530

    error handler concern added

--- app/controllers/internal_api/v1/application_controller.rb
+++ app/controllers/internal_api/v1/application_controller.rb
@@ -1,16 +1,13 @@
 # frozen_string_literal: true
 
 class InternalApi::V1::ApplicationController < ActionController::API
+  include ErrorHandler
+
   before_action :authenticate_user!
   helper_method :current_company
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   private
     def current_company
       @_current_company ||= current_user&.company
-    end
-
-    def record_not_found
-      render json: { success: false, message: "Record not found" }
     end
 end
