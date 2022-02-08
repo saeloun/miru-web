0e43313a1676e29bea0cbdd319e950a65925a9a9
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2022-02-08 13:30:58 +0530

    Visual design added for client listing page (#119)
    * Visual design added for client listing page
    
    * Add another th tag
    
    Co-authored-by: Rohit Joshi <rohit.joshiadvanced@gmail.com>

--- app/javascript/components/Clients/Client.tsx
+++ app/javascript/components/Clients/Client.tsx
@@ -1,49 +1,31 @@
 import * as React from "react";
-import { IClient } from "apis/clients";
 
-const Client = ({
-  id,
-  company_id,
-  name,
-  email,
-  phone,
-  address,
-  country,
-  timezone,
-  created_at
-}: IClient) => (
+export interface IClient {
+  id: number;
+  name: string;
+  email: string;
+  editIcon: string;
+  deleteIcon: string;
+}
+
+const Client = ({ id, name, email, editIcon, deleteIcon }: IClient) => (
   <tr key={id}>
-    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-      {id}
-    </td>
-    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-      {company_id}
-    </td>
-    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-miru-dark-purple-1000">
       {name}
     </td>
-    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-miru-dark-purple-1000">
       {email}
     </td>
-    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-      {phone}
+    <td className="px-6 py-4 whitespace-nowrap text-right font-black">123</td>
+    <td className="px-2 py-4 whitespace-nowrap text-right text-sm font-medium">
+      <button>
+        <img src={editIcon} alt="" />
+      </button>
     </td>
-    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-      {address}
-    </td>
-    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-      {country}
-    </td>
-    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-      {timezone}
-    </td>
-    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-      {created_at}
-    </td>
-    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-      <a href="#" className="text-indigo-600 hover:text-indigo-900">
-        Edit
-      </a>
+    <td className="px-2 py-4 whitespace-nowrap text-right text-sm font-medium">
+      <button>
+        <img src={deleteIcon} alt="" />
+      </button>
     </td>
   </tr>
 );
