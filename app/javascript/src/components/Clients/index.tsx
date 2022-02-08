5dc8936188331ebdcae727e5fdc4da816025fad5
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2022-02-08 20:53:11 +0530

    BugFix: Employee can able to edit and delete member issue fixed

--- app/javascript/src/components/Clients/index.tsx
+++ app/javascript/src/components/Clients/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Client from "./Client";
 
-const Clients = ({ clients, editIcon, deleteIcon }) => (
+const Clients = ({ clients, editIcon, deleteIcon, isAdminUser }) => (
   <div className="flex flex-col">
     <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
@@ -13,19 +13,19 @@ const Clients = ({ clients, editIcon, deleteIcon }) => (
                   scope="col"
                   className="px-6 py-5 text-left text-sm font-semibold text-miru-dark-purple-600 tracking-wider"
                 >
-                  CLIENT
+                    CLIENT
                 </th>
                 <th
                   scope="col"
                   className="px-6 py-5 text-left text-sm font-semibold text-miru-dark-purple-600 tracking-wider"
                 >
-                  EMAIL ID
+                    EMAIL ID
                 </th>
                 <th
                   scope="col"
                   className="px-6 py-5 text-right text-sm font-semibold text-miru-dark-purple-600 tracking-wider"
                 >
-                  HOURS LOGGED
+                    HOURS LOGGED
                 </th>
                 <th scope="col" className="relative px-6 py-3"></th>
                 <th scope="col" className="relative px-6 py-3"></th>
@@ -38,6 +38,7 @@ const Clients = ({ clients, editIcon, deleteIcon }) => (
                   {...client}
                   editIcon={editIcon}
                   deleteIcon={deleteIcon}
+                  isAdminUser={isAdminUser}
                 />
               ))}
             </tbody>
