1bdad00720d20efcb52ba5d97080eec0c81bdd54
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-02-09 13:06:57 +0530

    Use one-liners for company validation tests

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -3,36 +3,12 @@
 require "rails_helper"
 
 RSpec.describe Company, type: :model do
-  let(:company) { build(:company) }
-
-  it "is valid with valid attributes" do
-    expect(company).to be_valid
-  end
-
-  context "checking the presence of each attribute" do
-    it "is not valid without a name" do
-      company.name = nil
-      expect(company).to_not be_valid
-    end
-
-    it "is not valid without an address" do
-      company.address = nil
-      expect(company).to_not be_valid
-    end
-
-    it "is not valid without a standard_price" do
-      company.standard_price = nil
-      expect(company).to_not be_valid
-    end
-
-    it "is not valid without an country" do
-      company.country = nil
-      expect(company).to_not be_valid
-    end
-  end
-
   describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:address) }
     it { is_expected.to validate_presence_of(:business_phone) }
+    it { is_expected.to validate_presence_of(:standard_price) }
+    it { is_expected.to validate_presence_of(:country) }
     it { is_expected.to validate_presence_of(:base_currency) }
   end
 end
