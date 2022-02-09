2c2ba3ad1677c0254f1a1147efef4938a5d396fa
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-02-09 12:03:33 +0530

    Avoid instance vars in views, pass local vars instead

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,21 +2,24 @@
 
 class ClientsController < ApplicationController
   def index
-    @clients = current_company.clients
+    clients
   end
 
   def create
-    @client = Client.new(client_params)
-    @client.company_id = current_company.id
-    if @client.save
+    client = Client.new(client_params)
+    client.company_id = current_company.id
+    if client.save
       redirect_to clients_path
     else
-      render :index, status: :unprocessable_entity
+      render :index, client: client, status: :unprocessable_entity
       flash.now[:error] = "Client creation failed"
     end
   end
 
   private
+    def clients
+      @_clients ||= current_company.clients
+    end
     def client_params
       params.require(:client).permit(:name, :email, :phone, :address, :country, :timezone)
     end
