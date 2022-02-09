0a379c36f0ab45265fa4c82114fead61013f49e8
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-02-09 14:19:14 +0530

    Add validations for org setup page (#106)
    * Add validations for org setup page
    
    * Move helper methods to ApplicationHelper
    
    * Use  instead of  since  is called in next line
    
    * Enhance org settings
    
    * Align del button to center and move JS code to a separate file
    
    * Fix failure flash message being displayed even on success
    
    * Add validation tests for company model
    
    * Use Rspec's new expectation syntax
    
    * Add success message on company updation
    
    * Change success message on company updation
    
    * Use locales for success and failure messages
    
    * Change company creation success flash message
    
    * Remove unneeded var and unused param
    
    * Change success message on profile updation
    
    * Use one-liners for company validation tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,22 @@ module ApplicationHelper
       image_url "avatar.svg"
     end
   end
+
+  def error_message_on(resource, attribute)
+    return unless resource.respond_to?(:errors) && resource.errors.include?(attribute)
+    field_error(resource, attribute)
+  end
+
+  def error_message_class(resource, attribute)
+    if resource.respond_to?(:errors) && resource.errors.include?(attribute)
+      "border-red-600 focus:ring-red-600 focus:border-red-600"
+    else
+      "border-gray-100 focus:ring-miru-gray-1000 focus:border-miru-gray-1000"
+    end
+  end
+
+  private
+    def field_error(resource, attribute)
+      resource.errors[attribute].first
+    end
 end
