f5daf50765d8f0a730de2f92b1dd99c564a4621a
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2022-02-09 18:10:11 +0530

    BugFix: Employee can able to edit and delete member issue fixed (#125)
    * BugFix: Employee can able to edit and delete member issue fixed
    
    * eslint fix
    
    * eslint fix added

--- app/javascript/src/apis/axios.ts
+++ app/javascript/src/apis/axios.ts
@@ -15,7 +15,7 @@ export const setAuthHeaders = () => {
     Accept: "application/json",
     "Content-Type": "application/json",
     "X-CSRF-TOKEN": document
-      .querySelector('[name="csrf-token"]')
+      .querySelector("[name=\"csrf-token\"]")
       .getAttribute("content")
   } as CommonHeaderProperties;
 };
