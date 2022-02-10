56cf7d97e02ef7dcce6a985f449b7a1c9c7c9831
Rohit Joshi <rohit.joshiadvanced@gmail.com>
2022-02-10 22:50:30 +0530

    Use is expected to instead of should

--- spec/models/project_member_spec.rb
+++ spec/models/project_member_spec.rb
@@ -5,11 +5,11 @@ require "rails_helper"
 RSpec.describe ProjectMember, type: :model do
   subject { build(:project_member) }
   describe "validations" do
-    it { should validate_presence_of(:hourly_rate) }
+    it { is_expected.to validate_presence_of(:hourly_rate) }
   end
 
   describe "associations" do
-    it { should belong_to(:user) }
-    it { should belong_to(:project) }
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:project) }
   end
 end
