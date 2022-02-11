390bec8f79c2a5b77f5f44543a53e5d310a9a80f
Keshav Biswa <keshavbiswa21@gmail.com>
2022-02-11 13:31:23 +0530

    added company_spec

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -3,7 +3,14 @@
 require "rails_helper"
 
 RSpec.describe Company, type: :model do
-  describe "validations" do
+  describe "Associations" do
+    it { is_expected.to have_many(:users).dependent(:destroy) }
+    it { is_expected.to have_many(:clients).dependent(:destroy) }
+    it { is_expected.to have_many(:projects).through(:clients).dependent(:destroy) }
+    it { is_expected.to have_one_attached(:logo) }
+  end
+
+  describe "Validations" do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:address) }
     it { is_expected.to validate_presence_of(:business_phone) }
