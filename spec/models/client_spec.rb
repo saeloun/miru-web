db4bbf25ba7de20c0796c98a2c610f4758c6ef38
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-02-11 19:25:38 +0530

    Fix model spec

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -8,12 +8,12 @@ RSpec.describe Client, type: :model do
   describe "Validations" do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email) }
     it { is_expected.to allow_value("valid@email.com").for(:email) }
     it { is_expected.not_to allow_value("invalid@email").for(:email) }
   end
 
   describe "Associations" do
-    it { is_expected.to validate_uniqueness_of(:email) }
     it { is_expected.to have_many(:projects) }
     it { is_expected.to have_many(:timesheet_entries) }
     it { is_expected.to belong_to(:company) }
