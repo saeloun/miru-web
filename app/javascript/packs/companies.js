94fb230f91d1cfa21dc5bc03bfc54d7d074aa345
Aniket K <94526737+aniket-k-kaushik@users.noreply.github.com>
2022-02-11 21:35:54 +0530

    Organisation settings page- we can save Blank Company Address. (#146)
    * removed validate company
    
    * removed company address test validation from company_spec
    
    * change script position from company/show to pack/company.js
    
    * updated en.yml for company
    
    * en.yml for company/new
    
    * added en.yml fix
    
    * added bottom margin
    
    * name validation fixed

--- app/javascript/packs/companies.js
+++ app/javascript/packs/companies.js
@@ -19,3 +19,15 @@ window.showImage = function () {
     }
   };
 };
+function showImage () {
+  return {
+    showPreview (event) {
+      if (event.target.files.length > 0) {
+        const src = URL.createObjectURL(event.target.files[0]);
+        const preview = document.getElementById("preview");
+        preview.src = src;
+        preview.style.display = "block";
+      }
+    }
+  };
+}
