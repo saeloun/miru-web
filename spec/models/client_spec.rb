44a98a6a4fb87ef4d548b8b0000aab55779f66f6
Keshav Biswa <keshavbiswa21@gmail.com>
2022-02-11 13:28:56 +0530

    Added rubocop rspec cops and client_spec

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -3,30 +3,17 @@
 require "rails_helper"
 
 RSpec.describe Client, type: :model do
-  let(:client) { build(:client) }
+  subject { build(:client) }
 
-  it "is valid with valid attributes" do
-    expect(client).to be_valid
+  describe "Associations" do
+    it { is_expected.to have_many(:projects) }
+    it { is_expected.to belong_to(:company) }
   end
-
-  it "is not valid without a name" do
-    client.name = nil
-    expect(client).to_not be_valid
-  end
-
-  it "is not valid without an email" do
-    client.email = nil
-    expect(client).to_not be_valid
-  end
-
-  it "is not valid without a invalid email" do
-    client.email = "invalid_email"
-    expect(client).to_not be_valid
-  end
-
-  it "is not valid if email is not unique" do
-    client.save
-    client2 = build(:client, email: client.email)
-    expect(client2).to_not be_valid
+  describe "Validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.to allow_value("email@addresse.foo").for(:email) }
+    it { is_expected.to_not allow_value("foo").for(:email) }
   end
 end
