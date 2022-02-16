573c1c5f47d0d1d12a5ac073795fdec1c8d4a6da
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-02-16 18:19:48 +0530

    Attempt to render edit client view

--- app/javascript/src/components/Clients/Client.tsx
+++ app/javascript/src/components/Clients/Client.tsx
@@ -8,6 +8,7 @@ export interface IClient {
   editIcon: string;
   deleteIcon: string;
   isAdminUser: boolean;
+  setShowEditDialog: any;
 }
 
 const Client = ({
@@ -17,33 +18,40 @@ const Client = ({
   hoursLogged,
   editIcon,
   deleteIcon,
-  isAdminUser
-}: IClient) => (
-  <tr key={id}>
-    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-miru-dark-purple-1000">
-      {name}
-    </td>
-    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-miru-dark-purple-1000">
-      {email}
-    </td>
-    <td className="px-6 py-4 whitespace-nowrap text-right font-black">
-      {hoursLogged}
-    </td>
-    {isAdminUser && (
-      <>
-        <td className="px-2 py-4 whitespace-nowrap text-right text-sm font-medium">
-          <button>
-            <img src={editIcon} alt="" />
-          </button>
-        </td>
-        <td className="px-2 py-4 whitespace-nowrap text-right text-sm font-medium">
-          <button>
-            <img src={deleteIcon} alt="" />
-          </button>
-        </td>
-      </>
-    )}
-  </tr>
-);
+  isAdminUser,
+  setShowEditDialog
+}: IClient) => {
+  return (
+    <tr key={id}>
+      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-miru-dark-purple-1000">
+        {name}
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-miru-dark-purple-1000">
+        {email}
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap text-right font-black">
+        {hoursLogged}
+      </td>
+      {isAdminUser && (
+        <>
+          <td className="px-2 py-4 whitespace-nowrap text-right text-sm font-medium">
+            <button
+              onClick={() => {
+                setShowEditDialog(true);
+              }}
+            >
+              <img src={editIcon} alt="" />
+            </button>
+          </td>
+          <td className="px-2 py-4 whitespace-nowrap text-right text-sm font-medium">
+            <button>
+              <img src={deleteIcon} alt="" />
+            </button>
+          </td>
+        </>
+      )}
+    </tr>
+  );
+};
 
 export default Client;
