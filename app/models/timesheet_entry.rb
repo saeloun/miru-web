2a4c54fcfe83980293e21ab8af7bc17a5f008f6e
Abinash Panda <abinashp@protonmail.ch>
2022-02-17 17:54:22 +0530

    fixed typo

--- app/models/timesheet_entry.rb
+++ app/models/timesheet_entry.rb
@@ -51,7 +51,7 @@ class TimesheetEntry < ApplicationRecord
   private
     def insure_bill_status_is_set
       return if bill_status.present? || project.nil?
-      if project.billable?
+      if project.billable
         self.bill_status = :unbilled
       else
         self.bill_status = :non_billable
