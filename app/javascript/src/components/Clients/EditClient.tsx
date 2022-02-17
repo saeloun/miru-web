c05e056fdb0c086309738ff2f65329f2c0c154da
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-02-17 21:22:39 +0530

    Display validation errors

--- app/javascript/src/components/Clients/EditClient.tsx
+++ app/javascript/src/components/Clients/EditClient.tsx
@@ -16,6 +16,7 @@ const EditClient = ({ setShowEditDialog, client }: IEditClient) => {
   const [email, setEmail] = useState(client.email);
   const [phone, setPhone] = useState(client.phone);
   const [address, setAddress] = useState(client.address);
+  const [errors, setErrors] = useState({} as any);
 
   const handleNameChange = e => {
     setName(e.target.value);
@@ -36,17 +37,26 @@ const EditClient = ({ setShowEditDialog, client }: IEditClient) => {
   const handleSubmit = async e => {
     e.preventDefault();
 
-    const res = await clients.update(client.id, {
-      client: {
-        name,
-        email,
-        phone,
-        address
-      }
-    });
+    try {
+      const res = await clients.update(client.id, {
+        client: {
+          name,
+          email,
+          phone,
+          address
+        }
+      });
 
-    if (res.data?.success) {
-      window.location.reload();
+      if (res.data?.success) {
+        window.location.reload();
+      }
+    } catch (err) {
+      if (err.response.status == 422) {
+        setErrors({
+          name: err.response.data.name ? err.response.data.name[0] : null,
+          email: err.response.data.email ? err.response.data.email[0] : null
+        });
+      }
     }
   };
   return (
@@ -73,12 +83,18 @@ const EditClient = ({ setShowEditDialog, client }: IEditClient) => {
                 <div className="field">
                   <div className="field_with_errors">
                     <label className="form__label">Name</label>
-                    <div className="tracking-wider block text-xs text-red-600"></div>
+                    <div className="tracking-wider block text-xs text-red-600">
+                      {errors.name}
+                    </div>
                   </div>
                   <div className="mt-1">
                     <input
                       placeholder="Enter name"
-                      className="form__input border-gray-100 focus:ring-miru-gray-1000 focus:border-miru-gray-1000"
+                      className={`form__input ${
+                        errors.name
+                          ? "border-red-600 focus:ring-red-600 focus:border-red-600"
+                          : "border-gray-100 focus:ring-miru-gray-1000 focus:border-miru-gray-1000"
+                      }`}
                       type="text"
                       name="client[name]"
                       id="client_name"
@@ -93,12 +109,18 @@ const EditClient = ({ setShowEditDialog, client }: IEditClient) => {
                 <div className="field">
                   <div className="field_with_errors">
                     <label className="form__label">Email</label>
-                    <div className="tracking-wider block text-xs text-red-600"></div>
+                    <div className="tracking-wider block text-xs text-red-600">
+                      {errors.email}
+                    </div>
                   </div>
                   <div className="mt-1">
                     <input
                       placeholder="Enter email ID"
-                      className="form__input border-gray-100 focus:ring-miru-gray-1000 focus:border-miru-gray-1000"
+                      className={`form__input ${
+                        errors.email
+                          ? "border-red-600 focus:ring-red-600 focus:border-red-600"
+                          : "border-gray-100 focus:ring-miru-gray-1000 focus:border-miru-gray-1000"
+                      }`}
                       type="text"
                       name="client[email]"
                       id="client_email"
@@ -154,7 +176,6 @@ const EditClient = ({ setShowEditDialog, client }: IEditClient) => {
                   name="commit"
                   value="SAVE CHANGES"
                   className="form__input_submit"
-                  data-disable-with="SAVE CHANGES"
                 />
               </div>
             </form>
