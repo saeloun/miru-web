a8ca246e1cb3b874492a09e63678755bd348fd35
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-02-18 18:35:03 +0530

    Owners or admins should be able to edit client details  (#150)
    * Add update controller action for client
    
    * Check if the user is owner or admin before updating client details
    
    * Attempt to render edit client view
    
    * EditClient component
    
    * Modal issue fix
    
    * Edit client details
    
    * Remove the hidden input field containing authenticity_token
    
    * Order clients by updated_at
    
    * Remove action and method attr from form
    
    * Display validation errors
    
    * Move styling within the elem
    
    * Display success toast message
    
    * Use symbol instead of strings
    
    * react toaster fix
    
    * minor fix
    
    * Fix error handler
    
    * Replace find_by with find and use error handler to handle not found exception
    
    * Remove hardcoded close button and fix types
    
    * Create private method for client
    
    * Move notice to locales
    
    * Order by created_at desc
    
    * Move hardcoded strings to locales
    
    * Fix code style
    
    Co-authored-by: Akhil G Krishnan <akhilgkrishnan4u@gmail.com>

--- app/controllers/internal_api/v1/application_controller.rb
+++ app/controllers/internal_api/v1/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class InternalApi::V1::ApplicationController < ActionController::API
+  include ActionController::MimeResponds
   include ErrorHandler
 
   before_action :authenticate_user!
