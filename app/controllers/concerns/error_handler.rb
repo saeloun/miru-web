b7ed6a23f1dc393138323af36cd3ed1d5e5187b2
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-02-18 13:00:47 +0530

    Fix error handler

--- app/controllers/concerns/error_handler.rb
+++ app/controllers/concerns/error_handler.rb
@@ -5,14 +5,15 @@ module ErrorHandler
 
   included do
     rescue_from ActionController::RoutingError, with: :handle_not_found_error
+    rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found_error
   end
 
   private
-    def handle_not_found_error(_error)
+    def handle_not_found_error(exception)
       message = exception.message || "The resource you're looking for is not available"
 
       respond_to do |format|
-        format.json { render json: { errors: message }, status: :forbidden }
+        format.json { render json: { errors: message }, status: :not_found }
         format.html { render file: "public/401.html", status: :not_found, layout: false, alert: message }
       end
     end
