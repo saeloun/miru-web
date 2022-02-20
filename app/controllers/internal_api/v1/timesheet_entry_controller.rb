823fdb140e6f2fa0e41cf933b558d5e6281b7950
Abinash Panda <abinashp@protonmail.ch>
2022-02-21 01:43:07 +0530

    initial commit

--- app/controllers/internal_api/v1/timesheet_entry_controller.rb
+++ app/controllers/internal_api/v1/timesheet_entry_controller.rb
@@ -3,6 +3,8 @@
 class InternalApi::V1::TimesheetEntryController < InternalApi::V1::ApplicationController
   include Timesheet
 
+  before_action :check_bill_status, only: [:create, :update]
+
   def index
     timesheet_entries = current_user.timesheet_entries.during(params[:from], params[:to])
     entries = formatted_entries_by_date(timesheet_entries)
@@ -12,7 +14,6 @@ class InternalApi::V1::TimesheetEntryController < InternalApi::V1::ApplicationCo
   def create
     timesheet_entry = current_project.timesheet_entries.new(timesheet_entry_params)
     timesheet_entry.user = current_user
-    timesheet_entry.bill_status = params[:billable] ? :unbilled : :non_billable
     if timesheet_entry.save
       render json: { success: true, entry: timesheet_entry.formatted_entry }
     else
@@ -22,7 +23,6 @@ class InternalApi::V1::TimesheetEntryController < InternalApi::V1::ApplicationCo
 
   def update
     current_timesheet_entry.project = current_project
-    current_timesheet_entry.bill_status = params[:billable] ? :unbilled : :non_billable
     if current_timesheet_entry.update(timesheet_entry_params)
       render json: { success: true, entry: current_timesheet_entry.formatted_entry }
     else
@@ -40,14 +40,20 @@ class InternalApi::V1::TimesheetEntryController < InternalApi::V1::ApplicationCo
 
   private
     def current_project
-      @_project ||= current_company.projects.find(params[:project_id])
+      @_current_project ||= current_company.projects.find(params[:project_id])
     end
 
     def current_timesheet_entry
-      @_timesheet_entry ||= current_user.timesheet_entries.find(params[:id])
+      @_current_timesheet_entry ||= current_user.timesheet_entries.find(params[:id])
     end
 
     def timesheet_entry_params
-      params.require(:timesheet_entry).permit(:project_id, :duration, :work_date, :note)
+      params.require(:timesheet_entry).permit(:project_id, :duration, :work_date, :note, :bill_status)
+    end
+
+    def check_bill_status
+      if timesheet_entry_params[:bill_status] == "billed"
+        render json: { success: false, error: "You cannot bill an entry that has already been billed" }, status: :unprocessable_entity
+      end
     end
 end
