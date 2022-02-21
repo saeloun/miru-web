0617f0013703acc5bf65cc37beb206560e569b95
Judis <judismjeevan@gmail.com>
2022-02-21 13:51:04 +0530

    added link for new form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,6 +22,15 @@ module ApplicationHelper
     end
   end
 
+  def link_to_add_row(name, f, association, **args)
+    new_object = f.object.send(association).klass.new
+    id = new_object.object_id
+    fields = f.fields_for(association, new_object, child_index: id) do |builder|
+      render(association.to_s.singularize, f: builder)
+    end
+    link_to(name, "#", class: "add_fields " + args[:class], data: { id: id, fields: fields.delete("\n") })
+  end
+
   private
     def field_error(resource, attribute)
       resource.errors[attribute].first
