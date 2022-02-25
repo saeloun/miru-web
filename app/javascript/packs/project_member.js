a7ee72ea379e7827ad8e163904d03d98082e4b33
Judis <judismjeevan@gmail.com>
2022-02-25 19:41:11 +0530

    removed unnecessary js code

--- app/javascript/packs/project_member.js
+++ app/javascript/packs/project_member.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const link = document.querySelector(".add_fields");
   link.onclick = addNewFields;
-  console.log(link.getAttribute("data-fields"));
-  console.log("judis $$$$$$$%%%%");
 
   function addNewFields () {
     const fields = document.querySelectorAll("#new_project .field");
