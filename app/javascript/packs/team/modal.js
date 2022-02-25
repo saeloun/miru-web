7a0ecb951a54335b37d6d05443df26e705d39fca
Aniket K <94526737+aniket-k-kaushik@users.noreply.github.com>
2022-02-25 17:50:46 +0530

    Nevbar design | Image fitting in profile and company pictures | Input not cleared after clicking on cancel on Team New User modal (#159)
    * company name minor issue fixed
    
    * fix
    
    * navbar design changed
    
    * team page navbar design improved
    
    * mobile view button design
    
    * navbar classes
    
    * added alignment to settings
    
    * classes improved
    
    * alignment fix
    
    * fix
    
    * confirm before discard
    
    * test fix
    
    * image issue fixed Issue Number 133
    
    * image issue in profile edit fixed Issue Number 133
    
    * clear input on close button fixed Issue Number 147
    
    * changed name of classes for navbar
    
    * changed name of classes for contents
    
    * button issue fixed
    
    * navbar fix
    
    * policy for dashboard
    
    * slider width full for mobile view
    
    * button fix
    
    * comment issue fixed 1
    
    * en.yml added
    
    * test commit
    
    * Moved the JS scripts in team index to a separate JS file
    
    * Attached modal js logic only to team index page
    
    * outer inner and middle wrapper for div class names
    
    * class name update
    
    Co-authored-by: Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
    Co-authored-by: Gowsik <gowsik@saeloun.com>

---
+++ app/javascript/packs/team/modal.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const all_modals = ["main-modal", "another-modal"];
+  all_modals.forEach(modal => {
+    const modalSelected = document.querySelector("." + modal);
+    if (modalSelected === undefined || modalSelected === null) return;
+
+    modalSelected.classList.remove("fadeIn");
+    modalSelected.classList.add("fadeOut");
+    modalSelected.style.display = "none";
+  });
+
+  const modalClose = modal => {
+    const modalToClose = document.querySelector("." + modal);
+    modalToClose.classList.remove("fadeIn");
+    modalToClose.classList.add("fadeOut");
+    setTimeout(() => {
+      modalToClose.style.display = "none";
+    }, 500);
+  };
+
+  const openModal = modal => {
+    const modalToOpen = document.querySelector("." + modal);
+    modalToOpen.classList.remove("fadeOut");
+    modalToOpen.classList.add("fadeIn");
+    modalToOpen.style.display = "flex";
+  };
+
+  function ClearFields () {
+    document.getElementById("team_first_name").value = "";
+    document.getElementById("team_last_name").value = "";
+    document.getElementById("team_email").value = "";
+  }
+
+  const newUserBtn = document.querySelector(
+    "button[data-team-element=\"new-user-btn\"]"
+  );
+  const inviteModalClose = document.querySelector(
+    "div[data-team-element=\"invite-modal-close\"]"
+  );
+  const inviteModalCloseBtn = document.querySelector(
+    "button[data-team-element=\"invite-modal-close-btn\"]"
+  );
+  const teamModalClose = document.querySelector(
+    "div[data-team-element=\"modal-close\"]"
+  );
+  const editMembers = document.querySelectorAll(
+    "img[data-team-element=\"edit-member\"]"
+  );
+
+  if (newUserBtn)
+    newUserBtn.addEventListener("click", () => openModal("main-modal"));
+  if (inviteModalClose)
+    inviteModalClose.addEventListener("click", () => modalClose("main-modal"));
+  if (inviteModalCloseBtn)
+    inviteModalCloseBtn.addEventListener("click", () => ClearFields());
+  if (teamModalClose)
+    teamModalClose.addEventListener("click", () => modalClose("another-modal"));
+
+  editMembers.forEach(editMember => {
+    editMember.addEventListener("click", () => openModal("another-modal"));
+  });
+});
