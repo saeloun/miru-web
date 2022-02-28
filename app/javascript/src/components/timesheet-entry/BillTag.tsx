f58dc4712baf24b3d8ef235ca01c57978f6dfc3d
Abinash Panda <abinashp@protonmail.ch>
2022-02-28 17:54:09 +0530

    Props interface added in BillTag.tsx

--- app/javascript/src/components/timesheet-entry/BillTag.tsx
+++ app/javascript/src/components/timesheet-entry/BillTag.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
 
-const BillTag: React.FC<props> = ({ color, text }) => (
+interface Props {
+  color: string;
+  text: string;
+}
+
+const BillTag: React.FC<Props> = ({ color, text }) => (
   <p
     className={`bg-${color} text-miru-alert-green-1000 px-1 mr-6 text-xs h-4 flex justify-center font-semibold tracking-widest rounded-lg`}
   >
