bc2cb85e4ee29f25e76f773ce7790c3e04875088
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-02-28 14:02:34 +0530

    Owners or admins should be able to delete a client (#163)
    * Owners or admins should be able to delete a client
    
    * Integrate client deletion API with FE
    
    * Change func name
    
    * Handle record not found error
    
    * Add request specs
    
    * Show delete confirmation dialog on client delete
    
    * Create delete confirmation dialog as per the design
    
    * Fix specs
    
    * Handle discard failure in error handler
    
    * Handle update errors in error handler

--- app/policies/client_policy.rb
+++ app/policies/client_policy.rb
@@ -16,6 +16,15 @@ class ClientPolicy < ApplicationPolicy
     user_owner_or_admin?
   end
 
+  def destroy?
+    unless user.current_workspace_id == record.company_id
+      @error_message_key = :different_workspace
+      return false
+    end
+
+    user_owner_or_admin?
+  end
+
   def permitted_attributes
     [:name, :email, :phone, :address]
   end
