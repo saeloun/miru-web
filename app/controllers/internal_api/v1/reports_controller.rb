377cd8204b46ac700dab94a6b259034184c78ae6
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-03-02 17:15:18 +0530

    Allow only admins or owners to access the reports API

--- app/controllers/internal_api/v1/reports_controller.rb
+++ app/controllers/internal_api/v1/reports_controller.rb
@@ -2,9 +2,9 @@
 
 class InternalApi::V1::ReportsController < InternalApi::V1::ApplicationController
   include Timesheet
-  skip_after_action :verify_authorized
 
   def index
+    authorize :report
     render json: {
       success: true,
       entries: current_company.timesheet_entries.map do |e|
