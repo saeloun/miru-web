3751c194071aa3cb90bec29a78f295a723722966
Mohini Dahiya <mohini@saeloun.com>
2022-03-08 14:26:24 +0530

    Cypress tests for Sign Up Page (#188)
    * Cypress tests for Sign Up Page
    
    Tests:
    1. should give error on clicking signup with blank values
    2. should contain signUp with Google Link
    3. should contain signUp with Apple Link
    4. should contain signIn Link
    
    Haven't added Sign Up flow as its just a 1 time process and it is not required in the regression suit.
    
    * update data-cy format in rails erb helper.
    
    * Update app/views/devise/shared/_links.html.erb
    
    Co-authored-by: Keshav Biswa <keshavbiswa21@gmail.com>
    
    * Update app/views/devise/registrations/new.html.erb
    
    Co-authored-by: Keshav Biswa <keshavbiswa21@gmail.com>
    
    * Update app/views/devise/registrations/new.html.erb
    
    Co-authored-by: Keshav Biswa <keshavbiswa21@gmail.com>
    
    * Update app/views/devise/registrations/new.html.erb
    
    Co-authored-by: Keshav Biswa <keshavbiswa21@gmail.com>
    
    * Update app/views/devise/registrations/new.html.erb
    
    Co-authored-by: Keshav Biswa <keshavbiswa21@gmail.com>
    
    * Update app/views/devise/registrations/new.html.erb
    
    Co-authored-by: Keshav Biswa <keshavbiswa21@gmail.com>
    
    Co-authored-by: Keshav Biswa <keshavbiswa21@gmail.com>

---
+++ cypress/constants/selectors/auth.js
@@ -0,0 +1,8 @@
+import { dataCy } from "../../support/utils/datacy";
+
+export const authSelectors = {
+    signUpButton: dataCy('sign-up-button'),
+    signUpwithGoogleButton: dataCy('sign-up-with-google-button'),
+    signUpwithAppleButton: dataCy('sign-up-with-apple-button'),
+    signInLink: dataCy("sign-in-link")
+}
