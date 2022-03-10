8843f92e3ed03f63a4cdb2b166e0b9e568bca8f3
Ajinkya Deshmukh <ajinkya@saeloun.com>
2022-03-10 19:53:00 +0530

    UI/clients list page progress (#189)
    * client list progress bar
    
    * package json updated
    
    * package json updated
    
    * visual changes
    
    * lint fix
    
    * comments resolved
    
    * interface added
    
    * lint fix
    
    * vscode settings added
    
    * editor fixes

--- app/javascript/src/components/Clients/Client.tsx
+++ app/javascript/src/components/Clients/Client.tsx
@@ -31,41 +31,52 @@ export const Client = ({
   setClientToDelete,
   setShowDeleteDialog
 }: IClient) => {
+  const [grayColor, setGrayColor] = React.useState<string>("");
+  const [isHover, setHover] = React.useState<boolean>(false);
+
+  const handleMouseEnter = () => {
+    setGrayColor("bg-miru-gray-100");
+    setHover(true);
+  };
+
+  const handleMouseLeave = () => {
+    setGrayColor("");
+    setHover(false);
+  };
+
   return (
-    <tr key={id}>
-      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-miru-dark-purple-1000">
+    <tr key={id} className={`last:border-b-0 ${grayColor}`} onMouseLeave={handleMouseLeave} onMouseEnter={handleMouseEnter}>
+      <td className="px-6 py-6 whitespace-nowrap text-sm font-medium text-miru-dark-purple-1000">
         {name}
       </td>
-      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-miru-dark-purple-1000">
+      <td className="px-6 py-6 whitespace-nowrap text-sm font-medium text-miru-dark-purple-1000">
         {email}
       </td>
-      <td className="px-6 py-4 whitespace-nowrap text-right font-black">
+      <td className="px-6 py-6 whitespace-nowrap text-right font-black">
         {hoursLogged}
       </td>
-      {isAdminUser && (
-        <>
-          <td className="px-2 py-4 whitespace-nowrap text-right text-sm font-medium">
-            <button
-              onClick={() => {
-                setShowEditDialog(true);
-                setClientToEdit({ id, name, email, phone, address });
-              }}
-            >
-              <img src={editIcon} alt="" />
-            </button>
-          </td>
-          <td className="px-2 py-4 whitespace-nowrap text-right text-sm font-medium">
-            <button
-              onClick={() => {
-                setShowDeleteDialog(true);
-                setClientToDelete({ id, name });
-              }}
-            >
-              <img src={deleteIcon} alt="" />
-            </button>
-          </td>
-        </>
-      )}
+      <td className="px-2 py-4 whitespace-nowrap text-right text-sm font-medium">
+        {isAdminUser && isHover && <button
+          onClick={() => {
+            setShowEditDialog(true);
+            setClientToEdit({ id, name, email, phone, address });
+          }}
+        >
+          <img src={editIcon} alt="" />
+        </button>
+        }
+      </td>
+      <td className="px-2 py-4 whitespace-nowrap text-right text-sm font-medium">
+        { isAdminUser && isHover && <button
+          onClick={() => {
+            setShowDeleteDialog(true);
+            setClientToDelete({ id, name });
+          }}
+        >
+          <img src={deleteIcon} alt="" />
+        </button>
+        }
+      </td>
     </tr>
   );
 };
