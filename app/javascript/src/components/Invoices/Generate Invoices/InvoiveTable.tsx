0a3b56b255f27f917909bf6e4f051c1a5c29ba18
Shruti <shruti@saeloun.com>
2022-03-15 13:50:04 +0530

    Edited table

--- app/javascript/src/components/Invoices/Generate Invoices/InvoiveTable.tsx
+++ app/javascript/src/components/Invoices/Generate Invoices/InvoiveTable.tsx
@@ -1,35 +1,24 @@
 import * as React from "react";
 
 const InvoiceTable = () => {
+
   return (
-    <table className="w-full py-2">
+    <table className="w-full table-fixed">
       <thead className="py-2">
-        <th className="text-miru-dark-purple-600 font-normal text-xs text-left">
-          NAME
-        </th>
-        <th className="text-miru-dark-purple-600 font-normal text-xs text-left">
-          DATE
-        </th>
-        <th className="text-miru-dark-purple-600 font-normal text-xs text-left ">
-          DESCRIPTION
-        </th>
-        <th className="text-miru-dark-purple-600 font-normal text-xs text-left">
-          RATE
-        </th>
-        <th className="text-miru-dark-purple-600 font-normal text-xs text-left">
-          QTY
-        </th>
-        <th className="text-miru-dark-purple-600 font-normal text-xs text-left">
-          LINE TOTAL
-        </th>
+        <th className="text-miru-dark-purple-600 font-normal text-xs text-left">NAME</th>
+        <th className="text-miru-dark-purple-600 font-normal text-xs text-left">DATE</th>
+        <th className="text-miru-dark-purple-600 font-normal text-xs text-left ">DESCRIPTION</th>
+        <th className="text-miru-dark-purple-600 font-normal text-xs text-left">RATE</th>
+        <th className="text-miru-dark-purple-600 font-normal text-xs text-left">QTY</th>
+        <th className="text-miru-dark-purple-600 font-normal text-xs text-right">LINE TOTAL</th>
+
       </thead>
-      <tbody>
-        <tr className="m-3 ">
-          <td
-            colSpan={6}
-            className="bg-white font-bold text-base text-center text-miru-dark-purple-200 rounded-md border-2 border-miru-dark-purple-200 border-dashed"
-          >
-            + NEW LINE ITEM
+      <tbody className="w-full ">
+        <tr className="w-full">
+          <td colSpan={6} >
+            <button className=" w-full bg-white font-bold text-base text-center text-miru-dark-purple-200 rounded-md border-2 border-miru-dark-purple-200 border-dashed">
+              + NEW LINE ITEM
+            </button>
           </td>
         </tr>
         <tr className="w-full">
@@ -50,6 +39,7 @@ const InvoiceTable = () => {
           </td>
           <td className="p-2 pr-0 text-right">$90</td>
         </tr>
+
       </tbody>
     </table>
   );
