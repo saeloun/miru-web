03e34c55aaaf91299ca7c1b24dccfff92b49b527
Aniket Kaushik <aniket.k.kaushik@outlook.com>
2022-03-16 03:30:45 +0530

    minor fix

--- db/seeds/timesheet_entry.rb
+++ db/seeds/timesheet_entry.rb
@@ -7,7 +7,7 @@ vipul, supriya, akhil, keshav, rohit = User.first(5)
 ((Date.today.beginning_of_month - 7)..(Date.today.end_of_month + 7)).each do |date|
   [project_1_common_client_company_India, project_1_common_client_company_US].each do |project|
     [vipul, supriya, akhil, keshav].each do |user|
-      entry = { user: user, duration: 60, note: "Worked on #{project}", work_date: date }
+      entry = { user: user, duration: 60, note: "Worked on #{project.name}", work_date: date }
       project.timesheet_entries.create!(entry)
     end
   end
@@ -16,7 +16,7 @@ end
 ((Date.today.beginning_of_month - 7)..(Date.today.end_of_month + 7)).each do |date|
   [project_1_Client_1_company_India, project_1_Client_1_company_US].each do |project|
     [supriya, akhil, keshav, rohit].each do |user|
-      entry = { user: user, duration: 60, note: "Worked on #{project}", work_date: date }
+      entry = { user: user, duration: 60, note: "Worked on #{project.name}", work_date: date }
       project.timesheet_entries.create!(entry)
     end
   end
