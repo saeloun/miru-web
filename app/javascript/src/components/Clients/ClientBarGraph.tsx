d90c00f92629cc0f899c379c36950e7599c39239
Ajinkya Deshmukh <ajinkya@saeloun.com>
2022-03-15 20:34:35 +0530

    UI/clients list page progress with api update (#195)
    * api for client hour logged for each project
    
    * minor update
    
    * removed extra comma
    
    * added client create
    
    * created API for all the clients to show hours logged by them based on month year or week
    
    * added kept
    
    * index action for clients with total hours
    
    * added total hour for show action and email for index
    
    * added reusability in the client.rb model
    
    * client list progress bar
    
    * package json updated
    
    * package json updated
    
    * removed extra line
    
    * added a method project_team in the project.rb model to give name of all the team member who work on that project
    
    * name fix in the json api response
    
    * methods refactored
    
    * refactored week_month_year method in client.rb
    
    * visual changes
    
    * lint fix
    
    * comments resolved
    
    * minor updates
    
    * interface added
    
    * lint fix
    
    * tailwind config updated
    
    * design issues resolved
    
    * conflict resolved
    
    * minutes to hours changes completed
    
    * comment resolved
    
    Co-authored-by: Aniket Kaushik <aniket.k.kaushik@outlook.com>

--- app/javascript/src/components/Clients/ClientBarGraph.tsx
+++ app/javascript/src/components/Clients/ClientBarGraph.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import GetClientBar from "./ChartBar";
 import { IChartBar } from "./interface";
+import { minutesToHHMM } from "../../helpers/hhmm-parser";
 
-const ClientBarGraph = ({ clients , totalHours }:IChartBar) => (
+const ClientBarGraph = ({ clients , totalMinutes, handleSelectChange }:IChartBar) => (
   <div className="bg-miru-gray-100 py-10 px-10">
     <div className="flex justify-end">
       <select className="px-3
@@ -15,46 +16,52 @@ const ClientBarGraph = ({ clients , totalHours }:IChartBar) => (
           ease-in-out
           m-0
           focus:outline-none
-          text-miru-dark-purple-600">
-        <option className="text-miru-dark-purple-600" value="Jon Smith">
+          text-miru-han-purple-1000" onChange={handleSelectChange}>
+        <option className="text-miru-dark-purple-600" value="week">
             THIS WEEK
+        </option>
+        <option className="text-miru-dark-purple-600" value="month">
+            This MONTH
+        </option>
+        <option className="text-miru-dark-purple-600" value="year">
+            THIS YEAR
         </option>
       </select>
     </div>
-    <p className="mb-3 text-miru-dark-purple-600">
-          TOTAL HOURS: <span>{totalHours}</span>
+    <p className="mb-3 text-tiny text-miru-dark-purple-600 tracking-widest">
+          TOTAL HOURS: <span className="font-medium">{minutesToHHMM(totalMinutes)}</span>
     </p>
     <div className="w-full bg-gray-200 flex h-1">
       {clients.map((client, index) => <GetClientBar
         client={client}
         key={index}
         index={index}
-        totalHours={totalHours}
+        totalMinutes={totalMinutes}
       />)
       }
     </div>
-    <div className="flex miru-dark-purple-400 text-normal border-b-2 border-miru-gray-1000 border-b-miru-gray-1000 pb-6 justify-between mt-3">
+    <div className="flex text-tiny text-miru-dark-purple-400 tracking-widest border-b-2 border-miru-gray-1000 border-b-miru-gray-1000 pb-6 justify-between mt-3">
       <span>
             0
       </span>
       <span>
-        {totalHours}
+        {minutesToHHMM(totalMinutes)}
       </span>
     </div>
-    <div className="flex pt-6">
+    <div className="flex pt-6 tracking-widest">
       <div className="flex-auto border-r-2 py-2 border-miru-gray-1000 border-r-miru-gray-1000">
-        <p className="text-miru-dark-purple-600">
+        <p className="text-tiny text-miru-dark-purple-600">
                 OVERDUE
         </p>
-        <h4 className="text-4xl text-miru-dark-purple-600 mt-3">
+        <h4 className="text-4.5xl text-miru-dark-purple-600 mt-3">
                 $35.3K
         </h4>
       </div>
-      <div className="flex-auto py-2 pl-5">
-        <p className="text-miru-dark-purple-600">
+      <div className="flex-auto tracking-widest py-2 pl-5">
+        <p className="text-tiny text-miru-dark-purple-600">
                 OUTSTANDING
         </p>
-        <h4 className="text-4xl mt-3 text-miru-dark-purple-600">
+        <h4 className="text-4.5xl mt-3 text-miru-dark-purple-600">
                 $35.3K
         </h4>
       </div>
