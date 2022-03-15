4a8833b0c07e1d9b1403f036eb2382c127481c3a
Aniket Kaushik <aniket.k.kaushik@outlook.com>
2022-03-16 02:47:55 +0530

    name refactored

--- db/seeds/project_member.rb
+++ db/seeds/project_member.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 # Projects Members Start
-project_1_common_client_IN, project_1_Client_1_company_India, project_1_common_client_US, project_1_Client_1_company_US = Project.first(4)
+project_1_common_client_company_India, project_1_Client_1_company_India, project_1_common_client_company_US, project_1_Client_1_company_US = Project.first(4)
 vipul, supriya, akhil, keshav, rohit = User.first(5)
 
 # vipul, supriya, akhil, keshav are working on project_1_Common_client_one, project_1_Common_client_two which belong to common client of Company_India and Company_US respectively
 [vipul, supriya, akhil, keshav].each do |user|
-  [project_1_common_client_IN, project_1_common_client_US].each do |project|
+  [project_1_common_client_company_India, project_1_common_client_company_US].each do |project|
     user.project_members.create!(project_id: project.id, hourly_rate: 5000)
   end
 end
--- db/seeds/projects.rb
+++ db/seeds/projects.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 # Projects Start
-common_client_IN, client_one_company_India, common_client_US, client_one_company_US = Client.first(4)
+common_client_company_India, client_one_company_India, common_client_company_US, client_one_company_US = Client.first(4)
 
 projects = [
-  { name: "Project_1_common_client_IN", description: "Timesheet app", billable: false },
+  { name: "Project_1_common_client_company_India", description: "Timesheet app", billable: false },
   { name: "Project_1_Client_1_company_India", description: "Timesheet app", billable: false },
-  { name: "Project_1_common_client_US", description: "Timesheet app", billable: false },
+  { name: "Project_1_common_client_company_US", description: "Timesheet app", billable: false },
   { name: "Project_1_Client_1_company_US", description: "Timesheet app", billable: false },
 ]
 
 # Projects for Common Client
-common_client_IN.projects.create!(projects[0])
+common_client_company_India.projects.create!(projects[0])
 client_one_company_India.projects.create!(projects[1])
-common_client_US.projects.create!(projects[2])
+common_client_company_US.projects.create!(projects[2])
 client_one_company_US.projects.create!(projects[3])
 
 puts "Projects Created"
