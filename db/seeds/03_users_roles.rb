0a1155e5f4f323f8391af84111cb5303a2b97394
Aniket Kaushik <aniket.k.kaushik@outlook.com>
2022-03-16 22:46:03 +0530

    name fix 2

--- db/seeds/03_users_roles.rb
+++ db/seeds/03_users_roles.rb
@@ -4,15 +4,15 @@
 SAELOUN_INDIA, SAELOUN_US = ["Saeloun India Pvt. Ltd", "Saeloun USA INC."].map { |company| Company.find_by(name: company) }
 VIPUL, SUPRIYA, AKHIL, KESHAV, ROHIT = ["vipul@example.com", "supriya@example.com", "akhil@example.com", "keshav@example.com", "rohit@example.com"].map { |user| User.find_by(email: user) }
 
-vipul.add_role(:owner, SAELOUN_INDIA)    # Vipul is Owner in Company India
-vipul.add_role(:owner, SAELOUN_US)       # Vipul is Owner in Company US
-supriya.add_role(:admin, SAELOUN_INDIA)  # Supriya is Admin in Company India
-supriya.add_role(:admin, SAELOUN_US)     # Supriya is Admin in Company US
-akhil.add_role(:employee, SAELOUN_INDIA) # Akhil is Employee is Company India
-akhil.add_role(:employee, SAELOUN_US)    # Akhil is Employee is Company US
-keshav.add_role(:admin, SAELOUN_INDIA)   # Keshav is Admin is Company India
-keshav.add_role(:employee, SAELOUN_US)   # Keshav is Employee is Company US
-rohit.add_role(:employee, SAELOUN_INDIA) # Rohit is Employee is Company India
-rohit.add_role(:admin, SAELOUN_US)       # Rohit is Admin is Company US
+VIPUL.add_role(:owner, SAELOUN_INDIA)    # Vipul is Owner in Company India
+VIPUL.add_role(:owner, SAELOUN_US)       # Vipul is Owner in Company US
+SUPRIYA.add_role(:admin, SAELOUN_INDIA)  # Supriya is Admin in Company India
+SUPRIYA.add_role(:admin, SAELOUN_US)     # Supriya is Admin in Company US
+AKHIL.add_role(:employee, SAELOUN_INDIA) # Akhil is Employee is Company India
+AKHIL.add_role(:employee, SAELOUN_US)    # Akhil is Employee is Company US
+KESHAV.add_role(:admin, SAELOUN_INDIA)   # Keshav is Admin is Company India
+KESHAV.add_role(:employee, SAELOUN_US)   # Keshav is Employee is Company US
+ROHIT.add_role(:employee, SAELOUN_INDIA) # Rohit is Employee is Company India
+ROHIT.add_role(:admin, SAELOUN_US)       # Rohit is Admin is Company US
 puts "Users Roles Created"
 # User Roles End
