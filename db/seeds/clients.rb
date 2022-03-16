dfe609db0d2712551cf3c81a88529e04e20ebffc
Aniket Kaushik <aniket.k.kaushik@outlook.com>
2022-03-16 16:40:11 +0530

    seperated clients hash into three seperate hashes

--- db/seeds/clients.rb
+++ db/seeds/clients.rb
@@ -3,23 +3,18 @@
 # Client Create Start
 saeloun_India, saeloun_US = ["Saeloun India Pvt. Ltd", "Saeloun USA INC."].map { |company| Company.find_by(name: company) }
 
-clients_India = [
-    { name: "common client", email: "client@common.com", phone: "+91 9999999990", address: "Somewhere on Earth" },
-    { name: "client_one saeloun_India", email: "client_one@saeloun_india.com", phone: "+91 9999999991", address: "Somewhere on Earth" },
-]
+client_common = { name: "common client", email: "client@common.com", phone: "+91 9999999990", address: "Somewhere on Earth" }
+client_India = { name: "client_one saeloun_India", email: "client_one@saeloun_india.com", phone: "+91 9999999991", address: "Somewhere on Earth" }
+client_US = { name: "client_one saeloun_US", email: "client_one@saeloun_us.com", phone: "+91 9999999993", address: "Somewhere on Earth" }
 
-clients_US = [
-  { name: "common client", email: "client@common.com", phone: "+91 9999999990", address: "Somewhere on Earth" },
-  { name: "client_one saeloun_US", email: "client_one@saeloun_us.com", phone: "+91 9999999993", address: "Somewhere on Earth" }
-]
 
 # Clients for Company India
-clients_India.each do |client|
+[client_common, client_India].each do |client|
   saeloun_India.clients.create!(client)
 end
 
 # Clients for Company US
-clients_US.each do |client|
+[client_common, client_US].each do |client|
   saeloun_US.clients.create!(client)
 end
 
