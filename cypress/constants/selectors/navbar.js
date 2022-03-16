99358445b6688dad0fc03c9490c6a0e267e79f9f
“Apoorv <tiwari.apoorv1316@gmail.com>
2022-03-16 12:06:41 +0530

    chnaged all ocurrences from link to tab

--- cypress/constants/selectors/navbar.js
+++ cypress/constants/selectors/navbar.js
@@ -1,11 +1,11 @@
 import { dataCy } from "../../support/utils/datacy";
 
 export const navbarSelectors = {
-  dashboardTab: dataCy("dashboard-link"),
-  timeTrackingTab: dataCy("time-tracking-link"),
-  teamTab: dataCy("team-link"),
-  clientsTab: dataCy("clients-link"),
-  projectsTab: dataCy("projects-link"),
-  invoiceTab: dataCy("invoice-link"),
-  reportsTab: dataCy("reports-link")
+  dashboardTab: dataCy("dashboard-tab"),
+  timeTrackingTab: dataCy("time-tracking-tab"),
+  teamTab: dataCy("team-tab"),
+  clientsTab: dataCy("clients-tab"),
+  projectsTab: dataCy("projects-tab"),
+  invoiceTab: dataCy("invoice-tab"),
+  reportsTab: dataCy("reports-tab")
 };
