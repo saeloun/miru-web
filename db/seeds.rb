17c190c05ab09466acb40683fd0d63c55c229ef5
Aniket Kaushik <aniket.k.kaushik@outlook.com>
2022-03-16 16:49:56 +0530

    renamed seeds/ files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-load "db/seeds/company.rb"
-load "db/seeds/users.rb"
-load "db/seeds/users_roles.rb"
-load "db/seeds/company_user.rb"
-load "db/seeds/clients.rb"
-load "db/seeds/projects.rb"
-load "db/seeds/project_member.rb"
-load "db/seeds/timesheet_entry.rb"
+Dir[File.join(Rails.root, "db", "seeds", "*.rb")].sort.each do |seed|
+  load seed
+end
