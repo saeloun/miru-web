bb754e23af4a0c2ad83554521b9ef1c9900f747e
Aniket Kaushik <aniket.k.kaushik@outlook.com>
2022-03-16 16:37:08 +0530

    seperated client hashes into two sperate hashes clients_India and clients_US

--- db/seeds/clients.rb
+++ db/seeds/clients.rb
@@ -3,20 +3,24 @@
 # Client Create Start
 saeloun_India, saeloun_US = ["Saeloun India Pvt. Ltd", "Saeloun USA INC."].map { |company| Company.find_by(name: company) }
 
-clients = [
+clients_India = [
     { name: "common client", email: "client@common.com", phone: "+91 9999999990", address: "Somewhere on Earth" },
     { name: "client_one saeloun_India", email: "client_one@saeloun_india.com", phone: "+91 9999999991", address: "Somewhere on Earth" },
-    { name: "client_one saeloun_US", email: "client_one@saeloun_us.com", phone: "+91 9999999993", address: "Somewhere on Earth" }
+]
+
+clients_US = [
+  { name: "common client", email: "client@common.com", phone: "+91 9999999990", address: "Somewhere on Earth" },
+  { name: "client_one saeloun_US", email: "client_one@saeloun_us.com", phone: "+91 9999999993", address: "Somewhere on Earth" }
 ]
 
 # Clients for Company India
-[0, 1].each do |client_id|
-  saeloun_India.clients.create!(clients[client_id])
+clients_India.each do |client|
+  saeloun_India.clients.create!(client)
 end
 
 # Clients for Company US
-[0, 2].each do |client_id|
-  saeloun_US.clients.create!(clients[client_id])
+clients_US.each do |client|
+  saeloun_US.clients.create!(client)
 end
 
 puts "Clients Created"
