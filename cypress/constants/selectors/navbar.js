aa370f2a852964891739d8824aa77d85de4f6121
Mohini Dahiya <mohini@saeloun.com>
2022-03-17 00:20:50 +0530

    team test

---
+++ cypress/constants/selectors/navbar.js
@@ -0,0 +1,5 @@
+import { dataCy } from "../../support/utils/datacy";
+
+export const navbarSelectors = {
+  teamTab: dataCy("team-tab")
+};
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,5 @@
 import { authSelectors } from "../constants/selectors/auth";
+import { teamTabSelector } from "../constants/selectors/team";
 
 Cypress.Commands.add("loginAsOwner", function () {
   cy.get(authSelectors.emailField).clear().type(this.data.email);
@@ -25,4 +26,24 @@ Cypress.Commands.add("loginAsEmployee", function () {
   cy.get(authSelectors.passwordField).clear().type(this.data.employeePassword);
   cy.get(authSelectors.signInButton).click();
   cy.location("pathname").should("eq", "/time-tracking");
+});
+
+//add new admin user from Team tab
+Cypress.Commands.add("addNewAdminUser", function () {
+  cy.get(teamTabSelector.addNewUserButton).click();
+  cy.get(teamTabSelector.newMemberFirstName).type("Dummy Admin FirstName");
+  cy.get(teamTabSelector.newMemberLastname).type("Dummy Admin LastName");
+  cy.get(teamTabSelector.newMemberEmail).type("test@email.com");
+  cy.get(teamTabSelector.adminRadioButton).check();
+  cy.get(teamTabSelector.sendInviteButton).click();
+});
+
+//add new employee user from Team tab
+Cypress.Commands.add("addNewEmpUser", function () {
+  cy.get(teamTabSelector.addNewUserButton).click();
+  cy.get(teamTabSelector.newMemberFirstName).type("Dummy Emp FirstName");
+  cy.get(teamTabSelector.newMemberLastname).type("Dummy Emp LastName");
+  cy.get(teamTabSelector.newMemberEmail).type("test@email.com");
+  cy.get(teamTabSelector.employeeRadioButton).check();
+  cy.get(teamTabSelector.sendInviteButton).click();
 });
