65a6ed6cb28d3554ed5c4955b3e8454d3d9db9a5
Keshav Biswa <keshavbiswa21@gmail.com>
2022-03-16 14:04:11 +0530

    Added few rspec cops (#201)

--- spec/models/timesheet_entry_spec.rb
+++ spec/models/timesheet_entry_spec.rb
@@ -21,6 +21,7 @@ RSpec.describe TimesheetEntry, type: :model do
     it { is_expected.to validate_presence_of(:note) }
     it { is_expected.to validate_presence_of(:work_date) }
     it { is_expected.to validate_presence_of(:bill_status) }
+
     it do
       is_expected.to validate_numericality_of(:duration).
       is_less_than_or_equal_to(Minutes.in_a_day).
