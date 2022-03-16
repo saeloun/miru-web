2c6646804f4c5f5f71641eb470a3f3d2c3108da8
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-03-16 17:25:38 +0530

    Invoices Models (#191)
    * Add invoice model
    
    * Add invoice line item model
    
    * Specify relation between invoice and invoice line items
    
    * Add validations for invoice and invoice line item
    
    * Use faker instead of hardcoded values in factories
    
    * Add model specs for invoice and invoice line item
    
    * Fix memoized var name
    
    * Add status field

---
+++ db/migrate/20220309073708_create_invoices.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class CreateInvoices < ActiveRecord::Migration[7.0]
+  def change
+    create_table :invoices do |t|
+      t.date :issue_date
+      t.date :due_date
+      t.string :invoice_number
+      t.text :reference
+      t.decimal :amount, scale: 2, precision: 20, default: 0.0
+      t.decimal :outstanding_amount, scale: 2, precision: 20, default: 0.0
+      t.decimal :tax, scale: 2, precision: 20, default: 0.0
+      t.decimal :amount_paid, scale: 2, precision: 20, default: 0.0
+      t.decimal :amount_due, scale: 2, precision: 20, default: 0.0
+      t.decimal :discount, scale: 2, precision: 20, default: 0.0
+      t.integer :status, null: false, default: 0
+      t.references :company, null: false, foreign_key: true
+      t.references :client, null: false, foreign_key: true
+
+      t.timestamps
+    end
+
+    add_index :invoices, :invoice_number, unique: true
+  end
+end
