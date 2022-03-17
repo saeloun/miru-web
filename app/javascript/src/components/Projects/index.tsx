04308e0ecc650ec2868f16570a53d1f284d138ec
Shalaka Patil <shalu214patil@gmail.com>
2022-03-17 12:54:31 +0530

    Add component for project list
    Also added state to check if project is clicked for showing project details

--- app/javascript/src/components/Projects/index.tsx
+++ app/javascript/src/components/Projects/index.tsx
@@ -3,9 +3,11 @@ import { ToastContainer } from "react-toastify";
 import { setAuthHeaders, registerIntercepts } from "apis/axios";
 import projects from "apis/projects";
 import { Project } from "./project";
+import ProjectList from "./projectList";
 
 const Projects = (isAdminUser) => {
   const [allProjects, setAllProjects] = React.useState([]);
+  const [showProjectDetails, setShowProjectDetails] = React.useState();
 
   const fetchProjects = async () => {
     const res = await projects.get();
@@ -20,71 +22,22 @@ const Projects = (isAdminUser) => {
     fetchProjects();
   }, []);
 
-  return (
-    <>
-      <ToastContainer />
-      <div className="flex flex-col">
-        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div className="overflow-hidden">
-              <table className="min-w-full divide-y divide-gray-200 mt-4">
-                <thead>
-                  <tr>
-                    <th
-                      scope="col"
-                      className="table__header"
-                    >
-                      PROJECT/CLIENT
-                    </th>
-                    <th
-                      scope="col"
-                      className="table__header"
-                    >
+  const projectClickHandler = (pid) => {
+    setShowProjectDetails(pid);
+  };
 
-                    </th>
-                    <th
-                      scope="col"
-                      className="table__header text-right"
-                    >
-                      HOURS LOGGED
-                    </th>
-                    <th scope="col" className="table__header"></th>
-                    <th scope="col" className="table__header"></th>
-                  </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
-                  {allProjects.map((project, index) => (
-                    <Project
-                      key={index}
-                      {...project}
-                      isAdminUser={isAdminUser}
-                      /* editIcon={editIcon}
-                      deleteIcon={deleteIcon}
-                      setShowEditDialog={setShowEditDialog}
-                      setProjectToEdit={setProjectToEdit}
-                      setShowDeleteDialog={setShowDeleteDialog}
-                      setProjectToDelete={setProjectToDelete} */
-                    />
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* {showEditDialog ? (
-        <EditProject
-          setShowEditDialog={setShowEditDialog}
-          project={projectToEdit}
-        />
-      ) : null}
-      {showDeleteDialog && (
-        <DeleteProject
-          setShowDeleteDialog={setShowDeleteDialog}
-          project={projectToDelete}
-        />
-      )} */}
-    </>
+  const renderProjectDetails = () => (
+    <></>
+    //{<ProjectDetails />}
+  );
+
+  return (
+    showProjectDetails ?
+      renderProjectDetails() :
+      <ProjectList
+        allProjects={allProjects}
+        isAdminUser={isAdminUser}
+        projectClickHandler={projectClickHandler}/>
   );
 
 };
