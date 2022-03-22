f6438a2e5744fd602f7109c17c22d5edd8cc4935
Aniket Kaushik <aniket.k.kaushik@outlook.com>
2022-03-22 10:12:40 +0530

    lowercase project instance variable

--- db/seeds/07_project_member.rb
+++ db/seeds/07_project_member.rb
@@ -3,12 +3,12 @@
 # Projects Members Start
 # vipul, supriya, akhil, keshav are working on @project_1_Client_1_saeloun_india
 [@vipul, @supriya, @akhil, @keshav].each do |user|
-  user.project_members.create(project_id: @project_1_Client_1_saeloun_india.id, hourly_rate: 5000)
+  user.project_members.create(project_id: @project_1_client_1_saeloun_india.id, hourly_rate: 5000)
 end
 
 # supriya, akhil, keshav, rohit are working on @project_1_Client_1_saeloun_us
 [@supriya, @akhil, @keshav, @rohit].each do |user|
-  user.project_members.create(project_id: @project_1_Client_1_saeloun_us.id, hourly_rate: 5000)
+  user.project_members.create(project_id: @project_1_client_1_saeloun_us.id, hourly_rate: 5000)
 end
 puts "Projects Members Created"
 # Projects Members end
