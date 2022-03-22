8659ec1cfac925a121ade78e87b2308f39c9eec5
Aniket Kaushik <aniket.k.kaushik@outlook.com>
2022-03-23 00:01:00 +0530

    added new line

--- app/controllers/internal_api/v1/generate_invoice_controller.rb
+++ app/controllers/internal_api/v1/generate_invoice_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class InternalApi::V1::GenerateInvoiceController < InternalApi::V1::ApplicationController
-  skip_after_action :verify_authorized
+  # skip_after_action :verify_authorized
   def index
+    authorize :generate_invoice
     render :index, locals: { current_company: current_company }, status: :ok
   end
 end
