bf5960d5efd532d4f2ff4016c59044952ac300c4
Aniket Kaushik <aniket.k.kaushik@outlook.com>
2022-03-23 19:12:08 +0530

    minor changes

--- app/controllers/internal_api/v1/generate_invoice_controller.rb
+++ app/controllers/internal_api/v1/generate_invoice_controller.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 class InternalApi::V1::GenerateInvoiceController < InternalApi::V1::ApplicationController
-  # skip_after_action :verify_authorized
   def index
-    authorize :generate_invoice
+    authorize :index, policy_class: GenerateInvoicePolicy
     render :index, locals: { current_company: current_company }, status: :ok
   end
 end
--- spec/requests/internal_api/v1/generate_invoice/index_spec.rb
+++ spec/requests/internal_api/v1/generate_invoice/index_spec.rb
@@ -15,11 +15,11 @@ RSpec.describe "InternalApi::V1::GeneratInvoice#index", type: :request do
       send_request :get, internal_api_v1_generate_invoice_index_path
     end
 
-    it "returns the company deatils and list of clients" do
+    it "returns the company details and list of clients" do
       company_details = { id: user.current_workspace.id, phone_number: user.current_workspace.business_phone, address: user.current_workspace.address, country: user.current_workspace.country }
       company_client_list = user.current_workspace.client_list
-      issue_date = Date.today
-      due_date = Date.today + 30
+      issue_date = Date.Current
+      due_date = Date.Current + 30
       expect(response).to have_http_status(:ok)
       expect(json_response["company_details"]).to eq(JSON.parse(company_details.to_json))
       expect(json_response["company_client_list"]).to eq(JSON.parse(company_client_list.to_json))
