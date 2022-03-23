ba315a5445a9e6a078394698af652e131d286119
Aniket K <94526737+aniket-k-kaushik@users.noreply.github.com>
2022-03-23 20:36:04 +0530

    Fetch company details (#217)
    * added seed for invoice and relation of invoice with company and client
    
    * fetch company and client of that company details
    
    * added space
    
    * added new line
    
    * rspec minor fix
    
    * path fix in generate_invoice index_spec
    
    * minor changes
    
    * date fix
    
    * minor date fix

--- config/routes/internal_api.rb
+++ config/routes/internal_api.rb
@@ -7,5 +7,7 @@ namespace :internal_api, defaults: { format: "json" } do
     resources :timesheet_entry, only: [:index, :create, :update, :destroy]
     resources :reports, only: [:index]
     resources :workspaces, only: [:update]
+    resources :invoices, only: [:index, :show]
+    resources :generate_invoice, only: [:index]
   end
 end
