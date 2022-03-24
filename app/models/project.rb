ec75698aca481fb9cde7cc0ef2812fdc09a2abbb
Aniket K <94526737+aniket-k-kaushik@users.noreply.github.com>
2022-03-24 14:57:08 +0530

    Total hours logged against the project by team members - Backend API  (#210)
    * added projects controller in the internal API
    
    * created show project_users_details method on the project model
    
    * n+1 query realted changes
    
    * n+1 query related changes
    
    * removed extra code
    
    * rspec for project model project_team_member_details method
    
    * rspec for project_members model Delegates
    
    * rspec for project controller show action
    
    * changed delegate :full_name, :timesheet_entries, to: :user
    
    * tried with json builder for show page response
    
    * rspec minor fix
    
    * added frozen_string_literal: true
    
    * removed extra line
    
    * changed project instance variable to local variable
    
    * policy fix

--- app/models/project.rb
+++ app/models/project.rb
@@ -34,6 +34,27 @@ class Project < ApplicationRecord
   # Callbacks
   after_discard :discard_project_members
 
+  def project_team_member_details(time_frame)
+    from, to = week_month_year(time_frame)
+    project_members.map do |project_member|
+      minutes_logged = (project_member.timesheet_entries.where(project_id: project_member.project_id, work_date: from..to)).sum(:duration)
+      { user_id: project_member.user_id, user_name: project_member.full_name, user_hourly_rate: project_member.hourly_rate, minutes_logged: minutes_logged }
+    end
+  end
+
+  def week_month_year(time_frame)
+    case time_frame
+    when "last_week"
+      return ((Date.today.beginning_of_week) - 7), ((Date.today.end_of_week) - 7)
+    when "month"
+      return Date.today.beginning_of_month, Date.today.end_of_month
+    when "year"
+      return Date.today.beginning_of_year, Date.today.end_of_year
+    else
+      return Date.today.beginning_of_week, Date.today.end_of_week
+    end
+  end
+
   def project_member_full_names
     project_members.map do |member|
       user = User.find(member.user_id)
