b193d2de8cdf47ca3041c181d26defb0d5fa7c38
Abinash <abinashp@protonmail.ch>
2022-03-29 11:06:01 +0530

    Weekly time tracking (#187)
    * checkbox icons added
    
    * checking if project exist
    
    * fixed typo
    
    * spec/models/timesheet_entry_spec.rb updated
    
    * initial commit
    
    * weekly component
    
    * ui refactored
    
    * removed update_project controller
    
    * removed update_project controller
    
    * code refactor
    
    * crashing issue fix
    
    * try catch added in exios request
    
    * code refactor
    
    * DatesInWeek.tsx component added
    
    * minor ui changes
    
    * multiple edit entry bug fixes
    
    * rubocop issue fixed
    
    * removed note validation from rspec
    
    * large client, project name UI fixed
    
    * invoice spec update on invoice.due_date
    
    * fix invoices datacy label typo
    
    fixed root page tests. renamed "invoice-tab" to "invoices-tab"
    
    * migration for note added
    
    * interchange save & cancel button in SelectProject.tsx
    
    Co-authored-by: Mohini Dahiya <mohini@saeloun.com>

--- config/routes/internal_api.rb
+++ config/routes/internal_api.rb
@@ -3,6 +3,12 @@
 namespace :internal_api, defaults: { format: "json" } do
   namespace :v1 do
     resources :clients, only: [:index, :update, :destroy, :show]
+    resources :project, only: [:index]
+    resources :timesheet_entry do
+      collection do
+        resource :bulk_action, only: [:update, :destroy], controller: "timesheet_entry/bulk_action"
+      end
+    end
     resources :projects, only: [:index, :show]
     resources :timesheet_entry, only: [:index, :create, :update, :destroy]
     resources :reports, only: [:index]
