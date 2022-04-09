21e63135c0acf49e8ca8f2133910294d10db055f
Aniket Kaushik <aniket.k.kaushik@outlook.com>
2022-04-10 00:50:55 +0530

    added show action

--- app/controllers/internal_api/v1/generate_invoice_controller.rb
+++ app/controllers/internal_api/v1/generate_invoice_controller.rb
@@ -5,4 +5,15 @@ class InternalApi::V1::GenerateInvoiceController < InternalApi::V1::ApplicationC
     authorize :index, policy_class: GenerateInvoicePolicy
     render :index, locals: { current_company: }, status: :ok
   end
+
+  def show
+    authorize :show, policy_class: GenerateInvoicePolicy
+    render json: { client: }, status: :ok
+  end
+
+  private
+
+    def client
+      @_client ||= Client.find(params[:id])
+    end
 end
