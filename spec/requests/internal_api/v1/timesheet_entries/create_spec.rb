c84e513cf52d1350d77b03db1805865d9f4045de
Gowsik <gowsik@saeloun.com>
2022-04-11 10:58:46 +0530

    Added request specs for timesheet entries controller

---
+++ spec/requests/internal_api/v1/timesheet_entries/create_spec.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "InternalApi::V1::TimesheetEntry#update", type: :request do
+  let(:company) { create(:company) }
+  let(:user) { create(:user, current_workspace_id: company.id) }
+  let(:client) { create(:client, company:) }
+  let(:project) { create(:project, client:) }
+
+  context "when user is admin" do
+    before do
+      create(:company_user, company:, user:)
+      user.add_role :admin, company
+      sign_in user
+      send_request :post, internal_api_v1_timesheet_entry_index_path, params: {
+        project_id: project.id,
+        timesheet_entry: {
+          duration: 20,
+          work_date: Time.now,
+          note: "Test Note",
+          bill_status: :unbilled
+        }
+      }
+    end
+
+    it "is successful" do
+      expect(response).to be_successful
+    end
+
+    it "returns success json response" do
+      expect(json_response["entry"]["project_id"]).to match(project.id)
+      expect(json_response["notice"]).to match("Timesheet created")
+    end
+  end
+
+  context "when user is employee" do
+    before do
+      create(:company_user, company:, user:)
+      user.add_role :employee, company
+      sign_in user
+      send_request :post, internal_api_v1_timesheet_entry_index_path, params: {
+        project_id: project.id,
+        timesheet_entry: {
+          duration: 20,
+          work_date: Time.now,
+          note: "Test Note",
+          bill_status: :unbilled
+        }
+      }
+    end
+
+    it "is successful" do
+      expect(response).to be_successful
+    end
+
+    it "returns success json response" do
+      expect(json_response["entry"]["project_id"]).to match(project.id)
+      expect(json_response["notice"]).to match("Timesheet created")
+    end
+  end
+
+  context "when unauthenticated" do
+    it "user will be redirects to sign in path" do
+      send_request :post, internal_api_v1_timesheet_entry_index_path
+      expect(response).to have_http_status(:unauthorized)
+      expect(json_response["error"]).to match("You need to sign in or sign up before continuing.")
+    end
+  end
+end
---
+++ spec/requests/internal_api/v1/timesheet_entries/update_spec.rb
@@ -0,0 +1,111 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "InternalApi::V1::TimesheetEntry#update", type: :request do
+  let(:company) { create(:company) }
+  let(:user) { create(:user, current_workspace_id: company.id) }
+  let(:user2) { create(:user, current_workspace_id: company.id) }
+  let(:client) { create(:client, company:) }
+  let(:project) { create(:project, client:) }
+  let!(:timesheet_entry) {
+    create(
+      :timesheet_entry,
+      user:,
+      project:,
+      work_date: Time.now - 30.days,
+      duration: 10,
+      note: "Test note",
+      bill_status: :unbilled
+    )
+  }
+
+  context "when user is admin" do
+    before do
+      create(:company_user, company:, user:)
+      user.add_role :admin, company
+      sign_in user
+      send_request :patch, internal_api_v1_timesheet_entry_path(timesheet_entry.id), params: {
+        project_id: project.id,
+        timesheet_entry: {
+          duration: 20,
+          work_date: Time.now,
+          note: "Updated Note",
+          bill_status: :billed
+        }
+      }
+    end
+
+    it "is successful" do
+      expect(response).to be_successful
+    end
+
+    it "returns success json response" do
+      expect(json_response["entry"]["duration"]).to match(20.0)
+      expect(json_response["entry"]["note"]).to match("Updated Note")
+      expect(json_response["entry"]["bill_status"]).to match("billed")
+      expect(json_response["notice"]).to match("Timesheet updated")
+    end
+  end
+
+  context "when user is employee" do
+    before do
+      create(:company_user, company:, user:)
+      user.add_role :employee, company
+      sign_in user
+      send_request :patch, internal_api_v1_timesheet_entry_path(timesheet_entry.id), params: {
+        project_id: project.id,
+        timesheet_entry: {
+          duration: 20,
+          work_date: Time.now,
+          note: "Updated Note",
+          bill_status: :billed
+        }
+      }
+    end
+
+    it "is successful" do
+      expect(response).to be_successful
+    end
+
+    it "returns success json response" do
+      expect(json_response["entry"]["duration"]).to match(20.0)
+      expect(json_response["entry"]["note"]).to match("Updated Note")
+      expect(json_response["entry"]["bill_status"]).to match("billed")
+      expect(json_response["notice"]).to match("Timesheet updated")
+    end
+  end
+
+  context "when employee tries to update other user's timesheet entry" do
+    before do
+      create(:company_user, company:, user: user2)
+      user2.add_role :employee, company
+      sign_in user2
+      send_request :patch, internal_api_v1_timesheet_entry_path(timesheet_entry.id), params: {
+        project_id: project.id,
+        timesheet_entry: {
+          duration: 20,
+          work_date: Time.now,
+          note: "Updated Note",
+          bill_status: :billed
+        }
+      }
+    end
+
+    it "is successful" do
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "returns success json response" do
+      expect(json_response["errors"]).to include("Couldn't find TimesheetEntry")
+    end
+  end
+
+  context "when unauthenticated" do
+    it "user will be redirects to sign in path" do
+      send_request :patch, internal_api_v1_timesheet_entry_path(timesheet_entry.id)
+      expect(response).to have_http_status(:unauthorized)
+      expect(json_response["error"]).to match("You need to sign in or sign up before continuing.")
+    end
+  end
+end
