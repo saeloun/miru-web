079ece75b824ee47fb1a37daea38a58b939cd88f
Shalaka Patil <shalu214patil@gmail.com>
2022-04-11 17:07:14 +0530

    Add request specs for company_users index API

---
+++ spec/requests/internal_api/v1/company_users/index_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "InternalApi::V1::CompanyUsers#index", type: :request do
+  let(:company1) { create(:company) }
+  let(:company2) { create(:company) }
+  let(:user1) { create(:user, current_workspace_id: company1.id) }
+  let(:user2) { create(:user) }
+  let(:user3) { create(:user) }
+
+  before do
+    create(:company_user, company_id: company1.id, user_id: user1.id)
+    create(:company_user, company_id: company1.id, user_id: user2.id)
+    create(:company_user, company_id: company2.id, user_id: user3.id)
+  end
+
+  context "when user is admin" do
+    before do
+      user1.add_role :admin, company1
+      sign_in user1
+      send_request :get, internal_api_v1_company_users_path
+    end
+
+    it "returns the list of users of company" do
+      expect(response).to have_http_status(:ok)
+      result = [ { id: user1.id, name: user1.full_name }, { id: user2.id, name: user2.full_name }]
+      expect(json_response["users"]).to match_array(JSON.parse(result.to_json))
+    end
+  end
+
+  context "when user is employee" do
+    before do
+      user1.add_role :employee, company1
+      sign_in user1
+      send_request :get, internal_api_v1_company_users_path
+    end
+
+    it "returns the list of users of company" do
+      expect(response).to have_http_status(:forbidden)
+    end
+  end
+end
