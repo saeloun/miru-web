fbc7f38ebea7326298bfe6a09a5eee36b7aab045
Shalaka Patil <shalu214patil@gmail.com>
2022-04-12 14:53:16 +0530

    Fetch users using association instead of DB query

--- app/controllers/internal_api/v1/company_users_controller.rb
+++ app/controllers/internal_api/v1/company_users_controller.rb
@@ -3,18 +3,6 @@
 class InternalApi::V1::CompanyUsersController < InternalApi::V1::ApplicationController
   def index
     authorize CompanyUser
-    render :index, locals: { users: company_users }, status: :ok
+    render :index, locals: { users: current_company.users }, status: :ok
   end
-end
-
-private
-
-def company_users
-  @company_users = CompanyUser.includes(:user).where(company_id: current_company.id)
-    .map do |company_user|
-                        {
-                          id: company_user.user_id,
-                          name: company_user.user.full_name
-                        }
-                      end
 end
--- app/views/internal_api/v1/company_users/index.json.jbuilder
+++ app/views/internal_api/v1/company_users/index.json.jbuilder
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 json.users users do |user|
-  json.id user[:id]
-  json.name user[:name]
+  json.id user.id
+  json.name user.full_name
 end
