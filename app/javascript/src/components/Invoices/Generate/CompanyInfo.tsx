f9300202c8068912110726e9802de18b524ad1f6
Ajinkya Deshmukh <ajinkya@saeloun.com>
2022-04-12 13:52:20 +0530

    company details updated for generate invoice (#241)
    * company details updated for invoice
    
    * yarn lock updated

--- app/javascript/src/components/Invoices/Generate/CompanyInfo.tsx
+++ app/javascript/src/components/Invoices/Generate/CompanyInfo.tsx
@@ -1,24 +1,22 @@
 import React from "react";
-const logo = require("../../../../images/saeloun.svg"); // eslint-disable-line
 
-const CompanyInfo = () => (
+const CompanyInfo = ({ companyDetails }) => (
   <div className="flex justify-between border-b-2 border-miru-gray-400 p-10 h-40">
     <div className="flex">
-      <img src={logo} className="mr-5" />
+      <img src={companyDetails.logo} className="mr-5" />
       <div className="mt-2">
         <p className="font-bold text-3xl text-miru-dark-purple-1000">
-            Saeloun Inc.
+          {companyDetails.name}
         </p>
         <p className="mt-1 font-normal text-base text-miru-dark-purple-1000">
-            +1-234-454
+          {companyDetails.phone_number}
         </p>
       </div>
     </div>
 
     <div className="font-normal text-base text-right text-miru-dark-purple-1000 w-36">
-      <p>31R</p>
-      <p>Providence Rd</p>
-      <p>Westford MA,01886</p>
+      <p>{companyDetails.address}</p>
+      <p>{companyDetails.country}</p>
     </div>
   </div>
 );
