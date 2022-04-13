57e0faf3f7ea42f54800d734a7a686213c781305
Shruti <shruti@saeloun.com>
2022-04-13 23:27:23 +0530

    Add entries functionality added

--- app/javascript/src/components/Invoices/Generate/MultipleEntriesModal/Footer.tsx
+++ app/javascript/src/components/Invoices/Generate/MultipleEntriesModal/Footer.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-const Footer = () => (
+const Footer = ({ selectedRowCount, setAddEntries }) => (
   <div style={{ boxShadow: "0px 0px 40px 0px rgba(0,0,0,0.1)" }} className='flex justify-between rounded-lg px-6 py-4'>
-    <span className='font-medium text-sm text-miru-dark-purple-1000 pt-3'>5 entries selected</span>
-    <button className='py-2 px-7 bg-miru-han-purple-1000 rounded tracking-widest text-base text-white font-bold'>ADD ENTRIES</button>
+    <span className='font-medium text-sm text-miru-dark-purple-1000 pt-3'>{selectedRowCount} entries selected</span>
+    <button
+      onClick={() => setAddEntries(true)}
+      className='py-2 px-7 bg-miru-han-purple-1000 rounded tracking-widest text-base text-white font-bold'>ADD ENTRIES</button>
   </div>
 );
 
