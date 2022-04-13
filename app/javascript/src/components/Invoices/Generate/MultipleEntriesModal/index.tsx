6e7774be9b28250f5a1acd7c698ad0d5d0f80982
Shruti <shruti@saeloun.com>
2022-04-13 23:27:23 +0530

    Add entries functionality added

--- app/javascript/src/components/Invoices/Generate/MultipleEntriesModal/index.tsx
+++ app/javascript/src/components/Invoices/Generate/MultipleEntriesModal/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Table from "common/Table";
 import Footer from "./Footer";
 import Header from "./Header";
 
-const MultipleEntriesModal = ({ setShowMultilineModal }) => {
+const MultipleEntriesModal = ({ setShowMultilineModal, setNewLineItems, newLineItems }) => {
 
   const tableHeader = [
     {
@@ -34,65 +34,29 @@ const MultipleEntriesModal = ({ setShowMultilineModal }) => {
     Name: "Cameron Prichett",
     Description: "I am the description of single entry",
     Date: "23.12.2021",
-    Time: "8 Hours",
+    Time: 5,
     cssClass: "px-0 py-3"
   }, {
     id: 2,
     Name: "Chris Patt",
     Description: "I am the description of single entry",
     Date: "23.12.2021",
-    Time: "7 Hours",
+    Time: 5,
     cssClass: "px-0 py-3"
   }, {
     id: 3,
     Name: "Jake Sully",
     Description: "I am the description of single entry",
     Date: "23.12.2021",
-    Time: "3 Hours",
+    Time: 5,
     cssClass: "px-0 py-3"
   }, {
     id: 4,
     Name: "Robert jnr.",
     Description: "I am the description of single entry",
     Date: "23.12.2021",
-    Time: "6 Hours"
-  }, {
-    id: 5,
-    Name: "Paul Rud",
-    Description: "I am the description of single entry",
-    Date: "23.12.2021",
-    Time: "6 Hours",
-    cssClass: "px-0 py-3"
-  }, {
-    id: 6,
-    Name: "Chris evans",
-    Description: "I am the description of single entry",
-    Date: "23.12.2021",
-    Time: "6 Hours",
-    cssClass: "px-0 py-3"
-  }, {
-    id: 7,
-    Name: "Chris hemsworth",
-    Description: "I am the description of single entry",
-    Date: "23.12.2021",
-    Time: "6 Hours",
-    cssClass: "px-0 py-3"
-  }, {
-    id: 8,
-    Name: "Leo decaprio",
-    Description: "I am the description of single entry",
-    Date: "23.12.2021",
-    Time: "6 Hours",
-    cssClass: "px-0 py-3"
-  }, {
-    id: 9,
-    Name: "will smith",
-    Description: "I am the description of single entry",
-    Date: "23.12.2021",
-    Time: "6 Hours",
-    cssClass: "px-0 py-3"
-  }
-  ];
+    Time: 5
+  }];
 
   const getTableData = (Entries) => {
     if (Entries) {
@@ -107,6 +71,53 @@ const MultipleEntriesModal = ({ setShowMultilineModal }) => {
 
   const tableData = getTableData(MultipleEntries);
 
+  const [multipleEntries, setMultipleEntries] = useState<any>([]);
+  const [selectedRows, setSelectedRows] = useState<any>([]);
+  const [addEntries, setAddEntries] = useState<boolean>(false);
+
+  useEffect(() => {
+    const multipleItems = selectedRows.map(row => {
+      const rowItem = {
+        name: "",
+        description: "",
+        date: "",
+        qty: 0,
+        rate: 1,
+        lineTotal: 0
+      };
+      for (const key in row.original) {
+        switch (key) {
+          case "col1":
+            rowItem.name = row.original[key].props.children;
+            break;
+          case "col2":
+            rowItem.description = row.original[key].props.children;
+            break;
+          case "col3":
+            rowItem.date = row.original[key].props.children;
+            break;
+          case "col4":
+            rowItem.qty = row.original[key].props.children;
+            break;
+        }
+        rowItem.lineTotal = rowItem.qty * rowItem.rate;
+      }
+      return rowItem;
+    }
+    );
+    if (multipleItems) {
+      setMultipleEntries(multipleItems);
+    }
+  }, [selectedRows]);
+
+  useEffect(() => {
+    if (addEntries) {
+      const arr = newLineItems.concat(multipleEntries);
+      setNewLineItems(arr);
+      setShowMultilineModal(false);
+    }
+  }, [addEntries]);
+
   return (
     <div style={{ background: "rgba(29, 26, 49,0.6)" }} className="px-52 py-20 w-full h-full fixed inset-0 flex justify-center z-50">
       <div className="bg-white rounded-lg w-full h-full flex flex-col justify-between">
@@ -120,9 +131,13 @@ const MultipleEntriesModal = ({ setShowMultilineModal }) => {
             showRowBorder={false}
             tableHeader={tableHeader}
             tableRowArray={tableData}
+            setSelectedRows={setSelectedRows}
           />
         </div>
-        <Footer />
+        <Footer
+          selectedRowCount={selectedRows.length}
+          setAddEntries={setAddEntries}
+        />
       </div>
     </div>
   );
--- app/javascript/src/components/Invoices/Generate/NewLineItemRow.tsx
+++ app/javascript/src/components/Invoices/Generate/NewLineItemRow.tsx
@@ -9,7 +9,7 @@ const NewLineItemRow = ({ item }) => (
     <td className="border-b-2 border-miru-gray-200 px-1 py-3 font-normal text-base text-miru-dark-purple-1000 text-left ">
       {item.date}
     </td>
-    <td className="border-b-2 border-miru-gray-200 px-1 py-3 font-normal text-base text-miru-dark-purple-1000 text-left ">
+    <td className="border-b-2 border-miru-gray-200 px-1 py-3 font-normal text-base text-miru-dark-purple-1000 text-left">
       {item.description}
     </td>
     <td className="border-b-2 border-miru-gray-200 px-1 py-3 font-normal text-base text-miru-dark-purple-1000 text-right ">
