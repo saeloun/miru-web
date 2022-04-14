6b8638a74ae69215c31351cc9c7a7b027cde767a
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-04-14 14:33:56 +0530

    [BE]APIs to save invoices (#256)
    * API for invoice creation
    
    * API to update an invoice
    
    * Remove unnecessary if clause
    
    * use partials to dry up
    
    * abstract client.find and fix specs

--- config/routes/internal_api.rb
+++ config/routes/internal_api.rb
@@ -13,7 +13,7 @@ namespace :internal_api, defaults: { format: "json" } do
     resources :timesheet_entry, only: [:index, :create, :update, :destroy]
     resources :reports, only: [:index]
     resources :workspaces, only: [:update]
-    resources :invoices, only: [:index]
+    resources :invoices, only: [:index, :create, :update]
     resources :generate_invoice, only: [:index, :show]
   end
 end
