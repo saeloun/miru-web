02b26e87d64e4d6f5234cf85cda92de078205151
Apoorv Tiwari <tiwari.apoorv1316@gmail.com>
2022-04-14 15:55:30 +0530

    remove client and company code (#261)
    Co-authored-by: “Apoorv <“tiwari.apoorv1316@gmail.com”>

--- db/seeds/05_clients.rb
+++ db/seeds/05_clients.rb
@@ -3,15 +3,15 @@
 # Client Create Start
 client_1_india = {
   name: "client_1_saeloun_India", email: "client_one@saeloun_india.com", phone: "+91 9999999991",
-  address: "Somewhere on Earth", client_code: "SA"
+  address: "Somewhere on Earth"
 }
 client_2_india = {
   name: "client_2_saeloun_India", email: "client_two@saeloun_india.com", phone: "+91 9999999992",
-  address: "Somewhere on Earth", client_code: "SB"
+  address: "Somewhere on Earth"
 }
 client_us = {
   name: "client_1_saeloun_US", email: "client_one@saeloun_us.com", phone: "+91 9999999993",
-  address: "Somewhere on Earth", client_code: "SU"
+  address: "Somewhere on Earth"
 }
 
 @client_1_saeloun_india = @saeloun_india.clients.create!(client_1_india)
