afa62c69abb2c410db1fe9102c15a15f667325d1
Aniket Kaushik <aniket.k.kaushik@outlook.com>
2022-04-14 15:51:40 +0530

    added clients and projects in index api of project

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -9,7 +9,20 @@ class ProjectPolicy < ApplicationPolicy
     user_owner_or_admin?
   end
 
+  def update?
+    user_owner_or_admin?
+  end
+
   def show?
     user_owner_or_admin?
+  end
+
+  def permitted_attributes
+    [
+      :issue_date, :due_date,
+      :invoice_number, :reference, :amount,
+      :outstanding_amount, :tax, :amount_paid,
+      :amount_due, :discount, :client_id
+    ]
   end
 end
