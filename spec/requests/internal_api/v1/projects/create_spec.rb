8d725df08642d12d0b780be57512b5705ffbe27e
Aniket K <94526737+aniket-k-kaushik@users.noreply.github.com>
2022-04-15 08:15:49 +0530

    Project Create/Update/Delete API (#265)
    * added clients and projects in index api of project
    
    * added create and update action for projects
    
    * added delete action for project
    
    * delect action project
    
    * added rspec test for project index page
    
    * update rspec test for index action
    
    * updated index spec for project
    
    * rubocop fix
    
    * project index api minor fix
    
    * addred rspec test for update action
    
    * added rspec test for destroy action
    
    * schema fix
    
    * updated jbuilder _project partial to use json.extract!
    
    * grammatical mistakes fixed

---
+++ spec/requests/internal_api/v1/projects/create_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "InternalApi::V1::Project#create", type: :request do
+  let(:company) { create(:company) }
+  let(:user) { create(:user, current_workspace_id: company.id) }
+  let(:client) { create(:client, company:) }
+
+  context "when user is admin" do
+    before do
+      create(:company_user, company:, user:)
+      user.add_role :admin, company
+      sign_in user
+    end
+
+    describe "#create" do
+      it "creates the project successfully" do
+        project = attributes_for(:project, client_id: client.id)
+        send_request :post, internal_api_v1_projects_path(project:)
+        expect(response).to have_http_status(:ok)
+        expected_attrs = ["billable", "description", "id", "name"]
+        expect(json_response.keys.sort).to match(expected_attrs)
+      end
+
+      it "throws 422 if the client doesn't exist" do
+        send_request :post, internal_api_v1_projects_path(
+          project: {
+            name: "Test Project",
+            description: "Rspec Test",
+            billable: false
+          })
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(json_response["errors"]["client"].first).to eq("must exist")
+      end
+    end
+  end
+
+  context "when the user is an employee" do
+    before do
+      create(:company_user, company:, user:)
+      user.add_role :employee, company
+      sign_in user
+      send_request :post, internal_api_v1_projects_path
+    end
+
+    it "is not be permitted to generate an project" do
+      expect(response).to have_http_status(:forbidden)
+    end
+  end
+
+  context "when unauthenticated" do
+    it "is not be permitted to generate an project" do
+      send_request :post, internal_api_v1_projects_path
+      expect(response).to have_http_status(:unauthorized)
+      expect(json_response["error"]).to eq("You need to sign in or sign up before continuing.")
+    end
+  end
+end
---
+++ spec/requests/internal_api/v1/projects/update_spec.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "InternalApi::V1::Project#update", type: :request do
+  let(:company) { create(:company) }
+  let(:user) { create(:user, current_workspace_id: company.id) }
+  let(:client) { create(:client, company:) }
+  let(:project) { create(:project, client:) }
+
+  context "when user is an admin" do
+    before do
+      create(:company_user, company:, user:)
+      user.add_role :admin, company
+      sign_in user
+    end
+
+    describe "#update" do
+      it "updates project successfully" do
+        send_request :patch, internal_api_v1_project_path(
+          id: project.id, params: {
+            project: {
+              description: "test for update"
+            }
+          })
+        expect(response).to have_http_status(:ok)
+        expect(json_response["description"]).to eq("test for update")
+      end
+
+      it "throws 422 if the client doesn't exist" do
+        send_request :patch, internal_api_v1_project_path(
+          id: project.id, params: {
+            project: {
+              client_id: 100000,
+              description: "test for update"
+            }
+          })
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(json_response["errors"]["client"].first).to eq("must exist")
+      end
+    end
+  end
+
+  context "when user is an employee" do
+    before do
+      create(:company_user, company:, user:)
+      user.add_role :employee, company
+      sign_in user
+      send_request :patch, internal_api_v1_project_path(id: project.id)
+    end
+
+    it "is not be permitted to update an project" do
+      expect(response).to have_http_status(:forbidden)
+    end
+  end
+
+  context "when unauthenticated" do
+    it "is not be permitted to update an project" do
+      send_request :patch, internal_api_v1_project_path(id: project.id)
+      expect(response).to have_http_status(:unauthorized)
+      expect(json_response["error"]).to eq("You need to sign in or sign up before continuing.")
+    end
+  end
+end
