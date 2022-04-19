5bc16e33d7c99ace4b96794b7c889d99d7f1cfbf
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-04-19 06:19:19 +0530

    Integrate save invoice API (#285)
    * Make invoice number editable
    
    * Return invoices order by created_at desc
    
    * Rename as timesheet_entry_id
    
    * Integrate save invoices API

--- app/javascript/src/apis/invoices.ts
+++ app/javascript/src/apis/invoices.ts
@@ -5,6 +5,10 @@ const path = "/invoices";
 const get = async (query = "") =>
   axios.get(query ? `${path}?${query}` : `${path}`);
 
-const invoicesApi = { get };
+const post = async (body) => axios.post(`${path}`, body);
+
+const patch = async (id, body) => axios.post(`${path}/${id}`, body);
+
+const invoicesApi = { get, post, patch };
 
 export default invoicesApi;
