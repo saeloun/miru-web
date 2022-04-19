bf30262f1699a96cc30de40b1d8ebd3e68826e7b
Ajinkya Deshmukh <ajinkya@saeloun.com>
2022-04-19 06:17:57 +0530

    Client details page (#282)
    * client list page updated
    
    * clean up
    
    * folder structure updated
    
    * client details page
    
    * client side validation
    
    * ui lint fixes
    
    * lint fixes
    
    * pagy updated
    
    * client updates
    
    * ruby tests updated
    
    * commented index_spec.rb in spec/requests/clients folder
    
    Co-authored-by: Aniket Kaushik <aniket.k.kaushik@outlook.com>

--- app/javascript/src/components/Clients/Modals/NewClient.tsx
+++ app/javascript/src/components/Clients/Modals/NewClient.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-
+import React from "react";
 import clients from "apis/clients";
 import { Formik, Form, Field } from "formik";
 import { X } from "phosphor-react";
@@ -12,32 +11,20 @@ const newClientSchema = Yup.object().shape({
   address: Yup.string().required("Address cannot be blank")
 });
 
-const getInitialvalues = ({ name, email }) => ({
-  name: name,
-  email: email,
+const initialValues = {
+  name: "",
+  email: "",
   phoneNo: "",
   address: ""
-});
+};
 
-export interface IEditClient {
-  setShowEditDialog: any;
-  client: any;
-}
-
-const EditClient = ({ setShowEditDialog, client }: IEditClient) => {
-
-  const [apiError, setApiError] = useState<string>("");
-
-  const handleSubmit = async values => {
-    await clients.update(client.id, {
-      client: {
-        ...values
-      }
-    }).then(() => {
-      setShowEditDialog(false);
-    }).catch((e) => {
-      setApiError(e.message);
-    });
+const EditClient = ({ setnewClient, clientData, setClientData }) => {
+  const handleSubmit = (values) => {
+    clients.create(values)
+      .then(res => {
+        setClientData([...clientData, { ...res.data }]);
+        setnewClient(false);
+      });
   };
 
   return (
@@ -52,12 +39,12 @@ const EditClient = ({ setShowEditDialog, client }: IEditClient) => {
           <div className="rounded-lg px-6 pb-6 bg-white shadow-xl transform transition-all sm:align-middle sm:max-w-md modal-width">
             <div className="flex justify-between items-center mt-6">
               <h6 className="text-base font-extrabold">Add New Client</h6>
-              <button type="button" onClick={() => { setShowEditDialog(false); }}>
+              <button type="button" onClick={() => { setnewClient(false); }}>
                 <X size={16} color="#CDD6DF" weight="bold" />
               </button>
             </div>
             <Formik
-              initialValues={getInitialvalues(client)}
+              initialValues={initialValues}
               validationSchema={newClientSchema}
               onSubmit={handleSubmit}
             >
@@ -69,7 +56,7 @@ const EditClient = ({ setShowEditDialog, client }: IEditClient) => {
                         <label className="form__label">Name</label>
                         <div className="tracking-wider block text-xs text-red-600">
                           {errors.name && touched.name &&
-                          <div>{errors.name}</div>
+                            <div>{errors.name}</div>
                           }
                         </div>
                       </div>
@@ -84,7 +71,7 @@ const EditClient = ({ setShowEditDialog, client }: IEditClient) => {
                         <label className="form__label">Email</label>
                         <div className="tracking-wider block text-xs text-red-600">
                           {errors.email && touched.email &&
-                          <div>{errors.email}</div>
+                            <div>{errors.email}</div>
                           }
                         </div>
                       </div>
@@ -99,7 +86,7 @@ const EditClient = ({ setShowEditDialog, client }: IEditClient) => {
                         <label className="form__label">Phone number</label>
                         <div className="tracking-wider block text-xs text-red-600">
                           {errors.phoneNo && touched.phoneNo &&
-                          <div>{errors.phoneNo}</div>
+                            <div>{errors.phoneNo}</div>
                           }
                         </div>
                       </div>
@@ -114,7 +101,7 @@ const EditClient = ({ setShowEditDialog, client }: IEditClient) => {
                         <label className="form__label">Address</label>
                         <div className="tracking-wider block text-xs text-red-600">
                           {errors.address && touched.address &&
-                          <div>{errors.address}</div>
+                            <div>{errors.address}</div>
                           }
                         </div>
                       </div>
@@ -123,7 +110,6 @@ const EditClient = ({ setShowEditDialog, client }: IEditClient) => {
                       </div>
                     </div>
                   </div>
-                  <p className="tracking-wider mt-3 block text-xs text-red-600">{apiError}</p>
                   <div className="actions mt-4">
                     <input
                       type="submit"
