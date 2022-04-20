e5d255ba38b63044faaf9d3d6ca2511ffdb5735a
Murtaza Bagwala <bagwalamurtaza@gmail.com>
2022-04-20 16:01:02 +0530

    fix comments and add unit test case

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -63,8 +63,8 @@ class Invoice < ApplicationRecord
     @_sub_total ||= invoice_line_items.sum { |line_item| line_item[:rate] * line_item[:quantity] }
   end
 
-  def update_timesheet_entry_status
-    timesheet_entry_ids = invoice_line_items.map { |line_item| line_item.timesheet_entry_id }
-    TimesheetEntry.where(id: timesheet_entry_ids).update(bill_status: :billed)
+  def update_timesheet_entry_status!
+    timesheet_entry_ids = invoice_line_items.pluck(:timesheet_entry_id)
+    TimesheetEntry.where(id: timesheet_entry_ids).update!(bill_status: :billed)
   end
 end
