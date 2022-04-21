91f0b36787be0b8efec0fafc37157248b8b4d6e9
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-04-21 17:24:51 +0530

    Show invoice page (#299)
    * Setup route and component
    
    * Add the show action for invoice
    
    * Header for invoice page
    
    * Attach company details to invoice
    
    * Show company info
    
    * Show client info
    
    * Show invoice info
    
    * Show invoice line items
    
    * Handle back button click on invoice page
    
    * Fix warning for unique key
    
    * Fix sub total calculation
    
    * Address review comments

---
+++ app/javascript/src/components/Invoices/Invoice/InvoiceStatus.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import getStatusCssClass from "../../../utils/getStatusTag";
+
+const InvoiceStatus = ({ invoice }) => (
+  <div className="flex flex-row">
+    <div className="flex self-center mr-2">
+      <p className="text-4xl font-bold">Invoice #{invoice.invoiceNumber}</p>
+    </div>
+    <div className="flex self-center ml-2">
+      <span className={`${getStatusCssClass("draft")} uppercase`}>
+        {invoice.status}
+      </span>
+    </div>
+  </div>
+);
+
+export default InvoiceStatus;
