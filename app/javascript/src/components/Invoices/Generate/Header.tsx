8916f9f08e021dde571e7e893a020cfb7003bcb5
Shruti-Apte <72149587+Shruti-Apte@users.noreply.github.com>
2022-04-22 10:41:38 +0530

    Added toast message for empty Invoice number error (#302)
    Co-authored-by: Shruti <shruti@saeloun.com>

--- app/javascript/src/components/Invoices/Generate/Header.tsx
+++ app/javascript/src/components/Invoices/Generate/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import invoicesApi from "apis/invoices";
 import { X, FloppyDisk, PaperPlaneTilt } from "phosphor-react";
 
@@ -21,8 +22,8 @@ const Header = ({
   const navigate = useNavigate();
   const [isInvoiceSavedSuccessfully, setIsInvoiceSavedSuccessfully] = React.useState<boolean>(false);
 
-  const saveInvoice = async () => {
-    const res = await invoicesApi.post({
+  const saveInvoice = () => {
+    invoicesApi.post({
       client_id: client.value,
       invoice_number: invoiceNumber,
       reference: reference,
@@ -36,43 +37,45 @@ const Header = ({
         quantity: ilt.qty,
         timesheet_entry_id: ilt.timesheet_entry_id
       }))
-    });
+    })
+      .then(()=>navigate("/invoices"))
+      .catch();
 
-    if (res.status === 200) {
-      navigate("/invoices");
-    }
   };
 
   return (
-    <div className="sm:flex mt-6 mb-3 sm:items-center sm:justify-between">
-      <h2 className="header__title font-bold">Generate Invoice</h2>
+    <React.Fragment>
+      <ToastContainer/>
+      <div className="sm:flex mt-6 mb-3 sm:items-center sm:justify-between">
+        <h2 className="header__title font-bold">Generate Invoice</h2>
 
-      <div className="flex w-2/5">
-        <Link
-          to="/invoices"
-          type="button"
-          className="header__button w-1/3 p-0"
-        >
-          <X size={12} />
-          <span className="ml-2 inline-block">CANCEL</span>
-        </Link>
-        <button
-          type="button"
-          className="header__button bg-miru-han-purple-1000 text-white w-1/3 p-0 hover:text-white"
-          onClick={saveInvoice}
-        >
-          <FloppyDisk size={18} color="white" />
-          <span className="ml-2 inline-block">SAVE</span>
-        </button>
-        <button
-          type="button"
-          className="header__button bg-miru-han-purple-1000 text-white w-1/3 p-0 hover:text-white"
-        >
-          <PaperPlaneTilt size={18} color="White" />
-          <span className="ml-2 inline-block">SEND TO</span>
-        </button>
+        <div className="flex w-2/5">
+          <Link
+            to="/invoices"
+            type="button"
+            className="header__button w-1/3 p-0"
+          >
+            <X size={12} />
+            <span className="ml-2 inline-block">CANCEL</span>
+          </Link>
+          <button
+            type="button"
+            className="header__button bg-miru-han-purple-1000 text-white w-1/3 p-0 hover:text-white"
+            onClick={saveInvoice}
+          >
+            <FloppyDisk size={18} color="white" />
+            <span className="ml-2 inline-block">SAVE</span>
+          </button>
+          <button
+            type="button"
+            className="header__button bg-miru-han-purple-1000 text-white w-1/3 p-0 hover:text-white"
+          >
+            <PaperPlaneTilt size={18} color="White" />
+            <span className="ml-2 inline-block">SEND TO</span>
+          </button>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
