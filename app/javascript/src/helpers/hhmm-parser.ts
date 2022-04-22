57344a286364bee0c84c133ece110cf67a94a825
Abinash Panda <abinashp@protonmail.ch>
2022-04-22 12:47:50 +0530

    changed variable names in hhmm-parser.ts

--- app/javascript/src/helpers/hhmm-parser.ts
+++ app/javascript/src/helpers/hhmm-parser.ts
@@ -1,23 +1,23 @@
-export const minutesFromHHMM = (d: string) => {
-  const numD = Number(d);
-  if (d.includes(":")) {
-    const [h, m] = d.split(":");
+export const minutesFromHHMM = (duration: string) => {
+  const numberDuration = Number(duration);
+  if (duration.includes(":")) {
+    const [h, m] = duration.split(":");
     return Number(h) * 60 + Number(m);
-  } else if (numD > 0) {
-    return numD * 60;
+  } else if (numberDuration > 0) {
+    return numberDuration * 60;
   } else {
     return 0;
   }
 };
 
-export const minutesToHHMM = (t: number) => {
-  if (Number.isNaN(t) || t <= 0) {
+export const minutesToHHMM = (duration: number) => {
+  if (Number.isNaN(duration) || duration <= 0) {
     return "00:00";
   } else {
-    let h = (t / 60).toString().split(".")[0];
-    let m = (t % 60).toString();
-    if (h.length === 1) h = `0${h}`;
-    if (m.length === 1) m = `0${m}`;
-    return `${h}:${m}`;
+    let hours = (duration / 60).toString().split(".")[0];
+    let minutes = (duration % 60).toString();
+    if (hours.length === 1) hours = `0${hours}`;
+    if (minutes.length === 1) minutes = `0${minutes}`;
+    return `${hours}:${minutes}`;
   }
 };
