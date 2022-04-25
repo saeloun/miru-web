347418ad5907675c72eb3b780133480b64830a92
Shalaka Patil <shalu214patil@gmail.com>
2022-04-25 17:11:26 +0530

    Ignore deleted team members on project's page (#320)
    * Ignore deleted team members on project's page
    
    * Ignore deleted team members on project's page
    
    * Use pluck instead of map

--- app/controllers/internal_api/v1/company_users_controller.rb
+++ app/controllers/internal_api/v1/company_users_controller.rb
@@ -3,6 +3,6 @@
 class InternalApi::V1::CompanyUsersController < InternalApi::V1::ApplicationController
   def index
     authorize CompanyUser
-    render :index, locals: { users: current_company.users }, status: :ok
+    render :index, locals: { users: current_company.users.kept }, status: :ok
   end
 end
