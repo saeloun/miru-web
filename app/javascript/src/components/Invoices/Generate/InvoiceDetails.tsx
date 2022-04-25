4c413ac3e1fef5cd20466a98c9741b1386dd5a9d
Ajinkya Deshmukh <ajinkya@saeloun.com>
2022-04-25 15:42:04 +0530

    Datepick addition (#322)
    * datepicker addition
    
    * datepicker addition
    
    * datepicker added

--- app/javascript/src/components/Invoices/Generate/InvoiceDetails.tsx
+++ app/javascript/src/components/Invoices/Generate/InvoiceDetails.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useRef } from "react";
+import CustomDatePicker from "common/CutomDatePicker";
+import dayjs from "dayjs";
 import { currencyFormat } from "helpers/currency";
 import { PencilSimple } from "phosphor-react";
 import ClientSelection from "./ClientSelection";
@@ -9,33 +11,60 @@ const InvoiceDetails = ({
   clientList,
   selectedClient, setSelectedClient,
   amount,
-  issueDate,
-  dueDate,
+  issueDate, setIssueDate,
+  dueDate, setDueDate,
   invoiceNumber,
   setInvoiceNumber,
   reference
 }) => {
+
   const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [showDateOfIssuePicker, setShowDateOfIssuePicker] = useState<boolean>(false);
+  const [showDueDatePicker, setShowDueDatePicker] = useState<boolean>(false);
   const wrapperRef = useRef(null);
 
   useOutsideClick(wrapperRef, () => setIsEditing(false), isEditing);
 
+  const getIssuedDate = dayjs(issueDate).format("DD.MM.YYYY");
+  const getDueDate = dayjs(dueDate).format("DD.MM.YYYY");
+  const handleDatePickerChange = (date) => {
+    setIssueDate(date);
+    setShowDateOfIssuePicker(false);
+  };
+
+  const handleDueDatePicker = (date) => {
+    setDueDate(date);
+    setShowDueDatePicker(false);
+  };
+
   return (
     <div className="flex justify-between border-b-2 border-miru-gray-400 px-10 py-5 h-36">
-      <ClientSelection selectedClient={selectedClient} setSelectedClient={setSelectedClient} clientList ={clientList} />
+      <ClientSelection selectedClient={selectedClient} setSelectedClient={setSelectedClient} clientList={clientList} />
       <div className="group">
-        <p className="font-normal text-xs text-miru-dark-purple-1000 flex">
-          Date of Issue
-        </p>
-        <p className="font-normal text-base text-miru-dark-purple-1000">
-          {issueDate}
-        </p>
-        <p className="font-normal text-xs text-miru-dark-purple-1000 mt-4">
-          Due Date
-        </p>
-        <p className="font-normal text-base text-miru-dark-purple-1000">
-          {dueDate}
-        </p>
+        <div className="hoverPencil">
+          <p className="font-normal text-xs text-miru-dark-purple-1000 flex">
+            <span>Date of Issue</span>
+            <button className="ml-2 invisible" onClick={() => setShowDateOfIssuePicker(!showDateOfIssuePicker)}>
+              <PencilSimple size={13} color="#1D1A31" />
+            </button>
+          </p>
+          {showDateOfIssuePicker && <CustomDatePicker handleChange={handleDatePickerChange} />}
+          <p className="font-normal text-base text-miru-dark-purple-1000">
+            {getIssuedDate}
+          </p>
+        </div>
+        <div className="hoverPencil">
+          <p className="font-normal text-xs text-miru-dark-purple-1000 mt-4 flex">
+            <span>Due Date</span>
+            <button className="ml-2 invisible" onClick={() => setShowDueDatePicker(!showDueDatePicker)}>
+              <PencilSimple size={13} color="#1D1A31" />
+            </button>
+          </p>
+          {showDueDatePicker && <CustomDatePicker handleChange={handleDueDatePicker} />}
+          <p className="font-normal text-base text-miru-dark-purple-1000">
+            {getDueDate}
+          </p>
+        </div>
       </div>
 
       <div>
@@ -48,7 +77,7 @@ const InvoiceDetails = ({
               <PencilSimple size={13} color="#1D1A31" />
             </button>
           </div>
-          <input type="text" disabled={!isEditing} value={invoiceNumber} onChange={(e) => setInvoiceNumber(e.target.value) }/>
+          <input type="text" disabled={!isEditing} value={invoiceNumber} onChange={(e) => setInvoiceNumber(e.target.value)} />
         </div>
         <p className="font-normal text-xs text-miru-dark-purple-1000 mt-4">
           Reference
