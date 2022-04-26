e325786e28ab3b83498d68d833b88db265afc1e0
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-04-26 14:02:27 +0530

    Use react router for invoice page

--- app/javascript/src/components/Invoices/Invoice/index.tsx
+++ app/javascript/src/components/Invoices/Invoice/index.tsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { setAuthHeaders, registerIntercepts } from "apis/axios";
+import invoicesApi from "apis/invoices";
 import Header from "./Header";
 import InvoiceDetails from "./InvoiceDetails";
 
-const Invoice = ({ invoice }) => (
-  <>
-    <div className="max-w-6xl mx-auto px-2 md:px-11 font-manrope">
-      <Header invoice={invoice} />
-      <div className="bg-miru-gray-100 mt-5 mb-10 p-0 m-0 w-full">
-        <InvoiceDetails invoice={invoice}/>
+const Invoice = () => {
+  const params = useParams();
+
+  const [invoice, setInvoice] = useState<any>();
+
+  const fetchInvoice = async () => {
+    const res = await invoicesApi.getInvoice(params.id);
+    if (res.status === 200) {
+      setInvoice(res.data.invoice);
+    }
+  };
+
+  useEffect(() => {
+    setAuthHeaders();
+    registerIntercepts();
+    fetchInvoice();
+  }, []);
+
+  return (
+    <>
+      <div className="max-w-6xl mx-auto px-2 md:px-11 font-manrope">
+        { invoice && <Header invoice={invoice} /> }
+        <div className="bg-miru-gray-100 mt-5 mb-10 p-0 m-0 w-full">
+          { invoice && <InvoiceDetails invoice={invoice}/> }
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Invoice;
