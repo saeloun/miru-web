be70e2ee27140792128762b8ea4880933b3507b5
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-04-26 18:24:35 +0530

    Address review comments

--- app/javascript/src/components/Invoices/Invoice/index.tsx
+++ app/javascript/src/components/Invoices/Invoice/index.tsx
@@ -4,16 +4,24 @@ import { setAuthHeaders, registerIntercepts } from "apis/axios";
 import invoicesApi from "apis/invoices";
 import Header from "./Header";
 import InvoiceDetails from "./InvoiceDetails";
+import { ApiStatus as InvoiceStatus } from "../../../constants";
 
 const Invoice = () => {
   const params = useParams();
 
+  const [status, setStatus] = React.useState<InvoiceStatus>(
+    InvoiceStatus.IDLE
+  );
   const [invoice, setInvoice] = useState<any>();
 
   const fetchInvoice = async () => {
-    const res = await invoicesApi.getInvoice(params.id);
-    if (res.status === 200) {
+    try {
+      setStatus(InvoiceStatus.LOADING);
+      const res = await invoicesApi.getInvoice(params.id);
       setInvoice(res.data.invoice);
+      setStatus(InvoiceStatus.SUCCESS);
+    } catch (err) {
+      setStatus(InvoiceStatus.ERROR);
     }
   };
 
@@ -24,14 +32,16 @@ const Invoice = () => {
   }, []);
 
   return (
-    <>
-      <div className="max-w-6xl mx-auto px-2 md:px-11 font-manrope">
-        { invoice && <Header invoice={invoice} /> }
-        <div className="bg-miru-gray-100 mt-5 mb-10 p-0 m-0 w-full">
-          { invoice && <InvoiceDetails invoice={invoice}/> }
+    status === InvoiceStatus.SUCCESS && (
+      <>
+        <div className="max-w-6xl mx-auto px-2 md:px-11 font-manrope">
+          <Header invoice={invoice} />
+          <div className="bg-miru-gray-100 mt-5 mb-10 p-0 m-0 w-full">
+            <InvoiceDetails invoice={invoice}/>
+          </div>
         </div>
-      </div>
-    </>
+      </>
+    )
   );
 };
 
