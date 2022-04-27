f59c4c71dabb76c270778ff3b74dccaa39ed8895
ajinkyaa <ajinkya@saeloun.com>
2022-04-27 20:42:16 +0530

    employee and client updated

--- app/javascript/src/components/Clients/List/Header.tsx
+++ app/javascript/src/components/Clients/List/Header.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import { MagnifyingGlass, Plus } from "phosphor-react";
 
-const Header = ({ setnewClient }) => (
+const Header = ({ setnewClient, isAdminUser }) => (
   <div className="sm:flex mt-6 mb-3 sm:items-center sm:justify-between">
     <h2 className="header__title">
-            Clients
+      Clients
     </h2>
     <div className="header__searchWrap">
       <div className="header__searchInnerWrapper">
@@ -18,16 +18,18 @@ const Header = ({ setnewClient }) => (
         </button>
       </div>
     </div>
-    <div className="flex">
-      <button
-        type="button"
-        className="header__button"
-        onClick={() => setnewClient(true)}
-      >
-        <Plus weight="fill" size={16} />
-        <span className="ml-2 inline-block">NEW CLIENT</span>
-      </button>
-    </div>
+    {
+      isAdminUser && <div className="flex">
+        <button
+          type="button"
+          className="header__button"
+          onClick={() => setnewClient(true)}
+        >
+          <Plus weight="fill" size={16} />
+          <span className="ml-2 inline-block">NEW CLIENT</span>
+        </button>
+      </div>
+    }
   </div>
 );
 
