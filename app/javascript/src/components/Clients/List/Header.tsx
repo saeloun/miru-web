433ca02c6764ccfb29a12d6a676ae9951d4201bd
Shruti-Apte <72149587+Shruti-Apte@users.noreply.github.com>
2022-04-28 17:18:09 +0530

    restricted employee on client details page (#348)
    * employee and client updated
    
    * lint fixes
    
    * restricted employee on client details page
    
    Co-authored-by: ajinkyaa <ajinkya@saeloun.com>
    Co-authored-by: Shruti <shruti@saeloun.com>

--- app/javascript/src/components/Clients/List/Header.tsx
+++ app/javascript/src/components/Clients/List/Header.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 import { MagnifyingGlass, Plus } from "phosphor-react";
 
-const Header = ({ setnewClient }) => (
-  <div className="sm:flex mt-6 mb-3 sm:items-center sm:justify-between">
-    <h2 className="header__title">
-            Clients
-    </h2>
+const Header = ({ setnewClient, isAdminUser }) => (
+  <div
+    className={
+      isAdminUser
+        ? "sm:flex mt-6 mb-3 sm:items-center sm:justify-between"
+        : "sm:flex mt-6 mb-3 sm:items-center"
+    }
+  >
+    <h2 className="header__title">Clients</h2>
     <div className="header__searchWrap">
       <div className="header__searchInnerWrapper">
         <input
@@ -18,16 +22,18 @@ const Header = ({ setnewClient }) => (
         </button>
       </div>
     </div>
-    <div className="flex">
-      <button
-        type="button"
-        className="header__button"
-        onClick={() => setnewClient(true)}
-      >
-        <Plus weight="fill" size={16} />
-        <span className="ml-2 inline-block">NEW CLIENT</span>
-      </button>
-    </div>
+    {isAdminUser && (
+      <div className="flex">
+        <button
+          type="button"
+          className="header__button"
+          onClick={() => setnewClient(true)}
+        >
+          <Plus weight="fill" size={16} />
+          <span className="ml-2 inline-block">NEW CLIENT</span>
+        </button>
+      </div>
+    )}
   </div>
 );
 
