28d37ea1cece4c57d8baceee5dee2c31ef3b70ca
Ajinkya Deshmukh <ajinkya@saeloun.com>
2022-05-05 20:17:25 +0530

    Multiline modal invoice (#356)
    * invoice page multiline
    
    * multiline modal generate invoice
    
    * code reverted
    
    * search button aligned
    
    * comments resolved
    
    * lint fixes
    
    * lint fix
    
    * issues resolved
    
    * removed unwanted files

--- app/javascript/src/components/Invoices/Generate/index.tsx
+++ app/javascript/src/components/Invoices/Generate/index.tsx
@@ -26,11 +26,10 @@ const GenerateInvoices = () => {
   const [lineItems, setLineItems] = useState<any>([]);
   const [selectedClient, setSelectedClient] = useState<any>();
   const [invoiceNumber, setInvoiceNumber] = useState<any>("");
-  const [reference, setReference] = useState<any>("");
+  const [reference] = useState<any>("");
   const [amount, setAmount] = useState<any>(0);
-  const [outstandingAmount, setOutstandingAmount] = useState<any>(0);
   const [amountDue, setAmountDue] = useState<any>(0);
-  const [amountPaid, setAmountPaid] = useState<any>(0);
+  const [amountPaid] = useState<any>(0);
   const [discount, setDiscount] = useState<any>(0);
   const [tax, setTax] = useState<any>(0);
   const [issueDate, setIssueDate] = useState(new Date());
@@ -58,7 +57,6 @@ const GenerateInvoices = () => {
           amountPaid={amountPaid}
           discount={discount}
           tax={tax}
-          outstandingAmount={outstandingAmount}
           invoiceLineItems={selectedOption}
         />
         <Container
@@ -70,19 +68,15 @@ const GenerateInvoices = () => {
           invoiceNumber={invoiceNumber}
           setInvoiceNumber={setInvoiceNumber}
           reference={reference}
-          setReference={setReference}
           issueDate={issueDate}
           setIssueDate={setIssueDate}
           dueDate={dueDate}
           setDueDate={setDueDate}
           amount={amount}
           setAmount={setAmount}
-          outstandingAmount={outstandingAmount}
-          setOutstandingAmount={setOutstandingAmount}
           amountDue={amountDue}
           setAmountDue={setAmountDue}
           amountPaid={amountPaid}
-          setAmountPaid={setAmountPaid}
           discount={discount}
           setDiscount={setDiscount}
           tax={tax}
