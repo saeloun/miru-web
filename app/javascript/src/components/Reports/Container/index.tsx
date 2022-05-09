cdd16c16502fc0c35671baa0d133779d104faaef
Ajinkya Deshmukh <ajinkya@saeloun.com>
2022-05-09 17:23:54 +0530

    Reports filter addition (#364)
    * report page updated
    
    * lint fixes
    
    * aligment fixed
    
    * spec fixes

---
+++ app/javascript/src/components/Reports/Container/index.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import TableRow from "./TableRow";
+import { useEntry } from "../context/EntryContext";
+
+const Container = () => {
+  const { entries } = useEntry();
+
+  const getEntryList = () => {
+    if (entries.length > 0) {
+      return entries.map((timeEntry, index) => (
+        <TableRow key={index} {...timeEntry} />
+      ));
+    }
+  };
+
+  return (
+    <table className="min-w-full divide-y divide-gray-200 mt-4">
+      <thead>
+        <tr className="flex flex-row items-center">
+          <th
+            scope="col"
+            className="w-2/5 px-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest"
+          >
+            PROJECT/
+            <br />
+            CLIENT
+          </th>
+          <th
+            scope="col"
+            className="w-3/5 px-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest"
+          >
+            NOTE
+          </th>
+          <th
+            scope="col"
+            className="w-1/5 px-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest"
+          >
+            TEAM MEMBER/
+            <br />
+            DATE
+          </th>
+          <th
+            scope="col"
+            className="px-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest"
+          >
+            HOURS
+            <br />
+            LOGGED
+          </th>
+        </tr>
+      </thead>
+      <tbody className="bg-white divide-y divide-gray-200">
+        {getEntryList()}
+      </tbody>
+    </table>
+  );
+};
+
+export default Container;
