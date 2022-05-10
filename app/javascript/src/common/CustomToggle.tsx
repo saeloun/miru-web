a521d312eeb2e4f4b8ab8d8f2d023e7065b08ce3
Shruti <shruti@saeloun.com>
2022-05-10 12:34:37 +0530

    Invoice settings added

---
+++ app/javascript/src/common/CustomToggle.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CustomToggle = ({ isChecked = false ,setIsChecked ,toggleCss,id }) => (
+  <div className={`CustomToggle_Container ${toggleCss}`}>
+    <label>
+      <input
+        id={id}
+        className="toggle"
+        type="checkbox"
+        onChange={()=>setIsChecked(!isChecked)}
+        checked={isChecked}
+      />
+      <div>
+        <div></div>
+      </div>
+    </label>
+  </div>
+);
+
+export default CustomToggle;
