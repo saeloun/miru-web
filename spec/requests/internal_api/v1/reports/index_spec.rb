1f4212663b5a74888827aab2839f95efde594d3a
Shalaka Patil <shalu214patil@gmail.com>
2022-05-11 08:37:09 +0530

    Add request specs for reports index page

--- spec/requests/internal_api/v1/reports/index_spec.rb
+++ spec/requests/internal_api/v1/reports/index_spec.rb
@@ -7,20 +7,177 @@ RSpec.describe "InternalApi::V1::Reports#index", type: :request do
   let(:user) { create(:user, current_workspace_id: company.id) }
   let(:client) { create(:client, company:) }
   let(:project) { create(:project, client:) }
-  let!(:timesheet_entry) { create(:timesheet_entry, project:) }
+  let(:client2) { create(:client, company_id: company.id) }
+  let(:project2) { create(:project, client_id: client2.id) }
+  let(:client3) { create(:client, company_id: company.id) }
+  let(:project3) { create(:project, client_id: client3.id) }
+  let(:last_month_start_date) { 1.month.ago.beginning_of_month }
+  let(:this_week_start_date) { 0.weeks.ago.beginning_of_week }
 
   context "when user is admin" do
     before do
       create(:company_user, company:, user:)
       user.add_role :admin, company
       sign_in user
-      send_request :get, internal_api_v1_reports_path
     end
 
-    it "returns the time entry report" do
-      expect(response).to have_http_status(:ok)
-      expect(json_response["entries"].size).to eq(1)
-      expect(json_response["entries"].first["id"]).to eq(timesheet_entry.id)
+    context "when reports page's request is made without any filters" do
+      before do
+        @timesheet_entry1 = create(:timesheet_entry, project:)
+        get internal_api_v1_reports_path
+      end
+
+      it "returns the time entry report" do
+        expect(response).to have_http_status(:ok)
+        expect(json_response["entries"].size).to eq(1)
+        expect(json_response["entries"].first["id"]).to eq(@timesheet_entry1.id)
+      end
+    end
+
+    context "when reports page's request is made with date range filter" do
+      before do
+        @timesheet_entry1 = create(:timesheet_entry, project:, work_date: last_month_start_date)
+        @timesheet_entry2 = create(:timesheet_entry, project:, work_date: this_week_start_date)
+        @timesheet_entry3 = create(:timesheet_entry, project:, work_date: this_week_start_date)
+        get "/internal_api/v1/reports?date_range=this_week"
+      end
+
+      it "returns the time entry reports in given date range" do
+        expect(response).to have_http_status(:ok)
+        timesheet_ids_in_response = json_response["entries"].pluck("id")
+        expect(json_response["entries"].size).to eq(2)
+        expect(timesheet_ids_in_response).to include(@timesheet_entry2.id, @timesheet_entry3.id)
+        expect(timesheet_ids_in_response).not_to include(@timesheet_entry1.id)
+      end
+    end
+
+    context "when reports page's request is made with bill status filter" do
+      before do
+        @timesheet_entry1 = create(:timesheet_entry, project:, bill_status: "non_billable")
+        @timesheet_entry2 = create(:timesheet_entry, project:, bill_status: "unbilled")
+        @timesheet_entry3 = create(:timesheet_entry, project:, bill_status: "non_billable")
+      end
+
+      it "returns the time entry reports with given single status value" do
+        get "/internal_api/v1/reports?status=unbilled"
+        expect(response).to have_http_status(:ok)
+        timesheet_ids_in_response = json_response["entries"].pluck("id")
+        expect(json_response["entries"].size).to eq(1)
+        expect(timesheet_ids_in_response).to include(@timesheet_entry2.id)
+      end
+
+      it "returns the time entry reports with given multiple status values" do
+        get "/internal_api/v1/reports?status=unbilled,non_billable"
+        expect(response).to have_http_status(:ok)
+        timesheet_ids_in_response = json_response["entries"].pluck("id")
+        expect(json_response["entries"].size).to eq(3)
+        expect(timesheet_ids_in_response).to include(@timesheet_entry1.id, @timesheet_entry2.id, @timesheet_entry3.id)
+      end
+    end
+
+    context "when reports page's request is made with clients filter" do
+      before do
+        @timesheet_entry1 = create(:timesheet_entry, project:)
+        @timesheet_entry2 = create(:timesheet_entry, project:)
+        @timesheet_entry3 = create(:timesheet_entry, project: project2)
+        @timesheet_entry4 = create(:timesheet_entry, project: project3)
+      end
+
+      it "returns the time entry reports with given single client value" do
+        get "/internal_api/v1/reports?client_id=#{client.id}"
+        expect(response).to have_http_status(:ok)
+        timesheet_ids_in_response = json_response["entries"].pluck("id")
+        expect(json_response["entries"].size).to eq(2)
+        expect(timesheet_ids_in_response).to include(@timesheet_entry1.id, @timesheet_entry2.id)
+      end
+
+      it "returns the time entry reports with given multiple client values" do
+        get "/internal_api/v1/reports?client_id=#{client.id},#{client2.id}"
+        expect(response).to have_http_status(:ok)
+        timesheet_ids_in_response = json_response["entries"].pluck("id")
+        expect(json_response["entries"].size).to eq(3)
+        expect(timesheet_ids_in_response).to include(@timesheet_entry1.id, @timesheet_entry2.id, @timesheet_entry3.id)
+      end
+    end
+
+    context "when reports page's request is made with team members filter" do
+      before do
+        @user1 = create(:user)
+        @user2 = create(:user)
+        @user3 = create(:user)
+        @timesheet_entry1 = create(:timesheet_entry, user: @user1, project:)
+        @timesheet_entry2 = create(:timesheet_entry, user: @user1, project:)
+        @timesheet_entry3 = create(:timesheet_entry, user: @user2, project:)
+        @timesheet_entry4 = create(:timesheet_entry, user: @user3, project:)
+      end
+
+      it "returns the time entry reports with given single team member value" do
+        get "/internal_api/v1/reports?team_member=#{@user1.id}"
+        expect(response).to have_http_status(:ok)
+        timesheet_ids_in_response = json_response["entries"].pluck("id")
+        expect(json_response["entries"].size).to eq(2)
+        expect(timesheet_ids_in_response).to include(@timesheet_entry1.id, @timesheet_entry2.id)
+      end
+
+      it "returns the time entry reports with given multiple team members values" do
+        get "/internal_api/v1/reports?team_member=#{@user1.id},#{@user2.id}"
+        expect(response).to have_http_status(:ok)
+        timesheet_ids_in_response = json_response["entries"].pluck("id")
+        expect(json_response["entries"].size).to eq(3)
+        expect(timesheet_ids_in_response).to include(@timesheet_entry1.id, @timesheet_entry2.id, @timesheet_entry3.id)
+      end
+    end
+
+    context "when reports page's request is made with combination of filters" do
+      before do
+        @user1 = create(:user)
+        @user2 = create(:user)
+        @last_month_end_date = 1.month.ago.end_of_month
+        @timesheet_entry1 = create(
+          :timesheet_entry,
+          work_date: last_month_start_date,
+          project:,
+          user: @user1,
+          bill_status: "unbilled")
+        @timesheet_entry2 = create(
+          :timesheet_entry,
+          project:,
+          work_date: @last_month_end_date,
+          user: @user1,
+          bill_status: "unbilled")
+        @timesheet_entry3 = create(
+          :timesheet_entry,
+          project:,
+          work_date: @last_month_end_date,
+          user: @user1,
+          bill_status: "non_billable")
+        @timesheet_entry4 = create(
+          :timesheet_entry,
+          project:,
+          work_date: @last_month_end_date,
+          user: @user2,
+          bill_status: "unbilled")
+        @timesheet_entry5 = create(
+          :timesheet_entry,
+          project: project2,
+          work_date: @last_month_end_date,
+          user: @user1,
+          bill_status: "unbilled")
+        @timesheet_entry6 = create(
+          :timesheet_entry,
+          project:,
+          work_date: this_week_start_date,
+          user: @user1,
+          bill_status: "unbilled")
+      end
+
+      it "returns the time entry reports with given filter values" do
+        get "/internal_api/v1/reports?date_range=last_month&status=unbilled&team_member=#{@user1.id}&client_id=#{client.id}"
+        expect(response).to have_http_status(:ok)
+        timesheet_ids_in_response = json_response["entries"].pluck("id")
+        expect(json_response["entries"].size).to eq(2)
+        expect(timesheet_ids_in_response).to include(@timesheet_entry1.id, @timesheet_entry2.id)
+      end
     end
   end
 
