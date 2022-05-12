59f9c228878587c3aa2b82f1278170719196fba6
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-05-12 13:43:32 +0530

    Use jbuilder for resp json

--- app/controllers/internal_api/v1/payment_settings_controller.rb
+++ app/controllers/internal_api/v1/payment_settings_controller.rb
@@ -4,16 +4,7 @@ class InternalApi::V1::PaymentSettingsController < InternalApi::V1::ApplicationC
   def index
     authorize :index, policy_class: PaymentSettingsPolicy
 
-    render json: {
-      providers: {
-        stripe: {
-          connected: stripe_connected_account.nil? ? false : stripe_connected_account.details_submitted
-        },
-        paypal: {
-          connected: false
-        }
-      }
-    }
+    render :index, locals: { stripe_connected_account: }
   end
 
   def connect_stripe
@@ -21,7 +12,7 @@ class InternalApi::V1::PaymentSettingsController < InternalApi::V1::ApplicationC
 
     StripeConnectedAccount.create!({ company: current_company }) if stripe_connected_account.nil?
 
-    render json: { account_link: stripe_connected_account.url }
+    render :connect_stripe, locals: { stripe_connected_account: }
   end
 
   private
