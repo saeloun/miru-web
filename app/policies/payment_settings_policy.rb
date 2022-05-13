c323fe54845b783ca32b178898258ab4b13627c6
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-05-13 17:41:13 +0530

    Stripe connect integration (#363)
    * Add route, controller action and model associated with stripe connect
    
    * Add account_id col and corresponding index
    
    * Config routes and actions for stripe connect
    
    * Integrate stripe connect with FE
    
    * Fix specs
    
    * Add unique index for company_id
    
    * Refactor and handle intermediate states
    
    * Integrate with FE and handle different states
    
    * Add refresh_url for stripe connect
    
    * Use jbuilder for resp json
    
    * Modify FE as json attr is modified
    
    * Replace hardcoded urls with url helper methods
    
    * Fix rubocop
    
    * Redundant code
    
    * Add specs for model validations and associations
    
    * Add specs for model methods
    
    * Fix specs
    
    * Fix payment settings refs

---
+++ app/policies/payment_settings_policy.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class PaymentSettingsPolicy < ApplicationPolicy
+  def index?
+    user_owner_or_admin?
+  end
+
+  def connect_stripe?
+    user_owner_or_admin?
+  end
+
+  def refresh_stripe_connect?
+    user_owner_or_admin? and not record.nil?
+  end
+end
