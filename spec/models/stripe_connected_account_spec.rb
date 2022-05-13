d60cf53313c826f6a7b7c13374b6d880a2d0525c
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-05-13 15:25:50 +0530

    Add specs for model methods

--- spec/models/stripe_connected_account_spec.rb
+++ spec/models/stripe_connected_account_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 RSpec.describe StripeConnectedAccount, type: :model do
-  subject { build(:stripe_connected_account) }
+  subject(:stripe_connected_account) { build(:stripe_connected_account) }
 
   describe "Validations" do
     describe "validate uniqueness of" do
@@ -14,6 +14,40 @@ RSpec.describe StripeConnectedAccount, type: :model do
   describe "Associations" do
     describe "belongs to" do
       it { is_expected.to belong_to(:company) }
+    end
+  end
+
+  describe "Instance methods" do
+    describe "#details_submitted" do
+      it "returns false" do
+        allow(Stripe::Account).to receive(:retrieve)
+          .and_return(OpenStruct.new({ details_submitted: false }))
+        expect(stripe_connected_account.details_submitted).to be(false)
+      end
+
+      it "returns true" do
+        allow(Stripe::Account).to receive(:retrieve)
+          .and_return(OpenStruct.new({ details_submitted: true }))
+        expect(stripe_connected_account.details_submitted).to be(true)
+      end
+    end
+
+    describe "#url" do
+      it "returns non nil url" do
+        allow(Stripe::AccountLink).to receive(:create)
+          .and_return(OpenStruct.new({ url: "https://connect.stripe.com/setup/s/something" }))
+        allow(Stripe::Account).to receive(:retrieve)
+          .and_return(OpenStruct.new({ details_submitted: false }))
+        expect(stripe_connected_account.url).to be("https://connect.stripe.com/setup/s/something")
+      end
+
+      it "returns nil" do
+        allow(Stripe::AccountLink).to receive(:create)
+          .and_return(OpenStruct.new({ url: "https://connect.stripe.com/setup/s/something" }))
+        allow(Stripe::Account).to receive(:retrieve)
+          .and_return(OpenStruct.new({ details_submitted: true }))
+        expect(stripe_connected_account.url).to be(nil)
+      end
     end
   end
 end
