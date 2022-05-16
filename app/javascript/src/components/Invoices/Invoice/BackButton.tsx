63ea7b0af11ed7a8594b7961c239c7b50f9f152d
Ajinkya Deshmukh <ajinkya@saeloun.com>
2022-05-16 17:15:38 +0530

    invoice edit send functionality (#379)
    * invoice edit send functionality
    
    * react router code added

--- app/javascript/src/components/Invoices/Invoice/BackButton.tsx
+++ app/javascript/src/components/Invoices/Invoice/BackButton.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import {
   ArrowLeft
 } from "phosphor-react";
 
 const BackButton = ({ href }) => (
   <div className="h-14 w-14 flex flex-row justify-center items-center mr-1">
-    <a href={href}>
+    <Link to={href}>
       <ArrowLeft size={20} />
-    </a>
+    </Link>
   </div>
 );
 
