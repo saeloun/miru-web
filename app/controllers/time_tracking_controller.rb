da4097f0ae28b15ee219ef65e1c66bba170901d3
Abinash Panda <abinashp@protonmail.ch>
2022-05-17 12:37:34 +0530

    employee dropdown added

--- app/controllers/time_tracking_controller.rb
+++ app/controllers/time_tracking_controller.rb
@@ -5,7 +5,10 @@ class TimeTrackingController < ApplicationController
   skip_after_action :verify_authorized
 
   def index
-    is_admin = current_user.is_admin?
+    is_admin = current_user.has_role? [:owner, :admin], current_company
+    user_id = current_user.id
+    employees = is_admin ? current_user.current_workspace.users.select(:id, :first_name, :last_name) : [current_user]
+
     clients = current_company.clients.includes(:projects)
     projects = {}
     clients.map { |client| projects[client.name] = client.projects }
@@ -19,7 +22,6 @@ class TimeTrackingController < ApplicationController
         1.month.since.end_of_month
         )
     entries = formatted_entries_by_date(timesheet_entries)
-
-    render :index, locals: { is_admin:, clients:, projects:, entries: }
+    render :index, locals: { is_admin:, clients:, projects:, entries:, employees:, user_id: }
   end
 end
