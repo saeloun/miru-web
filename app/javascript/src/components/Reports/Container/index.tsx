c7a5e564f040f98506e4cb7f1bd3145e17ba962e
Mayank <mayankagnihotri7@gmail.com>
2022-05-19 12:52:11 +0530

    Added download as CSV

--- app/javascript/src/components/Reports/Container/index.tsx
+++ app/javascript/src/components/Reports/Container/index.tsx
@@ -15,47 +15,45 @@ const Container = forwardRef<HTMLTableElement>((props, ref) => {
   };
 
   return (
-    <>
-      <table className='min-w-full divide-y divide-gray-200 mt-4' ref={ref}>
-        <thead>
-          <tr className='flex flex-row items-center'>
-            <th
-              scope='col'
-              className='w-2/5 px-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest'
-            >
+    <table className='min-w-full divide-y divide-gray-200 mt-4' ref={ref}>
+      <thead>
+        <tr className='flex flex-row items-center'>
+          <th
+            scope='col'
+            className='w-2/5 px-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest'
+          >
                 PROJECT/
-              <br />
+            <br />
                 CLIENT
-            </th>
-            <th
-              scope='col'
-              className='w-3/5 px-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest'
-            >
+          </th>
+          <th
+            scope='col'
+            className='w-3/5 px-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest'
+          >
                 NOTE
-            </th>
-            <th
-              scope='col'
-              className='w-1/5 px-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest'
-            >
+          </th>
+          <th
+            scope='col'
+            className='w-1/5 px-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest'
+          >
                 TEAM MEMBER/
-              <br />
+            <br />
                 DATE
-            </th>
-            <th
-              scope='col'
-              className='px-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest'
-            >
+          </th>
+          <th
+            scope='col'
+            className='px-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest'
+          >
                 HOURS
-              <br />
+            <br />
                 LOGGED
-            </th>
-          </tr>
-        </thead>
-        <tbody className='bg-white divide-y divide-gray-200'>
-          {getEntryList()}
-        </tbody>
-      </table>
-    </>
+          </th>
+        </tr>
+      </thead>
+      <tbody className='bg-white divide-y divide-gray-200'>
+        {getEntryList()}
+      </tbody>
+    </table>
   );
 }
 );
--- app/javascript/src/components/Reports/index.tsx
+++ app/javascript/src/components/Reports/index.tsx
@@ -12,6 +12,12 @@ import Header from "./Header";
 
 import { ITimeEntry } from "./interface";
 
+// declare global {
+//   interface Navigator {
+//     msSaveBlob?: (blob: any, defaultName?: string) => boolean;
+//   }
+// }
+
 const Reports = () => {
   const [timeEntries, setTimeEntries] = useState<Array<ITimeEntry>>([]);
   const [isFilterVisible, setFilterVisibilty] = useState<boolean>(false);
@@ -43,6 +49,56 @@ const Reports = () => {
     pdf.save("print.pdf");
   };
 
+  // const exportToCsv = (
+  //   filename: string,
+  //   rows: object[],
+  //   headers?: string[]
+  // ): void => {
+  //   if (!rows || !rows.length) return;
+
+  //   const separator = ",";
+  //   const keys: string[] = Object.keys(rows[0]);
+  //   let columnHeaders: string[];
+
+  //   if (headers) {
+  //     columnHeaders = headers;
+  //   } else {
+  //     columnHeaders = keys;
+  //   }
+
+  //   const csvContent =
+  //     "sep=,\n" +
+  //     columnHeaders.join(separator) +
+  //     "\n" +
+  //     rows
+  //       .map((row) =>
+  //         keys
+  //           .map((k) => {
+  //             let cell = row[k] === null || row[k] === undefined ? "" : row[k];
+  //             cell =
+  //               cell instanceof Date
+  //                 ? cell.toLocaleString()
+  //                 : cell.toString().replace(/"/g, '""');
+
+  //             if (navigator.msSaveBlob) {
+  //               // eslint-disable-next-line no-control-regex
+  //               cell = cell.replace(/[^\x00-\x7F]/g, "");
+  //             }
+  //             if (cell.search(/("|,|\n)/g) >= 0) {
+  //               cell = `"${cell}"`;
+  //             }
+  //             return cell;
+  //           })
+  //           .join(separator)
+  //       )
+  //       .join("\n");
+
+  //   const blob = new Blob([csvContent], { type: "text/csv;charset=utf8;" });
+  //   if (navigator.msSaveBlob) {
+  //     navigator.msSaveBlob(blob, filename);
+  //   }
+  // };
+
   return (
     <div>
       <EntryContext.Provider value={{ entries: timeEntries }}>
@@ -50,6 +106,7 @@ const Reports = () => {
           setFilterVisibilty={setFilterVisibilty}
           isFilterVisible={isFilterVisible}
           downloadPdf={downloadPdf}
+          // downloadCsv={exportToCsv}
         />
         <Container ref={printRef} />
         {isFilterVisible && <Filters setFilterVisibilty={setFilterVisibilty} />}
