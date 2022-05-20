240d08591f7ef6a773897626907627e887d00d21
sudeeptarlekar <sudeeptarlekar@gmail.com>
2022-05-20 17:13:22 +0530

    Added shell UI for employee settings
    Signed-off-by: sudeeptarlekar <sudeeptarlekar@gmail.com>

---
+++ app/javascript/src/components/Profile/Billing/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Billing = () => {
+  return (
+    <React.Fragment>
+      <p>Payment Page Coming up</p>
+    </React.Fragment>
+  );
+};
+
+export default Billing;
---
+++ app/javascript/src/components/Profile/UserDetail/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserDetails = () => {
+  return (
+    <React.Fragment>
+      <p>User details page coming up</p>
+    </React.Fragment>
+  );
+};
+
+export default UserDetails;
