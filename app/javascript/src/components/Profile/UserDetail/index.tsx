fe21c9ef2f887f98e904235a7fecce0a07d568a7
sudeeptarlekar <sudeeptarlekar@gmail.com>
2022-05-20 18:07:56 +0530

    Added user details in Side Nav bar
    Signed-off-by: sudeeptarlekar <sudeeptarlekar@gmail.com>

--- app/javascript/src/components/Profile/UserDetail/index.tsx
+++ app/javascript/src/components/Profile/UserDetail/index.tsx
@@ -5,14 +5,14 @@ import Header from "../Header";
 const UserDetails = () => {
   return (
     <div className="flex flex-col w-4/5">
-    <Header
-      title={'Profile Settings'}
-      subTitle={'View and manage profile settings'}
-    />
-    <div className="py-10 px-20 mt-4 bg-miru-gray-100 h-screen">
-      <p>User Details Page Coming up</p>
+      <Header
+        title={'Profile Settings'}
+        subTitle={'View and manage profile settings'}
+      />
+      <div className="py-10 pl-10 mt-4 bg-miru-gray-100 h-screen">
+        <p>User Details Page Coming up</p>
+      </div>
     </div>
-  </div>
   );
 };
 
