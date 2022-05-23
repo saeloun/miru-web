56cca866112ce0d4d4728f161437be4757966af6
sudeeptarlekar <sudeeptarlekar@gmail.com>
2022-05-23 13:46:59 +0530

    Added profile edit page
    Signed-off-by: sudeeptarlekar <sudeeptarlekar@gmail.com>

--- app/javascript/src/components/Profile/Header.tsx
+++ app/javascript/src/components/Profile/Header.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 
-const Header = ({title, subTitle}) => {
+const Header = ({ title, subTitle, showButtons = false, cancelAction = function(){0}, saveAction = function(){0} }) => {
   return (
     <div className="h-16 pl-10 py-4 bg-miru-han-purple-1000 flex text-white">
       <span className="font-bold text-2xl">{title}</span>
       <span className="font-normal text-sm pt-2 ml-4">
         {subTitle}
       </span>
+      {
+        showButtons && (
+          <div className="place-items-center">
+            <button className="border rounded-md px-3 mx-1" onClick={cancelAction}>CANCEL</button>
+            <button className="border rounded-md px-3 mx-1 bg-white text-miru-han-purple-1000" onClick={saveAction}>UPDATE</button>
+          </div>
+        )
+      }
     </div>
   );
 };
