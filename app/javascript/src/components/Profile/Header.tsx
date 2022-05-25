bdef0b6119a2b7455655ca5d2a8301c1120b9e1f
onkar <onkar@saeloun.com>
2022-05-25 14:50:08 +0530

    added disabled option to header

--- app/javascript/src/components/Profile/Header.tsx
+++ app/javascript/src/components/Profile/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Header = ({ title, subTitle, showButtons = false, cancelAction = function(){0}, saveAction = function(){0} }) => {
+const Header = ({ title, subTitle, showButtons = false, cancelAction = function(){0}, saveAction = function(){0}, isDisableUpdateBtn = false }) => {
   return (
     <div className="h-16 pl-10 py-4 bg-miru-han-purple-1000 flex text-white">
       <span className="font-bold text-2xl">{title}</span>
@@ -11,7 +11,7 @@ const Header = ({ title, subTitle, showButtons = false, cancelAction = function(
         showButtons && (
           <div className="place-items-center">
             <button className="border rounded-md px-3 mx-1" onClick={cancelAction}>CANCEL</button>
-            <button className="border rounded-md px-3 mx-1 bg-white text-miru-han-purple-1000" onClick={saveAction}>UPDATE</button>
+            <button className="border rounded-md px-3 mx-1 bg-white text-miru-han-purple-1000" onClick={saveAction} disabled={isDisableUpdateBtn}>UPDATE</button>
           </div>
         )
       }
