0ab3563dd38d75d5620eeba1207cb70bc4891ac4
onkar <onkar@saeloun.com>
2022-05-25 14:48:55 +0530

    added org routes

--- app/javascript/src/components/Profile/RouteConfig.tsx
+++ app/javascript/src/components/Profile/RouteConfig.tsx
@@ -7,12 +7,19 @@ import {
 
 import UserDetails from './UserDetail';
 import Billing from './Billing';
+import OrgEdit from "./Organization/Edit";
+import OrgBilling from "./Organization/Billing";
+import OrgPayment from "./Organization/Payment";
 
 const RouteConfig = () => (
   <Routes>
     <Route path="/profile/edit">
       <Route path="billing" element={<Billing />} />
       <Route path="" element={<UserDetails />} />
+      <Route path="paymentsettings" element={<OrgPayment />} />
+      <Route path="billing" element={<OrgBilling />} />
+      <Route path="organization" element={<OrgEdit />} />
+    {/* </Route> */}
     </Route>
   </Routes>
 );
