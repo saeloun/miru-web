6971e9466f1c40ae08dd4a688c73a8e6d1bbe61a
onkar <onkar@saeloun.com>
2022-05-25 14:51:11 +0530

    added organization payment page

---
+++ app/javascript/src/components/Profile/Organization/Payment/index.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+import Header from "../../Header"
+const OrgPayment = () => {
+  return (
+    <div className="flex flex-col w-4/5">
+      <Header
+        title={'Bank Account Details'}
+        subTitle={'Settings to receive payment from your employer'}
+      />
+      <div className="py-10 pl-10 mt-4 bg-miru-gray-100 h-screen">
+        <p>Payment Page Coming up</p>
+      </div>
+    </div>
+  );
+};
+
+export default OrgPayment;
