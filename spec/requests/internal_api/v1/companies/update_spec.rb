827563506354e586504dc620575cc20c4683448e
Abinash Panda <abinashp@protonmail.ch>
2022-05-30 01:25:59 +0530

    negative test case added

--- spec/requests/internal_api/v1/companies/update_spec.rb
+++ spec/requests/internal_api/v1/companies/update_spec.rb
@@ -11,28 +11,57 @@ RSpec.describe "InternalApi::V1::Companies::update", type: :request do
       create(:company_user, company:, user:)
       user.add_role :admin, company
       sign_in user
-      send_request(
-        :put, "#{internal_api_v1_companies_path}/#{company[:id]}", params: {
-          company: {
-            name: "Test Company",
-            address: "test address",
-            business_phone: "Test phone",
-            country: "India",
-            timezone: "IN",
-            base_currency: "Rs",
-            standard_price: "1000",
-            fiscal_year_end: "April",
-            date_format: "DD/MM/YYYY"
-          }
-        })
     end
 
-    it "response should be successful" do
-      expect(response).to be_successful
+    context "when company is valid" do
+      before do
+        send_request(
+          :put, "#{internal_api_v1_companies_path}/#{company[:id]}", params: {
+            company: {
+              name: "Test Company",
+              address: "test address",
+              business_phone: "Test phone",
+              country: "India",
+              timezone: "IN",
+              base_currency: "Rs",
+              standard_price: "1000",
+              fiscal_year_end: "April",
+              date_format: "DD/MM/YYYY"
+            }
+          })
+      end
+
+      it "response should be successful" do
+        expect(response).to be_successful
+      end
+
+      it "returns success json response" do
+        expect(json_response["notice"]).to eq(I18n.t("companies.update.success"))
+      end
     end
 
-    it "returns success json response" do
-      expect(json_response["notice"]).to eq(I18n.t("companies.update.success"))
+    context "when company is invalid" do
+      before do
+        send_request(
+          :put, "#{internal_api_v1_companies_path}/#{company[:id]}", params: {
+            company: {
+              business_phone: "12345677",
+              timezone: "",
+              base_currency: "",
+              standard_price: "",
+              fiscal_year_end: "",
+              date_format: ""
+            }
+          })
+      end
+
+      it "will fail" do
+        expect(json_response["error"]).to eq("Standard price can't be blank")
+      end
+
+      it "will not be created" do
+        change(Company, :count).by(0)
+      end
     end
   end
 end
