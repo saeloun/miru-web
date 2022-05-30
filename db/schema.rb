eacf378d6bb941d4796d46277b9a150627c6ea34
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-05-30 18:09:23 +0530

    Fix db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_25_091818) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_27_065234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
