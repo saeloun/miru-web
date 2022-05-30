1aaf1a6a9db9f08a8bb9bc24a3984bf12321f78e
onkar <onkar@saeloun.com>
2022-05-30 11:27:42 +0530

    added divider

---
+++ app/javascript/src/common/Divider/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Divider = () => {
+  return (
+    <div className="border-[1px] border-solid border-miru-gray-400 w-[648px] border-t " >
+    </div>
+  );
+}
