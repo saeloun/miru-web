e83f2dab876d08efdab8c43bef7d573d776e56c2
sudeeptarlekar <sudeeptarlekar@gmail.com>
2022-05-30 21:13:32 +0530

    Fixed CI issues
    Signed-off-by: sudeeptarlekar <sudeeptarlekar@gmail.com>

--- app/javascript/src/components/InvoiceEmail/index.tsx
+++ app/javascript/src/components/InvoiceEmail/index.tsx
@@ -6,33 +6,33 @@ const Twitter = require("../../../../assets/images/Twitter.svg"); // eslint-disa
 const MiruLogowithText = require("../../../../assets/images/MiruWhiteLogowithText.svg"); // eslint-disable-line
 
 const InvoiceEmail = ({ url, invoice, logo, lineItems, company, client }) => (
-    <div className="flex flex-col justify-between">
-      <div className="font-manrope px-24 h-16 flex justify-start bg-miru-han-purple-1000 text-white">
-        <img src={MiruLogowithText} />
-      </div>
-      <div className="max-w-6xl mx-auto px-2 md:px-11 font-manrope">
-        <Header invoice={invoice} stripeUrl={url} />
-        <div className="bg-miru-gray-100 mt-5 mb-10 p-0 m-0 w-full">
-          <InvoiceDetails
-            invoice={invoice}
-            company={company}
-            lineItems={lineItems}
-            client={client}
-            logo={logo}
-          />
-        </div>
-      </div>
-      <div className="font-manrope px-28 py-3 flex justify-between bg-miru-han-purple-1000 text-white">
-        <span className="text-xs font-normal leading-4 text-center">
-          © Miru 2022. All rights reserved.
-        </span>
-        <span className="flex justify-between w-1/4 text-xs font-normal leading-4 text-center">
-          www.getmiru.com/
-          <img src={Instagram} height="16px" width="16px" />
-          <img src={Twitter} height="16px" width="16px" />
-        </span>
+  <div className="flex flex-col justify-between">
+    <div className="font-manrope px-24 h-16 flex justify-start bg-miru-han-purple-1000 text-white">
+      <img src={MiruLogowithText} />
+    </div>
+    <div className="max-w-6xl mx-auto px-2 md:px-11 font-manrope">
+      <Header invoice={invoice} stripeUrl={url} />
+      <div className="bg-miru-gray-100 mt-5 mb-10 p-0 m-0 w-full">
+        <InvoiceDetails
+          invoice={invoice}
+          company={company}
+          lineItems={lineItems}
+          client={client}
+          logo={logo}
+        />
       </div>
     </div>
-  );
+    <div className="font-manrope px-28 py-3 flex justify-between bg-miru-han-purple-1000 text-white">
+      <span className="text-xs font-normal leading-4 text-center">
+        © Miru 2022. All rights reserved.
+      </span>
+      <span className="flex justify-between w-1/4 text-xs font-normal leading-4 text-center">
+        www.getmiru.com/
+        <img src={Instagram} height="16px" width="16px" />
+        <img src={Twitter} height="16px" width="16px" />
+      </span>
+    </div>
+  </div>
+);
 
 export default InvoiceEmail;
