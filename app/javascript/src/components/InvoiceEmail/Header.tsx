b09b005703a0f0f0fba6ad8ee84a803a39e9e877
Shruti <shruti@saeloun.com>
2022-05-30 15:10:10 +0530

    resolving comments

--- app/javascript/src/components/InvoiceEmail/Header.tsx
+++ app/javascript/src/components/InvoiceEmail/Header.tsx
@@ -14,30 +14,6 @@ const Header = ({ invoice, stripeUrl }) => {
           </p>
         </div>
         <div className="flex flex-row justify-items-right">
-          <div className="flex flex-col justify-items-center edit-button-container mx-1">
-            <button className="flex flex-row justify-center border border-miru-han-purple-1000 rounded h-10 w-32">
-              <div className="self-center flex flex-row justify-between items-center">
-                <div className="mr-1">
-                  <Printer size={16} color="#5B34EA" weight="bold" />
-                </div>
-                <p className="font-bold tracking-widest text-base text-miru-han-purple-1000 ml-1">
-                  PRINT
-                </p>
-              </div>
-            </button>
-          </div>
-          <div className="flex flex-col justify-items-center edit-button-container mx-1">
-            <button className="flex flex-row justify-center border border-miru-han-purple-1000 rounded h-10 w-44">
-              <div className="self-center flex flex-row justify-between items-center">
-                <div className="mr-1">
-                  <DownloadSimple size={16} color="#5B34EA" weight="bold" />
-                </div>
-                <p className="font-bold tracking-widest text-base text-miru-han-purple-1000 ml-1">
-                  DOWNLOAD
-                </p>
-              </div>
-            </button>
-          </div>
           <div className="flex flex-col justify-items-center send-button-container ml-1">
             <button
               className="flex flex-row justify-center items-center bg-miru-han-purple-1000 rounded h-10 w-44"
--- app/javascript/src/components/InvoiceEmail/PayOnlineMenu.tsx
+++ app/javascript/src/components/InvoiceEmail/PayOnlineMenu.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 const stripe = require('../../../../assets/images/StripeDropdown.svg'); // eslint-disable-line
-const paypal = require('../../../../assets/images/PaypalDropdown.svg'); // eslint-disable-line
 
 const Discountmenu = ( { stripeUrl } ) => (
   <div className="bg-white rounded-lg w-44 py-2 mt-10 absolute shadow">
@@ -12,12 +11,6 @@ const Discountmenu = ( { stripeUrl } ) => (
           <img src={stripe} />
           <span className="pl-2 font-medium text-sm text-miru-han-purple-1000">Pay via Stripe</span>
         </a>
-      </li>
-      <li
-        className="flex py-2.5 px-4 cursor-pointer bg-white hover:bg-miru-gray-100 rounded"
-      >
-        <img src={paypal} />
-        <span className="pl-2 font-medium text-sm text-miru-han-purple-1000">Pay via PayPal</span>
       </li>
     </ul>
   </div>
