25f154aba9a616761eb1c4fa5dd576075e392ac8
Abinash Panda <abinashp@protonmail.ch>
2022-06-01 14:42:31 +0530

    negative test case added for purge logo api

--- spec/requests/internal_api/v1/companies/purge_logo/destroy_spec.rb
+++ spec/requests/internal_api/v1/companies/purge_logo/destroy_spec.rb
@@ -4,18 +4,38 @@ require "rails_helper"
 
 RSpec.describe "InternalApi::V1::Companies::PurgeLogo#destroy", type: :request do
   let(:company) { create(:company, :with_logo) }
-  let(:user) { create(:user, current_workspace: company) }
+  let(:user1) { create(:user, current_workspace: company) }
+  let(:company_user) { create(:company_user, company:, user: user1) }
+  let(:user2) { create(:user) }
 
-  before do
-    sign_in user
-    send_request(:delete, "/internal_api/v1/companies/#{company.id}/purge_logo")
+  context "when user is not admin" do
+    before do
+      sign_in user2
+      send_request(:delete, "/internal_api/v1/companies/#{company.id}/purge_logo")
+    end
+
+    it "response should be forbidded" do
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it "returns error json response" do
+      expect(json_response["errors"]).to match("You are not authorized to perform this action")
+    end
   end
 
-  it "successful status" do
-    expect(response).to be_successful
-  end
+  context "when user is admin" do
+    before do
+      user1.add_role :admin, company
+      sign_in user1
+      send_request(:delete, "/internal_api/v1/companies/#{company.id}/purge_logo")
+    end
 
-  it "successful notice" do
-    expect(json_response["notice"]).to eq(I18n.t("companies.purge_logo.destroy.success"))
+    it "successful status" do
+      expect(response).to be_successful
+    end
+
+    it "successful notice" do
+      expect(json_response["notice"]).to eq(I18n.t("companies.purge_logo.destroy.success"))
+    end
   end
 end
