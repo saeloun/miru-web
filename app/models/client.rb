aded89ef0e8cb3cc50d1d5d0ec18aff677d1b3f7
Abinash Panda <abinashp@protonmail.ch>
2022-06-01 14:42:31 +0530

    negative test case added for purge logo api

--- app/models/client.rb
+++ app/models/client.rb
@@ -131,12 +131,6 @@ class Client < ApplicationRecord
 
   private
 
-    def unique_client_name_for_company
-      if company.clients.where(name:).exists?
-        errors.add(:name, "is already taken")
-      end
-    end
-
     def stripe_connected_account
       StripeConnectedAccount.find_by!(company:)
     end
