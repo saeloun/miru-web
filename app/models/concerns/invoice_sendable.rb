9038de557dc918cd3df98d51864c3c91cff832d9
Aniket Kaushik <aniket.k.kaushik@outlook.com>
2022-06-02 18:32:54 +0530

    path fix

--- app/models/concerns/invoice_sendable.rb
+++ app/models/concerns/invoice_sendable.rb
@@ -4,6 +4,6 @@ module InvoiceSendable
   extend ActiveSupport::Concern
 
   def send_to_email(subject:, recipients:, message:)
-    InvoiceMailer.with(invoice: self, subject:, recipients:, message:).invoice.deliver_later
+    InvoiceMailer.with(invoice: self, subject:, recipients:, message:).invoice.deliver_now
   end
 end
