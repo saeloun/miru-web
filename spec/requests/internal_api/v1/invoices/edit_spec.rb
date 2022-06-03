3a0e1a4696466ab5a01214d9b4279513d06eff21
Sudeep Tarlekar <sudeeptarlekar@gmail.com>
2022-06-03 14:00:55 +0530

    Refactor specs (#435)
    * Refactored timezones controller and specs
    
    Signed-off-by: sudeeptarlekar <sudeeptarlekar@gmail.com>
    
    * Added specs for subscription controller
    
    Signed-off-by: sudeeptarlekar <sudeeptarlekar@gmail.com>
    
    * Fixed the description
    
    Signed-off-by: sudeeptarlekar <sudeeptarlekar@gmail.com>
    
    * Added specs for invoice payments controller
    
    Signed-off-by: sudeeptarlekar <sudeeptarlekar@gmail.com>
    
    * Refactored client policy specs
    
    Signed-off-by: sudeeptarlekar <sudeeptarlekar@gmail.com>
    
    * Refactored specs
    
    Signed-off-by: sudeeptarlekar <sudeeptarlekar@gmail.com>
    
    * Removed specs
    
    Signed-off-by: sudeeptarlekar <sudeeptarlekar@gmail.com>
    
    * Refactored invoices controller
    
    Signed-off-by: sudeeptarlekar <sudeeptarlekar@gmail.com>

---
+++ spec/requests/internal_api/v1/invoices/edit_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "InternalApi::V1::InvoicesController", type: :request do
+  let(:company) { create(:company) }
+  let(:admin) { create(:user, current_workspace_id: company.id) }
+  let(:employee) { create(:user, current_workspace_id: company.id) }
+  let(:client) { create(:client, company:) }
+  let(:invoice) { create(:invoice, client:) }
+
+  before do
+    admin.add_role :admin, company
+    employee.add_role :employee, company
+  end
+
+  describe "GET edit" do
+    subject { send_request :get, edit_internal_api_v1_invoice_path(invoice) }
+
+    context "when user is an admin" do
+      before { sign_in admin }
+
+      it "returns 200 successfull response with data" do
+        expect(subject).to eq 200
+        expect(json_response["id"]).to eq invoice.id
+        expect(json_response["company"]["id"]).to eq invoice.company.id
+      end
+    end
+
+    context "when user is an employee" do
+      before { sign_in employee }
+
+      it "returns 403 status with an error message" do
+        expect(subject).to eq 403
+        expect(json_response["errors"]).to eq "You are not authorized to perform this action."
+      end
+    end
+  end
+end
