f10cfb4709814c32afc14912d8079c42feee1a61
Abinash <abinashp@protonmail.ch>
2022-06-06 14:19:01 +0530

    flipkart, microsoft client seed updated (#438)

--- db/seeds/06_projects.rb
+++ db/seeds/06_projects.rb
@@ -1,10 +1,32 @@
 # frozen_string_literal: true
 
-# Projects Start
-project_india = { name: "Project_1_Client_1_saeloun_India", description: "Timesheet app", billable: false }
-project_us = { name: "Project_1_Client_1_saeloun_us", description: "Timesheet app", billable: false }
+project_flipkart_com = { name: "Flipkart.com", description: "e-commerce site", billable: true }
+project_myntra_com = { name: "Myntra.com", description: "e-commerce fashion site", billable: true }
+project_phonepe_com = { name: "PhonePe.com", description: "Payment App", billable: true }
+project_cleartrip_com = { name: "Cleartrip.com", description: "Travel App", billable: true }
 
-@project_1_client_1_saeloun_india = @client_1_saeloun_india.projects.create(project_india)
-@project_1_client_1_saeloun_us = @client_1_saeloun_us.projects.create(project_us)
+@project_flipkart_com = @flipkart_client.projects.create!(project_flipkart_com)
+@project_myntra_com = @flipkart_client.projects.create!(project_myntra_com)
+@project_phonepe_com = @flipkart_client.projects.create!(project_phonepe_com)
+@project_cleartrip_com = @flipkart_client.projects.create!(project_cleartrip_com)
+
+project_office_com = { name: "Office.com", description: "Office 365", billable: true }
+project_skype_com = { name: "Skype.com", description: "Communication App", billable: true }
+project_azure_com = { name: "Azure.com", description: "Cloud Computing", billable: true }
+
+@project_office_com = @microsoft_client.projects.create!(project_office_com)
+@project_skype_com = @microsoft_client.projects.create!(project_skype_com)
+@project_azure_com = @microsoft_client.projects.create!(project_azure_com)
+
+project_alpha = { name: "Alpha", description: "Alpha description", billable: true }
+project_beta = { name: "Beta", description: "Beta description", billable: false }
+project_charle = { name: "Charle", description: "Charle description", billable: true }
+project_delta = { name: "Delta", description: "Delta description", billable: false }
+
+@client_one_us_project_alpha = @client_one_us.projects.create(project_alpha)
+@client_one_us_project_beta = @client_one_us.projects.create(project_beta)
+@client_two_us_project_charle = @client_two_us.projects.create(project_charle)
+@client_two_us_project_delta = @client_two_us.projects.create(project_delta)
+
 puts "Projects Created"
 # Projects End
