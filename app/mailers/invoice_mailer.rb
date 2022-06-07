770061236ac93ed564e84e6149012c5c7c086207
Shruti-Apte <72149587+Shruti-Apte@users.noreply.github.com>
2022-06-07 16:35:45 +0530

    Invoice email design fixes (#449)
    Co-authored-by: Shruti <shruti@saeloun.com>
    Co-authored-by: Akhil G Krishnan <akhilgkrishnan4u@gmail.com>

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -8,10 +8,13 @@ class InvoiceMailer < ApplicationMailer
     recipients = params[:recipients]
     subject = params[:subject]
     @message = params[:message]
-    @invoice_url = "http://#{ENV.fetch("APP_BASE_URL", "getmiru.com")}/invoices/#{@invoice.id}/view"
+    @invoice_url = "#{ENV.fetch("APP_BASE_URL", "getmiru.com")}/invoices/#{@invoice.id}/view"
 
     pdf = InvoicePayment::PdfGeneration.process(@invoice, company_logo)
     attachments["invoice_#{@invoice.invoice_number}.pdf"] = pdf
+    attachments.inline["miruLogoWithText.svg"] = File.read("public/miruLogoWithText.svg")
+    attachments.inline["Instagram.svg"] = File.read("public/Instagram.svg")
+    attachments.inline["Twitter.svg"] = File.read("public/Twitter.svg")
 
     mail(to: recipients, subject:, reply_to: "no-reply@miru.com")
   end
