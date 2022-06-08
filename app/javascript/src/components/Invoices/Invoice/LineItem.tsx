04e23696e6126331930dc06108b5ae745b1c4b62
Harshaan Nihal Khan <harshaan.n.khan@gmail.com>
2022-06-08 12:54:30 +0530

    Fix Invoice Date Format and icon  (#451)
    * format date on invoice page
    
    * Fix entrycard css
      - remove hardcoaded width
      - solve icon crop on safari bug
    
    * update date format

--- app/javascript/src/components/Invoices/Invoice/LineItem.tsx
+++ app/javascript/src/components/Invoices/Invoice/LineItem.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import dayjs from "dayjs";
 
 const LineItem = ({ item }) => {
-  const date = dayjs(item.date).format("DD.MM.YYYY");
+  const date = dayjs(item.date).format("DD-MM-YYYY");
   return (
     <tr>
       <td className="border-b-2 border-miru-gray-200 px-1 py-3 font-normal text-base text-miru-dark-purple-1000 text-left ">
