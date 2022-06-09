1308490eb7b6dbf11eafeef91e12d1b2fdd78079
Harshaan Nihal Khan <harshaan.n.khan@gmail.com>
2022-06-09 09:51:15 +0530

    Add Discard to company_user   - Update user with company_user in team_controller   - use shordhand for team policy   - add spec for multiple company condition   - add descard specs.

--- app/controllers/internal_api/v1/team_controller.rb
+++ app/controllers/internal_api/v1/team_controller.rb
@@ -2,17 +2,17 @@
 
 class InternalApi::V1::TeamController < InternalApi::V1::ApplicationController
   def destroy
-    authorize current_user, policy_class: TeamPolicy
-    user.discard!
+    authorize :team
+    company_user.discard!
     render json: {
-      user:,
+      user: company_user.user,
       notice: I18n.t("team.delete.success.message")
     }, status: :ok
   end
 
   private
 
-    def user
-      @_user ||= current_company.users.kept.find(params[:id])
+    def company_user
+      @company_user ||= current_company.company_users.kept.find_by!(user_id: params[:id])
     end
 end
