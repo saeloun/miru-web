d4b5a649d482f90bfac8ba6e87ff33817995d3d1
Sudeep Tarlekar <sudeeptarlekar@gmail.com>
2022-06-10 17:25:52 +0530

    Time entry report export (#457)
    * Added functionality to export time entries
    
    Signed-off-by: sudeeptarlekar <sudeeptarlekar@gmail.com>
    
    * Resolved rubocop
    
    Signed-off-by: sudeeptarlekar <sudeeptarlekar@gmail.com>
    
    * Resolved PR comments
    
    Signed-off-by: sudeeptarlekar <sudeeptarlekar@gmail.com>
    
    * Fixed group by clause while report export
    
    Signed-off-by: sudeeptarlekar <sudeeptarlekar@gmail.com>

---
+++ app/services/report/generate_csv.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "csv"
+
+module Report
+  class GenerateCsv
+    attr_reader :entries
+
+    def initialize(entries)
+      @entries = entries
+    end
+
+    def process
+      CSV.generate(headers: true) do |csv|
+        csv << ["Project", "Client", "Note", "Team Member", "Date", "Hours Logged"]
+        entries.each do |entry|
+          csv << [
+            "#{entry.project_name}",
+            "#{entry.client_name}",
+            "#{entry.note}",
+            "#{entry.user_full_name}",
+            "#{entry.work_date.strftime('%Y-%m-%d')}",
+            "#{entry.formatted_duration}"
+          ]
+        end
+      end
+    end
+  end
+end
