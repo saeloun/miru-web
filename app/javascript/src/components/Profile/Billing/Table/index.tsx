77545e781984028ba2ad03fb4cc40bac6ab039c6
onkar <onkar@saeloun.com>
2022-06-13 16:16:01 +0530

    removed data

--- app/javascript/src/components/Profile/Billing/Table/index.tsx
+++ app/javascript/src/components/Profile/Billing/Table/index.tsx
@@ -3,31 +3,7 @@ import * as React from "react";
 import TableHeader from "./TableHeader";
 import TableRow from "./TableRow";
 
-const data = [{
-  date: "22.04.2022",
-  description: "Monthly (22.04.2022 - 21.05.2022)",
-  team_members: 13,
-  total_bill_amt: "$65",
-  payment_type: "Online/Stripe"
-}, {
-  date: "22.03.2022",
-  description: "Monthly (22.03.2022 - 21.04.2022)",
-  team_members: 11,
-  total_bill_amt: "$55",
-  payment_type: "Online/Stripe"
-}, {
-  date: "22.02.2022",
-  description: "Monthly (22.02.2022 - 21.03.2022)",
-  team_members: 11,
-  total_bill_amt: "$55",
-  payment_type: "Online/Stripe"
-}, {
-  date: "22.01.2022",
-  description: "Buy Basic Plan",
-  team_members: "-",
-  total_bill_amt: "$10",
-  payment_type: "Online/Stripe"
-}];
+const data = [];
 
 const Table = () => (
   <table className="min-w-full mt-1 divide-y divide-gray-200">
