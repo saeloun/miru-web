64be8884f511421db5cd3484d7343ef99563d57f
Aniket Kaushik <aniket.k.kaushik@outlook.com>
2022-06-14 23:40:08 +0530

    provider policy

--- spec/policies/payments/provider_policy_spec.rb
+++ spec/policies/payments/provider_policy_spec.rb
@@ -7,11 +7,13 @@ RSpec.describe Payments::ProviderPolicy, type: :policy do
   let(:admin) { create(:user, current_workspace_id: company.id) }
   let(:owner) { create(:user, current_workspace_id: company.id) }
   let(:employee) { create(:user, current_workspace_id: company.id) }
+  let(:book_keeper) { create(:user, current_workspace_id: company.id) }
 
   before do
     admin.add_role :admin, company
     owner.add_role :owner, company
     employee.add_role :employee, company
+    employee.add_role :book_keeper, company
   end
 
   permissions :index?, :create?, :update? do
@@ -22,6 +24,7 @@ RSpec.describe Payments::ProviderPolicy, type: :policy do
 
     it "denies permission to employee" do
       expect(described_class).not_to permit(employee)
+      expect(described_class).not_to permit(book_keeper)
     end
   end
 end
