28b00971a08207111bcbdcbdbc0ece6c0c1d8800
Mohini Dahiya <mohini@saeloun.com>
2022-06-14 21:45:26 +0530

    Adds new personal detail fields to Users table (#473)
    * Adds new personal detail fields to Users table
    
    * update callback to after_initialize
    
    Co-authored-by: Akhil G Krishnan <akhilgkrishnan4u@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_13_094006) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_14_060405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -240,6 +240,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_13_094006) do
     t.bigint "invited_by_id"
     t.integer "invitations_count", default: 0
     t.datetime "discarded_at"
+    t.string "personal_email_id"
+    t.date "date_of_birth"
+    t.jsonb "social_accounts"
     t.index ["current_workspace_id"], name: "index_users_on_current_workspace_id"
     t.index ["discarded_at"], name: "index_users_on_discarded_at"
     t.index ["email"], name: "index_users_on_email", unique: true
