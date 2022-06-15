e25df32649d79b52b90ec8f2b80ba7c36a620659
Aniket Kaushik <aniket.k.kaushik@outlook.com>
2022-06-15 13:50:34 +0530

    session controller more readable

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  def after_sign_in_path_for(resource)
-    return new_company_path if resource.companies.empty? && resource.has_role?(:owner)
-
-    resource.has_book_keeper_role?(current_company) ? payments_path : time_tracking_index_path
+  def after_sign_in_path_for(user)
+    if user.has_role?(:owner) && user.companies.empty
+      new_company_path
+    elsif user.has_book_keeper_role?(current_company)
+      payments_path
+    else
+      time_tracking_index_path
+    end
   end
 end
