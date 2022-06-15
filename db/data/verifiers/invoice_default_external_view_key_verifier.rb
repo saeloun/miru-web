7bcc147f1b77affdc542162437f3b977db0f0d5d
Aniket Kaushik <aniket.k.kaushik@outlook.com>
2022-06-15 15:43:19 +0530

    minor fix

--- db/data/verifiers/invoice_default_external_view_key_verifier.rb
+++ db/data/verifiers/invoice_default_external_view_key_verifier.rb
@@ -4,7 +4,7 @@ class InvoiceDefaultExternalViewKeyVerifier
   def self.verify!
     Invoice.find_each do |invoice|
       unless invoice.external_view_key?
-        raise "external_key is not populated for Invoice with id #{invoice.id}"
+        raise "external_view_key is not populated for Invoice with id #{invoice.id}"
       end
     end
 
