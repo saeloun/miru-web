97f74590261076ededca316083ce8a30346b71c0
Gowsik Vivekanandan <52308930+gowsik-ragunath@users.noreply.github.com>
2022-06-16 21:51:55 +0530

    Fixed manual line item (#479)
    * Fixed the issue in considering line item values for price calculation
    
    * Fixed the issue in setting the manual line item entry with updated values
    
    * Included manual line item entries in invoice amount calculation
    
    * Added ability to add multiple line items in generate invoice page
    
    * Fixed the issue in adding multiple line items in generate invoice page
    
    * Added option to delete a manual line item
    
    * Fixed the issue in update the line item date
    
    * Fixed the issue in adding decimal value in quantity field
    
    * Fixed the issue in loading company logo when logo is not present and grover dev environment issue
    
    * Fixed the issue in calcualting the invoice total
    
    Co-authored-by: Gowsik <gowsik@saeloun.com>

---
+++ app/javascript/src/components/Invoices/common/utils.js
@@ -0,0 +1,40 @@
+import dayjs from "dayjs";
+
+export const generateInvoiceLineItems = (selectedLineItems, manualEntryArr) => {
+  let invoiceLineItems = [];
+  invoiceLineItems = invoiceLineItems.concat(
+    selectedLineItems.map((item) => ({
+      id: item.id,
+      name: item.name ? item.name : `${item.first_name} ${item.last_name}`,
+      description: item.description,
+      date: dayjs(item.date).format("DD/MM/YYYY"),
+      rate: item.rate,
+      quantity: item.qty,
+      timesheet_entry_id: item.time_sheet_entry ? item.time_sheet_entry : item.timesheet_entry_id
+    }))
+  );
+
+  invoiceLineItems = invoiceLineItems.concat(
+    manualEntryArr.map((item) => ({
+      idx: item.id,
+      name: item.name,
+      description: item.description,
+      date: dayjs(item.date).format("DD/MM/YYYY"),
+      rate: item.rate,
+      quantity: Number(item.qty) * 60,
+      timesheet_entry_id: item.time_sheet_entry
+    }))
+  );
+
+  return invoiceLineItems;
+};
+
+export const getMaxIdx = (arr) => {
+  const ids = arr.map(object => object.idx);
+
+  if (ids.length > 0) {
+    return Math.max(...ids);
+  }
+
+  return 0;
+};
