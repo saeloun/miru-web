dee9f71f4c5989cee656637e324b0283018b0dd5
Mohini Dahiya <mohini@saeloun.com>
2022-06-16 12:52:43 +0530

    Creates previous_employment_details table (#481)
    * Creates previous_employment_details table
    
    * Remove invoice changes from schema.rb
    
    * Update reference from CompanyUser to User
    
    * Additional tests for model
    
    * Addedd dependent-destroy to User-PreviousEmploymentDetails association test
    
    Co-authored-by: Akhil G Krishnan <akhilgkrishnan4u@gmail.com>

---
+++ spec/models/previous_employment_detail_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe PreviousEmploymentDetail, type: :model do
+  subject(:previous_employment_detail) { build(:previous_employment_detail) }
+
+  describe "Validations" do
+    describe "Associations" do
+        it { is_expected.to belong_to(:user) }
+        it { is_expected.to belong_to(:employment_detail) }
+      end
+
+    describe "Validations" do
+      it { is_expected.to validate_length_of(:company_name).is_at_most(50) }
+      it { is_expected.to validate_length_of(:role).is_at_most(50) }
+    end
+  end
+end
