6d8a592acb0e9599e9559f9d596482c43e9c84cd
Ajinkya Deshmukh <ajinkya@saeloun.com>
2022-06-16 12:11:15 +0530

    Team page updates (#482)
    * team page enhancement
    
    * mappers, context and modals added
    
    * admin checked updated
    
    * rb tests fix
    
    * rubocop fix

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -10,42 +10,42 @@ class TeamController < ApplicationController
     render :index, locals: { query:, teams: }
   end
 
-  def edit
-    authorize user, policy_class: TeamPolicy
-    @user = user
-  end
+  #   def edit
+  #     authorize user, policy_class: TeamPolicy
+  #     @user = user
+  #   end
 
-  def update
-    authorize user, policy_class: TeamPolicy
-    user.skip_reconfirmation! unless user.invitation_accepted?
-    user_email = user.email
-    user.update(user_params)
-    user.invite! if user_email != (user_params[:email]) && !user.invitation_accepted?
-    redirect_to team_index_path
-  end
+  #   def update
+  #     authorize user, policy_class: TeamPolicy
+  #     user.skip_reconfirmation! unless user.invitation_accepted?
+  #     user_email = user.email
+  #     user.update(user_params)
+  #     user.invite! if user_email != (user_params[:email]) && !user.invitation_accepted?
+  #     redirect_to team_index_path
+  #   end
 
-  def destroy
-    authorize user, policy_class: TeamPolicy
-    user.discard
-    redirect_to team_index_path
-  end
+  #   def destroy
+  #     authorize user, policy_class: TeamPolicy
+  #     user.discard
+  #     redirect_to team_index_path
+  #   end
 
-  private
+  #   private
 
-    def user_params
-      params.require(:user).permit(policy(:team).permitted_attributes)
-    end
+  #     def user_params
+  #       params.require(:user).permit(policy(:team).permitted_attributes)
+  #     end
 
-    def user
-      @_user ||= User.kept.find(params[:id])
-    end
+  #     def user
+  #       @_user ||= User.kept.find(params[:id])
+  #     end
 
-    def assign_role
-      return flash[:error] = I18n.t("team.update.error.role") if params.dig(:user, :roles).nil?
+# def assign_role
+#   return flash[:error] = I18n.t("team.update.error.role") if params.dig(:user, :roles).nil?
 
-      user.remove_role(user.roles.first.name) if user.roles.present?
-      if user.errors.empty? && current_company.present?
-        user.add_role(params[:user][:roles].downcase.to_sym, current_company)
-      end
-    end
+#   user.remove_role(user.roles.first.name) if user.roles.present?
+#   if user.errors.empty? && current_company.present?
+#     user.add_role(params[:user][:roles].downcase.to_sym, current_company)
+#   end
+# end
 end
