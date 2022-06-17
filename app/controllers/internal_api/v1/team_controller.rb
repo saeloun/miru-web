4cccfaca9d6cfac2a3a105192aec18c5dccb9d6f
Shalaka Patil <shalu214patil@gmail.com>
2022-06-17 14:02:13 +0530

    Add API to get team members (#471)
    * Add API to get team members
    
    * Add suggested changes
    
    * Fix robocop errors
    
    * Use Application helper instead of including all helpers
    
    * Use has_role method

--- app/controllers/internal_api/v1/team_controller.rb
+++ app/controllers/internal_api/v1/team_controller.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class InternalApi::V1::TeamController < InternalApi::V1::ApplicationController
+  helper ApplicationHelper
+
+  def index
+    authorize :index, policy_class: TeamPolicy
+    query = current_company.users.includes([:avatar_attachment, :roles]).ransack(params[:q])
+    team = query.result(distinct: true)
+    render :index, locals: { team: }, status: :ok
+  end
+
   def destroy
     authorize :team
     company_user.discard!
