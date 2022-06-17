be0bf4a1ee90d35509469bc2605996c230eeb90a
Shalaka Patil <shalu214patil@gmail.com>
2022-06-17 14:02:13 +0530

    Add API to get team members (#471)
    * Add API to get team members
    
    * Add suggested changes
    
    * Fix robocop errors
    
    * Use Application helper instead of including all helpers
    
    * Use has_role method

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -16,5 +16,10 @@ FactoryBot.define do
         user.avatar.attach(io: File.open(file_path), filename: file_name, content_type: "image/png")
       end
     end
+
+    trait :with_pending_invitation do
+      invitation_token { Faker::String.random(length: 10) }
+      invitation_created_at { Time.current }
+    end
   end
 end
