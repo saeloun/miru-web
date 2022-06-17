601e0be3dd7ccbba1a87ac1e57cef414d2bd8f6d
Ajinkya Deshmukh <ajinkya@saeloun.com>
2022-06-17 14:30:52 +0530

    Team details page (#485)
    * team page enhancement
    
    * mappers, context and modals added
    
    * Teaam details page
    
    * team details page updated
    
    * team details layout updated
    
    * team details page
    
    * conflict resolved
    
    * team details page updates

---
+++ app/javascript/src/components/Team/Details/ReimburstmentDetails/index.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import StaticPage from "./StaticPage";
+
+const ReimburstmentDetails = () => (
+  <Fragment>
+    <div className="px-10 py-4 bg-miru-han-purple-1000 flex items-center justify-between">
+      <h1 className="text-white font-bold text-2xl">Reimburstment Details</h1>
+    </div>
+    <StaticPage />
+  </Fragment>
+);
+
+export default ReimburstmentDetails;
