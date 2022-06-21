a316458346fbd099ae2b1a4d43abf7677c6a33b6
Mohini Dahiya <mohini@saeloun.com>
2022-06-21 13:38:33 +0530

    Update PreviousEmployment reference and Table name (#500)
    * update PreviousEmployment reference and table name
    
    * Remove unrelated changes

--- spec/factories/previous_employments.rb
+++ spec/factories/previous_employments.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :previous_employment_detail do
+  factory :previous_employment do
     user
-    employment_detail
     company_name { Faker::Company.name }
     role { Faker::Company.profession }
   end
