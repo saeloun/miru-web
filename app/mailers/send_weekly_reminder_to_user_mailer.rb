596228b6389c08b3dfd9dc3b71eda79aa00656e4
Harshaan Nihal Khan <harshaan.n.khan@gmail.com>
2022-06-22 12:07:17 +0530

    Weekly email reminder for Missing time Entries (#496)
    * Add mailer service and email template
      - Add mailer service with rough email template in html and text format
    
    * Add Specs and Preview for Mailer
    
    * Setup Sidekiq-scheduler
     - Add the gem sidekiq-scheduler
     - setup a schedule in sidekiq.yml
     - create a job to get triggered by scheduler
    
    * Add weekly_reminder_service to trigger mailer
     - Send mail to all users who has less than 40 hours for a company.
    
    * Update a Mailer Template and add preview
    
    * Add feature flag for weekly reminder
     - add a string value to turn on the weekly reminder
    
    * Add timezone and fix failing spec
    
    * fix cron
    
    * Fix review comments
    
    * Update email templates folder to views/mailers

---
+++ app/mailers/send_weekly_reminder_to_user_mailer.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class SendWeeklyReminderToUserMailer < ApplicationMailer
+  def notify_user_about_missed_entries
+    recipients = params[:recipients]
+    @company = params[:company_name]
+    @name = params[:name]
+    @starting_date = params[:start_date].strftime("%d-%b-%Y")
+    @ending_date = params[:end_date].strftime("%d-%b-%Y")
+    subject = "Reminder to Update your Timesheet in Miru"
+
+    mail(to: recipients, subject:, reply_to: "no-reply@getmiru.com")
+  end
+end
