c5cfd5f0a6c3d9bc19e223d279ee031b3bb9c147
mohinid <mohinidahiya4@gmail.com>
2022-06-22 22:41:17 +0530

    Changes - Renamed table company_users as employments - Updated Model file name and replaced company_user instances with employment - Updated reference name in User and Company model - Updated RSpecs- Employment, User and Company

---
+++ spec/models/employment_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Employment, type: :model do
+  let(:employment) { create(:employment) }
+  let(:company) { employment.company }
+  let(:user) { employment.user }
+
+  describe "Associations" do
+    it { is_expected.to belong_to(:company) }
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to have_one(:employment_detail) }
+  end
+
+  describe "Discard" do
+    let(:company_user) { create(:company_user) }
+    let(:company) { company_user.company }
+    let(:user) { company_user.user }
+
+    it "discards the company user" do
+      expect { employment.discard! }.to change(company.employments.discarded, :count).by(1)
+      expect(employment.reload.discarded?).to be_truthy
+      expect(user.reload.employments.discarded.count).to eq(1)
+    end
+
+    it "does not discard the company user if already discarded" do
+      employment.discard!
+      expect { employment.discard! }.to raise_error(Discard::RecordNotDiscarded)
+    end
+  end
+
+  describe "Presence" do
+    it { is_expected.to validate_presence_of(:designation) }
+    it { is_expected.to validate_presence_of(:employment_type) }
+    it { is_expected.to validate_presence_of(:joined_at) }
+    it { is_expected.to validate_presence_of(:employee_id) }
+  end
+
+  describe "Comparisons" do
+    it "resignation date should be nil by default" do
+      expect(employment.resigned_at).to be nil
+    end
+  end
+end
