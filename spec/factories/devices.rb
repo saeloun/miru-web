763678df0121165e9e99dd811c58c5f4f46ee683
Mohini Dahiya <mohini@saeloun.com>
2022-06-22 12:41:27 +0530

    Creates devices table and associations (#507)
    * Creates devices table and associations
    
    * remove unrelated changes from schema
    
    * Capitalize comments
    
    * Renaming column 'model' as 'name'
    
    * removing unrelated changes from schema.rb
    
    * Adds test for enum

---
+++ spec/factories/devices.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :device do
+    issued_to factory: :user
+    issued_by factory: :company
+    name { Faker::Alphanumeric.alphanumeric }
+    serial_number { Faker::Number.number(digits: 100) }
+  end
+end
