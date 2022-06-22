eecfed88f8ea1517fd134623ca9532559ef8d029
Harshaan Nihal Khan <harshaan.n.khan@gmail.com>
2022-06-22 12:07:17 +0530

    Weekly email reminder for Missing time Entries (#496)
    * Add mailer service and email template
      - Add mailer service with rough email template in html and text format
    
    * Add Specs and Preview for Mailer
    
    * Setup Sidekiq-scheduler
     - Add the gem sidekiq-scheduler
     - setup a schedule in sidekiq.yml
     - create a job to get triggered by scheduler
    
    * Add weekly_reminder_service to trigger mailer
     - Send mail to all users who has less than 40 hours for a company.
    
    * Update a Mailer Template and add preview
    
    * Add feature flag for weekly reminder
     - add a string value to turn on the weekly reminder
    
    * Add timezone and fix failing spec
    
    * fix cron
    
    * Fix review comments
    
    * Update email templates folder to views/mailers

---
+++ app/services/weekly_reminder_for_missed_entries_service.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+class WeeklyReminderForMissedEntriesService
+  WEEKLY_LIMIT = 40.hours.in_minutes
+
+  def process
+    Company.find_each do |company|
+      company.users.kept.find_each do |user|
+        check_entries_and_send_mail(user, company)
+      end
+    end
+  end
+
+  def check_entries_and_send_mail(user, company)
+    return unless user_is_admin_or_employee?(user, company)
+
+    name = user.full_name
+    company_name = company.name
+    start_date = 1.week.ago.beginning_of_week
+    end_date = 1.week.ago.end_of_week
+
+    entries = get_user_company_entries(user:, company:, start_date:, end_date:)
+
+    total_company_entries_durations = entries.sum(:duration)
+
+    if total_company_entries_durations < WEEKLY_LIMIT
+      send_mail(recipients: user.email, name:, start_date:, end_date:, company_name:)
+    end
+  end
+end
+
+def user_is_admin_or_employee?(user, company)
+  user.has_role?(:admin, company) || user.has_role?(:employee, company)
+end
+
+def send_mail(recipients:, name:, start_date:, end_date:, company_name:)
+  SendWeeklyReminderToUserMailer.with(
+    recipients:,
+    name:,
+    start_date:,
+    end_date:,
+    company_name:
+
+  ).notify_user_about_missed_entries.deliver_later
+end
+
+def get_user_company_entries(user:, start_date:, end_date:, company:)
+  user.timesheet_entries.in_workspace(company).during(start_date, end_date)
+end
