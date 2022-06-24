2d94c5c2942dc9504b0b6b594cea9aee7b039026
Harshaan Nihal Khan <harshaan.n.khan@gmail.com>
2022-06-24 15:44:54 +0530

    disable payment navtab

--- app/policies/payment_policy.rb
+++ app/policies/payment_policy.rb
@@ -2,6 +2,7 @@
 
 class PaymentPolicy < ApplicationPolicy
   def index?
-    user_book_keeper_role? || user_owner_role? || user_admin_role?
+    # user_book_keeper_role? || user_owner_role? || user_admin_role?
+    false # TODO: Temprary disabling this feature (navebar payment links)
   end
 end
