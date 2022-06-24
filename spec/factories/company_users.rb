2cabf65d1ad98508f4e0029c203433b7dc48558c
Mohini Dahiya <mohini@saeloun.com>
2022-06-24 14:16:18 +0530

    Update 'company_users' table with employment details (#515)
    * Remove association between employment_details and company_users
    
    * Update company_users table with employment details
    
    * Drop table 'employment_details'
    
    * Changes
    - Renamed table company_users as employments
    - Updated Model file name and replaced company_user instances with employment
    - Updated reference name in User and Company model
    - Updated RSpecs- Employment, User and Company
    
    * Revert "Changes"
    
    This reverts commit 6872b6e7fe0d197a487fc14193862e1f1b187501.
    
    * Remove 'address' changes from schema
    
    * Update spec/models/company_user_spec.rb
    
    Co-authored-by: Keshav Biswa <keshavbiswa21@gmail.com>
    
    Co-authored-by: Keshav Biswa <keshavbiswa21@gmail.com>

--- spec/factories/company_users.rb
+++ spec/factories/company_users.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
   factory :company_user do
     company
     user
+    employee_id { Faker::Alphanumeric.alphanumeric(number: 10, min_alpha: 3) }
+    designation { "SDE" }
+    employment_type { "Salaried" }
+    joined_at { Faker::Date.between(from: "2020-01-01", to: "2021-01-01") }
+    resigned_at {}
   end
 end
