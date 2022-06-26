6bb6963d94abe67ff948bd2000ce1374220e1fa6
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2022-06-26 14:59:44 +0530

    [Testing Review App] Invoice no-reply mail id typo fixed (#523)
    Invoice no-reply mail id fixed

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -17,7 +17,7 @@ class InvoiceMailer < ApplicationMailer
     attachments.inline["Instagram.png"] = File.read("public/Instagram.png")
     attachments.inline["Twitter.png"] = File.read("public/Twitter.png")
 
-    mail(to: recipients, subject:, reply_to: "no-reply@miru.com")
+    mail(to: recipients, subject:, reply_to: "no-reply@getmiru.com")
   end
 
   private
