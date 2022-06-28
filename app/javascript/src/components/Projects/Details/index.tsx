e315bdba17c0a183982ad01872e65629df0774d8
Gowsik Vivekanandan <52308930+gowsik-ragunath@users.noreply.github.com>
2022-06-28 18:52:17 +0530

    Amount in project detail page will have company default currency symbol (#529)
    * Added correct currency symbol to project index page
    
    * Updated project model specs
    
    * Fixed inundation issue
    
    * Used FormatAmountService to format the project related amount values
    
    * Reused format method in project model in test cases
    
    Co-authored-by: Gowsik <gowsik@saeloun.com>

--- app/javascript/src/components/Projects/Details/index.tsx
+++ app/javascript/src/components/Projects/Details/index.tsx
@@ -31,7 +31,6 @@ const getTableData = (project) => {
     return project.members.map((member) => {
       const hours = member.minutes / 60;
       const hour = hours.toFixed(2);
-      const cost = (hours * parseInt(member.hourlyRate)).toFixed(2);
       return {
         col1: (
           <div className="text-base text-miru-dark-purple-1000">
@@ -40,7 +39,7 @@ const getTableData = (project) => {
         ),
         col2: (
           <div className="text-base text-miru-dark-purple-1000 text-right">
-            ${member.hourlyRate}
+            {member.formattedHourlyRate}
           </div>
         ),
         col3: (
@@ -50,7 +49,7 @@ const getTableData = (project) => {
         ),
         col4: (
           <div className="text-lg font-bold text-miru-dark-purple-1000 text-right">
-            ${cost}
+            {member.formattedCost}
           </div>
         )
       };
