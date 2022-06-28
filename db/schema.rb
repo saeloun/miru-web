73b74ecd1497a2758fb426ffa511bb3ccaeb5a68
Onkar Hasabe <104751221+onkar-saeloun@users.noreply.github.com>
2022-06-28 23:11:44 +0530

    Reports List Page (#530)
    * added width and height properties in tailwind config
    
    * added reports routes
    
    * added route conf
    
    * added reports header
    
    * added outstanding amt page
    
    * added report card component
    
    * added report card
    
    * added revenue by client page
    
    * added time entry page
    
    * added total hours logged page
    
    * added routes path for reports
    
    * added svgs for report list page
    
    * added route in navbar
    
    * added flag to show the report in list
    
    * resolved comment

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_06_22_160123) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
