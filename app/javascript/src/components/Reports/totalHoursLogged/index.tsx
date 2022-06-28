720b20e6978e2f96f52cebc584945b5dbb79074e
onkar <onkar@saeloun.com>
2022-06-28 09:17:46 +0530

    added total hours logged page

---
+++ app/javascript/src/components/Reports/totalHoursLogged/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../Header";
+
+const TotalHoursReport = () => (
+  <div>
+    <Header
+      showNavFilters={true}
+      setFilterVisibilty={true}
+      isFilterVisible={() => { }} // eslint-disable-line  @typescript-eslint/no-empty-function
+      resetFilter={() => { }} // eslint-disable-line  @typescript-eslint/no-empty-function
+      handleDownload={() => { }} // eslint-disable-line  @typescript-eslint/no-empty-function
+      type={"Total Hours Logged"}
+    />
+    <div>
+      {/** Container will be listed here */}
+    </div>
+  </div>
+);
+
+export default TotalHoursReport;
