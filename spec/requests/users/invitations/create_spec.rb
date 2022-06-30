46e363d6583c8a44652932eaed0e68978490d088
Gowsik Vivekanandan <52308930+gowsik-ragunath@users.noreply.github.com>
2022-06-30 13:22:03 +0530

    Prevent employee from inviting team members and fixed invitation flow (#522)
    * Fixed the issue in inviting a existing and new user
    
    * Added policy to prevent employee from inviting team member and fixed invitation flow
    
    * Removed the overridden devise_controller? method
    
    * Commented company user related validation
    
    * Removed test cases for company users validation
    
    Co-authored-by: Gowsik <gowsik@saeloun.com>

--- spec/requests/users/invitations/create_spec.rb
+++ spec/requests/users/invitations/create_spec.rb
@@ -6,35 +6,79 @@ RSpec.describe "Users::Invitations#create", type: :request do
   let(:company) { create(:company) }
   let(:user) { create(:user, current_workspace_id: company.id) }
 
-  before do
-    create(:company_user, company:, user:)
-    user.add_role :admin, company
-    sign_in user
-    send_request(
-      :post, user_invitation_path, params: {
-        user: {
-          first_name: user.first_name,
-          last_name: user.last_name,
-          email: "invited@example.com",
-          roles: "employee"
-        }
-      })
+  context "when new user is invited" do
+    before do
+      create(:company_user, company:, user:)
+      user.add_role :admin, company
+      sign_in user
+      send_request(
+        :post, user_invitation_path, params: {
+          user: {
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: "invited@example.com",
+            roles: "employee"
+          }
+        })
+      @user = User.find_by(email: "invited@example.com")
+    end
+
+    it "creates new user with invitation token" do
+      expect(User.count).to eq(2)
+      expect(@user.invitation_token).not_to be_nil
+    end
+
+    it "assigns company to new user" do
+      expect(@user.companies).to include(company)
+    end
+
+    it "assigns role to new user" do
+      expect(@user.has_role?(:employee, company)).to be_truthy
+    end
+
+    it "successfully redirects to team_index_path" do
+      expect(response).to redirect_to(team_index_path)
+    end
   end
 
-  it "creates new user with invitation token" do
-    expect(User.count).to eq(2)
-    expect(User.last.invitation_token).not_to be_nil
-  end
+  context "when existing user is invited" do
+    before do
+      ActionMailer::Base.deliveries.clear
+      create(:company_user, company:, user:)
+      user.add_role :admin, company
+      sign_in user
+      @user2 = create(:user, current_workspace_id: company.id)
+      send_request(
+        :post, user_invitation_path, params: {
+          user: {
+            first_name: @user2.first_name,
+            last_name: @user2.last_name,
+            email: @user2.email,
+            roles: "employee"
+          }
+        })
+      @user2.reload
+    end
 
-  it "assigns company to new user" do
-    expect(User.last.companies).to include(company)
-  end
+    it "existing user won't have invitation token" do
+      expect(@user2.invitation_token).to be_nil
+    end
 
-  it "assigns role to new user" do
-    expect(User.last.has_role?(:employee, company)).to be_truthy
-  end
+    it "assigns company to existing user" do
+      expect(@user2.companies).to include(company)
+    end
 
-  it "successfully redirects to team_index_path" do
-    expect(response).to redirect_to(team_index_path)
+    it "assigns role to existing user" do
+      expect(@user2.has_role?(:employee, company)).to be_truthy
+    end
+
+    it "sents mail to invited users" do
+      expect(ActionMailer::Base.deliveries.count).to eq(1)
+      expect(ActionMailer::Base.deliveries.last.to).to include(@user2.email)
+    end
+
+    it "successfully redirects to team_index_path" do
+      expect(response).to redirect_to(team_index_path)
+    end
   end
 end
