10f2d257f294b08ca6b3dba4a39b2dd763f43899
Shalaka Patil <shalu214patil@gmail.com>
2022-07-01 14:10:03 +0530

    Add API for outstanstding and overdue invoices report

---
+++ app/controllers/internal_api/v1/reports/outstanding_overdue_invoices_controller.rb
@@ -0,0 +1,27 @@
+
+# frozen_string_literal: true
+
+class InternalApi::V1::Reports::OutstandingOverdueInvoicesController < InternalApi::V1::ApplicationController
+  def index
+    authorize :report
+    render :index, locals: { clients:, summary: }, status: :ok
+  end
+
+  private
+
+    def clients
+      @_clients ||= current_company.clients.order("name asc").includes(:invoices).map do |client|
+                      client.outstanding_and_overdue_invoices.merge(name: client.name)
+                    end
+    end
+
+    def summary
+      total_outstanding_amount = clients.pluck(:total_outstanding_amount).sum
+      total_overdue_amount = clients.pluck(:total_overdue_amount).sum
+      {
+        total_outstanding_amount:,
+        total_overdue_amount:,
+        total_invoice_amount: total_outstanding_amount + total_overdue_amount
+      }
+    end
+end
