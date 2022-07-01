6e5317c005f081be147629da0a4b477a656f0cde
gyandip-chauhan <gyandip3395@gmail.com>
2022-07-01 15:42:59 +0530

    fix relative button issue

--- app/javascript/src/components/Leads/Timelines/Header.tsx
+++ app/javascript/src/components/Leads/Timelines/Header.tsx
@@ -26,7 +26,7 @@ const Header = ({
       }>
       <h2 className="header__title"></h2>
       {isAdminUser && (
-        <div className="flex relative flex-col">
+        <div className="flex flex-col">
           <button
             ref={wrapperRef}
             type="button"
@@ -36,34 +36,36 @@ const Header = ({
             <span className="ml-2 inline-block">NEW TIMELINE</span>
             <CaretDown weight="fill" size={16} />
           </button>
-          {toggleMenu && <div className="my-10 ml-1 w-full absolute bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700">
-            <ul className="cursor-pointer py-1 text-sm text-gray-700 dark:text-gray-200">
-              <li>
-                <a onClick={() => setNewCommentTimeline(true)} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Comment</a>
-              </li>
-              <li>
-                <a onClick={null} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Appointment</a>
-              </li>
-              <li>
-                <a onClick={null} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Email</a>
-              </li>
-              <li>
-                <a onClick={null} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Phone Call</a>
-              </li>
-              <li>
-                <a onClick={null} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Skype DM</a>
-              </li>
-              <li>
-                <a onClick={null} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">LinkedIn DM</a>
-              </li>
-              <li>
-                <a onClick={null} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Other DM</a>
-              </li>
-              <li>
-                <a onClick={null} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Task</a>
-              </li>
-            </ul>
-          </div>}
+          <div className="relative">
+            {toggleMenu && <div className=" ml-1 w-full absolute bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700">
+              <ul className="cursor-pointer py-1 text-sm text-gray-700 dark:text-gray-200">
+                <li>
+                  <a onClick={() => setNewCommentTimeline(true)} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Comment</a>
+                </li>
+                <li>
+                  <a onClick={null} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Appointment</a>
+                </li>
+                <li>
+                  <a onClick={null} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Email</a>
+                </li>
+                <li>
+                  <a onClick={null} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Phone Call</a>
+                </li>
+                <li>
+                  <a onClick={null} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Skype DM</a>
+                </li>
+                <li>
+                  <a onClick={null} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">LinkedIn DM</a>
+                </li>
+                <li>
+                  <a onClick={null} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Other DM</a>
+                </li>
+                <li>
+                  <a onClick={null} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Task</a>
+                </li>
+              </ul>
+            </div>}
+          </div>
         </div>
       )}
     </div>
