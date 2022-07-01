46fcb408160cc6977ca7379f0c27d40a7aeb5283
onkar <onkar@saeloun.com>
2022-07-01 13:21:36 +0530

    added css changes

--- app/javascript/src/components/Reports/revenueByClient/Container/TableRow.tsx
+++ app/javascript/src/components/Reports/revenueByClient/Container/TableRow.tsx
@@ -1,40 +1,31 @@
 import React from "react";
 
-import { minutesToHHMM } from "helpers/hhmm-parser";
-import { ITimeEntry } from "../../interface";
+import { RevenueByClients } from "../interface";
 
 const TableRow = ({
   id,
-  project,
-  client,
-  note,
-  teamMember,
-  workDate,
-  duration
-}: ITimeEntry) => (
+  name,
+  unpaidAmt,
+  paidAmt,
+  total
+}: RevenueByClients) => (
 
   <tr key={id} className="flex flex-row items-center">
-    <td className="w-2/5 pr-6 py-4 text-left whitespace-nowrap">
-      <p className="font-semibold whitespace-normal text-base text-miru-dark-purple-1000">
-        {project}
-      </p>
-      <p className="font-normal text-sm text-miru-dark-purple-400">
-        {client}
+    <td className="w-3/5 pr-6 py-4 text-left whitespace-nowrap">
+      <p className="font-normal whitespace-normal text-base text-miru-dark-purple-1000">
+        {name}
       </p>
     </td>
-    <td className="w-2/5 px-6 py-4 text-left text-xs font-normal text-miru-dark-purple-1000 whitespace-pre-wrap">
-      {note}
+    <td className="w-2/5 px-6 py-4 text-left text-base font-normal text-miru-dark-purple-1000 whitespace-pre-wrap">
+      {unpaidAmt}
     </td>
     <td className="w-1/5 px-6 py-4 text-left whitespace-nowrap">
-      <p className="font-semibold text-base text-miru-dark-purple-1000">
-        {teamMember}
-      </p>
-      <p className="font-normal text-sm text-miru-dark-purple-400">
-        {workDate}
+      <p className="font-normal	 text-base text-miru-dark-purple-1000">
+        {paidAmt}
       </p>
     </td>
     <td className="w-1/5 pl-6 py-4 text-xl text-right whitespace-nowrap font-bold text-miru-dark-purple-1000">
-      {minutesToHHMM(duration)}
+      {total}
     </td>
   </tr>
 );
