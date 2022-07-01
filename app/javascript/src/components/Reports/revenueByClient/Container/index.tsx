7cd0431fcbf8d15f850ac272e95d8b5148fe80a6
onkar <onkar@saeloun.com>
2022-07-01 13:21:14 +0530

    added css changes

--- app/javascript/src/components/Reports/revenueByClient/Container/index.tsx
+++ app/javascript/src/components/Reports/revenueByClient/Container/index.tsx
@@ -1,20 +1,19 @@
 import React, { Fragment } from "react";
 import TotalHeader from "common/TotalHeader";
 import TableRow from "./TableRow";
-import { useEntry } from "../../context/EntryContext";
 
 const TableHeader = () => (
   <thead>
     <tr className="flex flex-row items-center">
       <th
         scope="col"
-        className="w-2/5 pr-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest"
+        className="w-3/5 pr-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest"
       >
         CLIENT
       </th>
       <th
         scope="col"
-        className="w-2/5 px-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest"
+        className="w-2/5 px-0 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest"
       >
         UNPAID AMOUNT
       </th>
@@ -35,7 +34,12 @@ const TableHeader = () => (
 );
 
 const Container = () => {
-  const { timeEntryReport } = useEntry();
+  const dumRep = [{
+    "name": "Neflix",
+    "unpaidAmt": "$446.41",
+    "paidAmt": "$60113",
+    "total": "$655.29"
+  }];
 
   const getEntryList = (entries) =>
     entries.map((timeEntry, index) => (
@@ -46,22 +50,21 @@ const Container = () => {
     <Fragment>
       <TotalHeader
         firstTitle={"TOTAL UNPAID AMOUNT"}
-        firstAmount={"1000K"}
+        firstAmount={"$35.5K"}
         secondTitle={"TOTAL PAID AMOUNT"}
-        secondAmount={"1000K"}
+        secondAmount={"$35.5K"}
         thirdTitle={"TOTAL REVENUE"}
-        thirdAmount={"1500K"}
+        thirdAmount={"$71.0K"}
       />
       <div>
       </div>
       {
-        timeEntryReport.reports.map((report, index) => (
+        dumRep.map((report, index) => (
           <Fragment key={index}>
-            {report.label !== "" && <h1 className="text-miru-han-purple-1000 font-bold text-xl py-5 border-b border-miru-han-purple-1000">{report.label}</h1>}
             <table className="min-w-full divide-y divide-gray-200 mt-4">
               <TableHeader />
               <tbody className="bg-white divide-y divide-gray-200">
-                {report.entries.length > 0 && getEntryList(report.entries)}
+                {dumRep.length > 0 && getEntryList(dumRep)}
               </tbody>
             </table>
           </Fragment>
