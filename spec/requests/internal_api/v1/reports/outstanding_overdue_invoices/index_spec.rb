ea9312df3c683fc26ced7cbd703f5c8101425d46
Shalaka Patil <shalu214patil@gmail.com>
2022-07-04 11:18:49 +0530

    Fix spec

--- spec/requests/internal_api/v1/reports/outstanding_overdue_invoices/index_spec.rb
+++ spec/requests/internal_api/v1/reports/outstanding_overdue_invoices/index_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe "InternalApi::V1::Reports::OutstandingOverdueInvoicesController::
 
   context "when user is an admin" do
     before do
-      create(:company_user, company:, user:)
+      create(:employment, company:, user:)
       user.add_role :admin, company
       sign_in user
     end
@@ -112,7 +112,7 @@ RSpec.describe "InternalApi::V1::Reports::OutstandingOverdueInvoicesController::
 
   context "when user is an employee" do
     before do
-      create(:company_user, company:, user:)
+      create(:employment, company:, user:)
       user.add_role :employee, company
       sign_in user
       send_request :get, internal_api_v1_reports_outstanding_overdue_invoices_path
@@ -125,7 +125,7 @@ RSpec.describe "InternalApi::V1::Reports::OutstandingOverdueInvoicesController::
 
   context "when user is a book keeper" do
     before do
-      create(:company_user, company:, user:)
+      create(:employment, company:, user:)
       user.add_role :book_keeper, company
       sign_in user
       send_request :get, internal_api_v1_reports_outstanding_overdue_invoices_path
