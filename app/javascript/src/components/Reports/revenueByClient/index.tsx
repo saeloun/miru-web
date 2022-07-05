403ec874205d634d4a93e1293dff21d84c54b3b0
Onkar Hasabe <104751221+onkar-saeloun@users.noreply.github.com>
2022-07-05 13:02:40 +0530

    Revenue by clients report page UI and common context for all reports. (#537)
    * added width and height properties in tailwind config
    
    * added reports routes
    
    * added route conf
    
    * added reports header
    
    * added outstanding amt page
    
    * added report card component
    
    * added report card
    
    * added revenue by client page
    
    * added time entry page
    
    * added total hours logged page
    
    * added routes path for reports
    
    * added svgs for report list page
    
    * added route in navbar
    
    * added flag to show the report in list
    
    * resolved comment
    
    * added total header page
    
    * changed time enty import
    
    * changed entry context
    
    * added z index
    
    * added revenue by client context
    
    * added time entry context
    
    * added time entry context import
    
    * added time entry context
    
    * added navigation changes
    
    * added revenue by client page
    
    * added header
    
    * added table
    
    * added filter
    
    * added styles
    
    * added time entry context
    
    * added changes in date util
    
    * added css
    
    * added filter
    
    * added custom date rage
    
    * added filter counter
    
    * added filte rrelated changes
    
    * added interface
    
    * added css changes
    
    * added css changes
    
    * resolved comments

--- app/javascript/src/components/Reports/revenueByClient/index.tsx
+++ app/javascript/src/components/Reports/revenueByClient/index.tsx
@@ -1,20 +1,134 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/no-var-requires */
+import React, { useState, useEffect } from "react";
+
+import { sendGAPageView } from "utils/googleAnalytics";
+import Container from "./Container";
+
+import Filters from "./Filters";
+import { getQuarter } from "./Filters/filterOptions";
+import EntryContext from "../context/EntryContext";
+
+import TimeEntryReportContext from "../context/TimeEntryReportContext";
 import Header from "../Header";
 
-const RevenueByClientReport = () => (
-  <div>
-    <Header
-      showNavFilters={true}
-      setFilterVisibilty={true}
-      isFilterVisible={() => { }} // eslint-disable-line  @typescript-eslint/no-empty-function
-      resetFilter={() => { }} // eslint-disable-line  @typescript-eslint/no-empty-function
-      handleDownload={() => { }} // eslint-disable-line  @typescript-eslint/no-empty-function
-      type={"Revenue By Client"}
-    />
+const RevenueByClientReport = () => {
+
+  const filterIntialValues = {
+    dateRange: { label: getQuarter(true), value: "this_quarter" },
+    clients: []
+  };
+
+  const [filterOptions, getFilterOptions] = useState({ clients: [] }); //eslint-disable-line
+  const [selectedFilter, setSelectedFilter] = useState(filterIntialValues);
+  const [isFilterVisible, setFilterVisibilty] = useState<boolean>(false);
+  const [showNavFilters, setNavFilters] = useState<boolean>(false);
+  const [filterCounter, setFilterCounter] = useState(0);
+  const [dateRange, setDateRange] = useState({ from: "", to: "" });
+  const [selectedInput, setSelectedInput] = useState("from-input");
+
+  useEffect(() => {
+    sendGAPageView();
+  }, []);
+
+  const onClickInput = (e) => {
+    setSelectedInput(e.target.name);
+  };
+
+  const updateFilterCounter = async () => {
+    let counter = 0;
+    for (const filterkey in selectedFilter) {
+      const filterValue = selectedFilter[filterkey];
+      if (Array.isArray(filterValue)) {
+        counter = counter + filterValue.length;
+      } else {
+        if (filterValue.value !== "") {
+          counter = counter + 1;
+        }
+      }
+    }
+    await setFilterCounter(counter);
+  };
+
+  const handleRemoveSingleFilter = (key, value) => {
+    const filterValue = selectedFilter[key];
+    if (Array.isArray(filterValue)) {
+      const closedFilter = filterValue.filter(item => item.label !== value);
+      setSelectedFilter({ ...selectedFilter, [key]: closedFilter });
+    } else {
+      const label = key === "dateRange" ? "All" : "None";
+      setSelectedFilter({ ...selectedFilter, [key]: { label, value: "" } });
+    }
+  };
+
+  useEffect(() => {
+    updateFilterCounter();
+    setNavFilters(true);
+    setFilterVisibilty(false);
+  }, [selectedFilter]);
+
+  const contextValues = {
+    timeEntryReport: TimeEntryReportContext,
+    revenueByClientReport: {
+      filterOptions: {
+        clients: [{ label: "Microsoft", value: 1 }, { label: "Flipkart", value: 2 }]
+      },
+      selectedFilter: selectedFilter,
+      customDateFilter: {
+        from: dateRange.from,
+        to: dateRange.to
+      },
+      filterCounter: filterCounter,
+      handleRemoveSingleFilter: handleRemoveSingleFilter, //eslint-disable-line
+    },
+    currentReport: "RevenueByClientReport"
+  };
+
+  const handleApplyFilter = async (filters) => {
+    setSelectedFilter(filters);
+  };
+
+  const resetFilter = () => {
+    setSelectedFilter(filterIntialValues);
+    setFilterVisibilty(false);
+    setNavFilters(false);
+  };
+
+  const handleDownload = () => {}; //eslint-disable-line
+
+  const handleSelectDate = (date) => {
+    if (selectedInput === "from-input") {
+      setDateRange({ ...dateRange, ...{ from: date } });
+    } else {
+      setDateRange({ ...dateRange, ...{ to: date } });
+    }
+  };
+
+  return (
     <div>
-      {/** Container will be listed here */}
+      <EntryContext.Provider value={{
+        ...contextValues
+      }}>
+        <Header
+          showNavFilters={showNavFilters}
+          setFilterVisibilty={setFilterVisibilty}
+          isFilterVisible={isFilterVisible}
+          resetFilter={resetFilter}
+          handleDownload={handleDownload}
+          type={"Revenue By Client"}
+        />
+        <Container />
+        {isFilterVisible && <Filters
+          handleApplyFilter={handleApplyFilter}
+          resetFilter={resetFilter}
+          setFilterVisibilty={setFilterVisibilty}
+          onClickInput={onClickInput}
+          handleSelectDate={handleSelectDate}
+          selectedInput={selectedInput}
+          dateRange={dateRange}
+        />}
+      </EntryContext.Provider>
     </div>
-  </div>
-);
+  );
+};
 
 export default RevenueByClientReport;
