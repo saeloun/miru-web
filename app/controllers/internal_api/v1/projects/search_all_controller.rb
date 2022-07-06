3b636b187cad2983b17f9254b5738029a35037e8
Abinash Panda <abinashp@protonmail.ch>
2022-07-07 01:05:00 +0530

    seach for user's name query updated

--- app/controllers/internal_api/v1/projects/search_all_controller.rb
+++ app/controllers/internal_api/v1/projects/search_all_controller.rb
@@ -4,13 +4,22 @@ class InternalApi::V1::Projects::SearchAllController < InternalApi::V1::Applicat
   skip_after_action :verify_authorized, only: [:index]
 
   def index
-    projects = []
     search_term = search_params.downcase.strip
     search_term = search_term.gsub(/\s+/, "%")
-    search_term = "%#{search_term}%"
-    projects += current_company.projects.where("lower(projects.name) LIKE ?", search_term)
-    projects += current_company.clients.where("lower(clients.name) LIKE ?", search_term)
-    # projects += current_company.users.where("lower(concat(users.first_name, " ", users.last_name))  LIKE ?", search_term).projects
+    search_term = "#{search_term}%"
+    projects = User.joins(
+      "JOIN project_members ON users.id = project_members.user_id
+      JOIN projects ON projects.id = project_members.project_id
+      JOIN clients ON projects.client_id = clients.id"
+    ).where(
+      "projects.discarded_at IS NULL
+      AND clients.company_id = ?
+      AND (
+      lower(projects.name) LIKE ?
+      OR lower(clients.name) LIKE ?
+      OR lower(concat(users.first_name, ' ', users.last_name)) LIKE ?
+      )", current_company.id, search_term, search_term, search_term
+    ).select("DISTINCT projects.id, projects.name, clients.name AS client_name")
     render json: { projects: }
   end
 
