ab4ec984e59f37c9f215a14b4129fff512bc6ec5
Gowsik <gowsik@saeloun.com>
2022-07-11 18:07:25 +0530

    Fixed the robocop issues

--- app/services/create_invited_user_service.rb
+++ app/services/create_invited_user_service.rb
@@ -44,7 +44,7 @@ class CreateInvitedUserService
     end
 
     def find_or_create_user!
-      if (@user = User.find_by(email: invitation.recipient_email))
+      if (@user = User.find_by(email: invitation.recipient_email)).present?
         @user.update!(current_workspace_id: invitation.company.id)
       else
         create_invited_user!
