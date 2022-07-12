815eb6720d555d33d70a11c312a3f5da7d9aeac7
Gowsik <gowsik@saeloun.com>
2022-07-12 12:57:29 +0530

    Replaced table with grid in team member page

--- app/javascript/src/components/Reports/Container/index.tsx
+++ app/javascript/src/components/Reports/Container/index.tsx
@@ -3,40 +3,26 @@ import TableRow from "./TableRow";
 import { useEntry } from "../context/EntryContext";
 
 const TableHeader = () => (
-  <thead>
-    <tr className="flex flex-row items-center">
-      <th
-        scope="col"
-        className="w-2/5 pr-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest"
-      >
-        PROJECT/
-        <br />
-        CLIENT
-      </th>
-      <th
-        scope="col"
-        className="w-2/5 px-10 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest"
-      >
-        NOTE
-      </th>
-      <th
-        scope="col"
-        className="w-1/5 px-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest"
-      >
-        TEAM MEMBER/
-        <br />
-        DATE
-      </th>
-      <th
-        scope="col"
-        className="w-1/5 pl-6 py-5 text-right text-xs font-normal text-miru-dark-purple-600 tracking-widest"
-      >
-        HOURS
-        <br />
-        LOGGED
-      </th>
-    </tr>
-  </thead>
+  <div className="grid grid-cols-5 gap-2">
+    <div className="pr-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest">
+      PROJECT/
+      <br />
+      CLIENT
+    </div>
+    <div className="col-span-2 px-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest">
+      NOTE
+    </div>
+    <div className="px-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest">
+      TEAM MEMBER/
+      <br />
+      DATE
+    </div>
+    <div className="pl-6 py-5 text-right text-xs font-normal text-miru-dark-purple-600 tracking-widest">
+      HOURS
+      <br />
+      LOGGED
+    </div>
+  </div>
 );
 
 const Container = () => {
@@ -53,12 +39,12 @@ const Container = () => {
         timeEntryReport.reports.map((report, index) => (
           <Fragment key={index}>
             {report.label !== "" && <h1 className="text-miru-han-purple-1000 font-bold text-xl py-5 border-b border-miru-han-purple-1000">{report.label}</h1>}
-            <table className="min-w-full divide-y divide-gray-200 mt-4">
-              <TableHeader />
-              <tbody className="bg-white divide-y divide-gray-200">
-                {report.entries.length > 0 && getEntryList(report.entries)}
-              </tbody>
-            </table>
+            {/* <table className="min-w-full divide-y divide-gray-200 mt-4"> */}
+            <TableHeader />
+            {/* <tbody className="bg-white divide-y divide-gray-200"> */}
+            {report.entries.length > 0 && getEntryList(report.entries)}
+            {/* </tbody> */}
+            {/* </table> */}
           </Fragment>
         ))
       }
