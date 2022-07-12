30106a96396f284b000fcdc64d873ecfd8f5e01c
Mohini Dahiya <mohini@saeloun.com>
2022-07-12 20:02:12 +0530

    Create API to view & update user's personal details (#543)
    * add team_member details API
    
    * add double quotes to urls
    
    * add update action to details API
    
    * update detail_policy.rb for roles
    
    * add rspecs for details api
    
    * rspec fixes
    
    * refactor and fix rspecs
    
    * fix model/user_spec.rb
    
    * add first name to details
    
    * remove success: true
    
    * update route name in specs
    
    * add response validation to show spec for positive tests
    
    * add response validations to update and show specs for positive cases
    
    Co-authored-by: Akhil G Krishnan <akhilgkrishnan4u@gmail.com>

---
+++ app/controllers/internal_api/v1/team_members/details_controller.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class InternalApi::V1::TeamMembers::DetailsController < InternalApi::V1::ApplicationController
+  def show
+    authorize employment, policy_class: TeamMembers::DetailPolicy
+    render :show, locals: { user: employment.user }, status: :ok
+  end
+
+  def update
+    authorize employment, policy_class: TeamMembers::DetailPolicy
+    user = employment.user
+    user.update!(detail_params)
+    render json: {
+      user:,
+      notice: ("User updated successfully.")
+    }, status: :ok
+  end
+
+  private
+
+    def employment
+      @employment ||= Employment.find(params[:team_id])
+    end
+
+    def detail_params
+      params.require(:user).permit(
+        :first_name, :last_name, :date_of_birth, :phone, :personal_email_id,
+        social_accounts: {}
+      )
+    end
+end
