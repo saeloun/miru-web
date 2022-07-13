887b4abfaacdd524df2e6124888d0a47a220b9b4
Anas Ansari <anasansari157@gmail.com>
2022-07-13 13:26:03 +0530

    Fixes getReports.

--- app/javascript/src/components/Reports/Header/fetchReport.tsx
+++ app/javascript/src/components/Reports/Header/fetchReport.tsx
@@ -1,9 +1,11 @@
-export const getReports = ({ currentReport, timeEntryReport, revenueByClientReport }) => {
+export const getReports = ({ currentReport, timeEntryReport, revenueByClientReport, totalHoursLoggedReport }) => {
   switch (currentReport) {
     case "RevenueByClientReport":
       return revenueByClientReport;
     case "TimeEntryReport":
       return timeEntryReport;
+    case "TotalHoursLoggedReport":
+      return totalHoursLoggedReport;
     default:
       break;
   }
