9dfb38f470b9fd7982d922026259424705988ed6
onkar <onkar@saeloun.com>
2022-07-13 09:43:21 +0530

    added response to APIs

---
+++ app/javascript/src/components/Reports/api/outstandingOverdueInvoice.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import reports from "apis/reports/outstandingOverdueInvoice";
+
+const getReportData = async ({ selectedFilter, setClientList, setNavFilters, setFilterVisibilty, setSummary, setCurrency }) => {
+  const res = await reports.get();
+  setClientList(res.data.clients);
+  setCurrency(res.data.currency);
+  setSummary(res.data.summary);
+  setNavFilters(true);
+  setFilterVisibilty(false);
+};
+
+export default getReportData;
