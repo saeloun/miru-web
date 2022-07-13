0cc8f1d0857dcd2064739247e9b4c9633e9a7702
Abinash Panda <abinashp@protonmail.ch>
2022-07-13 12:09:56 +0530

    ran db:migrate on tailwind-width-patch

--- db/schema.rb
+++ db/schema.rb
@@ -74,6 +74,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_04_070130) do
     t.string "stripe_id"
     t.index ["company_id"], name: "index_clients_on_company_id"
     t.index ["discarded_at"], name: "index_clients_on_discarded_at"
+    t.index ["email", "company_id"], name: "index_clients_on_email_and_company_id", unique: true
   end
 
   create_table "companies", force: :cascade do |t|
