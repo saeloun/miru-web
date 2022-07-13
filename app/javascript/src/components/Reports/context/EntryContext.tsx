9850f01237f22526c8007e82fa1500faad5d9b9d
Anas Ansari <anasansari157@gmail.com>
2022-07-13 13:24:29 +0530

    Adds context for totalHoursReport.

--- app/javascript/src/components/Reports/context/EntryContext.tsx
+++ app/javascript/src/components/Reports/context/EntryContext.tsx
@@ -1,11 +1,13 @@
 import { createContext, useContext } from "react";
 import RevenueByClientReportContext from "./RevenueByClientContext";
 import TimeEntryReportContext from "./TimeEntryReportContext";
+import TotalHoursLoggedReportContext from "./TotalHoursLoggedReportContext";
 
 const EntryContext = createContext(({
   currentReport: "",
   revenueByClientReport: RevenueByClientReportContext,
-  timeEntryReport: TimeEntryReportContext
+  timeEntryReport: TimeEntryReportContext,
+  totalHoursLoggedReport: TotalHoursLoggedReportContext
 }));
 
 export const useEntry = () => useContext(EntryContext);
