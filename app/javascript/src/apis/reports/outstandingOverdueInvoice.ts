412fe21b96aed67ba989d5c0be8b63d909ed24c9
onkar <onkar@saeloun.com>
2022-07-14 11:04:15 +0530

    resolved path comment

--- app/javascript/src/apis/reports/outstandingOverdueInvoice.ts
+++ app/javascript/src/apis/reports/outstandingOverdueInvoice.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const path = "/reports/outstanding_overdue_invoices/";
 
-const get = () => axios.get(`${path}`);
+const get = () => axios.get(path);
 
 const reports = { get };
 
