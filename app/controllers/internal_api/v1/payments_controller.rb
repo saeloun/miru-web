37ee9897c86d2bd47a79139f5c55c6015dcb98ac
Shalaka Patil <shalu214patil@gmail.com>
2022-07-14 14:32:19 +0530

    Fix suggested changes

--- app/controllers/internal_api/v1/payments_controller.rb
+++ app/controllers/internal_api/v1/payments_controller.rb
@@ -4,8 +4,8 @@ class InternalApi::V1::PaymentsController < ApplicationController
   def create
     authorize :create, policy_class: PaymentPolicy
     invoice = Invoice.find(params[:payment][:invoice_id])
-    payment = Payment.create!(create_payment_params.merge(status: payment_status(invoice)))
-    update_invoice(invoice)
+    payment = Payment.create!(payment_params.merge(status: payment_status(invoice)))
+    InvoicePayment::UpdateInvoice.process(payment, invoice)
     render :create, locals: {
       payment:
     }
@@ -13,34 +13,17 @@ class InternalApi::V1::PaymentsController < ApplicationController
 
   private
 
-    def create_payment_params
+    def payment_params
       params.require(:payment).permit(
         :invoice_id, :transaction_date, :transaction_type, :amount, :note
       )
     end
 
     def payment_status(invoice)
-      if invoice.amount_due <= params[:payment][:amount].to_f
+      if invoice.amount_due <= payment_params[:amount].to_f
         "paid"
       else
         "partially_paid"
-      end
-    end
-
-    def update_invoice(invoice)
-      updates = {
-        amount_due: invoice.amount_due - params[:payment][:amount].to_f,
-        amount_paid: invoice.amount_paid + params[:payment][:amount].to_f
-      }
-        .merge(invoice_status(invoice))
-      invoice.update!(updates)
-    end
-
-    def invoice_status(invoice)
-      if payment_status(invoice) == "paid"
-        { status: "paid" }
-      else
-        {}
       end
     end
 end
---
+++ app/services/invoice_payment/update_invoice.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module InvoicePayment
+  class UpdateInvoice < ApplicationService
+    attr_reader :es_response, :group_by
+
+    def initialize(payment, invoice)
+      @payment = payment
+      @invoice = invoice
+    end
+
+    def process
+      invoice_updates = {
+        amount_due: @invoice.amount_due - @payment.amount,
+        amount_paid: @invoice.amount_paid + @payment.amount
+      }
+        .merge(invoice_status)
+      @invoice.update!(invoice_updates)
+    end
+
+    private
+
+      def invoice_status
+        if @invoice.amount_due <= @payment.amount
+          { status: "paid" }
+        else
+          {}
+        end
+      end
+  end
+end
