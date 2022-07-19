5a8b8a15df8855b0ab336d4629a277caeaa15f47
Shalaka Patil <shalu214patil@gmail.com>
2022-07-19 10:45:32 +0530

    Add manual payment entry (#554)
    * Add table for payments
    
    * Add MVC skeleton for invoices payment
    
    * Add InvoicePayemnt model details
    
    * Add API to create manual payment entry
    
    * Add spec for creating manual payment entry
    
    * Update invoice status on adding payment entry
    
    * Rename InvoicePayment to Payment
    
    * Fix policy class
    
    * Fix suggested changes
    
    * Remove not required code
    
    * Move payment creation to the service

---
+++ app/models/payment.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: payments
+#
+#  id               :bigint           not null, primary key
+#  amount           :decimal(20, 2)   default(0.0)
+#  note             :text
+#  status           :integer          not null
+#  transaction_date :date             not null
+#  transaction_type :integer          not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  invoice_id       :bigint           not null
+#
+# Indexes
+#
+#  index_payments_on_invoice_id  (invoice_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (invoice_id => invoices.id)
+#
+class Payment < ApplicationRecord
+  enum status: [
+    :paid,
+    :partially_paid,
+    :failed
+  ]
+
+  enum transaction_type: [
+    :visa,
+    :mastercard,
+    :bank_transfer,
+    :ach,
+    :amex,
+    :cash,
+    :cheque,
+    :credit_card,
+    :debit_card,
+    :paypal,
+    :stripe
+  ]
+
+  belongs_to :invoice
+
+  validates :invoice, :transaction_date, :transaction_type, :amount, :status, presence: true
+  validates :amount, numericality: { greater_than: 0 }
+end
