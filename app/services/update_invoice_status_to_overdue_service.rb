67df8d06e2fee01953d0695198d596f52c7709e7
Shalaka Patil <shalu214patil@gmail.com>
2022-07-25 11:02:43 +0530

    fix syggested changes

--- app/services/update_invoice_status_to_overdue_service.rb
+++ app/services/update_invoice_status_to_overdue_service.rb
@@ -1,19 +1,7 @@
 # frozen_string_literal: true
 
 class UpdateInvoiceStatusToOverdueService
-  TODAY = Date.current
-
   def process
-    Invoice.where({ status: "sent" }).find_each do |invoice|
-      if invoice.due_date < TODAY
-        update_invoice_status(invoice)
-      end
-    end
+    Invoice.sent.where("due_date < ?", Date.current).update_all(status: "overdue")
   end
-
-  private
-
-    def update_invoice_status(invoice)
-      invoice.update!({ status: :overdue })
-    end
 end
