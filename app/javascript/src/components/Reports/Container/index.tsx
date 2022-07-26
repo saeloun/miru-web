b8caa115d02927a2bd7b2e4b443869a9423ee420
Gowsik Vivekanandan <52308930+gowsik-ragunath@users.noreply.github.com>
2022-07-26 12:46:01 +0530

    Fixed time entry report UI bug (#569)
    * Added scollbat to filter and fixed the date in last week filter
    
    * Replaced table with grid in team member page
    
    * Renamed the report file names
    
    * Added border to report grid row
    
    * Fixed the issue in listing records with group by filter when record doesnt exist for specific group
    
    * Added custom date filter
    
    * Updated the backend logic to support custom date filter
    
    * Fixed the failed test cases
    
    * Fixed the issue in removing a date range filter and date issue
    
    Co-authored-by: Gowsik <gowsik@saeloun.com>

--- app/javascript/src/components/Reports/Container/index.tsx
+++ app/javascript/src/components/Reports/Container/index.tsx
@@ -1,42 +1,28 @@
 import React, { Fragment } from "react";
-import TableRow from "./TableRow";
+import ReportRow from "./ReportRow";
 import { useEntry } from "../context/EntryContext";
 
-const TableHeader = () => (
-  <thead>
-    <tr className="flex flex-row items-center">
-      <th
-        scope="col"
-        className="w-2/5 pr-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest"
-      >
-        PROJECT/
-        <br />
-        CLIENT
-      </th>
-      <th
-        scope="col"
-        className="w-2/5 px-10 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest"
-      >
-        NOTE
-      </th>
-      <th
-        scope="col"
-        className="w-1/5 px-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest"
-      >
-        TEAM MEMBER/
-        <br />
-        DATE
-      </th>
-      <th
-        scope="col"
-        className="w-1/5 pl-6 py-5 text-right text-xs font-normal text-miru-dark-purple-600 tracking-widest"
-      >
-        HOURS
-        <br />
-        LOGGED
-      </th>
-    </tr>
-  </thead>
+const ReportHeader = () => (
+  <div className="grid grid-cols-5 gap-2 border-b">
+    <div className="pr-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest">
+      PROJECT/
+      <br />
+      CLIENT
+    </div>
+    <div className="col-span-2 px-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest">
+      NOTE
+    </div>
+    <div className="px-6 py-5 text-left text-xs font-normal text-miru-dark-purple-600 tracking-widest">
+      TEAM MEMBER/
+      <br />
+      DATE
+    </div>
+    <div className="pl-6 py-5 text-right text-xs font-normal text-miru-dark-purple-600 tracking-widest">
+      HOURS
+      <br />
+      LOGGED
+    </div>
+  </div>
 );
 
 const Container = () => {
@@ -44,7 +30,7 @@ const Container = () => {
 
   const getEntryList = (entries) =>
     entries.map((timeEntry, index) => (
-      <TableRow key={`${timeEntry.client}-${index}`} {...timeEntry} />
+      <ReportRow key={`${timeEntry.client}-${index}`} {...timeEntry} />
     ));
 
   return (
@@ -53,12 +39,8 @@ const Container = () => {
         timeEntryReport.reports.map((report, index) => (
           <Fragment key={index}>
             {report.label !== "" && <h1 className="text-miru-han-purple-1000 font-bold text-xl py-5 border-b border-miru-han-purple-1000">{report.label}</h1>}
-            <table className="min-w-full divide-y divide-gray-200 mt-4">
-              <TableHeader />
-              <tbody className="bg-white divide-y divide-gray-200">
-                {report.entries.length > 0 && getEntryList(report.entries)}
-              </tbody>
-            </table>
+            <ReportHeader />
+            {report.entries.length > 0 && getEntryList(report.entries)}
           </Fragment>
         ))
       }
