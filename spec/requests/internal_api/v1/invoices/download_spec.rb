adc5777ce296f1beaf7e8542438c6482260e4c1c
Shalaka Patil <shalu214patil@gmail.com>
2022-07-27 10:30:27 +0530

    Add API for invoice payment download for admin

---
+++ spec/requests/internal_api/v1/invoices/download_spec.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "InternalApi::V1::Invoices#download", type: :request do
+  let(:company) { create(:company) }
+  let(:user) { create(:user, current_workspace_id: company.id) }
+  let!(:client) { create(:client, company:) }
+  let!(:invoice) { create(:invoice, client:, status: "sent") }
+
+  context "when user is an admin" do
+    before do
+      create(:employment, company:, user:)
+      user.add_role :admin, company
+      sign_in user
+    end
+
+    context "when request is made to download the Invoice" do
+      it "downloads the invoice in PDF format" do
+        send_request :get, download_internal_api_v1_invoice_path(id: invoice.id)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  context "when user is an employee" do
+    before do
+      create(:employment, company:, user:)
+      user.add_role :employee, company
+      sign_in user
+    end
+
+    context "when request is made to download the Invoice" do
+      it "returns 403 status" do
+        send_request :get, download_internal_api_v1_invoice_path(id: invoice.id)
+        expect(json_response["errors"]).to eq "You are not authorized to perform this action."
+      end
+    end
+  end
+
+  context "when user is a book keeper" do
+    before do
+      create(:employment, company:, user:)
+      user.add_role :book_keeper, company
+      sign_in user
+    end
+
+    context "when request is made to download the Invoice" do
+      it "returns 403 status" do
+        send_request :get, download_internal_api_v1_invoice_path(id: invoice.id)
+        expect(json_response["errors"]).to eq "You are not authorized to perform this action."
+      end
+    end
+  end
+
+  context "when unauthenticated" do
+    context "when request is made to download the Invoice" do
+      it "returns 403 status" do
+        send_request :get, download_internal_api_v1_invoice_path(id: invoice.id)
+        expect(response).to have_http_status(:unauthorized)
+        expect(json_response["error"]).to eq("You need to sign in or sign up before continuing.")
+      end
+    end
+  end
+end
