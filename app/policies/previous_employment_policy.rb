76ca44d96d05a4dd1927962c24ec4608c9c31b87
Keshav Biswa <keshavbiswa21@gmail.com>
2022-07-27 12:36:41 +0530

    fixed policy tests for previous_employments

--- app/policies/previous_employment_policy.rb
+++ app/policies/previous_employment_policy.rb
@@ -2,21 +2,21 @@
 
 class PreviousEmploymentPolicy < ApplicationPolicy
   def show?
-    (record_user_employee_of?(user.current_workspace.id) &&
+    (record_user_employee_of?(user.current_workspace_id) &&
     user.has_any_role?(
       { name: :admin, resource: user.current_workspace },
       { name: :owner, resource: user.current_workspace })) || record_belongs_to_user?
   end
 
   def update?
-    (record_user_employee_of?(user.current_workspace.id) &&
+    (record_user_employee_of?(user.current_workspace_id) &&
     user.has_any_role?(
       { name: :admin, resource: user.current_workspace },
       { name: :owner, resource: user.current_workspace })) || record_belongs_to_user?
   end
 
   def create?
-    (record_user_employee_of?(user.current_workspace.id) &&
+    (record_user_employee_of?(user.current_workspace_id) &&
     user.has_any_role?(
       { name: :admin, resource: user.current_workspace },
       { name: :owner, resource: user.current_workspace })) || record_belongs_to_user?
--- spec/policies/previous_employment_policy_spec.rb
+++ spec/policies/previous_employment_policy_spec.rb
@@ -3,50 +3,73 @@
 require "rails_helper"
 
 RSpec.describe PreviousEmploymentPolicy, type: :policy do
-  let!(:company) { create(:company) }
-  let(:user) { create(:user, current_workspace_id: company.id) }
-  let(:employee) { create(:user, current_workspace_id: company.id) }
-  # let!(:employment) { create(:employment, company:, user: employee) }
-  let(:user_employment) { create(:employment, company:, user:) }
+  let(:company) { create(:company) }
+  let(:user) { create(:user, current_workspace: company) }
+  let(:employee) { create(:user, current_workspace: company) }
   let(:previous_employment) { create(:previous_employment, user: employee) }
 
-  context "when user is an admin" do
+  context "when user is within the same workspace as the previous_employment user" do
     before do
-      user.add_role :admin, company
-      employee.add_role :employee, company
       create(:employment, company:, user: employee)
     end
 
-    permissions :update?, :show? do
-      it "grants permission to an admin to perform actions of same company and within same company workspace" do
-        expect(described_class).to permit(user, previous_employment)
+    context "when user is an admin" do
+      before do
+        user.add_role(:admin, company)
+      end
+
+      permissions :show?, :update?, :create? do
+        it "grants permission" do
+          expect(described_class).to permit(user, previous_employment)
+        end
+      end
+    end
+
+    context "when user is an owner" do
+      before do
+        user.add_role :owner, company
+      end
+
+      permissions :update?, :show?, :create? do
+        it "grants permission" do
+          expect(described_class).to permit(user, previous_employment)
+        end
+      end
+    end
+
+    context "when user is an employee" do
+      before do
+        user.add_role :employee, company
+      end
+
+      permissions :update?, :show?, :create? do
+        it "does not grant permission" do
+          expect(described_class).not_to permit(user, previous_employment)
+        end
       end
     end
   end
 
-  context "when user is an owner" do
-    before do
-      user.add_role :owner, company
-      employee.add_role :employee, company
-      create(:employment, company:, user: employee)
-    end
+  context "when user is not within the same workspace as the previous_employment user" do
+    context "when user is an admin, owner and employee" do
+      before do
+        user.add_role(:admin, company)
+        user.add_role(:owner, company)
+        user.add_role(:employee, company)
+      end
 
-    permissions :update?, :show? do
-      it "grants permission to an admin to perform actions of same company and within same company workspace" do
-        expect(described_class).to permit(user, previous_employment)
+      permissions :show?, :update?, :create? do
+        it "does not grant permission" do
+          expect(described_class).not_to permit(user, previous_employment)
+        end
       end
     end
   end
 
-  context "when user is an employee" do
-    before do
-      user.add_role :employee, company
-      create(:employment, company:, user: employee)
-    end
-
-    permissions :update?, :show? do
-      it "grants permission to an admin to perform actions of same company and within same company workspace" do
-        expect(described_class).to permit(user, previous_employment)
+  context "when previous_employment belongs to the user" do
+    permissions :update?, :show?, :create? do
+      it "grants permission to show and update the previous_employment" do
+        expect(described_class).to permit(employee, previous_employment)
       end
     end
   end
