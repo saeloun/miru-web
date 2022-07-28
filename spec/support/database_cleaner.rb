6be5061562588b757287b429cec74cdfb90379b0
Keshav Biswa <keshavbiswa21@gmail.com>
2022-07-28 12:04:52 +0530

    database_cleaner change

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -2,18 +2,13 @@
 
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner.clean_with :truncation, except: %w(ar_internal_metadata)
-  end
-
-  config.before do
     DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation, except: %w(ar_internal_metadata))
   end
 
-  config.before do
-    DatabaseCleaner.start
-  end
-
-  config.after do
-    DatabaseCleaner.clean
+  config.around do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
   end
 end
