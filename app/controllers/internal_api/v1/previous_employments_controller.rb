8a6ecf4032b4160b14a0da46ccd4c85fa4075373
Mohini Dahiya <mohini@saeloun.com>
2022-07-28 14:02:09 +0530

    Create API for Previous Employments with Create, View and Update methods (#564)
    * API for Previous employments with 'update' and 'show' methods
    
    * fix rubocop error
    
    * Add create action
    
    * reorganise PreviousEmployments Api
    
    * reorganise PreviousEmployments Api
    
    * reorganise model tests for previous employment
    
    * Update app/controllers/internal_api/v1/previous_employments_controller.rb
    
    Co-authored-by: Keshav Biswa <keshavbiswa21@gmail.com>
    
    * add policy tests
    
    * fixed policy tests for previous_employments
    
    * removed old previous employment policy spec
    
    * removed the before block
    
    * database_cleaner change
    
    * replaced employment_ids with company_ids
    
    Co-authored-by: Keshav Biswa <keshavbiswa21@gmail.com>

---
+++ app/controllers/internal_api/v1/previous_employments_controller.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class InternalApi::V1::PreviousEmploymentsController < InternalApi::V1::ApplicationController
+  def show
+    authorize previous_employment
+    render :show, locals: { previous_employment: }, status: :ok
+  end
+
+  def update
+    authorize previous_employment
+    previous_employment.update!(previous_employment_params)
+    render :update, locals: { previous_employment: }, status: :ok
+  end
+
+  def create
+    previous_employment = current_user.previous_employments.new(previous_employment_params)
+    authorize previous_employment
+    previous_employment.save!
+    render :create, locals: { previous_employment: }, status: :ok
+ end
+
+  private
+
+    def previous_employment
+      @previous_employment ||= PreviousEmployment.find(params[:id])
+    end
+
+    def previous_employment_params
+      params.require(:previous_employment).permit(
+        :company_name, :role
+      )
+    end
+end
