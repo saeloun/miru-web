508ee31adb55bc43acac1e5cd056973330daedc5
Abinash Panda <abinashp@protonmail.ch>
2022-08-01 07:10:59 +0530

    fix for duration on getReportData

--- app/javascript/src/components/Reports/api/revenueByClient.ts
+++ app/javascript/src/components/Reports/api/revenueByClient.ts
@@ -1,7 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import clients from "apis/clients";
 import clientRevenueApi from "apis/reports/clientRevenue";
 import dayjs from "dayjs";
+import Logger from "js-logger";
 
 dayjs.Ls.en.weekStart = 1;
 
@@ -14,54 +13,54 @@ const getReportData = async ({
   setCurrency
 }
 ) => {
-  let fromDate, toDate;
-  let clientIds = [];
+  try {
+    let fromDate, toDate;
+    let clientIds = [];
 
-  const currentYear = dayjs().year();
-  const currentMonth = dayjs().month();
-  const currentQuarter = dayjs().month();
-  const lastMonthOfQuarter = dayjs()["quarter"]() * 3;
-  const firstMonthOfQuarter = lastMonthOfQuarter -3;
+    const currentYear = dayjs().year();
+    const lastMonthOfQuarter = dayjs()["quarter"]() * 3 -1;
+    const firstMonthOfQuarter = dayjs()["quarter"]() * 3 -3;
+    const thisQuarterFirstDate = dayjs().month(firstMonthOfQuarter).format("DD-MM-YYYY");
+    const thisQuarterLastDate = dayjs(dayjs().month(lastMonthOfQuarter)).endOf("month").format("DD-MM-YYYY");
 
-  switch (selectedFilter.dateRange.label) {
-    case "this_quarter":
-      fromDate = dayjs(dayjs().month(firstMonthOfQuarter)).endOf("month").format("DD-MM-YYYY");
-      toDate = dayjs(dayjs().month(lastMonthOfQuarter)).endOf("month").format("DD-MM-YYYY");
-      break;
+    switch (selectedFilter.dateRange.value) {
+      case "this_quarter":
+        fromDate = thisQuarterFirstDate;
+        toDate = thisQuarterLastDate;
+        break;
 
-    case "last_quarter":
-      fromDate = dayjs(dayjs().month(firstMonthOfQuarter)).endOf("month").subtract(3, "month").format("DD-MM-YYYY");
-      toDate = dayjs(dayjs().month(lastMonthOfQuarter)).endOf("month").subtract(3, "month").format("DD-MM-YYYY");
-      break;
+      case "last_quarter":
+        fromDate = dayjs(dayjs().month(firstMonthOfQuarter)).subtract(3, "month").format("DD-MM-YYYY");
+        toDate = dayjs(dayjs().month(lastMonthOfQuarter)).subtract(3, "month").format("DD-MM-YYYY");
+        break;
 
-    case "this_year":
-      fromDate = `01-01-${dayjs().year()}`;
-      toDate = `01-01-${dayjs().year()}`;
-      break;
+      case "this_year":
+        fromDate = `01-01-${currentYear}`;
+        toDate = `31-12-${currentYear}`;
+        break;
 
-    case "last_year":
-      fromDate = `01-01-${dayjs().year() -1}`;
-      toDate = `01-01-${dayjs().year() -1}`;
-      break;
+      case "last_year":
+        fromDate = `01-01-${currentYear -1}`;
+        toDate = `31-12-${currentYear -1}`;
+        break;
 
-    case "custom":
-
-      break;
-
-    default:
-      fromDate = "01-01-2022";
-      toDate = dayjs().format("DD-MM-YYYY");
-      break;
+      default:
+        fromDate = "01-01-2022";
+        toDate = dayjs().format("DD-MM-YYYY");
+        break;
+    }
+    if (! (selectedFilter.clients[0]["label"] === "All Clients")) {
+      clientIds = selectedFilter.clients.map(client => client.value);
+    }
+    const res = await clientRevenueApi.get(fromDate, toDate, clientIds);
+    setClientList(res.data.clients);
+    setCurrency(res.data.currency);
+    setSummary(res.data.summary);
+    setNavFilters(true);
+    setFilterVisibilty(false);
+  } catch (e) {
+    Logger.error(e.message);
   }
-  if (! (selectedFilter.clients[0]["label"] === "All Clients")) {
-    clientIds = selectedFilter.clients.map(client => client.value);
-  }
-  const res = await clientRevenueApi.get(fromDate, toDate, clientIds);
-  setClientList(res.data.clients);
-  setCurrency(res.data.currency);
-  setSummary(res.data.summary);
-  setNavFilters(true);
-  setFilterVisibilty(false);
 };
 
 export default getReportData;
