4c8e01af628073251b8fb56406b1cabac8c33d6d
Vipul Amler <vipul@saeloun.com>
2022-08-02 11:42:44 -0400

    x

--- app/controllers/internal_api/v1/project_members_controller.rb
+++ app/controllers/internal_api/v1/project_members_controller.rb
@@ -44,7 +44,7 @@ class InternalApi::V1::ProjectMembersController < InternalApi::V1::ApplicationCo
 
       member_ids.each do |member_id|
           ProjectMember.where(user_id: member_id, project_id: params[:id])
-            .delete_all
+            .delete_all #FIXME: Use Soft delete
         end
     end
 
