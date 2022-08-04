aebdc48c0ff76445b99c6472c3a1f43009c0fa50
Niraj <niraj.virani@atharvasystem.com>
2022-08-04 12:19:23 +0530

    Async slack notification

--- app/controllers/internal_api/v1/space_usages_controller.rb
+++ app/controllers/internal_api/v1/space_usages_controller.rb
@@ -21,59 +21,10 @@ class InternalApi::V1::SpaceUsagesController < InternalApi::V1::ApplicationContr
     space_usage = current_company.space_usages.new(space_usage_params)
     space_usage.user = current_company.users.find(params[:user_id])
     if space_usage.save
-      slack_webhook_url = ENV.fetch("SLACK_WEBHOOK_URL")
-      entry = space_usage.formatted_entry
-      user_name = entry[:user_name]
-      space_name = entry[:space_name]
-      purpose_name = entry[:purpose_name]
-      note = entry[:note]
-      start_duration = space_usage.formatted_duration_12hr(:start)
-      end_duration = space_usage.formatted_duration_12hr(:end)
-
-      payload_msg = {
-        "blocks": [
-          {
-            "type": "header",
-            "text": {
-              "type": "plain_text",
-              "text": "Space Occupied :white_check_mark:",
-              "emoji": true
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "Please take a note the *#{space_name}* will be occupied for *#{purpose_name}* meeting."
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "By: *#{user_name}*\nFrom: *#{start_duration} - #{end_duration}*\nNote: #{note}"
-            },
-            "accessory": {
-              "type": "image",
-              "image_url": "https://api.slack.com/img/blocks/bkb_template_images/notifications.png",
-              "alt_text": "calendar thumbnail"
-            }
-          }
-        ]
-      }
-      if slack_webhook_url
-        response_data = RestClient.post slack_webhook_url, payload_msg.to_json, { content_type: :json, accept: :json }
-        slack_info = response_data.body
-      end
+      SpaceUsageSlackNotifyJob.perform_later("create", space_usage.attributes.as_json)
       render json: {
         notice: I18n.t("space_usage.create.message"),
-        entry:
+        entry: space_usage.formatted_entry
       }
     else
       render json: { error: space_usage.errors.full_messages.to_sentence }, status: :unprocessable_entity
@@ -83,56 +34,7 @@ class InternalApi::V1::SpaceUsagesController < InternalApi::V1::ApplicationContr
   def update
     authorize current_space_usage
     if current_space_usage.update(space_usage_params)
-      slack_webhook_url = ENV.fetch("SLACK_WEBHOOK_URL")
-      entry = current_space_usage.formatted_entry
-      user_name = entry[:user_name]
-      space_name = entry[:space_name]
-      purpose_name = entry[:purpose_name]
-      note = entry[:note]
-      start_duration = current_space_usage.formatted_duration_12hr(:start)
-      end_duration = current_space_usage.formatted_duration_12hr(:end)
-
-      payload_msg = {
-        "blocks": [
-          {
-            "type": "header",
-            "text": {
-              "type": "plain_text",
-              "text": "Space Slot Updated :twisted_rightwards_arrows:",
-              "emoji": true
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "Please take a note, the *#{space_name}* will be occupied for *#{purpose_name}* meeting."
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "By: *#{user_name}*\nFrom: *#{start_duration} - #{end_duration}*\nNote: #{note}"
-            },
-            "accessory": {
-              "type": "image",
-              "image_url": "https://api.slack.com/img/blocks/bkb_template_images/notifications.png",
-              "alt_text": "calendar thumbnail"
-            }
-          }
-        ]
-      }
-      if slack_webhook_url
-        response_data = RestClient.post slack_webhook_url, payload_msg.to_json, { content_type: :json, accept: :json }
-        slack_info = response_data.body
-      end
+      SpaceUsageSlackNotifyJob.perform_later("update", current_space_usage.attributes.as_json)
       render json: {
         notice: I18n.t("space_usage.update.message"),
         entry: current_space_usage.formatted_entry
@@ -145,54 +47,7 @@ class InternalApi::V1::SpaceUsagesController < InternalApi::V1::ApplicationContr
   def destroy
     authorize current_space_usage
     if current_space_usage.destroy
-      slack_webhook_url = ENV.fetch("SLACK_WEBHOOK_URL")
-      entry = current_space_usage.formatted_entry
-      user_name = entry[:user_name]
-      space_name = entry[:space_name]
-      start_duration = current_space_usage.formatted_duration_12hr(:start)
-      end_duration = current_space_usage.formatted_duration_12hr(:end)
-
-      payload_msg = {
-        "blocks": [
-          {
-            "type": "header",
-            "text": {
-              "type": "plain_text",
-              "text": "Space Slot Unoccupied :no_entry:",
-              "emoji": true
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "Please take a note, the *#{space_name}* will be unoccupied."
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "By: *#{user_name}*\nFrom: *#{start_duration} - #{end_duration}*"
-            },
-            "accessory": {
-              "type": "image",
-              "image_url": "https://api.slack.com/img/blocks/bkb_template_images/notifications.png",
-              "alt_text": "calendar thumbnail"
-            }
-          }
-        ]
-      }
-      if slack_webhook_url
-        response_data = RestClient.post slack_webhook_url, payload_msg.to_json, { content_type: :json, accept: :json }
-        slack_info = response_data.body
-      end
+      SpaceUsageSlackNotifyJob.perform_later("delete", current_space_usage.attributes.as_json)
       render json: { notice: I18n.t("space_usage.destroy.message") }
     else
       # TBD
---
+++ app/jobs/space_usage_slack_notify_job.rb
@@ -0,0 +1,158 @@
+# frozen_string_literal: true
+
+class SpaceUsageSlackNotifyJob < ApplicationJob
+  SLACK_WEBHOOK_URL = ENV.fetch("SLACK_WEBHOOK_URL")
+
+  queue_as :default
+
+  def perform(action_name, space_usage_attributes)
+    action_name = action_name.to_sym
+    return unless SLACK_WEBHOOK_URL
+
+    @space_usage = if [:create, :update].include?(action_name)
+      SpaceUsage.find(space_usage_attributes["id"])
+    else
+      SpaceUsage.new(space_usage_attributes)
+    end
+
+    payload_msg = case action_name
+                  when :create
+                    create_space_payload_msg
+                  when :update
+                    update_space_payload_msg
+                  when :delete
+                    delete_space_payload_msg
+                  else
+                    nil
+    end
+    if payload_msg
+      RestClient.post SLACK_WEBHOOK_URL, payload_msg.to_json, { content_type: :json, accept: :json }
+    end
+  end
+
+  def create_space_payload_msg
+    entry = @space_usage.formatted_entry
+
+    {
+      blocks: [
+        {
+          type: "header",
+          text: {
+            type: "plain_text",
+            text: "Space Occupied :white_check_mark:",
+            emoji: true
+          }
+        },
+        {
+          type: "divider"
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Please take a note the *#{entry[:space_name]}* will be occupied for *#{entry[:purpose_name]}* meeting."
+          }
+        },
+        {
+          type: "divider"
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "By: *#{entry[:user_name]}*\nFrom: *#{@space_usage.formatted_duration_12hr(:start)} - #{@space_usage.formatted_duration_12hr(:end)}*\nNote: #{entry[:note]}"
+          },
+          accessory: {
+            type: "image",
+            image_url: "https://api.slack.com/img/blocks/bkb_template_images/notifications.png",
+            alt_text: "calendar thumbnail"
+          }
+        }
+      ]
+    }
+  end
+
+  def update_space_payload_msg
+    entry = @space_usage.formatted_entry
+
+    {
+      blocks: [
+        {
+          type: "header",
+          text: {
+            type: "plain_text",
+            text: "Space Slot Updated :twisted_rightwards_arrows:",
+            emoji: true
+          }
+        },
+        {
+          type: "divider"
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Please take a note, the *#{entry[:space_name]}* will be occupied for *#{entry[:purpose_name]}* meeting."
+          }
+        },
+        {
+          type: "divider"
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "By: *#{entry[:user_name]}*\nFrom: *#{@space_usage.formatted_duration_12hr(:start)} - #{@space_usage.formatted_duration_12hr(:end)}*\nNote: #{entry[:note]}"
+          },
+          accessory: {
+            type: "image",
+            image_url: "https://api.slack.com/img/blocks/bkb_template_images/notifications.png",
+            alt_text: "calendar thumbnail"
+          }
+        }
+      ]
+    }
+  end
+
+  def delete_space_payload_msg
+    entry = @space_usage.formatted_entry
+
+    {
+      blocks: [
+        {
+          type: "header",
+          text: {
+            type: "plain_text",
+            text: "Space Slot Unoccupied :no_entry:",
+            emoji: true
+          }
+        },
+        {
+          type: "divider"
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Please take a note, the *#{entry[:space_name]}* is unoccupied."
+          }
+        },
+        {
+          type: "divider"
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "By: *#{entry[:user_name]}*\nFrom: *#{@space_usage.formatted_duration_12hr(:start)} - #{@space_usage.formatted_duration_12hr(:end)}*"
+          },
+          accessory: {
+            type: "image",
+            image_url: "https://api.slack.com/img/blocks/bkb_template_images/notifications.png",
+            alt_text: "calendar thumbnail"
+          }
+        }
+      ]
+    }
+  end
+end
