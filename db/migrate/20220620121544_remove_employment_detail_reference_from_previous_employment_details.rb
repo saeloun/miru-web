0fbebde4cc2f150a079b8a90b98bb28b675498c9
Niraj <niraj.virani@atharvasystem.com>
2022-08-23 20:59:09 +0530

    fix migration

--- db/migrate/20220620121544_remove_employment_detail_reference_from_previous_employment_details.rb
+++ db/migrate/20220620121544_remove_employment_detail_reference_from_previous_employment_details.rb
@@ -2,6 +2,8 @@
 
 class RemoveEmploymentDetailReferenceFromPreviousEmploymentDetails < ActiveRecord::Migration[7.0]
   def change
-    remove_reference :previous_employment_details, :employment_detail, null: false, foreign_key: true
+    if column_exists?(:previous_employment_details, :employment_detail_id)
+      remove_reference :previous_employment_details, :employment_detail, null: false, foreign_key: true
+    end
   end
 end
