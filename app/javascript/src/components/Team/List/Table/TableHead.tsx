a35c9a6e7264bebc82d7b4b93a28ccb0d768537d
“Apoorv <tiwari.apoorv1316@gmail.com>
2022-08-23 14:16:00 +0530

    remove profile picture from team page

--- app/javascript/src/components/Team/List/Table/TableHead.tsx
+++ app/javascript/src/components/Team/List/Table/TableHead.tsx
@@ -7,9 +7,6 @@ const TableHead = () => {
     <thead  className=" border-miru-gray-200" data-cy="team-table-header">
       <tr>
         <th scope="col" className="table__header p-6">
-          PHOTO
-        </th>
-        <th scope="col" className="table__header p-6">
           NAME
         </th>
         <th scope="col" className="table__header p-6">
