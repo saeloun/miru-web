576ca388ebca9b8e809f8e0330345064ec70998f
Niraj <niraj.virani@atharvasystem.com>
2022-08-30 20:53:57 +0530

    compact slack notification

--- app/jobs/space_usage_slack_notify_job.rb
+++ app/jobs/space_usage_slack_notify_job.rb
@@ -37,42 +37,30 @@ class SpaceUsageSlackNotifyJob < ApplicationJob
       .map(&:full_name).map { |i| "*#{i}*" }.to_sentence
 
     {
-      text: "Space Occupied :white_check_mark:",
+      text: "Space occupied :white_check_mark:",
       blocks: [
         {
           type: "header",
           text: {
             type: "plain_text",
-            text: "Space Occupied :white_check_mark:",
+            text: "Space occupied :white_check_mark: by #{entry[:user_name]}",
             emoji: true
           }
         },
         {
-          type: "divider"
-        },
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: "Please take a note the *#{entry[:space_name]}* will be occupied for *#{entry[:purpose_name]}* meeting."
-          }
-        },
-        {
-          type: "divider"
-        },
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: "By: *#{entry[:user_name]}*\nOn: *#{bookingDate}* from *#{@space_usage.formatted_duration_12hr(:start)}* to *#{@space_usage.formatted_duration_12hr(:end)}*\n#{member_names.present? ? "Team Members: #{member_names}\n" : ""}Note: #{entry[:note]}"
-          },
-          accessory: {
-            type: "image",
-            image_url: "https://api.slack.com/img/blocks/bkb_template_images/notifications.png",
-            alt_text: "calendar thumbnail"
-          }
+          "type": "context",
+          "elements": [
+            {
+              "type": "mrkdwn",
+              "text": [
+                "*#{entry[:space_name]}* will be occupied for *#{entry[:purpose_name]}* meeting on *#{bookingDate}* from *#{@space_usage.formatted_duration_12hr(:start)}* to *#{@space_usage.formatted_duration_12hr(:end)}*",
+                member_names.present? ? "Team Members: #{member_names}" : nil,
+                entry[:note].present? ? "Note: #{entry[:note]}" : nil,
+              ].compact.join(" \n")
+            }
+          ]
         }
-      ]
+      ].compact
     }
   end
 
@@ -83,42 +71,30 @@ class SpaceUsageSlackNotifyJob < ApplicationJob
       .map(&:full_name).map { |i| "*#{i}*" }.to_sentence
 
     {
-      text: "Space Occupation Changed :twisted_rightwards_arrows:",
+      text: "Space occupation changed :twisted_rightwards_arrows:",
       blocks: [
         {
           type: "header",
           text: {
             type: "plain_text",
-            text: "Space Occupation Changed :twisted_rightwards_arrows:",
+            text: "Space occupation changed :twisted_rightwards_arrows: by #{entry[:user_name]}",
             emoji: true
           }
         },
         {
-          type: "divider"
-        },
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: "Please take a note, the *#{entry[:space_name]}* will be occupied for *#{entry[:purpose_name]}* meeting."
-          }
-        },
-        {
-          type: "divider"
-        },
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: "By: *#{entry[:user_name]}*\nOn: *#{bookingDate}* from *#{@space_usage.formatted_duration_12hr(:start)}* to *#{@space_usage.formatted_duration_12hr(:end)}*\n#{member_names.present? ? "Team Members: #{member_names}\n" : ""}Note: #{entry[:note]}"
-          },
-          accessory: {
-            type: "image",
-            image_url: "https://api.slack.com/img/blocks/bkb_template_images/notifications.png",
-            alt_text: "calendar thumbnail"
-          }
+          "type": "context",
+          "elements": [
+            {
+              "type": "mrkdwn",
+              "text": [
+                "*#{entry[:space_name]}* will be occupied for *#{entry[:purpose_name]}* meeting on *#{bookingDate}* from *#{@space_usage.formatted_duration_12hr(:start)}* to *#{@space_usage.formatted_duration_12hr(:end)}*",
+                member_names.present? ? "Team Members: #{member_names}" : nil,
+                entry[:note].present? ? "Note: #{entry[:note]}" : nil,
+              ].compact.join(" \n")
+            }
+          ]
         }
-      ]
+      ].compact
     }
   end
 
@@ -127,40 +103,24 @@ class SpaceUsageSlackNotifyJob < ApplicationJob
     bookingDate = DateTime.parse("#{entry[:work_date]}").strftime("%d/%m/%Y")
 
     {
-      text: "Space Unoccupied :no_entry:",
+      text: "Space unoccupied :no_entry:",
       blocks: [
         {
           type: "header",
           text: {
             type: "plain_text",
-            text: "Space Unoccupied :no_entry:",
+            text: "Space unoccupied :no_entry: by #{entry[:user_name]}",
             emoji: true
           }
         },
         {
-          type: "divider"
-        },
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: "Please take a note, the *#{entry[:space_name]}* is unoccupied."
-          }
-        },
-        {
-          type: "divider"
-        },
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: "By: *#{entry[:user_name]}*\nOn: *#{bookingDate}* from *#{@space_usage.formatted_duration_12hr(:start)}* to *#{@space_usage.formatted_duration_12hr(:end)}*"
-          },
-          accessory: {
-            type: "image",
-            image_url: "https://api.slack.com/img/blocks/bkb_template_images/notifications.png",
-            alt_text: "calendar thumbnail"
-          }
+          "type": "context",
+          "elements": [
+            {
+              "type": "mrkdwn",
+              "text": "*#{entry[:space_name]}* is unoccupied for *#{entry[:purpose_name]}* meeting on *#{bookingDate}* from *#{@space_usage.formatted_duration_12hr(:start)}* to *#{@space_usage.formatted_duration_12hr(:end)}*."
+            }
+          ]
         }
       ]
     }
