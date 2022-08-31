a9d80d4008263f1e6bd2574884fabc25d6fa6d86
Deepak Upadhyay <deepaku@atharvasystem.com>
2022-08-31 18:20:45 +0530

    added device api policy and updated params in DEVICE_AP.md file

---
+++ app/policies/device_api/device_usage_api_policy.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class DeviceApi::DeviceUsageApiPolicy < ApplicationPolicy
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
+
+  def approve?
+    true
+  end
+
+  def permitted_attributes
+    [:device_id, :assignee_id, :created_by_id, :approve]
+  end
+end
