9c38a59f89ff36c23451c6c362ae13bdf6d1e89e
Swaathi Kakarla <swaathi16@gmail.com>
2022-09-06 16:04:11 +0530

    Update weekly mailer content (#618)
    * Update send_weekly_reminder_to_user_mailer.rb
    
    * Update notify_user_about_missed_entries.html.erb
    
    * Update notify_user_about_missed_entries.text.erb
    
    * Update invoice.text.erb
    
    * Update send_user_invitation.html.erb
    
    * Update invoice.text.erb
    
    * Update invoice.html.erb
    
    * Update invoice.html.erb
    
    * Update invoice.rb
    
    * Updated content for all emails
    
    * Fixing ActiveSupport::TimeWithZone spec in CI
    
    * Removing unwanted text files in mailers
    
    Co-authored-by: Akhil G Krishnan <akhilgkrishnan4u@gmail.com>

---
+++ spec/monkey_patches/active_support/time_with_zone_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ActiveSupport::TimeWithZone, type: :class do
+  describe "#ordinalize" do
+    let(:date) { Time.zone.now }
+
+    context "when year is true" do
+      it "returns ordinalized date with year" do
+        expect(date.ordinalize).to eq(date.strftime("%B #{date.day.ordinalize}, %Y"))
+      end
+    end
+
+    context "when year is false" do
+      it "returns ordinalized date without year" do
+        expect(date.ordinalize(year: false)).to eq(date.strftime("%B #{date.day.ordinalize}"))
+      end
+    end
+  end
+end
