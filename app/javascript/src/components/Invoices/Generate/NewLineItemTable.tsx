7fb3dffe76b27102649e09d729a9b2af40deb7db
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2022-09-12 09:25:28 +0530

    Fix: Inaccurate quantity while generating a invoice (#641)

--- app/javascript/src/components/Invoices/Generate/NewLineItemTable.tsx
+++ app/javascript/src/components/Invoices/Generate/NewLineItemTable.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import dayjs from "dayjs";
 import InfiniteScroll from "react-infinite-scroll-component";
 
+import { minutesToHHMM } from "helpers/hhmm-parser";
+
 import { DropdownHeader } from "./CustomComponents";
 
 import { getMaxIdx } from "../common/utils";
@@ -60,14 +62,14 @@ const NewLineItemTable = ({
           }
         >
           {lineItems.map((item, index) => {
-            const hoursLogged = (item.qty / 60).toFixed(2);
+            const hoursLogged = minutesToHHMM(item.qty);
             const date = dayjs(item.date).format("DD.MM.YYYY");
             return (
               <div key={index} onClick={() => { selectRowId(item); }} className="py-2 px-3 flex justify-between cursor-pointer hover:bg-miru-gray-100" data-cy="entries-list">
                 <span className="font-medium w-1/4 text-base text-miru-dark-purple-1000 text-left">
                   {item.first_name} {item.last_name}
                 </span>
-                <span className="font-medium w-2/4 text-xs text-miru-dark-purple-600 text-left w-1/2">
+                <span className="font-medium text-xs text-miru-dark-purple-600 text-left w-1/2">
                   {item.description}
                 </span>
                 <span className="font-medium text-xs text-miru-dark-purple-1000 text-center">
