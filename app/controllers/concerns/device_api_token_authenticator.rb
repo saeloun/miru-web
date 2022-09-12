90012a32a4e9ce46d69fe60cda6e87f1b9f15c59
Niraj <niraj.virani@atharvasystem.com>
2022-09-12 16:43:25 +0530

    secret_key change

--- app/controllers/concerns/device_api_token_authenticator.rb
+++ app/controllers/concerns/device_api_token_authenticator.rb
@@ -14,7 +14,13 @@ module DeviceApiTokenAuthenticator
       authenticate(self) do |token, _options|
         # Compare the tokens in a time-constant manner, to mitigate
         # timing attacks.
-        ActiveSupport::SecurityUtils.secure_compare(token, Rails.application.credentials.device_api_secret_key)
+
+        secret_key = Rails.application.credentials.device_api_secret_key
+        if Rails.env.production?
+          secret_key = Rails.application.credentials.production.device_api_secret_key
+        end
+
+        ActiveSupport::SecurityUtils.secure_compare(token, secret_key)
       end || authentication_request(self, "Application", nil)
     end
 end
