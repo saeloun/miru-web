d0a12e523ea238b24b5a28d7afc3fa7f087223a9
Niraj <niraj.virani@atharvasystem.com>
2022-09-13 19:00:06 +0530

    Tackle app

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,9 +7,7 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.paths << Rails.root.join("app/assets/apps")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( Tackle.apk )
