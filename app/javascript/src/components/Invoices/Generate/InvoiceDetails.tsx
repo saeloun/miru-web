563f8eb30f948d1235e523ec32b030db71ca05c4
“Apoorv <tiwari.apoorv1316@gmail.com>
2022-09-19 11:43:00 +0530

    fix the date of issue props

--- app/javascript/src/components/Invoices/Generate/InvoiceDetails.tsx
+++ app/javascript/src/components/Invoices/Generate/InvoiceDetails.tsx
@@ -57,7 +57,7 @@ const InvoiceDetails = ({
               <PencilSimple size={13} color="#1D1A31" />
             </button>
           </p>
-          {showDateOfIssuePicker && <CustomDatePicker handleChange={handleDatePickerChange} dueDate={dueDate} />}
+          {showDateOfIssuePicker && <CustomDatePicker handleChange={handleDatePickerChange} date={issueDate} />}
           <p className="font-normal text-base text-miru-dark-purple-1000">
             {getIssuedDate}
           </p>
@@ -69,7 +69,7 @@ const InvoiceDetails = ({
               <PencilSimple size={13} color="#1D1A31" />
             </button>
           </p>
-          {showDueDatePicker && <CustomDatePicker handleChange={handleDueDatePicker} dueDate={dueDate} />}
+          {showDueDatePicker && <CustomDatePicker handleChange={handleDueDatePicker} date={dueDate} />}
           <p className="font-normal text-base text-miru-dark-purple-1000">
             {getDueDate}
           </p>
