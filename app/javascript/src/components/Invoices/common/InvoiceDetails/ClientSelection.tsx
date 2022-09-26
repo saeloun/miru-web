02d50d526cfffdcb13ee5481cd47265bf5b588e3
Prasanth Chaduvula <prasanthchaduvula@Prasanths-MacBook-Air.local>
2022-09-26 23:05:18 +0530

    Created InvoiceDetails & ClientSelection common components

---
+++ app/javascript/src/components/Invoices/common/InvoiceDetails/ClientSelection.tsx
@@ -0,0 +1,108 @@
+import React, { useEffect, useState, useRef } from "react";
+
+import { MagnifyingGlass, PencilSimple } from "phosphor-react";
+import Select, { components, DropdownIndicatorProps } from "react-select";
+
+import useOutsideClick from "helpers/outsideClick";
+
+import { reactSelectStyles } from "./Styles";
+
+const ClientSelection = ({
+  clientList,
+  selectedClient,
+  setSelectedClient,
+  optionSelected,
+  clientVisible
+}) => {
+  const [isOptionSelected, setOptionSelection] = useState<boolean>(optionSelected);
+  const [isClientVisible, setClientVisible] = useState<boolean>(clientVisible);
+  const wrapperRef = useRef(null);
+
+  useOutsideClick(wrapperRef, () => setClientVisible(false), isClientVisible);
+
+  useEffect(() => {
+    const prePopulatedClient = window.location.search.split("?").join("").replace(/%20/g, " ");
+
+    if (prePopulatedClient) {
+      const selection = clientList.filter(
+        (client) => client.label == prePopulatedClient
+      );
+      selection[0] && handleClientChange(selection[0]);
+    }
+  }, []);
+
+  const handleSelectClientClick = async () => {
+    setClientVisible(true);
+    setOptionSelection(false);
+  };
+
+  const handleGetClientList = async () => {
+    setClientVisible(true);
+  };
+
+  const handleClientChange = (selection) => {
+    setSelectedClient(selection);
+    setClientVisible(false);
+    setOptionSelection(true);
+  };
+
+  const DropdownIndicator = (props: DropdownIndicatorProps<true>) => (
+    <components.DropdownIndicator {...props}>
+      <MagnifyingGlass size={20} color="#1D1A31" />
+    </components.DropdownIndicator>
+  );
+
+  return (
+    <div className="group" ref={wrapperRef}>
+      <p className="font-normal text-xs text-miru-dark-purple-1000 flex">
+        Billed to
+        {isOptionSelected && (
+          <button
+            onClick={handleSelectClientClick}
+            className="bg-miru-gray-1000 rounded mx-1  p-1 hidden group-hover:block"
+          >
+            <PencilSimple size={13} color="#1D1A31" />
+          </button>
+        )}
+      </p>
+
+      {isClientVisible && (
+        <Select
+          defaultValue={null}
+          onChange={handleClientChange}
+          options={clientList}
+          placeholder="Search"
+          isSearchable={true}
+          className="m-0 mt-2 w-52 text-white"
+          classNamePrefix="m-0 font-medium text-sm text-miru-dark-purple-1000 bg-white"
+          defaultMenuIsOpen={true}
+          styles={reactSelectStyles.InvoiceDetails}
+          components={{ DropdownIndicator, IndicatorSeparator: () => null }}
+        />
+      )}
+      {!isOptionSelected && !isClientVisible && (
+        <button
+          className="py-5 mt-2 px-6 font-bold text-base text-miru-dark-purple-200 bg-white border-2 border-dashed border-miru-dark-purple-200 rounded-md tracking-widest"
+          onClick={handleGetClientList}
+          data-cy= "add-client-button"
+        >
+          + ADD CLIENT
+        </button>
+      )}
+      {isOptionSelected && (
+        <div>
+          <p className="font-bold text-base text-miru-dark-purple-1000">
+            {selectedClient.label}
+          </p>
+          <p className="font-normal text-xs text-miru-dark-purple-400 w-52">
+            {selectedClient.address}
+            <br />
+            {selectedClient.phone}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ClientSelection;
---
+++ app/javascript/src/components/Invoices/common/InvoiceDetails/index.tsx
@@ -0,0 +1,109 @@
+import React, { useState, useRef } from "react";
+
+import dayjs from "dayjs";
+import { PencilSimple } from "phosphor-react";
+
+import CustomDatePicker from "common/CustomDatePicker";
+import { currencyFormat } from "helpers/currency";
+
+import ClientSelection from "./ClientSelection";
+
+const InvoiceDetails = ({
+  currency,
+  clientList,
+  amount,
+  selectedClient,
+  setSelectedClient,
+  issueDate,
+  setIssueDate,
+  dueDate,
+  setDueDate,
+  invoiceNumber,
+  setInvoiceNumber,
+  reference,
+  optionSelected,
+  clientVisible
+}) => {
+  const [showDateOfIssuePicker, setShowDateOfIssuePicker] = useState<boolean>(false);
+  const [showDueDatePicker, setShowDueDatePicker] = useState<boolean>(false);
+  const wrapperRef = useRef(null);
+
+  const getIssuedDate = dayjs(issueDate).format("DD.MM.YYYY");
+  const getDueDate = dayjs(dueDate).format("DD.MM.YYYY");
+
+  const handleDatePickerChange = (date) => {
+    setIssueDate(date);
+    setShowDateOfIssuePicker(false);
+    const newDueDate = new Date(date);
+    setDueDate(new Date(newDueDate.setMonth(newDueDate.getMonth() + 1)));
+  };
+
+  const handleDueDatePicker = (date) => {
+    setDueDate(date);
+    setShowDueDatePicker(false);
+  };
+
+  return (
+    <div className="flex justify-between border-b-2 border-miru-gray-400 px-10 py-5 h-36">
+      <ClientSelection
+        selectedClient={selectedClient}
+        setSelectedClient={setSelectedClient}
+        clientList={clientList}
+        optionSelected={optionSelected}
+        clientVisible={clientVisible}
+      />
+      <div className="group">
+        <div className="hoverPencil">
+          <p className="font-normal text-xs text-miru-dark-purple-1000 flex">
+            <span>Date of Issue</span>
+            <button className="ml-2 invisible" onClick={() => setShowDateOfIssuePicker(!showDateOfIssuePicker)}>
+              <PencilSimple size={13} color="#1D1A31" />
+            </button>
+          </p>
+          {showDateOfIssuePicker && <CustomDatePicker handleChange={handleDatePickerChange} date={issueDate} />}
+          <p className="font-normal text-base text-miru-dark-purple-1000">
+            {getIssuedDate}
+          </p>
+        </div>
+        <div className="hoverPencil">
+          <p className="font-normal text-xs text-miru-dark-purple-1000 mt-4 flex">
+            <span>Due Date</span>
+            <button className="ml-2 invisible" onClick={() => setShowDueDatePicker(!showDueDatePicker)}>
+              <PencilSimple size={13} color="#1D1A31" />
+            </button>
+          </p>
+          {showDueDatePicker && <CustomDatePicker handleChange={handleDueDatePicker} date={dueDate} />}
+          <p className="font-normal text-base text-miru-dark-purple-1000">
+            {getDueDate}
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <div className="flex flex-col" ref={wrapperRef}>
+          <p className="font-normal text-xs text-miru-dark-purple-1000">
+            Invoice Number
+          </p>
+          <input type="text" value={invoiceNumber} onChange={(e) => setInvoiceNumber(e.target.value)} className="px-2 w-3/5" data-cy="invoice-number"/>
+        </div>
+        <p className="font-normal text-xs text-miru-dark-purple-1000 mt-4">
+          Reference
+        </p>
+        <p className="font-normal text-base text-miru-dark-purple-1000">
+          {reference}
+        </p>
+      </div>
+
+      <div>
+        <p className="font-normal text-xs text-miru-dark-purple-1000 text-right">
+          Amount
+        </p>
+        <p className="font-normal text-4xl text-miru-dark-purple-1000 mt-6">
+          {currencyFormat({ baseCurrency: currency, amount: amount })}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default InvoiceDetails;
