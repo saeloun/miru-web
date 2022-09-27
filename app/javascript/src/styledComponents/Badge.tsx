b4132af32db1b985bc7af9a67f84e8a1f51eebf0
Prasanth Chaduvula <prasanthchaduvula@Prasanths-MacBook-Air.local>
2022-09-27 21:40:34 +0530

    Used typescript props inside Badge component

--- app/javascript/src/styledComponents/Badge.tsx
+++ app/javascript/src/styledComponents/Badge.tsx
@@ -1,29 +1,25 @@
 import React from "react";
 
 import classnames from "classnames";
-import PropTypes from "prop-types";
 
 const DEFAULT_STYLE = "inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-semibold leading-4 tracking-wider text-purple-800 bg-purple-100";
+
+type BadgeProps = {
+  text: string | number,
+  color: string,
+  bgColor: string,
+  className?: string
+}
 
 const Badge = ({
   text = null,
   color = "",
   bgColor = "",
   className = ""
-}) => (
+}: BadgeProps) => (
   <span className={classnames(DEFAULT_STYLE, color, bgColor, className)}>
     {text || "Badge"}
   </span>
 );
-
-Badge.propTypes = {
-  text: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number
-  ]),
-  color: PropTypes.string,
-  bgColor: PropTypes.string,
-  className: PropTypes.string
-};
 
 export default Badge;
