a2ae7340c0c2b37093150c0542d4008be1acc59a
Prasanth Chaduvula <prasanthchaduvula@Prasanths-MacBook-Air.local>
2022-09-27 21:45:09 +0530

    Added default values to Badge component

--- app/javascript/src/styledComponents/Badge.tsx
+++ app/javascript/src/styledComponents/Badge.tsx
@@ -2,23 +2,23 @@ import React from "react";
 
 import classnames from "classnames";
 
-const DEFAULT_STYLE = "inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-semibold leading-4 tracking-wider text-purple-800 bg-purple-100";
+const DEFAULT_STYLE = "inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-semibold leading-4 tracking-wider";
 
 type BadgeProps = {
-  text: string | number,
-  color: string,
-  bgColor: string,
+  text?: string | number,
+  color?: string,
+  bgColor?: string,
   className?: string
 }
 
 const Badge = ({
-  text = null,
-  color = "",
-  bgColor = "",
-  className = ""
+  text = "Badge",
+  color = "text-purple-800",
+  bgColor = "bg-purple-100",
+  className
 }: BadgeProps) => (
   <span className={classnames(DEFAULT_STYLE, color, bgColor, className)}>
-    {text || "Badge"}
+    {text}
   </span>
 );
 
