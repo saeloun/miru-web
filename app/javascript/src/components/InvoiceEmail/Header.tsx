67ea9bf3421d0cc544361605d9257bffb86edc07
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2022-10-13 12:09:48 +0530

    Added the invoice status badge to mail view

--- app/javascript/src/components/InvoiceEmail/Header.tsx
+++ app/javascript/src/components/InvoiceEmail/Header.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 
 import { Wallet } from "phosphor-react";
+import { Badge } from "StyledComponents";
+
+import getStatusCssClass from "utils/getBadgeStatus";
 
 const Header = ({ invoice, stripeUrl }) => (
   <>
     <div className="mt-6 mb-3 sm:flex sm:items-center sm:justify-between">
-      <div className="flex self-center mr-2">
-        <p className="text-4xl font-bold">
-            Invoice #{invoice.invoice_number}
-        </p>
+      <div className="flex flex-row">
+        <div className="flex self-center mr-2">
+          <p className="text-4xl font-bold">Invoice #{invoice.invoice_number}</p>
+        </div>
+        <div className="flex self-center ml-2">
+          <Badge
+            text={invoice.status}
+            className={`${getStatusCssClass(invoice.status)} uppercase`}
+          />
+        </div>
       </div>
       <div className="flex flex-row justify-items-right">
         <div className="flex flex-col justify-items-center send-button-container ml-1">
