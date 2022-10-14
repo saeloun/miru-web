5f81b85534de54c68e7f3ce20870c3dbcf58c212
Shruti-Apte <72149587+Shruti-Apte@users.noreply.github.com>
2022-10-14 13:16:01 +0530

    Api integration recently updated invoices (#727)
    * custom avatar and logo added to invoice list table
    
    * hover effect added
    
    * menu added
    
    * recently added section
    
    * Arrow added for menu and tooltip
    
    * Styledcomponent folder structure fixed
    
    * resolved comments
    
    * API integration
    
    * Fixed width
    
    * Resolved comments

--- app/javascript/src/components/Invoices/List/container.tsx
+++ app/javascript/src/components/Invoices/List/container.tsx
@@ -9,6 +9,7 @@ const Container = ({
   summary,
   invoices,
   selectedInvoices,
+  recentlyUpdatedInvoices,
   selectInvoices,
   deselectInvoices,
   setShowDeleteDialog,
@@ -23,9 +24,12 @@ const Container = ({
 
       <div className="my-20">
         <h1 className="mb-4 text-miru-dark-purple-1000 font-normal text-2xl">Recently updated</h1>
-        <div className="flex justify-between md:justify-start overflow-x-auto overflow-y-hidden">
-          <RecentlyUpdated/>
-          <RecentlyUpdated/>
+        <div className="grid grid-cols-10 gap-44 overflow-x-auto overflow-y-hidden">
+          {
+            recentlyUpdatedInvoices.length > 0 ?
+              recentlyUpdatedInvoices.map(( invoice ) => <RecentlyUpdated invoice={invoice} />)
+              : <span className="text-xl font-medium text-miru-dark-purple-200 grid col-span-5">No Recently Updated invoices available.</span>
+          }
         </div>
       </div>
 
