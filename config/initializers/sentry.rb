08ae11bce662978bea4d677278ab3dac4c676456
Nishant Samel <nishant@saeloun.com>
2022-10-17 15:35:43 +0530

    I#738: Add basic Sentry setup to Miru
    - Fix https://github.com/saeloun/miru-web/issues/738
    - Add `Sentry` gem and update configuration setting
    - Remove `Honeybadger` gem

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+sentry_dsn = ENV.fetch("SENTRY_DSN", nil)
+return unless sentry_dsn
+
+Sentry.init do |config|
+  config.dsn = sentry_dsn
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+
+  # Excluding Exceptions
+  config.excluded_exceptions += ["ActionController::RoutingError"]
+
+  config.enabled_environments = %w[development production]
+  config.debug = true
+end
