ca6a743d2ccedb23e26903075f7c525296121c2a
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2022-10-18 21:50:40 +0530

    Added authorization on project actions

--- spec/policies/project_policy_spec.rb
+++ spec/policies/project_policy_spec.rb
@@ -4,52 +4,71 @@ require "rails_helper"
 
 RSpec.describe ProjectPolicy, type: :policy do
   let(:company) { create(:company) }
+  let(:another_company) { create(:company) }
   let(:user) { create(:user, current_workspace_id: company.id) }
+  let(:admin) { create(:user, current_workspace_id: company.id) }
+  let(:owner) { create(:user, current_workspace_id: company.id) }
+  let(:employee) { create(:user, current_workspace_id: company.id) }
+  let(:book_keeper) { create(:user, current_workspace_id: company.id) }
+
+  before do
+    admin.add_role :admin, company
+    owner.add_role :owner, company
+    employee.add_role :employee, company
+    book_keeper.add_role :book_keeper, company
+  end
 
   subject { described_class }
 
-  context "when user is an admin" do
-    before do
-      create(:employment, company:, user:)
-      user.add_role :admin, company
-    end
-
-    permissions :create? do
-      it "is permitted to create project" do
-        expect(subject).to permit(user, Project)
+  permissions :index? do
+    context "when user is an admin or owner" do
+      it "grants permission" do
+        expect(described_class).to permit(admin, Project)
+        expect(described_class).to permit(owner, Project)
       end
-    end
 
-    # Will move this to correct file once commit changes get approved
-    # permissions :update_members? do
-    #   it "is permitted to update project members" do
-    #     expect(subject).to permit(user, Project)
-    #   end
-    # end
-  end
-
-  context "when user is an employee" do
-    before do
-      create(:employment, company:, user:)
-      user.add_role :employee, company
-    end
-
-    permissions :create? do
-      it "is not permitted to create project" do
-        expect(subject).not_to permit(user, Project)
+      context "when user is employee or book keeper" do
+        it "does not grants permission" do
+          expect(described_class).not_to permit(book_keeper, Project)
+        end
       end
     end
   end
 
-  context "when user is a book keeper" do
-    before do
-      create(:employment, company:, user:)
-      user.add_role :book_keeper, company
-    end
+  permissions :create? do
+    context "when user is an admin or owner" do
+      it "grants permission" do
+        expect(described_class).to permit(admin, Project)
+        expect(described_class).to permit(owner, Project)
+      end
 
-    permissions :index?, :create?, :update?, :show?, :destroy? do
-      it "is not permitted to create project" do
-        expect(subject).not_to permit(user, Project)
+      context "when user is employee or book keeper" do
+        it "does not grants permission" do
+          expect(described_class).not_to permit(employee, Project)
+          expect(described_class).not_to permit(book_keeper, Project)
+        end
+      end
+    end
+  end
+
+  permissions :show?, :update?, :destroy? do
+    context "when user is an admin or owner" do
+      let(:client) { create(:client, company:) }
+      let(:project) { create(:project, client:) }
+
+      it "grants permission" do
+        expect(described_class).to permit(admin, project)
+        expect(described_class).to permit(owner, project)
+      end
+
+      context "when from another company" do
+        let(:another_admin) { create(:user, current_workspace_id: another_company.id) }
+        let(:another_owner) { create(:user, current_workspace_id: another_company.id) }
+
+        it "does not grants permission" do
+          expect(described_class).not_to permit(another_admin, project)
+          expect(described_class).not_to permit(another_owner, project)
+        end
       end
     end
   end
