1f0f645949ccc53ec8699a1545589cfcb6b12bca
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2022-10-19 16:42:38 +0530

    Updated client_policy spec with new authorization rules

--- spec/policies/client_policy_spec.rb
+++ spec/policies/client_policy_spec.rb
@@ -4,21 +4,28 @@ require "rails_helper"
 
 RSpec.describe ClientPolicy, type: :policy do
   let(:company) { create(:company) }
-  let(:another_company) { create(:company) }
-
   let(:client) { create(:client, company:) }
-  let(:another_client) { create(:client, company: another_company) }
-
   let(:admin) { create(:user, current_workspace_id: company.id) }
   let(:employee) { create(:user, current_workspace_id: company.id) }
   let(:owner) { create(:user, current_workspace_id: company.id) }
   let(:book_keeper) { create(:user, current_workspace_id: company.id) }
+
+  let(:another_company) { create(:company) }
+  let(:another_admin) { create(:user, current_workspace_id: another_company.id) }
+  let(:another_employee) { create(:user, current_workspace_id: another_company.id) }
+  let(:another_owner) { create(:user, current_workspace_id: another_company.id) }
+  let(:another_book_keeper) { create(:user, current_workspace_id: another_company.id) }
 
   before do
     owner.add_role :owner, company
     admin.add_role :admin, company
     employee.add_role :employee, company
     book_keeper.add_role :book_keeper, company
+
+    another_owner.add_role :owner, another_company
+    another_admin.add_role :admin, another_company
+    another_employee.add_role :employee, another_company
+    another_book_keeper.add_role :book_keeper, another_company
   end
 
   permissions :index? do
@@ -54,8 +61,8 @@ RSpec.describe ClientPolicy, type: :policy do
 
       context "when from another company" do
         it "does not grants permission" do
-          expect(described_class).not_to permit(admin, another_client)
-          expect(described_class).not_to permit(owner, another_client)
+          expect(described_class).not_to permit(another_admin, client)
+          expect(described_class).not_to permit(another_owner, client)
         end
       end
     end
@@ -63,9 +70,9 @@ RSpec.describe ClientPolicy, type: :policy do
     context "when user is an employee, book_keeper" do
       it "does not grant permission" do
         expect(described_class).not_to permit(employee, client)
-        expect(described_class).not_to permit(employee, another_client)
+        expect(described_class).not_to permit(another_employee, client)
         expect(described_class).not_to permit(book_keeper, client)
-        expect(described_class).not_to permit(book_keeper, another_client)
+        expect(described_class).not_to permit(another_book_keeper, client)
       end
     end
   end
