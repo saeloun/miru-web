8e13d13356567438e48cb9eb98430b7d3a571380
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2022-10-19 16:23:42 +0530

    Fixed authorization on team & team members

--- app/controllers/internal_api/v1/team_controller.rb
+++ app/controllers/internal_api/v1/team_controller.rb
@@ -2,7 +2,7 @@
 
 class InternalApi::V1::TeamController < InternalApi::V1::ApplicationController
   helper ApplicationHelper
-  before_action :set_employment, only: [:update, :destroy]
+  # before_action :set_employment, only: [:update, :destroy]
 
   def index
     authorize :index, policy_class: TeamPolicy
@@ -17,42 +17,42 @@ class InternalApi::V1::TeamController < InternalApi::V1::ApplicationController
   end
 
   def update
-    authorize @employment, policy_class: TeamPolicy
+    authorize employment, policy_class: TeamPolicy
     User.transaction do
-      @employment.user.skip_reconfirmation!
-      @employment.user.update!(user_params)
+      employment.user.skip_reconfirmation!
+      employment.user.update!(user_params)
       update_company_user_role
     end
     render json: {
-      user: @employment.user,
+      user: employment.user,
       notice: I18n.t("team.update.success.message")
     }, status: :ok
   end
 
   def destroy
-    authorize @employment, policy_class: TeamPolicy
-    @employment.discard!
+    authorize employment, policy_class: TeamPolicy
+    employment.discard!
     render json: {
-      user: @employment.user,
+      user: employment.user,
       notice: I18n.t("team.delete.success.message")
     }, status: :ok
   end
 
   private
 
+    def employment
+      @_employment ||= current_company.employments.kept.find_by!(user_id: params[:id])
+    end
+
     def user_params
       params.permit(policy(:team).permitted_attributes)
     end
 
-    def set_employment
-      @employment = current_company.employments.kept.find_by!(user_id: params[:id])
-    end
-
     def update_company_user_role
-      current_role = current_company_role(@employment.user)
+      current_role = current_company_role(employment.user)
 
-      @employment.user.remove_role(current_role.name.to_sym, current_company) if current_role.present?
-      @employment.user.add_role(params[:role].downcase.to_sym, current_company)
+      employment.user.remove_role(current_role.name.to_sym, current_company) if current_role.present?
+      employment.user.add_role(params[:role].downcase.to_sym, current_company)
     end
 
     def current_company_role(user)
