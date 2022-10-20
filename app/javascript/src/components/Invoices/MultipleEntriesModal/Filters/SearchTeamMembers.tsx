55c25f35aae6734b72876c2596d5c28d426d4ad1
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2022-10-20 20:40:19 +0530

    Added custom multi select team members

--- app/javascript/src/components/Invoices/MultipleEntriesModal/Filters/SearchTeamMembers.tsx
+++ app/javascript/src/components/Invoices/MultipleEntriesModal/Filters/SearchTeamMembers.tsx
@@ -1,52 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import Select from "react-select";
+import { MultiSelect } from "react-multi-select-component";
 
 const SearchTeamMembers = ({
   teamMembers,
   filters,
-  handleSelectFilter
+  setFilters
 }) => {
-  const customStyles = {
-    container: base => ({
-      ...base,
-      width: "13rem"
-    }),
-    control: (provided) => ({
-      ...provided,
-      "&:hover": {
-        borderColor: "#5B34EA"
-      },
-      "&:focus": {
-        borderColor: "#5B34EA"
-      },
-      "&:active": {
-        borderColor: "#5B34EA"
-      }
-    }),
-    option: (
-      styles,{ isSelected }
-    ) => ({
-      ...styles,
-      backgroundColor: isSelected && "#5B34EA",
-      "&:hover": {
-        backgroundColor: isSelected ? "#5B34EA" : "#F5F7F9"
-      }
-    })
+  const [selected, setSelected] = useState([]);
+
+  useEffect(() => {
+    setSelected(teamMembers);
+  }, [teamMembers.length]);
+
+  const handleChange = (selectedOptions) => {
+    setSelected(selectedOptions);
+    setFilters({ ...filters, ["teamMembers"]: selectedOptions });
   };
 
+  const customValueRenderer = (selected) => selected.length
+    ? selected.length == teamMembers.length ? "All team members selected" : `${selected.length} members selected`
+    : "All team members selected";
+
   return (
-    <Select
-      isSearchable={true}
-      isMulti={true}
-      placeholder="All"
-      classNamePrefix="react-select-filter"
-      name="teamMembers"
-      value={filters.teamMembers}
-      options={teamMembers}
-      onChange={handleSelectFilter}
-      styles={customStyles}
-    />
+    <div>
+      <MultiSelect
+        className="w-52"
+        valueRenderer={customValueRenderer}
+        options={teamMembers}
+        value={selected}
+        onChange={handleChange}
+        labelledBy={"Select"}
+        isCreatable={true}
+      />
+    </div>
   );
 };
 
