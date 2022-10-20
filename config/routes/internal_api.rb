318de86f58a3e836eefd0832f393cb8b15377e85
Mohini Dahiya <mohini@saeloun.com>
2022-10-20 13:49:04 +0530

    Remove complex queries from client.rb and project.rb (#723)
    * Remove complex queries from client.rb and project.rb
    - rewrite project/search_controller.rb using searchkick
    
    * Client name conflict fixed
    
    * Fixed fetching new lineitems even on client change on frontend with respective backend changes
    
    * remove commented code
    
    * add reindexing of project to seed and rspec files
    
    * remove new_invoice_line_items tests from client and update internal_api.rb path
    
    * remove tests for discarded complex queries
    
    * fix typo in model/client_spec.rb
    
    * update company search
    
    * add project reindexing to client
    
    * refactor projects
    
    * memoize searchkick methods
    
    * update gem  nokogiri
    
    Co-authored-by: Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
    Co-authored-by: prasanthchaduvula <chaduvulaprasanth1998@gmail.com>

--- config/routes/internal_api.rb
+++ config/routes/internal_api.rb
@@ -2,9 +2,7 @@
 
 namespace :internal_api, defaults: { format: "json" } do
   namespace :v1 do
-    resources :clients, only: [:index, :update, :destroy, :show, :create] do
-      get "new_invoice_line_items", to: "clients/new_invoice_line_items", on: :member
-    end
+    resources :clients, only: [:index, :update, :destroy, :show, :create]
     resources :project, only: [:index]
     resources :timesheet_entry do
       collection do
