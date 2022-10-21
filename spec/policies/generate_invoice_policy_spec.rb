fa508b92a00783b8e33de6480139dd59275169d3
Prasanth Chaduvula <chaduvulaprasanth1998@gmail.com>
2022-10-21 15:32:09 +0530

    Fixed authorization bug on actions (#742)
    * Added authorization on client actions
    
    * Added authorization on generate invoice actions
    
    * Added authorization on project actions
    
    * Added authorization on project member actions
    
    * Added authorization on invoice actions
    
    * Added authorization on team members actions
    
    * Fixed authorization on generate invoice
    
    * Fixed authorization on project member
    
    * Fixed authorization on team & team members
    
    * Fixed authorization on invoices
    
    * Updated client_policy spec with new authorization rules
    
    * Updated generate_invoice_policy spec with new authorization rules
    
    * Updated client_policy spec with new authorization rules
    
    * Updated invoice_policy spec with new authorization rules
    
    * Updated project_policy spec with new authorization rules
    
    * Updated team_policy spec with new authorization rules
    
    * Updated client_policy spec with new authorization rules
    
    * Updated team_controller with new authorization rules

--- spec/policies/generate_invoice_policy_spec.rb
+++ spec/policies/generate_invoice_policy_spec.rb
@@ -4,49 +4,53 @@ require "rails_helper"
 
 RSpec.describe GenerateInvoicePolicy, type: :policy do
   let(:company) { create(:company) }
-  let(:company2) { create(:company) }
-  let(:user) { create(:user, current_workspace_id: company.id) }
   let(:client) { create(:client, company:) }
-  let(:project) { create(:project, client:) }
-  let(:timesheet_entry) { create(:timesheet_entry, project:) }
+  let(:admin) { create(:user, current_workspace_id: company.id) }
+  let(:employee) { create(:user, current_workspace_id: company.id) }
+  let(:owner) { create(:user, current_workspace_id: company.id) }
+  let(:book_keeper) { create(:user, current_workspace_id: company.id) }
+
+  let(:another_company) { create(:company) }
+  let(:another_admin) { create(:user, current_workspace_id: another_company.id) }
+  let(:another_employee) { create(:user, current_workspace_id: another_company.id) }
+  let(:another_owner) { create(:user, current_workspace_id: another_company.id) }
+  let(:another_book_keeper) { create(:user, current_workspace_id: another_company.id) }
 
   subject { described_class }
 
-  context "when user is an admin" do
-    before do
-      create(:employment, company:, user:)
-      user.add_role :admin, company
-    end
+  before do
+    owner.add_role :owner, company
+    admin.add_role :admin, company
+    employee.add_role :employee, company
+    book_keeper.add_role :book_keeper, company
 
-    permissions :index? do
-      it "is permitted to view details" do
-        expect(subject).to permit(user, client)
-      end
-    end
+    another_owner.add_role :owner, another_company
+    another_admin.add_role :admin, another_company
+    another_employee.add_role :employee, another_company
+    another_book_keeper.add_role :book_keeper, another_company
   end
 
-  context "when user is an owner" do
-    before do
-      create(:employment, company:, user:)
-      user.add_role :owner, company
-    end
-
-    permissions :index? do
-      it "is permitted to view details" do
-        expect(subject).to permit(user, client)
+  permissions :index? do
+    context "when user is an admin or owner" do
+      it "grants permission" do
+        expect(described_class).to permit(admin, client)
+        expect(described_class).to permit(owner, client)
       end
     end
-  end
 
-  context "when user is an employee" do
-    before do
-      create(:employment, company:, user:)
-      user.add_role :employee, company
+    context "when user is an employee, book_keeper" do
+      it "does not grant permission" do
+        expect(described_class).not_to permit(employee, client)
+        expect(described_class).not_to permit(book_keeper, client)
+      end
     end
 
-    permissions :index? do
-      it "is not permitted to view details" do
-        expect(subject).not_to permit(user, client)
+    context "when user is from another company" do
+      it "does not grants permission" do
+        expect(described_class).not_to permit(another_admin, client)
+        expect(described_class).not_to permit(another_owner, client)
+        expect(described_class).not_to permit(another_employee, client)
+        expect(described_class).not_to permit(another_book_keeper, client)
       end
     end
   end
