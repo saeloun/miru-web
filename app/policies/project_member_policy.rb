19326a249a9d8afda7e72dbb98058c6fd6662439
Prasanth Chaduvula <chaduvulaprasanth1998@gmail.com>
2022-10-21 15:32:09 +0530

    Fixed authorization bug on actions (#742)
    * Added authorization on client actions
    
    * Added authorization on generate invoice actions
    
    * Added authorization on project actions
    
    * Added authorization on project member actions
    
    * Added authorization on invoice actions
    
    * Added authorization on team members actions
    
    * Fixed authorization on generate invoice
    
    * Fixed authorization on project member
    
    * Fixed authorization on team & team members
    
    * Fixed authorization on invoices
    
    * Updated client_policy spec with new authorization rules
    
    * Updated generate_invoice_policy spec with new authorization rules
    
    * Updated client_policy spec with new authorization rules
    
    * Updated invoice_policy spec with new authorization rules
    
    * Updated project_policy spec with new authorization rules
    
    * Updated team_policy spec with new authorization rules
    
    * Updated client_policy spec with new authorization rules
    
    * Updated team_controller with new authorization rules

--- app/policies/project_member_policy.rb
+++ app/policies/project_member_policy.rb
@@ -2,6 +2,15 @@
 
 class ProjectMemberPolicy < ApplicationPolicy
   def update?
+    authorize_current_user
+  end
+
+  def authorize_current_user
+    unless user.current_workspace_id == record.client.company_id
+      @error_message_key = :different_workspace
+      return false
+    end
+
     user_owner_role? || user_admin_role?
   end
 end
