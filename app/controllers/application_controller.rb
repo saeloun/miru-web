424f557802ae5397fc12ca22a27da6d3eb160539
mohinid <mohinidahiya4@gmail.com>
2022-10-25 15:02:09 +0530

    revert changes for JWT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   include CurrentCompanyConcern
   include Pagy::Backend
 
-  before_action :process_token, :validate_company!
+  before_action :authenticate_user!, :validate_company!
 
   private
 
@@ -15,34 +15,5 @@ class ApplicationController < ActionController::Base
       return if current_user.nil? || devise_controller?
 
       authorize current_company, :company_present?, policy_class: CompanyPolicy
-    end
-
-    # Check for auth headers - if present, decode or send unauthorized response (called always to allow current_user)
-    def process_token
-      if request.headers["Authorization"].present?
-        begin
-          jwt_payload = JWT.decode(
-            request.headers["Authorization"].split(" ")[1],
-            Rails.application.secrets.secret_key_base).first
-          @current_user_id = jwt_payload["id"]
-        rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
-          head :unauthorized
-        end
-      end
-    end
-
-    # If user has not signed in, return unauthorized response (called only when auth is needed)
-    def authenticate_user!(options = {})
-      head :unauthorized unless signed_in?
-    end
-
-    # set Devise's current_user using decoded JWT instead of session
-    def current_user
-      @current_user ||= super || User.find(@current_user_id)
-    end
-
-    # check that authenticate_user has successfully returned @current_user_id (user is authenticated)
-    def signed_in?
-      @current_user_id.present?
     end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,23 +1,6 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  skip_before_action :verify_authenticity_token
-
-  def create
-    respond_to do |format|
-       format.json do
-         user = User.find_by_email(params[:email])
-         if user && user.valid_password?(params[:password])
-           token = user.generate_jwt
-           render json: token.to_json
-         else
-           render json: { errors: { "email or password" => ["is invalid"] } }, status: :unprocessable_entity
-         end
-       end
-       format.html { super }
-     end
-  end
-
   def after_sign_in_path_for(user)
     if user.has_role?(:owner) && user.companies.empty?
       new_company_path
