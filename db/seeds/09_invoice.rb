5cc0faab26e712332e5472fe1b04d4b0a85429d4
Abinash Panda <abinashp@protonmail.ch>
2022-10-27 09:51:50 +0530

    changed reference in invoice seed

--- db/seeds/09_invoice.rb
+++ db/seeds/09_invoice.rb
@@ -3,28 +3,28 @@
 # Invoice Start
 invoice_1_india = {
   issue_date: Date.parse("2022-01-01"), due_date: Date.parse("2022-03-15") + 30,
-  invoice_number: "SAI-C1-01", reference: "Client 1 Saeloun India 1st Invoice",
+  invoice_number: "SAI-C1-01", reference: "C1 1st",
   amount: 5000, outstanding_amount: 2000, tax: 500, amount_paid: 3000,
   amount_due: 2000, discount: 500, status: 0, client_id: @flipkart_client.id,
   external_view_key: "982c5256269ba98e8f5ca88f96801e73"
 }
 invoice_2_india = {
   issue_date: Date.parse("2022-02-10"), due_date: Date.parse("2022-03-15") + 30,
-  invoice_number: "SAI-C1-02", reference: "Client 1 Saeloun India 2nd Invoice",
+  invoice_number: "SAI-C1-02", reference: "C1 2nd",
   amount: 5000, outstanding_amount: 2000, tax: 500, amount_paid: 3000,
   amount_due: 2000, discount: 500, status: 1, client_id: @microsoft_client.id,
   external_view_key: "403dc4e964d2dedd7727ad556df58437"
 }
 invoice_3_india = {
   issue_date: Date.parse("2022-02-10"), due_date: Date.parse("2022-03-15") + 30,
-  invoice_number: "SAI-C1-03", reference: "Client 1 Saeloun India 2nd Invoice",
+  invoice_number: "SAI-C1-03", reference: "C1 3rd",
   amount: 5000, outstanding_amount: 0, tax: 500, amount_paid: 5000,
   amount_due: 0, discount: 500, status: 3, client_id: @microsoft_client.id,
   external_view_key: "403dc4e964d2dedd7727ad556df58437"
 }
 invoice_1_us = {
   issue_date: Date.parse("2022-03-15"), due_date: Date.parse("2022-03-15") + 30,
-  invoice_number: "SAI-C2-01", reference: "Client 1 Saeloun US 1st Invoice",
+  invoice_number: "SAI-C2-01", reference: "C2 1st",
   amount: 5000, outstanding_amount: 2000, tax: 500, amount_paid: 3000,
   amount_due: 2000, discount: 500, status: 2, client_id: @client_one_us.id,
   external_view_key: "2f5c0435c1ffe4d4fe524a1ff9acbe35"
