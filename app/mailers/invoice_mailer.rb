0207fc16beb448562bb23e2f79aa7458dd250f02
Nishant Samel <nishant.samel@gmail.com>
2022-10-31 18:36:40 +0530

    Move `reply_to` email in environmental configuration (#757)
    Co-authored-by: Nishant Samel <nishant@saeloun.com>
    Co-authored-by: Akhil G Krishnan <akhilgkrishnan4u@gmail.com>

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -19,7 +19,7 @@ class InvoiceMailer < ApplicationMailer
     attachments.inline["Instagram.png"] = File.read("public/Instagram.png")
     attachments.inline["Twitter.png"] = File.read("public/Twitter.png")
 
-    mail(to: recipients, subject:, reply_to: "no-reply@getmiru.com")
+    mail(to: recipients, subject:, reply_to: ENV["REPLY_TO_EMAIL"])
   end
 
   private
