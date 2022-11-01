7dc6cfeac0aa68b195429bcc3caeee993bc1d17f
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2022-11-01 17:53:26 +0530

    Fixed styling of date range filter

--- app/javascript/src/components/Invoices/MultipleEntriesModal/Filters/index.tsx
+++ app/javascript/src/components/Invoices/MultipleEntriesModal/Filters/index.tsx
@@ -135,10 +135,7 @@ const Filters = ({
     }),
     control: (provided) => ({
       ...provided,
-      marginTop: "8px",
-      backgroundColor: "#F5F7F9",
       color: "#1D1A31",
-      minHeight: 32,
       padding: "0"
     }),
     menu: (provided) => ({
