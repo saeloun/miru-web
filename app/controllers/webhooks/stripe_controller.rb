eccbb65f1e66690fa27af4ae4f12253961ed31a6
Shalaka Patil <shalu214patil@gmail.com>
2022-11-03 13:57:10 +0530

    Exclude CSRF check for stripe webhook

--- app/controllers/webhooks/stripe_controller.rb
+++ app/controllers/webhooks/stripe_controller.rb
@@ -6,6 +6,7 @@ class Webhooks::StripeController < ApplicationController
   skip_before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
   skip_after_action :verify_authorized
+  protect_from_forgery except: :fulfill_stripe_checkout
 
   def fulfill_stripe_checkout
     handle_stripe_checkout_event(request)
