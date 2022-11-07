1eb475643b8851f8f7ec9d9880b363fa90c8ac30
Shalaka Patil <shalu214patil@gmail.com>
2022-11-07 11:10:00 +0530

    Add spec for channel

--- spec/channels/bulk_invoice_download_channel_spec.rb
+++ spec/channels/bulk_invoice_download_channel_spec.rb
@@ -3,5 +3,13 @@
 require "rails_helper"
 
 RSpec.describe BulkInvoiceDownloadChannel, type: :channel do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    stub_connection
+  end
+
+  it "subscribes to a stream when room id is provided" do
+    subscribe(download_id: 42)
+    expect(subscription).to be_confirmed
+    expect(subscription).to have_stream_for("bulk_invoice_download_channel_42")
+  end
 end
