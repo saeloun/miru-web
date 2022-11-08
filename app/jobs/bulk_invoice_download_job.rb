b2472b453126b091ef7012f987e2e926431bb200
Shalaka Patil <shalu214patil@gmail.com>
2022-11-08 14:12:06 +0530

    Add specs for bulk invoice download job

--- app/jobs/bulk_invoice_download_job.rb
+++ app/jobs/bulk_invoice_download_job.rb
@@ -3,13 +3,11 @@
 class BulkInvoiceDownloadJob < ApplicationJob
   queue_as :default
 
-  def perform(invoice_ids, company_logo, download_id)
-    zip_content = BulkInvoiceDownloadService.process(invoice_ids, company_logo)
-
+  def perform(invoice_ids, company_logo, download_id, root_url)
+    zip_content = BulkInvoiceDownloadService.new(invoice_ids, company_logo, root_url).process
     ActionCable.server.broadcast(
       "bulk_invoice_download_channel_#{download_id}",
-      zip_file: {
-        file_name: "Invoices-#{Time.now.utc.iso8601}.zip",
+      {
         content: zip_content
       }
     )
--- spec/jobs/bulk_invoice_download_job_spec.rb
+++ spec/jobs/bulk_invoice_download_job_spec.rb
@@ -3,5 +3,24 @@
 require "rails_helper"
 
 RSpec.describe BulkInvoiceDownloadJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#perform_later" do
+    it "enqueues the job" do
+      ActiveJob::Base.queue_adapter = :test
+      expect {
+        BulkInvoiceDownloadJob.perform_later([6], nil, "abc", "")
+      }.to enqueue_job
+    end
+  end
+
+  describe "executed job" do
+    before do
+      allow_any_instance_of(BulkInvoiceDownloadService).to receive(:process).and_return("zipped_content")
+    end
+
+    it "broadcast the data on the expected channel" do
+      expect {
+        BulkInvoiceDownloadJob.perform_now([6], nil, "abc", "")
+      }.to have_broadcasted_to("bulk_invoice_download_channel_abc").with(content: "zipped_content")
+    end
+  end
 end
