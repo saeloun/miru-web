8fd14477ef47b4ed3270647f7b0f361580ea4c6a
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2022-11-09 21:10:05 +0530

    Created TimeInput reusable styled component

---
+++ app/javascript/src/StyledComponents/TimeInput/index.tsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect, useRef } from 'react'
+import {isValid } from "./validate"
+
+const TimeInput = ({
+  autoFocus=false,
+	initTime,
+  disabled,
+  mountFocus,
+  onTimeChange,
+  type,
+  onFocusHandler,
+  placeholder,
+  className,
+  name,
+  onBlurHandler
+}: Iprops) => {
+
+	const [time, setTime] = useState(initTime || '');
+
+	const _input = useRef(null)
+
+	useEffect(() => {
+		if (!disabled && mountFocus) {
+			setTimeout(() => {
+				autoFocus && _input.current.focus();
+			}, 0);
+		}
+	});
+
+  let lastVal = '';
+
+	const onChangeHandler = (val) => {
+		if (val == time) {
+			return;
+		}
+		if (isValid(val)) {
+			if (val.length === 2 && lastVal.length !== 3 && val.indexOf(':') === -1) {
+				val = val + ':';
+			}
+
+			if (val.length === 2 && lastVal.length === 3) {
+				val = val.slice(0, 1);
+			}
+
+			if (val.length > 5) {
+				return false;
+			}
+
+			lastVal = val;
+
+			setTime(val);
+
+			if (val.length === 5) {
+				onTimeChange(val);
+			}
+		}
+    }
+
+	const getType = () => {
+		if (type) {
+				return type;
+		}
+		return 'tel'
+	}
+
+	return (
+		<input
+			name={name ? name : undefined}
+			className={className}
+			type={getType()}
+			disabled={disabled}
+			placeholder={placeholder}
+			value={time}
+			onChange={(e) => onChangeHandler(e.target.value)}
+			onFocus={(onFocusHandler) ? (e) => onFocusHandler(e) : undefined}
+			onBlur={(onBlurHandler) ? (e) => onBlurHandler(e) : undefined}
+			ref={_input}
+		/>
+	);
+
+}
+
+TimeInput.defaultProps = {
+    placeholder: ' '
+}
+
+interface Iprops {
+	autoFocus?: boolean;
+	initTime?: string;
+	disabled?: boolean;
+	mountFocus?: string;
+	classNameInitials?: string;
+	classNameInitialsWrapper?: string;
+	onTimeChange?: (val) => any,
+	type?: string,
+	onFocusHandler?: (e) => any,
+	placeholder?: string,
+	className?: string ,
+	name?: string,
+	onBlurHandler?: (e) => any
+  }
+
+export default TimeInput
--- app/javascript/src/StyledComponents/index.tsx
+++ app/javascript/src/StyledComponents/index.tsx
@@ -3,11 +3,13 @@ import Badge from "./Badge";
 import Button from "./Button";
 import SidePanel from "./SidePanel/index";
 import Tooltip from "./Tooltip";
+import TimeInput from "./TimeInput";
 
 export {
   Avatar,
   Badge,
   Button,
   SidePanel,
+  TimeInput,
   Tooltip
 };
