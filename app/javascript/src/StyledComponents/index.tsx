415cf272ec393a6918e8c965beb06a422da58948
Shruti-Apte <72149587+Shruti-Apte@users.noreply.github.com>
2022-11-11 10:51:47 +0530

    Invoice filter UI update (#745)
    * Added filter option style,Removed select
    
    * Filter UI updated
    
    * resuable component and filterlist on invoice list page added
    
    * Reusable button and sidepanel
    
    * review comments resolved
    
    * react-multi-select added
    
    * merge with develop
    
    * search logic added for client filters
    
    * fixed remove date filter issue
    
    * Fixed custom date range issue
    
    * Fixed custom date range issue
    
    * overflow address fixed generate invoice

--- app/javascript/src/StyledComponents/index.tsx
+++ app/javascript/src/StyledComponents/index.tsx
@@ -1,9 +1,13 @@
 import Avatar from "./Avatar";
 import Badge from "./Badge";
+import Button from "./Button";
+import SidePanel from "./SidePanel/index";
 import Tooltip from "./Tooltip";
 
 export {
   Avatar,
   Badge,
+  Button,
+  SidePanel,
   Tooltip
 };
