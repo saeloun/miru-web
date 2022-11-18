f01cf55c068f2cb6972e40afc4cd9193b4019741
Shruti-Apte <72149587+Shruti-Apte@users.noreply.github.com>
2022-11-18 14:58:04 +0530

    Single invoice download (#780)
    * Single invoice download functionality added
    
    * try-catch block added
    
    * pending records migration
    
    * download button disabled for draft invoices
    
    * more options dropdown added on view invoice and edit invoice page
    
    * common function created for download invoice
    
    * fixed draft invoice issue

---
+++ app/javascript/src/components/Invoices/common/MoreButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { DotsThreeVertical } from "phosphor-react";
+
+const MoreButton = ({ onClick }) => (
+  <button
+    onClick={onClick}
+    className="p-2.5 ml-2 bg-miru-gray-1000 border border-miru-han-purple-1000 text-miru-han-purple-1000 rounded opacity-50"
+  >
+    <DotsThreeVertical size={16}/>
+  </button>
+);
+
+export default MoreButton;
