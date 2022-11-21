9ee943641164ff311c74f46b738c4c3dce87aa62
mohinid <mohinidahiya4@gmail.com>
2022-11-21 15:10:47 +0530

    add-token-to-create-invited-user-api

--- app/services/create_invited_user_service.rb
+++ app/services/create_invited_user_service.rb
@@ -84,7 +84,8 @@ class CreateInvitedUserService
         last_name: invitation.last_name,
         email: invitation.recipient_email,
         confirmed_at: Time.current,
-        current_workspace_id: invitation.company.id
+        current_workspace_id: invitation.company.id,
+        token: SecureRandom.base58(50)
       )
       @user.skip_password_validation = true
       @user.save!
