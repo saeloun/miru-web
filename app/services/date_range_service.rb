e3e1d57e858becba6847e77d61fe5c8faaacde79
Prasanth Chaduvula <chaduvulaprasanth1998@gmail.com>
2022-11-22 09:21:16 +0530

    Fixed DateRange service bug (#794)
    * Fixed DateRange service :to attribute bug
    
    * Fixed DateRange service custom range bug

--- app/services/date_range_service.rb
+++ app/services/date_range_service.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class DateRangeService
-  attr_reader :timeframe, :from, to
+  attr_reader :timeframe, :from, :to
+
+  class InvalidDatePassed < StandardError
+    def message
+      "Date value passed is invalid"
+    end
+  end
 
   def initialize(timeframe:, from: nil, to: nil)
     @timeframe = timeframe
@@ -29,4 +35,12 @@ class DateRangeService
       0.year.ago.beginning_of_year..0.year.ago.end_of_year
     end
   end
+
+  private
+
+    def convert_to_date(value)
+      raise InvalidDatePassed if value.nil?
+
+      Date.parse(value)
+    end
 end
