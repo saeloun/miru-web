7febc5572a93f61c4d9ff1c0523d826a11d1078e
Abinash Panda <abinashp@protonmail.ch>
2022-11-30 09:29:35 +0530

    rspec test added for client revenue API request with client id

--- spec/requests/internal_api/v1/reports/client_revenue/index_spec.rb
+++ spec/requests/internal_api/v1/reports/client_revenue/index_spec.rb
@@ -68,6 +68,47 @@ RSpec.describe "InternalApi::V1::Reports::ClientRevenuesController::#index", typ
         expect(json_response["summary"]).to eq(JSON.parse(expected_summary.to_json))
       end
     end
+
+    context "when reports page's request is made with client_ids" do
+      before do
+        @client1_paid_amount = client1_paid_invoice1.amount + client1_paid_invoice2.amount
+        @client1_unpaid_amount = client1_viewed_invoice1.amount + client1_sent_invoice1.amount +
+                                 client1_sent_invoice2.amount
+
+        get internal_api_v1_reports_client_revenues_path,
+          params: { client_ids: [client1.id].to_json, from_date: 1.month.ago, to_date: Date.today }
+      end
+
+      it "returns the 200 http response" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "returns the clients data in alaphabetical order with amount details" do
+        expected_clients =
+          [
+            {
+              name: client1.name,
+              paidAmount: @client1_paid_amount,
+              unpaidAmount: @client1_unpaid_amount,
+              totalAmount: @client1_paid_amount + @client1_unpaid_amount
+            }
+          ]
+        expect(json_response["clients"]).to eq(JSON.parse(expected_clients.to_json))
+      end
+
+      it "returns the base currency" do
+        expect(json_response["currency"]).to eq(company.base_currency)
+      end
+
+      it "returns the summary of first client" do
+        expected_summary = {
+          totalPaidAmount: @client1_paid_amount,
+          totalUnpaidAmount: @client1_unpaid_amount,
+          totalRevenue: @client1_paid_amount + @client1_unpaid_amount
+        }
+        expect(json_response["summary"]).to eq(JSON.parse(expected_summary.to_json))
+      end
+    end
   end
 
   context "when user is an employee" do
