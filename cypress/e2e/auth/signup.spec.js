0d279d7011abfff7c061c8e32745869aed636cd1
Apoorv Tiwari <tiwari.apoorv1316@gmail.com>
2022-12-01 17:15:47 +0530

    Cypress signup specs (#805)
    add a few signup specs
    
    Co-authored-by: “Apoorv <“tiwari.apoorv1316@gmail.com”>

--- cypress/e2e/auth/signup.spec.js
+++ cypress/e2e/auth/signup.spec.js
@@ -1,6 +1,7 @@
 import { signInPath } from "../../constants/routes/paths";
 import { signUpPath } from "../../constants/routes/paths";
 import { authSelectors } from "../../constants/selectors/auth";
+import { fake } from "../../fixtures/fake";
 
 describe("Sign Up", () => {
   beforeEach(function () {
@@ -24,5 +25,35 @@ describe("Sign Up", () => {
     cy.get(authSelectors.signInLink).should("be.visible");
     cy.get(authSelectors.signInLink).click();
     cy.url().should("include", signInPath);
+  });
+
+  it("should throw an error if both password don't match", function () {
+    cy.get(authSelectors.firstNameInput).type("test");
+    cy.get(authSelectors.lastNameInput).type("user");
+    cy.get(authSelectors.emailInput).type("testuser@hotmail.com");
+    cy.get(authSelectors.passwordInput).type("password");
+    cy.get(authSelectors.confirmPasswordInput).type("password123");
+    cy.get(authSelectors.signUpButton).click();
+    cy.contains("Passwords dont' match");
+  });
+
+  it("should throw an error if email is already registered", function () {
+    cy.get(authSelectors.firstNameInput).type("test");
+    cy.get(authSelectors.lastNameInput).type("user");
+    cy.get(authSelectors.emailInput).type("supriya@example.com");
+    cy.get(authSelectors.passwordInput).type("password");
+    cy.get(authSelectors.confirmPasswordInput).type("password");
+    cy.get(authSelectors.signUpButton).click();
+    cy.contains("Email ID already exists");
+  });
+
+  it("user should be able to signup by entering correct values", function () {
+    cy.get(authSelectors.firstNameInput).type(fake.firstName);
+    cy.get(authSelectors.lastNameInput).type(fake.lastName);
+    cy.get(authSelectors.emailInput).type(testEmail);
+    cy.get(authSelectors.passwordInput).type("password");
+    cy.get(authSelectors.confirmPasswordInput).type("password");
+    cy.get(authSelectors.signUpButton).click();
+    cy.contains("Verification link has been sent to your email ID");
   });
 });
