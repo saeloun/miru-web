86a6c6cfc6f120cafcf965e3c531618ab3bd81f5
Shalaka Patil <shalu214patil@gmail.com>
2022-12-08 14:32:03 +0530

    Remove unused fns

--- app/controllers/internal_api/v1/payments/providers_controller.rb
+++ app/controllers/internal_api/v1/payments/providers_controller.rb
@@ -27,18 +27,4 @@ class InternalApi::V1::Payments::ProvidersController < ApplicationController
     def provider_params
       params.require(:provider).permit(:name, :enabled, accepted_payment_methods: [])
     end
-
-    def stripe_connected_account
-      current_company.stripe_connected_account
-    end
-
-    def save_stripe_settings
-      current_company.payments_providers.create(
-        {
-          name: "stripe",
-          connected: true,
-          enabled: true,
-          accepted_payment_methods: [ "card" ]
-        }) if stripe_connected_account.present? && stripe_connected_account.details_submitted
-    end
 end
