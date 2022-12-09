ee8126338eecd4d4c2138c2d4e8861a9cb1d5c13
Prasanth Chaduvula <chaduvulaprasanth1998@gmail.com>
2022-12-09 19:41:19 +0530

    Fixed broken search functionality on the client’s page and project page (#838)
    * Created common AutoSearch component and fixed search on client page
    
    * Fixed project search with AutoSearch
    
    * Removed AutoComplete from the codebase
    
    * Added loading to AutoSearch component
    
    * Made fetch clients on search asnyc with try/catch
    
    * Made fetch projects on search asnyc with try/catch
    
    * Added interface props to AutoSearch components

---
+++ app/javascript/src/common/AutoSearch/index.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+
+import { useDebounce } from "helpers";
+import { SearchIcon, XIcon } from "miruIcons";
+
+import SearchDropdown from "./SearchDropdown";
+
+const AutoSearch = ({ searchAction, SearchDataRow }: Iprops) => {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const debouncedSearchQuery = useDebounce(searchQuery, 500);
+
+  const handleSearchAction = async () => {
+    if (searchQuery) {
+      const result = await searchAction(debouncedSearchQuery);
+      setSearchResult(result);
+      setLoading(false);
+    } else {
+      setSearchResult([]);
+    }
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    handleSearchAction();
+  }, [debouncedSearchQuery]);
+
+  const onSearchClear = () => {
+    setSearchQuery("");
+  };
+
+  return (
+    <div className="header__searchWrap">
+      <div className="header__searchInnerWrapper relative">
+        <div>
+          <input
+            className="header__searchInput"
+            data-cy="search-invoice"
+            placeholder="Search"
+            type="text"
+            value={searchQuery}
+            onChange={e => setSearchQuery(e.target.value)}
+          />
+          <button className=" absolute inset-y-0 right-0 flex cursor-pointer items-center pr-3 ">
+            {searchQuery ? (
+              <XIcon size={12} onClick={onSearchClear} />
+            ) : (
+              <SearchIcon size={12} />
+            )}
+          </button>
+          <SearchDropdown
+            SearchedDataRow={SearchDataRow}
+            list={searchResult}
+            loading={loading}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+interface Iprops {
+  searchAction: (val) => any; // eslint-disable-line
+  SearchDataRow;
+}
+
+export default AutoSearch;
