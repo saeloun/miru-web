e1c0ef6b7d90055e4d0da49e2c3d45f144e58674
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2022-12-12 20:07:53 +0530

    rubocop fix added

--- config/initializers/grover.rb
+++ config/initializers/grover.rb
@@ -12,7 +12,7 @@ Grover.configure do |config|
     cache: false,
     timeout: 0, # Timeout in ms. A value of `0` means 'no timeout'
     wait_until: "domcontentloaded",
-    launch_args: ['--no-sandbox', '--disable-setuid-sandbox']
+    launch_args: ["--no-sandbox", "--disable-setuid-sandbox"]
   }
 
   if !(Rails.env.development?)
