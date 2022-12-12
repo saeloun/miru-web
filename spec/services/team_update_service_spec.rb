782c1ff1f8e7e7ebc8a8292632ccf04fc79f1d6b
Harshaan Nihal Khan <harshaan.n.khan@gmail.com>
2022-12-12 13:09:39 +0530

    Add specs

---
+++ spec/services/team_update_service_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe TeamUpdateService do
+  let(:company) { create(:company) }
+  let(:user) { create(:user, current_workspace_id: company.id) }
+  let(:employment) { create(:employment, company:, user:) }
+  let(:user_params) { { first_name: "Jane", last_name: "Smith" } }
+
+  describe "#process" do
+    before do
+      user.add_role :employee, company
+    end
+
+    subject { described_class.new(employment:, user_params:, current_company: company, new_role: "admin").process }
+
+    it "updates the user's attributes" do
+      expect { subject }.to change { employment.user.reload.first_name }.to("Jane").and change {
+          employment.user.reload.last_name
+        }.to("Smith")
+    end
+
+    it "updates the user's role" do
+      expect { subject }.to change {
+        employment.user.reload.roles.find_by(resource: company).name
+      }.from("employee").to("admin")
+    end
+  end
+end
