9eff508ffe517f4070e4fa61259106a58168b703
Shalaka Patil <shalu214patil@gmail.com>
2022-12-12 13:08:59 +0530

    Set class variables in the functions instead of returing vals from fn

--- app/controllers/internal_api/v1/time_tracking_controller.rb
+++ app/controllers/internal_api/v1/time_tracking_controller.rb
@@ -7,15 +7,6 @@ class InternalApi::V1::TimeTrackingController < InternalApi::V1::ApplicationCont
   def index
     authorize :index, policy_class: TimeTrackingPolicy
     data = TimeTrackingIndexService.new(current_user, current_company).process
-    data[:entries] = TimesheetEntriesPresenter.new(data[:timesheet_entries]).group_snippets_by_work_date
     render json: data, status: :ok
   end
-
-  private
-
-    def format_timesheet_entries(timesheet_entries)
-      entries = formatted_entries_by_date(timesheet_entries)
-      entries[:currentUserRole] = current_user.primary_role current_company
-      entries
-    end
 end
--- app/services/time_tracking_index_service.rb
+++ app/services/time_tracking_index_service.rb
@@ -7,62 +7,68 @@ class TimeTrackingIndexService
   def initialize(user, company)
     @current_user = user
     @current_company = company
-    @is_admin = is_current_user_admin
-    @clients = get_clients
-    @projects = get_projects
-    @timesheet_entries = get_timesheet_entries
-    @employees = get_employees
+
+    set_is_admin
+    set_clients
+    set_projects
+    set_timesheet_entries
+    set_employees
   end
 
   def process
     {
       clients:,
       projects:,
-      timesheet_entries:,
+      entries: formatted_timesheet_entries,
       employees:
     }
   end
 
   private
 
-    def get_employees
-      is_admin ? current_company.users.select(:id, :first_name, :last_name) : [current_user]
+    def set_employees
+      @employees = is_admin ? current_company.users.select(:id, :first_name, :last_name) : [current_user]
     end
 
-    def get_timesheet_entries
-      current_user
+    def set_timesheet_entries
+      @timesheet_entries = current_user
         .timesheet_entries
         .includes([:project, :user])
         .in_workspace(current_company)
         .during(
           1.month.ago.beginning_of_month,
           1.month.since.end_of_month
-        )
+                            )
     end
 
-    def is_current_user_admin
-      @_is_admin = current_user.has_role?(:owner, current_company) || current_user.has_role?(:admin, current_company)
+    def formatted_timesheet_entries
+      entries = TimesheetEntriesPresenter.new(timesheet_entries).group_snippets_by_work_date
+      entries[:currentUserRole] = current_user.primary_role current_company
+      entries
     end
 
-    def get_clients
+    def set_is_admin
+      @is_admin = current_user.has_role?(:owner, current_company) || current_user.has_role?(:admin, current_company)
+    end
+
+    def set_clients
       if is_admin
-        current_company.clients.kept.order(name: :asc).includes(:projects)
+        @clients = current_company.clients.kept.order(name: :asc).includes(:projects)
       else
-        current_user.clients.kept
+        @clients = current_user.clients.kept
           .where(company_id: current_company.id)
           .order(name: :asc)
           .includes(:projects).distinct
       end
     end
 
-    def get_projects
-      projects = {}
+    def set_projects
+      @projects = {}
       if is_admin
-        clients.each { |client| projects[client.name] = client.projects.kept }
+        clients.each { |client| @projects[client.name] = client.projects.kept }
       else
         employee_projects = current_user.projects.kept.joins(:client).where(clients: { company_id: current_company.id })
-        clients.each { |client| projects[client.name] = client.projects.kept & employee_projects }
+        clients.each { |client| @projects[client.name] = client.projects.kept & employee_projects }
       end
-      projects
     end
 end
