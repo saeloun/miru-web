922dba74a997a06fabef71975f4090fd23c2ae10
Harshaan Nihal Khan <harshaan.n.khan@gmail.com>
2022-12-13 13:33:24 +0530

    update specs

--- spec/services/team/update_service_spec.rb
+++ spec/services/team/update_service_spec.rb
@@ -2,14 +2,16 @@
 
 require "rails_helper"
 
-RSpec.describe TeamUpdateService do
+RSpec.describe Team::UpdateService do
   let(:company) { create(:company) }
   let(:user) { create(:user, current_workspace_id: company.id) }
   let(:employment) { create(:employment, company:, user:) }
   let(:user_params) { { first_name: "Jane", last_name: "Smith" } }
 
   describe "#process" do
-    subject { described_class.new(employment:, user_params:, current_company: company, new_role: "admin").process }
+    subject {
+  described_class.new(user: employment.user, user_params:, current_company: company, new_role: "admin").process
+}
 
     it "updates the user's attributes" do
       expect { subject }.to change { employment.user.reload.first_name }.to("Jane").and change {
