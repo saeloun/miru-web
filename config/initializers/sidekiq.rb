96319df5c41388edce7210f59becbef7df808b8c
xrimpy <xrimpy@gmail.com>
2022-12-20 10:42:48 +0000

    Removed Sidekiq concurrency. Added Timeout on ES

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,14 +2,14 @@
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: ENV["REDIS_URL"], size: 4, network_timeout: 5,
+    url: ENV["REDIS_URL"], network_timeout: 5,
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
 end
 
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: ENV["REDIS_URL"], size: 4, network_timeout: 5,
+    url: ENV["REDIS_URL"], network_timeout: 5,
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
 end
