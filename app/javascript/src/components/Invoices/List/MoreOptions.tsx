8ec7165cea4c0ae4117e579768efab1ce435e150
Shruti-Apte <72149587+Shruti-Apte@users.noreply.github.com>
2022-12-20 14:45:25 +0530

    Mobile responsive navigation menu and invoice list page (#827)
    * Mobile responsive navigation menu added
    
    * Comments resolved and invoice list page made mobile responsive
    
    * comment resolved
    
    * Resolved comments

--- app/javascript/src/components/Invoices/List/MoreOptions.tsx
+++ app/javascript/src/components/Invoices/List/MoreOptions.tsx
@@ -1,46 +1,172 @@
-import React from "react";
+import React, { useRef } from "react";
 
-import { useDebounce } from "helpers";
-import { PaperPlaneTiltIcon, DeleteIcon, PrinterIcon } from "miruIcons";
+import { useOutsideClick } from "helpers";
+import {
+  PaperPlaneTiltIcon,
+  DeleteIcon,
+  PrinterIcon,
+  PenIcon,
+  DotsThreeVerticalIcon,
+  DownloadSimpleIcon,
+} from "miruIcons";
+import { Link } from "react-router-dom";
+import { Tooltip } from "StyledComponents";
+
+import { handleDownloadInvoice } from "../common/utils";
 
 const MoreOptions = ({
-  isMenuOpen,
-  setIsMenuOpen,
   setShowDeleteDialog,
   setInvoiceToDelete,
   invoice,
+  isMenuOpen,
+  setIsMenuOpen,
+  setIsSending,
+  isSending,
+  isDesktop,
+  setShowMoreOptions,
 }) => {
-  useDebounce(isMenuOpen, 500);
+  const wrapperRef = useRef(null);
 
-  return (
-    <div
-      className="absolute top-4 right-5 z-10 flex-col items-end group-hover:flex"
-      onMouseLeave={() => setIsMenuOpen(false)}
-    >
-      <div className="w-12 overflow-hidden">
-        <div className="h-6 w-6 origin-bottom-left rotate-45 transform border-2 border-miru-gray-200 bg-white" />
+  useOutsideClick(wrapperRef, () => {
+    setShowMoreOptions(false);
+  });
+
+  return isDesktop ? (
+    <>
+      <div className="absolute bottom-16 left-24 flex hidden w-40 items-center justify-between rounded-xl border-2 border-miru-gray-200 bg-white p-3 lg:group-hover:flex">
+        <Tooltip content="Send To">
+          <button
+            className="text-miru-han-purple-1000"
+            onClick={() => setIsSending(!isSending)}
+          >
+            <PaperPlaneTiltIcon size={16} />
+          </button>
+        </Tooltip>
+        <Tooltip content="Download">
+          <button
+            data-cy="invoice-download"
+            disabled={invoice.status == "draft"}
+            className={
+              invoice.status == "draft"
+                ? "text-miru-gray-1000"
+                : "text-miru-han-purple-1000"
+            }
+            onClick={() => handleDownloadInvoice(invoice)}
+          >
+            <DownloadSimpleIcon size={16} />
+          </button>
+        </Tooltip>
+        <Tooltip content="Edit">
+          <Link
+            className="text-miru-han-purple-1000"
+            data-cy="edit-invoice"
+            to={`/invoices/${invoice.id}/edit`}
+            type="button"
+          >
+            <PenIcon size={16} />
+          </Link>
+        </Tooltip>
+        <Tooltip content="More">
+          <button
+            className="text-miru-han-purple-1000"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            <DotsThreeVerticalIcon size={16} />
+          </button>
+        </Tooltip>
       </div>
-      <ul className="border-2 border-t-0 border-miru-gray-200 bg-white p-4">
-        <li className="flex cursor-pointer items-center py-1">
-          <PrinterIcon className="mr-4 text-miru-han-purple-1000" size={16} />
+      {isMenuOpen && (
+        <div
+          className="absolute top-4 right-5 z-10 flex-col items-end group-hover:flex"
+          onMouseLeave={() => setIsMenuOpen(false)}
+        >
+          <div className="w-12 overflow-hidden">
+            <div className="h-6 w-6 origin-bottom-left rotate-45 transform border-2 border-miru-gray-200 bg-white" />
+          </div>
+          <ul className="border-2 border-t-0 border-miru-gray-200 bg-white p-4">
+            <li className="flex cursor-pointer items-center py-2">
+              <PrinterIcon
+                className="mr-4 text-miru-han-purple-1000"
+                size={16}
+              />
+              Print
+            </li>
+            <li
+              className="flex cursor-pointer items-center py-2 text-miru-red-400"
+              onClick={() => {
+                setShowDeleteDialog(true);
+                setInvoiceToDelete(invoice.id);
+              }}
+            >
+              <DeleteIcon className="mr-4 text-miru-red-400" size={16} />
+              Delete
+            </li>
+            <li className="flex cursor-pointer items-center py-2">
+              <PaperPlaneTiltIcon
+                className="mr-4 text-miru-han-purple-1000"
+                size={16}
+              />
+              Send via link
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
+  ) : (
+    <div
+      className="modal__modal main-modal "
+      style={{ background: "rgba(29, 26, 49,0.6)" }}
+    >
+      <ul className="shadow-2 w-full rounded-lg bg-white p-4" ref={wrapperRef}>
+        <li>
+          <button
+            className="flex cursor-pointer items-center py-2 text-miru-han-purple-1000"
+            onClick={() => setIsSending(!isSending)}
+          >
+            <PaperPlaneTiltIcon className="mr-4" size={16} /> Send Invoice
+          </button>
+        </li>
+        <li className="flex cursor-pointer items-center py-2">
+          <button
+            data-cy="invoice-download"
+            disabled={invoice.status == "draft"}
+            className={
+              invoice.status == "draft"
+                ? "flex cursor-pointer items-center py-2 text-miru-gray-1000"
+                : "flex cursor-pointer items-center py-2 text-miru-han-purple-1000"
+            }
+            onClick={() => handleDownloadInvoice(invoice)}
+          >
+            <DownloadSimpleIcon className="mr-4" size={16} /> Download Invoice
+          </button>
+        </li>
+        <li>
+          <Link
+            className="flex cursor-pointer items-center py-2 text-miru-han-purple-1000"
+            data-cy="edit-invoice"
+            to={`/invoices/${invoice.id}/edit`}
+            type="button"
+          >
+            <PenIcon className="mr-4" size={16} /> Edit Invoice
+          </Link>
+        </li>
+        <li className="flex cursor-pointer items-center py-2 text-miru-han-purple-1000">
+          <PrinterIcon className="mr-4" size={16} />
           Print
         </li>
+        <li className="flex cursor-pointer items-center py-2 text-miru-han-purple-1000">
+          <PaperPlaneTiltIcon className="mr-4" size={16} />
+          Send via link
+        </li>
         <li
-          className="flex cursor-pointer items-center py-1"
+          className="flex cursor-pointer items-center py-2 text-miru-red-400"
           onClick={() => {
             setShowDeleteDialog(true);
             setInvoiceToDelete(invoice.id);
           }}
         >
-          <DeleteIcon className="mr-4 text-miru-han-purple-1000" size={16} />
+          <DeleteIcon className="mr-4 text-miru-red-400" size={16} />
           Delete
-        </li>
-        <li className="flex cursor-pointer items-center py-1">
-          <PaperPlaneTiltIcon
-            className="mr-4 text-miru-han-purple-1000"
-            size={16}
-          />
-          Send via link
         </li>
       </ul>
     </div>
--- app/javascript/src/components/Main.tsx
+++ app/javascript/src/components/Main.tsx
@@ -22,7 +22,7 @@ const RestrictedRoute = ({ user, role, authorisedRoles }) => {
 };
 
 const Main: React.FC<Iprops> = props => (
-  <div className="overflow-y-0 absolute top-0 bottom-0 right-0 w-5/6 px-8 font-manrope md:px-20 md:py-3">
+  <div className="overflow-x-scroll px-4 py-12 font-manrope lg:absolute lg:top-0 lg:bottom-0 lg:right-0 lg:w-5/6 lg:px-20 lg:py-3">
     <Routes>
       {ROUTES.map(parentRoute => (
         <Route
@@ -50,6 +50,7 @@ interface Iprops {
   user: object;
   companyRole: Roles;
   company: object;
+  isDesktop: boolean;
 }
 
 export default Main;
