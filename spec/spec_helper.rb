8a764a908122752d31385544ccb92597235cbff9
Vipul Amler <vipul@saeloun.com>
2022-12-26 17:08:48 +0530

    Add Rspec Retry

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require "simplecov"
 require "pundit/rspec"
 require "sidekiq/testing"
 require "webmock/rspec"
+require "rspec/retry"
 
 if ENV.fetch("COVERAGE", false)
   SimpleCov.start "rails" do
@@ -114,4 +115,25 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+end
+
+https :/ / github.com / NoRedInk / rspec - retry # installation
+RSpec.configure do |config|
+  # show retry status in spec process
+  config.verbose_retry = true
+  # show exception that triggers a retry if verbose_retry is set to true
+  config.display_try_failure_messages = true
+
+  # run retry only on features
+  config.around do |ex|
+    ex.run_with_retry retry: 3
+  end
+
+  # callback to be run between retries
+  # config.retry_callback = proc do |ex|
+  #   # run some additional clean up task - can be filtered by example metadata
+  #   if ex.metadata[:js]
+  #     Capybara.reset!
+  #   end
+  # end
 end
