89bf42ae9f02fa507189c677b30d25ef22a31b4f
Shalaka Patil <shalu214patil@gmail.com>
2022-12-28 09:11:59 +0530

    Add suggested changes

--- app/services/create_company_service.rb
+++ app/services/create_company_service.rb
@@ -7,18 +7,17 @@ class CreateCompanyService
   def initialize(params, current_user)
     @params = params
     @current_user = current_user
-    @company = nil
+    @company = Company.new(params)
   end
 
   def process
-    @company = Company.new(params)
     company.save!
-    update_current_user
+    add_current_user_to_company
   end
 
   private
 
-    def update_current_user
+    def add_current_user_to_company
       current_user.companies << company
       current_user.current_workspace_id = company.id
       current_user.add_role(:owner, company)
