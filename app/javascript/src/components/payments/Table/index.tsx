5402bba0355ec34a2c921275343054e5b8e9c2fa
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2022-12-28 08:18:38 +0530

    Fixed eslint error

--- app/javascript/src/components/payments/Table/index.tsx
+++ app/javascript/src/components/payments/Table/index.tsx
@@ -10,9 +10,13 @@ const Table = ({ payments, baseCurrency }) => (
     </thead>
     <tbody className="min-w-full divide-y divide-gray-200 bg-white">
       {payments.map(
-        (payment, idx) =>
+        (payment, index) =>
           payment && (
-            <TableRow baseCurrency={baseCurrency} key={idx} payment={payment} />
+            <TableRow
+              baseCurrency={baseCurrency}
+              key={index}
+              payment={payment}
+            />
           )
       )}
     </tbody>
