0042d2da23bc8088eaabc112c41baaf2978f5a16
Shalaka Patil <shalu214patil@gmail.com>
2022-12-29 21:05:42 +0530

    Refactor timesheet entry controller (#831)
    * Refactor timesheet entry controller
    
    * Update the service name
    
    * Set class variables in the functions instead of returing vals from fn
    
    * Add pesenter class for listing projects by clients
    
    * Add query optimisation
    
    * Pass current_company and current_user to presenter
    
    * Add suggested changes
    
    Co-authored-by: Supriya Agarwal <91338424+supriya3105@users.noreply.github.com>

---
+++ app/presenters/clients_presenter.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class ClientsPresenter
+  attr_reader :clients, :current_user, :current_company
+
+  def initialize(clients, current_company, current_user)
+    @clients = clients
+    @current_user = current_user
+    @current_company = current_company
+  end
+
+  def group_projects_by_client_name
+    projects = {}
+    if is_admin
+      clients.each { |client| projects[client.name] = client.projects.kept }
+    else
+      employee_projects = current_user.projects.kept.joins(:client).where(clients: { company_id: current_company.id })
+      clients.each { |client| projects[client.name] = client.projects.kept & employee_projects }
+    end
+    projects
+  end
+
+  private
+
+    def is_admin
+      current_user.has_role?(:owner, current_company) || current_user.has_role?(:admin, current_company)
+    end
+end
