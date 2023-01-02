e8e5c10c509074dd6d7b4d702557ca61ad6fa33f
Abinash <abinashp@protonmail.ch>
2023-01-02 12:26:16 +0530

    Client revenue flaky rspec test fixes (#809)
    * client revenue flaky test fixed
    
    * rspec test added for client revenue API request with client id
    
    * refactoed invoice spec
    
    * changed issue date in invoice factory
    
    * changed issue date, date in invoice and payment factory
    
    * changes date in invoices spec
    
    * changes from date in client revenue controller
    
    * replace "during" with "where" in total_amount_due_during
    
    * added if condition in invoice during scope
    
    * removed else condition in duration_params
    
    Co-authored-by: Apoorv Tiwari <tiwari.apoorv1316@gmail.com>
    Co-authored-by: Supriya Agarwal <91338424+supriya3105@users.noreply.github.com>

--- spec/factories/invoice_line_items.rb
+++ spec/factories/invoice_line_items.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :invoice_line_item do
     name { Faker::Name.name }
     description { Faker::Alphanumeric.alpha(number: 20) }
-    date { Faker::Date.between(from: "2019-04-01", to: Date.today) }
+    date { Faker::Date.between(from: "2022-04-01", to: Date.today) }
     rate { Faker::Number.between(from: 0, to: 1000) }
     quantity { Faker::Number.between(from: 1, to: 10) }
     invoice
