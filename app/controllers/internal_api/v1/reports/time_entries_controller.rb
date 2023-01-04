7e424b83dba870f310c7dc6a13499a7ce36b2927
Shalaka Patil <shalu214patil@gmail.com>
2023-01-04 10:33:44 +0530

    Refactor time entries report controller  (#884)
    * Move fatty time entries controller code to service
    
    * Fix missed refactoring case
    
    * Add suggested changes
    
    * Use keyword argument

--- app/controllers/internal_api/v1/reports/time_entries_controller.rb
+++ app/controllers/internal_api/v1/reports/time_entries_controller.rb
@@ -3,16 +3,19 @@
 class InternalApi::V1::Reports::TimeEntriesController < InternalApi::V1::ApplicationController
   def index
     authorize :report
-    render :index, locals: { reports:, filter_options: }, status: :ok
+    render :index,
+      locals: Reports::TimeEntries::ReportService.new(params, current_company).process,
+      status: :ok
   end
 
   def download
     authorize :report
 
-    entries = reports.map { |e| e[:entries] }.flatten
-
     respond_to do |format|
-      format.csv { send_data Reports::TimeEntries::GenerateCsv.new(entries).process }
+      format.csv { send_data Reports::TimeEntries::GenerateCsv
+        .new(Reports::TimeEntries::ReportService.new(params, current_company, download: true).process[:entries])
+        .process
+      }
       format.pdf { send_data Reports::TimeEntries::GeneratePdf.new(reports).process }
     end
   end
