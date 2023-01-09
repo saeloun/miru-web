4603011f60f776e5f9499fa84d5bf072ceaebb32
Shalaka Patil <shalu214patil@gmail.com>
2023-01-09 15:43:27 +0530

    Refactor show action to use presenter and optimised query

---
+++ app/presenters/client_presenter.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class ClientPresenter
+  attr_reader :client
+
+  def initialize(client)
+    @client = client
+  end
+
+  def snippet
+    {
+      id: client.id,
+      name: client.name,
+      email: client.email,
+      address: client.address,
+      phone: client.phone
+    }
+  end
+end
