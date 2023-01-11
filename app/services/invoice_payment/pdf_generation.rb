10ba59a3a85a21d514d65cc9a72ffe7e43298289
Swaathi Kakarla <swaathi16@gmail.com>
2023-01-11 08:40:48 +0530

    Refactor PDF operations [updated] (#916)
    * Bulk Downloads
    
    * GemFile fix rspec-sidekiq
    
    * Refactoring PDF operations across codebase
    
    * Adding PDF and PDF::Temporary
    
    * Resolved conflicts
    
    Co-authored-by: Vipul Amler <vipul@saeloun.com>
    Co-authored-by: xrimpy <xrimpy@gmail.com>
    Co-authored-by: Shalaka Patil <shalu214patil@gmail.com>
    Co-authored-by: Supriya Agarwal <91338424+supriya3105@users.noreply.github.com>

--- app/services/invoice_payment/pdf_generation.rb
+++ app/services/invoice_payment/pdf_generation.rb
@@ -4,6 +4,7 @@ module InvoicePayment
   class PdfGeneration < ApplicationService
     def initialize(invoice, company_logo, root_url, filepath = nil)
       @invoice = invoice
+      @invoice_line_items = @invoice.invoice_line_items
       @company_logo = company_logo || ""
       @base_currency = invoice.company.base_currency
       @root_url = root_url
@@ -11,79 +12,47 @@ module InvoicePayment
     end
 
     def process
-      formatted_invoice = format_invoice(@invoice.invoice_line_items)
-      controller = ActionController::Base.new
-      html = controller.render_to_string(
-        template: "invoices/pdf",
-        layout: "layouts/pdf",
-        locals: {
-          invoice: @invoice,
-          invoice_amount: format_currency(@base_currency, @invoice.amount),
-          invoice_tax: format_currency(@base_currency, @invoice.tax),
-          invoice_amount_due: format_currency(@base_currency, @invoice.amount_due),
-          invoice_amount_paid: format_currency(@base_currency, @invoice.amount_paid),
-          invoice_discount: format_currency(@base_currency, @invoice.discount),
-          company_logo: @company_logo,
-          client: @invoice.client,
-          invoice_line_items: formatted_invoice[:invoice_line_items],
-          sub_total: format_currency(@base_currency, formatted_invoice[:sub_total]),
-          total: format_currency(@base_currency, formatted_invoice[:total])
-        }
-      )
-
-      options = {
-        wait_until: ["networkidle0", "load", "domcontentloaded", "networkidle2"],
-        path: @filepath
+      invoice_data = build_invoice
+      locals = {
+        invoice: @invoice,
+        invoice_amount: format_currency(@invoice.amount),
+        invoice_tax: format_currency(@invoice.tax),
+        invoice_amount_due: format_currency(@invoice.amount_due),
+        invoice_amount_paid: format_currency(@invoice.amount_paid),
+        invoice_discount: format_currency(@invoice.discount),
+        company_logo: @company_logo,
+        client: @invoice.client,
+        invoice_line_items: invoice_data[:invoice_line_items],
+        sub_total: format_currency(invoice_data[:sub_total]),
+        total: format_currency(invoice_data[:total])
       }
 
-      absolute_html = Grover::HTMLPreprocessor.process html, "#{@root_url}/", "http"
-
-      grover = Grover.new(absolute_html, **options)
-      grover.to_pdf
+      Pdf::HtmlGenerator.new(
+        :invoices,
+        locals:,
+        path: @filepath,
+        root_url: @root_url
+      ).make
     end
 
     private
 
-      def format_invoice(invoice_line_items)
-        sub_total = 0
+      def build_invoice
+        sub_total = @invoice_line_items.total_cost
 
-        new_invoice_line_items = []
-
-        invoice_line_items.each do |line_item|
-          if line_item.quantity <= 0
-            quantity = "00:00"
-          else
-            hours = line_item.quantity / 60
-            minutes = line_item.quantity % 60
-            hours = "0#{hours}" if hours.digits.count == 1
-            minutes = "0#{minutes}" if minutes.digits.count == 1
-            quantity = "#{hours}:#{minutes}"
-          end
-
-          total_rate = ((line_item.quantity.to_f / 60) * line_item.rate)
-          line_total_val = "%.2f" % total_rate
-
-          new_line_item = {}
-          new_line_item[:name] = line_item.name
-          new_line_item[:date] = line_item.date
-          new_line_item[:description] = line_item.description
-          new_line_item[:rate] = format_currency(@base_currency, line_item.rate)
-          new_line_item[:quantity] = quantity
-          new_line_item[:line_total] = format_currency(@base_currency, total_rate)
-          new_invoice_line_items << new_line_item
-
-          sub_total += total_rate
+        rows = @invoice_line_items.map do |invoice_line_item|
+          InvoiceLineItemPresenter.new(invoice_line_item).pdf_row(@base_currency)
         end
 
         {
-          invoice_line_items: new_invoice_line_items,
+          invoice_line_items: rows,
           sub_total:,
           total: sub_total + @invoice.tax - @invoice.discount
         }
       end
 
-      def format_currency(base_currency, amount)
-        FormatAmountService.new(base_currency, amount).process
+      def format_currency(amount)
+        FormatAmountService.new(@base_currency, amount).process
       end
   end
 end
