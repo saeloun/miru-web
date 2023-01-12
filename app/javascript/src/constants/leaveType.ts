dd963219166f2c2000854e53a849d1dda62adc51
onkar <onkar@saeloun.com>
2023-01-12 08:42:12 +0530

    added constant and css

---
+++ app/javascript/src/constants/leaveType.ts
@@ -0,0 +1,22 @@
+export const leaveTypes = [
+  {
+    value: "annual",
+    label: "Annual leaves",
+  },
+  {
+    value: "sick",
+    label: "Sick leaves",
+  },
+  {
+    value: "maternity",
+    label: "Maternity leave",
+  },
+  {
+    value: "paternity",
+    label: "Paternity leave",
+  },
+  {
+    value: "period",
+    label: "Paternity leave",
+  },
+];
