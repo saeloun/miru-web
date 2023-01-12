1a470573a4a17707220a2e82b55a510eae9e48e8
“Apoorv <tiwari.apoorv1316@gmail.com>
2023-01-12 23:14:51 +0530

    testing wss

--- app/javascript/src/components/Invoices/popups/consumer.js
+++ app/javascript/src/components/Invoices/popups/consumer.js
@@ -1,6 +1,12 @@
 import { createConsumer } from "@rails/actioncable";
 
-const { APP_BASE_URL } = process.env;
+// const {APP_BASE_URL, NODE_ENV} = process.env;
 
-const consumer = createConsumer(`ws://${APP_BASE_URL}/cable`);
+// const url = NODE_ENV === 'production' ? `wss://${APP_BASE_URL}/cable` : `ws://${APP_BASE_URL}/cable`;
+// const consumer = createConsumer(url);
+const isProd = process.env.NODE_ENV === "production";
+const protocol = isProd ? "wss" : "ws";
+const consumer = createConsumer(
+  `${protocol}://${process.env.APP_BASE_URL}/cable`
+);
 export default consumer;
