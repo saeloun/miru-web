879aa571657e4c92d327a2b1c7b2624ff69d262f
Apoorv Tiwari <tiwari.apoorv1316@gmail.com>
2023-01-12 18:23:32 +0530

    Remove hardcoded url (#935)
    remove hardcoded url
    
    Co-authored-by: “Apoorv <“tiwari.apoorv1316@gmail.com”>

--- app/javascript/src/components/Invoices/popups/consumer.js
+++ app/javascript/src/components/Invoices/popups/consumer.js
@@ -1,8 +1,6 @@
-// Action Cable provides the framework to deal with WebSockets in Rails.
-// You can generate new channels where WebSocket features live using the `bin/rails generate channel` command.
-
 import { createConsumer } from "@rails/actioncable";
 
-const URL = "ws://localhost:3000/cable";
-const consumer = createConsumer(URL);
+const { APP_BASE_URL } = process.env;
+
+const consumer = createConsumer(`ws://${APP_BASE_URL}/cable`);
 export default consumer;
