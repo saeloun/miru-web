719ffa938d92ad257ae46ea5f51846482e370177
Prasanth Chaduvula <chaduvulaprasanth1998@gmail.com>
2023-01-13 09:07:50 +0530

    Can't click on disabled save & update buttons on timesheet entry with updated validations (#937)

--- app/javascript/src/helpers/validateTimesheetEntry.ts
+++ app/javascript/src/helpers/validateTimesheetEntry.ts
@@ -1,6 +1,22 @@
-export const validateTimesheetEntry = (tse: object) => {
+export const validateTimesheetEntry = (
+  tse: object,
+  client?: string,
+  projectId?: number
+) => {
   if (tse["duration"] <= 0 || tse["duration"] >= 6000000) {
     return "Please enter a valid duration";
+  }
+
+  if (!client) {
+    return "Please select the client";
+  }
+
+  if (!projectId) {
+    return "Please select the project";
+  }
+
+  if (!tse["note"]) {
+    return "Please add notes";
   }
 
   return "";
