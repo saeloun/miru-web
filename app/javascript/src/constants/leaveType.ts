74bc11a30518e943f54e44c1f09eef465f7ab1ca
onkar <onkar@saeloun.com>
2023-01-16 12:27:50 +0530

    added constants

--- app/javascript/src/constants/leaveType.ts
+++ app/javascript/src/constants/leaveType.ts
@@ -20,3 +20,23 @@ export const leaveTypes = [
     label: "Paternity leave",
   },
 ];
+
+export const repitationTypeObj = {
+  per_month: "per month",
+  per_year: "per year",
+  per_week: "per_week",
+  per_quarter: "per_quarter",
+};
+
+export const countTypeOptions = [
+  { value: "days", label: "days" },
+  { value: "weeks", label: "weeks" },
+  { value: "months", label: "months" },
+];
+
+export const repetitionType = [
+  { value: "per_week", label: "per week" },
+  { value: "per_month", label: "per month" },
+  { value: "per_quarter", label: "per quarter" },
+  { value: "per_year", label: "per year" },
+];
