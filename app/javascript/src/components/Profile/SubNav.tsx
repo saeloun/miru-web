958732be9612707234ba4f7c8ea4015fc141efa8
sanjibroy360 <sanjibroy0098@gmail.com>
2023-01-16 20:49:52 +0530

    Hide Billing tab under settings for admin and owner user role

--- app/javascript/src/components/Profile/SubNav.tsx
+++ app/javascript/src/components/Profile/SubNav.tsx
@@ -53,15 +53,17 @@ const SideNav = ({ isAdmin, firstName, company, lastName, email }) => {
           PAYMENT SETTINGS
         </NavLink>
       </li>
-      <li className="border-b-2 border-miru-gray-400">
-        <NavLink
-          end
-          className={({ isActive }) => getActiveClassName(isActive)}
-          to="/profile/edit/billing"
-        >
-          BILLING
-        </NavLink>
-      </li>
+      {!isAdmin && (
+        <li className="border-b-2 border-miru-gray-400">
+          <NavLink
+            end
+            className={({ isActive }) => getActiveClassName(isActive)}
+            to="/profile/edit/billing"
+          >
+            BILLING
+          </NavLink>
+        </li>
+      )}
       <li className="border-b-2 border-miru-gray-400">
         {/* <NavLink end to="/profile/edit/import" className={({ isActive }) => getActiveClassName(isActive)}>
           IMPORT
