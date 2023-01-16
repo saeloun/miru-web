aaf33f2ba06b2e8dd1b2de241885a22dc378cd93
onkar <onkar@saeloun.com>
2023-01-16 11:54:07 +0530

    added details component

--- app/javascript/src/components/Profile/Organization/LeavesAndHolidays/Details.tsx
+++ app/javascript/src/components/Profile/Organization/LeavesAndHolidays/Details.tsx
@@ -1,251 +1,303 @@
-/* eslint-disable */
 import React from "react";
-import Calendar from "react-calendar";
 
-// import dayjs from "dayjs";
-// import { CalendarIcon, DeleteIcon } from "miruIcons";
-// import Select from "react-select";
+import dayjs from "dayjs";
 
-// import CustomDatePicker from "common/CustomDatePicker";
 import { Divider } from "common/Divider";
-// import Loader from "common/Loader/index";
-// import Toastr from "common/Toastr";
-// import { leaveTypes } from "constants/leaveType";
-// import { sendGAPageView } from "utils/googleAnalytics";
+import { repitationTypeObj } from "constants/leaveType";
 
-// import Header from "../../Header";
-// import { Tooltip } from "StyledComponents";
+import HolidayModal from "./HolidaysModal";
 
-const Details = () => {
-  // console.log(new Date().toJSON().slice(0, 10))
-  let tileContent = ({ date }) => {
-    // console.log(date, "<<");
-    if (true) {
-      // console.log(date, "<<");
+const Details = ({ showCalendar, toggleCalendarModal }) => {
+  const holiday = [
+    {
+      date: "26-01-2023",
+      name: "Republic Day",
+    },
+    {
+      date: "19-02-2023",
+      name: "shivjayanti",
+    },
+  ];
+
+  const annualLeave = [
+    {
+      date: "12-01-2023",
+      name: "Family Function",
+    },
+  ];
+
+  const partialAnnualLeave = [
+    {
+      date: "11-01-2023",
+      name: "patial Annual leave",
+    },
+  ];
+
+  const sickLeave = [
+    {
+      date: "10-01-2023",
+      name: "Sick Leave",
+    },
+  ];
+
+  const maternityLeave = [
+    {
+      date: "23-01-2023",
+      name: "Maternity Leave",
+    },
+    {
+      date: "24-01-2023",
+      name: "Maternity Leave",
+    },
+    {
+      date: "25-01-2023",
+      name: "Maternity Leave",
+    },
+    {
+      date: "26-01-2023",
+      name: "Maternity Leave",
+    },
+    {
+      date: "27-01-2023",
+      name: "Maternity Leave",
+    },
+  ];
+
+  const upcomingOptionHoliday = [
+    {
+      date: "31-01-2023",
+      name: "optional Holiday",
+    },
+    {
+      date: "10-01-2023",
+      name: "optional Holiday",
+    },
+  ];
+
+  const leaveBalanceList = [
+    {
+      leaveType: "annual",
+      total: 0,
+      countType: "days",
+      repetitionType: "per_year",
+      carryforwardedCount: 0,
+    },
+    {
+      leaveType: "sick",
+      total: 0,
+      countType: "days",
+      repetitionType: "per_year",
+      carryforwardedCount: 0,
+    },
+    {
+      leaveType: "maternity",
+      total: 0,
+      countType: "days",
+      repetitionType: "per_year",
+      carryforwardedCount: 0,
+    },
+    {
+      leaveType: "paternity",
+      total: 0,
+      countType: "days",
+      repetitionType: "per_quarter",
+      carryforwardedCount: 0,
+    },
+    {
+      leaveType: "period",
+      total: 0,
+      countType: "days",
+      repetitionType: "per_month",
+      carryforwardedCount: 0,
+    },
+  ];
+
+  const holidaysList = [
+    {
+      date: "26-01-2023",
+      name: "Republic Day",
+    },
+    {
+      date: "19-02-2023",
+      name: "shivjayanti",
+    },
+  ];
+  const totalOptionalHolidays = 1;
+  const optionalRepetitionType = "per_year";
+  const optionalHolidayList = holidaysList;
+
+  const CalendarButton = ({ result, date, className }) => (
+    <button
+      className={`holiday-wrapper ${className}`}
+      data-bs-placement="right"
+      data-bs-toggle="tooltip"
+      title={result.name}
+    >
+      {date.getDate()}
+    </button>
+  );
+
+  const tileContent = ({ date }) => {
+    let result;
+    if (
+      (result = holiday.find(o => o.date === dayjs(date).format("DD-MM-YYYY")))
+    ) {
+      return <CalendarButton className="holiday" date={date} result={result} />;
+    } else if (
+      (result = annualLeave.find(
+        o => o.date === dayjs(date).format("DD-MM-YYYY")
+      ))
+    ) {
       return (
-        <button
-          data-bs-toggle="tooltip"
-          data-bs-placement="right"
-          title="Tooltip on right"
-        >
-          {date.getDate()}
-        </button>
+        <CalendarButton className="annual-leave" date={date} result={result} />
+      );
+    } else if (
+      (result = partialAnnualLeave.find(
+        o => o.date === dayjs(date).format("DD-MM-YYYY")
+      ))
+    ) {
+      return (
+        <CalendarButton
+          className="annual-leave-partial"
+          date={date}
+          result={result}
+        />
+      );
+    } else if (
+      (result = sickLeave.find(
+        o => o.date === dayjs(date).format("DD-MM-YYYY")
+      ))
+    ) {
+      return (
+        <CalendarButton className="sick-leave" date={date} result={result} />
+      );
+    } else if (
+      (result = maternityLeave.find(
+        o => o.date === dayjs(date).format("DD-MM-YYYY")
+      ))
+    ) {
+      return (
+        <CalendarButton
+          className="maternity-leave"
+          date={date}
+          result={result}
+        />
+      );
+    } else if (
+      (result = upcomingOptionHoliday.find(
+        o => o.date === dayjs(date).format("DD-MM-YYYY")
+      ))
+    ) {
+      return (
+        <CalendarButton
+          className="optional-holiday"
+          date={date}
+          result={result}
+        />
       );
     }
-    //  else {
-    //   return null
-    // }
+
+    return <button>{date.getDate()}</button>;
   };
 
-  const mark = ["2023-01-12", "2023-02-15", "2023-01-20"];
-
-  // console.log(new Date(mark[0]).toJSON().slice(0, 10))
-
   return (
-    <div className="mt-4 h-full bg-miru-gray-100 p-10">
+    <div className="mt-4 min-h-80v bg-miru-gray-100 p-10">
       <div className="flex w-full flex-row py-6">
-        <div className="w-3/12 p-2 text-sm">Leave Balance</div>
-        <div className="w-3/12 p-2">
-          <div className="text-xs">Leave type</div>
-          <div className="text-base">Annual leaves</div>
-        </div>
-        <div className="w-3/12 p-2">
-          <div className="text-xs">Total</div>
-          <div className="text-base">2 days per month</div>
-        </div>
-        <div className="w-3/12 p-2">
-          <div className="text-xs">Carry forward</div>
-          <div className="text-base">5 days</div>
+        <div className="w-2/12 p-2 text-sm">Leave Balance</div>
+        <div className="flex w-10/12 flex-col">
+          {leaveBalanceList.map((leaveBalance, index) => (
+            <div className=" flex w-full flex-row" key={index}>
+              <div className="w-4/12 p-2">
+                <div className="text-xs">Leave type</div>
+                <div className="text-base">{leaveBalance.leaveType}</div>
+              </div>
+              <div className="w-4/12 p-2">
+                <div className="text-xs">Total</div>
+                <div className="text-base">
+                  {leaveBalance.total} {leaveBalance.countType}{" "}
+                  {repitationTypeObj[leaveBalance.repetitionType]}
+                </div>
+              </div>
+              <div className="w-4/12 p-2">
+                <div className="text-xs">Carry forward</div>
+                <div className="text-base">
+                  {leaveBalance.carryforwardedCount} days
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
       <Divider />
       <div className="flex w-full flex-row py-6">
-        <div className="w-3/12 p-2 text-sm">Holidays</div>
-        <div className="flex w-9/12 flex-row">
-          <div className="w-1/2 p-2">
-            <div className="text-xs">Date</div>
-            <div className="text-base">01.01.2022</div>
+        <div className="w-2/12 p-2 text-sm">
+          <div>Holidays</div>
+          <div>
+            <button
+              className="mt-2 rounded border border-miru-han-purple-1000 px-1 text-xs text-miru-han-purple-1000"
+              onClick={toggleCalendarModal}
+            >
+              View Calendar
+            </button>
           </div>
-          <div className="w-1/2 p-2">
-            <div className="text-xs">Name</div>
-            <div className="text-base">Independence day</div>
-          </div>
+        </div>
+        <div className="flex w-10/12 flex-col">
+          {holidaysList.map((holiday, index) => (
+            <div className="w-fullflex-row flex" key={index}>
+              <div className="w-1/2 p-2">
+                <div className="text-xs">Date</div>
+                <div className="text-base">{holiday.date}</div>
+              </div>
+              <div className="w-1/2 p-2">
+                <div className="text-xs">Name</div>
+                <div className="text-base">{holiday.name}</div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
       <Divider />
       <div className="flex w-full flex-row py-6">
-        <div className="w-3/12 p-2 text-sm">Optional Holidays</div>
-        <div className="w-9/12">
+        <div className="w-2/12 p-2 text-sm">
+          <div>Optional</div>
+          <div>Holidays</div>
+          <div>
+            <button
+              className="mt-2 rounded border border-miru-han-purple-1000 px-1 text-xs text-miru-han-purple-1000"
+              onClick={toggleCalendarModal}
+            >
+              View Calendar
+            </button>
+          </div>
+        </div>
+        <div className="flex w-10/12 flex-col">
           <div className="p-2">
             <div className="text-xs">Total optional holidays</div>
-            <div className="text-base">1 per quarter</div>
-          </div>
-          <div className="flex w-full flex-row">
-            <div className="w-1/2 p-2">
-              <div className="text-xs">Date</div>
-              <div className="text-base">01.01.2022</div>
-            </div>
-            <div className="w-1/2 p-2">
-              <div className="text-xs">Name</div>
-              <div className="text-base">Independence day</div>
+            <div className="text-base">
+              {totalOptionalHolidays}{" "}
+              {repitationTypeObj[optionalRepetitionType]}
             </div>
           </div>
+          {optionalHolidayList.map((holiday, index) => (
+            <div className="flex w-full flex-row" key={index}>
+              <div className="w-1/2 p-2">
+                <div className="text-xs">Date</div>
+                <div className="text-base">{holiday.date}</div>
+              </div>
+              <div className="w-1/2 p-2">
+                <div className="text-xs">Name</div>
+                <div className="text-base">{holiday.name}</div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-
-      <div
-        className="modal__modal main-modal"
-        style={{ background: "rgba(29, 26, 49,0.6)" }}
-      >
-        <div className="modal__container modal-container overflow-y-auto; z-50 mx-auto w-full max-w-4xl rounded-xl bg-white shadow-lg">
-          <div className="modal__content modal-content max-h-90v overflow-y-auto">
-            <div className="modal__position">
-              <h6 className="modal__title"> Add New Project </h6>
-              <div className="modal__close">
-                {/* <button
-                  className="modal__button"
-                  onClick={() => {
-                    setShowProjectModal(false);
-                  }}
-                >
-                  <XIcon color="#CDD6DF" size={15} />
-                </button> */}
-              </div>
-            </div>
-            <div className="modal__form flex-col">
-              <button
-                className="text-miru-han-purple-1000"
-                // onClick={() => setIsSending(!isSending)}
-                data-bs-placement="right"
-                data-bs-toggle="tooltip"
-                title="Tooltip on right"
-              >
-                Hello
-              </button>
-
-              <div className="flex flex-row justify-around">
-                <div key={1}>
-                  <Calendar
-                    showNavigation={false}
-                    // tileClassName={ }
-                    className={"react-calendar-month-picker relative max-h-64"}
-                    tileContent={tileContent}
-                    tileClassName={({ date }) => {
-                      //eslint-disable-line
-                      if (
-                        mark.find(
-                          x => x === new Date(date).toJSON().slice(0, 10)
-                        )
-                      ) {
-                        return "highlight";
-                      }
-                      // else {
-                      //   // console.log(date)
-                      //   return "date-calendar"
-                      // }
-                    }}
-                    value={new Date(2023, 0, 1)}
-                  />
-                </div>
-                {/* <div key={2}>
-                  <Calendar
-                    showNavigation={false}
-                    value={new Date(2023, 1, 1)}
-                    className={"react-calendar-month-picker max-h-64"}
-                  // tileClassName={ }
-                  />
-                </div>
-                <div key={3}>
-                  <Calendar
-                    showNavigation={false}
-                    value={new Date(2023, 2, 1)}
-                    className={"react-calendar-month-picker max-h-64"}
-                  // tileClassName={ }
-                  />
-                </div> */}
-              </div>
-              {/* <div className="flex flex-row justify-around">
-                <div key={4}>
-                  <Calendar
-                    showNavigation={false}
-                    value={new Date(2023, 3, 1)}
-                    className={"react-calendar-month-picker max-h-64"}
-                  // tileClassName={ }
-                  />
-                </div>
-                <div key={5}>
-                  <Calendar
-                    showNavigation={false}
-                    value={new Date(2023, 4, 1)}
-                    className={"react-calendar-month-picker max-h-64"}
-                  // tileClassName={ }
-                  />
-                </div>
-                <div key={6}>
-                  <Calendar
-                    showNavigation={false}
-                    value={new Date(2023, 5, 1)}
-                    className={"react-calendar-month-picker max-h-64"}
-                  // tileClassName={ }
-                  />
-                </div>
-              </div>
-              <div className="flex flex-row justify-around">
-                <div key={7}>
-                  <Calendar
-                    showNavigation={false}
-                    value={new Date(2023, 6, 1)}
-                    className={"react-calendar-month-picker max-h-64"}
-                  // tileClassName={ }
-                  />
-                </div>
-                <div key={8}>
-                  <Calendar
-                    showNavigation={false}
-                    value={new Date(2023, 7, 1)}
-                    className={"react-calendar-month-picker max-h-64"}
-                  // tileClassName={ }
-                  />
-                </div>
-                <div key={9}>
-                  <Calendar
-                    showNavigation={false}
-                    value={new Date(2023, 8, 1)}
-                    className={"react-calendar-month-picker max-h-64"}
-                  // tileClassName={ }
-                  />
-                </div>
-              </div>
-              <div className="flex flex-row justify-around">
-                <div key={10}>
-                  <Calendar
-                    showNavigation={false}
-                    value={new Date(2023, 9, 1)}
-                    className={"react-calendar-month-picker max-h-64"}
-                  // tileClassName={ }
-                  />
-                </div>
-                <div key={11}>
-                  <Calendar
-                    showNavigation={false}
-                    value={new Date(2023, 10, 1)}
-                    className={"react-calendar-month-picker max-h-64"}
-                  // tileClassName={ }
-                  />
-                </div>
-                <div key={12}>
-                  <Calendar
-                    showNavigation={false}
-                    value={new Date(2023, 11, 1)}
-                    className={"react-calendar-month-picker max-h-64"}
-                  // tileClassName={ }
-                  />
-                </div>
-              </div> */}
-            </div>
-          </div>
-        </div>
-      </div>
+      {showCalendar && (
+        <HolidayModal
+          tileContent={tileContent}
+          toggleCalendarModal={toggleCalendarModal}
+        />
+      )}
     </div>
   );
 };
