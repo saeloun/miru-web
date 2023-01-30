513b8e57fcad7e613d05518e580669c2db37326f
Shalaka Patil <shalu214patil@gmail.com>
2023-01-30 09:59:46 +0530

    Fix expense create API

---
+++ app/presenters/expense_presenter.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class ExpensePresenter
+  attr_reader :expense
+
+  def initialize(expense)
+    @expense = expense
+  end
+
+  def snippet
+    {
+      id: expense.id,
+      vendor_name: expense.vendor&.name,
+      category_name: expense.expense_category.name,
+      amount: expense.amount,
+      date: expense.date,
+      description: expense.description,
+      type: expense.expense_type
+    }
+  end
+end
