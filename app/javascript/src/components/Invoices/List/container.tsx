a73e13fc5212d2a20ca338cc92020b76b8bd201c
Shruti Apte <shrutiapte20@gmail.com>
2023-02-01 12:59:23 +0530

    empty state added for invoice list page

--- app/javascript/src/components/Invoices/List/container.tsx
+++ app/javascript/src/components/Invoices/List/container.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import BulkActionsWrapper from "./BulkActionsWrapper";
+import EmptyState from "./EmptyState";
 import RecentlyUpdated from "./RecentlyUpdated";
 import Table from "./Table";
 
@@ -67,7 +68,7 @@ const Container = ({
       />
     </div>
   ) : (
-    <div>No invoices to show</div>
+    <EmptyState isDesktop={isDesktop} />
   );
 
 export default Container;
