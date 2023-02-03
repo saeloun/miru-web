dc2b01e7579efc809853b9a9f44fa1b100f2e813
onkar <onkar@saeloun.com>
2023-02-03 18:40:26 +0530

    added mapper

---
+++ app/javascript/src/mapper/teams.mapper.ts
@@ -0,0 +1,18 @@
+export const teamsMapper = (user, address) => ({
+  first_name: user.first_name,
+  last_name: user.last_name,
+  date_of_birth: user.date_of_birth,
+  phone_number: user.phone,
+  email_id: user.personal_email_id,
+  addresses: {
+    address_type: address?.address_type,
+    address_line_1: address?.address_line_1,
+    address_line_2: address?.address_line_2,
+    country: address?.country,
+    state: address?.state,
+    city: address?.city,
+    pin: address?.pin,
+  },
+  linkedin: user.social_accounts.linkedin_url,
+  github: user.social_accounts.github_url,
+});
