55302c0e591b55c0ea4316ecce7949ba6d0540d5
Vipul Amler <vipul@saeloun.com>
2023-02-03 18:46:37 +0530

    Fix Seeds

--- db/seeds/01_company.rb
+++ db/seeds/01_company.rb
@@ -1,43 +1,18 @@
 # frozen_string_literal: true
 
-# Company Create Start
-companies = [
+company = Company.create!(
   {
-    name: "Saeloun India Pvt. Ltd",
-    address: "somewhere in India",
-    business_phone: "+91 0000000000",
-    base_currency: "INR",
-    standard_price: 100000,
-    fiscal_year_end: "apr-mar",
-    date_format: "DD-MM-YYYY",
-    country: "IN",
-    timezone: "Asia - Kolkata"
-  },
-
-  {
-    name: "Saeloun USA INC.",
-    address: "somewhere in USA",
-    business_phone: "+1 111111111",
+    name: "Saeloun Inc",
+    address: "475 Clermont Ave, Brooklyn, NY-12238",
+    business_phone: "+1 9296207865",
     base_currency: "USD",
     standard_price: 1000,
     fiscal_year_end: "jan-dec",
-    date_format: "YYYY-MM-DD",
+    date_format: "MM-DD-YYYY",
     country: "US",
     timezone: "America - New York"
-  }
-]
-
-@companies = companies.map { |company| Company.create!(company) }
+  })
 
 puts "Company Created"
 
-@saeloun_india, @saeloun_us = ["Saeloun India Pvt. Ltd", "Saeloun USA INC."].map { |company|
-  Company.find_by(name: company)
-}
-
-[@saeloun_india, @saeloun_us].each do |company|
-  company.logo.attach(
-    io: File.open(Rails.root.join("app/assets/images/saeloun_logo.png")),
-    filename: "saeloun_logo.png")
-end
-# Company Create End
+company.logo.attach(io: File.open(Rails.root.join("app/assets/images/saeloun_logo.png")), filename: "saeloun_logo.png")
