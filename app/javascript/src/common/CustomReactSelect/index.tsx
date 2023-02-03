47fdca87049d98ae143a7fd249dda467ad6eee23
onkar <onkar@saeloun.com>
2023-02-03 17:43:10 +0530

    updated custom react select component for error state designs

--- app/javascript/src/common/CustomReactSelect/index.tsx
+++ app/javascript/src/common/CustomReactSelect/index.tsx
@@ -4,40 +4,8 @@ import React from "react";
 
 import Select from "react-select";
 
+import { customErrStyles, customStyles } from "./CustomStyle";
 import { CustomValueContainer } from "./CustomValueContainer";
-
-const customStyles = {
-  control: provided => ({
-    ...provided,
-    backgroundColor: "#FFFFFF",
-    color: "red",
-    minHeight: 48,
-    padding: "0",
-    border: "1px solid #CDD6DF",
-    "border-color": "#CDD6DF",
-  }),
-  menu: provided => ({
-    ...provided,
-    fontSize: "12px",
-    letterSpacing: "2px",
-  }),
-  valueContainer: provided => ({
-    ...provided,
-    overflow: "visible",
-  }),
-  placeholder: base => ({
-    ...base,
-    position: "absolute",
-    top: "-40%",
-    transition: "top 0.2s, font-size 0.2s",
-    fontSize: 9,
-    backgroundColor: "#FFFFFF",
-  }),
-  dropdownIndicator: base => ({
-    ...base,
-    color: "#5B34EA", // Custom colour
-  }),
-};
 
 export const CustomReactSelect = ({
   classNamePrefix,
@@ -46,6 +14,7 @@ export const CustomReactSelect = ({
   handleOnChange,
   name,
   value,
+  isErr,
 }) => (
   <div className="outline relative">
     <Select
@@ -53,7 +22,7 @@ export const CustomReactSelect = ({
       name={name}
       options={options}
       placeholder={label}
-      styles={customStyles}
+      styles={isErr ? customErrStyles : customStyles}
       value={value}
       components={{
         ValueContainer: CustomValueContainer,
@@ -67,6 +36,7 @@ export const CustomReactSelect = ({
 CustomReactSelect.defaultProps = {
   classNamePrefix: "react-select-filter",
   label: "Select",
-  placeholder: "Date Format",
+  placeholder: "Please select...",
   handleOnChange: () => {}, // eslint-disable-line  @typescript-eslint/no-empty-function
+  isErr: false,
 };
