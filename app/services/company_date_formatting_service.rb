c16ec2b264a0f75acb4d54bbe96b6027e77fa8b3
Vipul A M <vipulnsward@gmail.com>
2023-02-08 02:55:26 +0530

    Use date formats for display from Company date formats settings (#1008)

---
+++ app/services/company_date_formatting_service.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class CompanyDateFormattingService
+  attr_reader :date, :company
+
+  def initialize(date, company: nil)
+    @date = date
+    @company = company
+  end
+
+  def process
+    return if !company
+
+    date.strftime(company_date_format)
+  end
+
+  private
+
+    def company_date_format
+      case company.date_format
+      when "DD-MM-YYYY"
+        "%d.%m.%Y"
+      when "YYYY-MM-DD"
+        "%Y.%m.%d"
+      else
+        "%m.%d.%Y"
+      end
+    end
+end
