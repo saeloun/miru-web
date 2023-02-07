b946e06d1b5bcee915131a4ab47c22eabe845a22
onkar <onkar@saeloun.com>
2023-02-07 16:08:33 +0530

    updated custom styling

--- app/javascript/src/common/CustomReactSelect/CustomStyle.ts
+++ app/javascript/src/common/CustomReactSelect/CustomStyle.ts
@@ -1,12 +1,19 @@
+let focusedState = false;
+
 export const customStyles = {
-  control: provided => ({
-    ...provided,
-    backgroundColor: "#FFFFFF",
-    minHeight: 48,
-    padding: "0",
-    border: "1px solid #CDD6DF",
-    "border-color": "#CDD6DF",
-  }),
+  control: (provided, state) => {
+    focusedState = state.isFocused;
+
+    return {
+      ...provided,
+      backgroundColor: "#FFFFFF",
+      minHeight: 48,
+      padding: "0",
+      border: "1px solid #CDD6DF",
+      "border-color": "#CDD6DF",
+      fontWight: 500,
+    };
+  },
   menu: provided => ({
     ...provided,
     fontSize: "12px",
@@ -16,17 +23,37 @@ export const customStyles = {
     ...provided,
     overflow: "visible",
   }),
-  placeholder: base => ({
-    ...base,
+  placeholder: (baseStyles, state) => ({
+    ...baseStyles,
     position: "absolute",
-    top: "-40%",
-    transition: "top 0.2s, font-size 0.2s",
-    fontSize: 9,
+    top:
+      focusedState ||
+      state.selectProps.inputValue ||
+      (state.selectProps.value && state.selectProps.value?.value !== "")
+        ? "-40%"
+        : "20%",
+    transition: "top 0.5s",
+    fontSize:
+      focusedState ||
+      state.selectProps.inputValue ||
+      (state.selectProps.value && state.selectProps.value?.value !== "")
+        ? 9
+        : 16,
     backgroundColor: "#FFFFFF",
   }),
   dropdownIndicator: base => ({
     ...base,
     color: "#5B34EA", // Custom colour
+  }),
+  input: base => ({
+    ...base,
+    '[type="text"]': {
+      fontWeight: "500 !important",
+    },
+  }),
+  singleValue: base => ({
+    ...base,
+    fontWeight: 500,
   }),
 };
 
