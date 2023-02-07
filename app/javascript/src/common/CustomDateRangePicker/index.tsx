55f21781acbcd74f954e690d8d53f143daa33d24
sanjibroy360 <sanjibroy0098@gmail.com>
2023-02-07 15:50:57 +0530

    Ensure that user should be able to enter date range inputs

--- app/javascript/src/common/CustomDateRangePicker/index.tsx
+++ app/javascript/src/common/CustomDateRangePicker/index.tsx
@@ -61,6 +61,8 @@ const CustomDateRangePicker = ({
     if (isValidDate) {
       handleSelectDate(dateInput);
       resetErrors(fieldName);
+    } else {
+      handleSelectDate("");
     }
   };
 
