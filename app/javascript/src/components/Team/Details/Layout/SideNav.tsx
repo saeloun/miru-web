444e9939cc3acec6cd3d6a6626f4e9bd9aedd272
onkar <onkar@saeloun.com>
2023-02-07 16:11:07 +0530

    updated tooltip styling

--- app/javascript/src/components/Team/Details/Layout/SideNav.tsx
+++ app/javascript/src/components/Team/Details/Layout/SideNav.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { UserAvatarSVG } from "miruIcons";
 import { NavLink, useParams } from "react-router-dom";
+import { Tooltip } from "StyledComponents";
 
 import { useTeamDetails } from "context/TeamDetailsContext";
 
@@ -55,9 +56,16 @@ const UserInformation = () => {
           </div>
         </div>
         <div className="mt-3 flex flex-col items-center justify-center border-b-8 border-miru-gray-200 pb-8">
-          <span className="mb-1 max-w-full overflow-hidden truncate whitespace-nowrap px-4 text-xl font-bold text-miru-han-purple-1000">
-            {`${personalDetails.first_name} ${personalDetails.last_name}`}
-          </span>
+          <Tooltip
+            content={`${personalDetails.first_name} ${personalDetails.last_name}`}
+            wrapperClassName="relative block max-w-full "
+          >
+            <div className="mb-1 max-w-full overflow-hidden truncate whitespace-nowrap px-4">
+              <span className=" text-xl font-bold text-miru-han-purple-1000">
+                {`${personalDetails.first_name} ${personalDetails.last_name}`}
+              </span>
+            </div>
+          </Tooltip>
           <span className="text-xs leading-4 tracking-wider text-miru-dark-purple-1000">
             {/* SENIOR SOFTWARE DEVELOPER */}
           </span>
