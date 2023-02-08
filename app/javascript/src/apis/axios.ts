de113f56dbd18f4cbb086905ca8af7522542ff14
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2023-02-08 17:13:50 +0530

    Added default axios headers

--- app/javascript/src/apis/axios.ts
+++ app/javascript/src/apis/axios.ts
@@ -5,20 +5,38 @@ import Toastr from "common/Toastr";
 
 axios.defaults.baseURL = "/internal_api/v1";
 
-interface CommonHeaderProperties extends HeadersDefaults {
-  Accept: string;
-  "Content-Type": string;
-  "X-CSRF-TOKEN": string;
-}
+// interface CommonHeaderProperties extends HeadersDefaults {
+//   Accept: string;
+//   "Content-Type": string;
+//   "X-CSRF-TOKEN": string;
+//   "X-Auth-Email": string;
+//   "X-Auth-Token": string;
+// }
+
+// const setAuthHeaders = () => {
+//   axios.defaults.headers = {
+//     Accept: "application/json",
+//     "Content-Type": "application/json",
+//     "X-CSRF-TOKEN": document
+//       .querySelector('[name="csrf-token"]')
+//       .getAttribute("content"),
+//     "X-Auth-Email": "supriya@example.com",
+//     "X-Auth-Token": "KQhEfwe7rDPcxUaxs8Bo8d1yVeGYJE5v8R5KbhRWjJJr2jnfon"
+//   } as CommonHeaderProperties;
+// };
 
 const setAuthHeaders = () => {
   axios.defaults.headers = {
+    // @ts-ignore
     Accept: "application/json",
     "Content-Type": "application/json",
     "X-CSRF-TOKEN": document
       .querySelector('[name="csrf-token"]')
       .getAttribute("content"),
-  } as CommonHeaderProperties;
+    "X-Auth-Email": "supriya@example.com",
+    "X-Auth-Token": "KQhEfwe7rDPcxUaxs8Bo8d1yVeGYJE5v8R5KbhRWjJJr2jnfon",
+  };
+  console.log("hello", axios.defaults.headers)
 };
 
 const handleSuccessResponse = response => {
