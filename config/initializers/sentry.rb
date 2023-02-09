ef43e64a9ed570a3ac3a8bfc54cb3e872ae9de24
Vipul Amler <vipul@saeloun.com>
2023-02-09 16:42:22 +0530

    Init Sentry

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+Sentry.init do |config|
+  config.dsn = "https://c4c00a5cc17647ac83f27f48af7d5e4f@o744686.ingest.sentry.io/4504649746087936"
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+end
