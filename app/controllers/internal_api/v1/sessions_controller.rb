af6b61d0d5a283509e0f9816d9a712cbf98908bc
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2023-02-11 01:22:51 +0530

    Added custom sessions controller to handle all the requests

--- app/controllers/internal_api/v1/sessions_controller.rb
+++ app/controllers/internal_api/v1/sessions_controller.rb
@@ -1,19 +1,38 @@
 # frozen_string_literal: true
 
-class InternalApi::V1::SessionsController < Devise::SessionsController
-  respond_to :json
+class InternalApi::V1::SessionsController < InternalApi::V1::ApplicationController
+  skip_before_action :authenticate_user_using_x_auth_token
+  skip_before_action :authenticate_user!
+  skip_after_action :verify_authorized
+
+  def create
+    user = User.find_for_database_authentication(email: user_params[:email])
+    if invalid_password?(user)
+      render json: { error: "Invalid email or password" }, status: :unprocessable_entity
+    end
+
+    if user.confirmed?
+      sign_in(user)
+      render json: { notice: "Signed in successfully", user: }, status: :ok
+    end
+
+    unless user.confirmed?
+      render json: { notice: "Please confirm your email address to continue", user: }, status: :ok
+    end
+  end
+
+  def destroy
+    sign_out(@user)
+    reset_session
+  end
 
   private
 
-    def respond_with(user, _opts = {})
-      if user.persisted?
-        if user.has_role?(:owner) && user.companies.empty?
-          render json: { notice: "Signed in successfully", user:, redirect_route: new_company_path }
-        elsif user.has_role?(:book_keeper, current_company)
-          render json: { notice: "Signed in successfully", user:, redirect_route: root_path + "payments" }
-        else
-          render json: { notice: "Signed in successfully", user:, redirect_route: root_path + "time-tracking" }
-        end
-      end
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
+
+    def invalid_password?(user)
+      user.blank? || !user.valid_password?(user_params[:password])
     end
 end
