190c767308283c3e6593d73c9f2b6daf6b246674
Vipul Amler <vipul@saeloun.com>
2023-02-13 03:07:52 +0530

    x

--- spec/feature/users/login_spec.rb
+++ spec/feature/users/login_spec.rb
@@ -3,21 +3,26 @@
 require "rails_helper"
 
 describe "User Sign In", type: :feature do
-  before do
-    let(:user) { create(:user, password: "password", password_confirmation: "password") }
-  end
+  let(:company) { create(:company) }
+  let(:user) { create(:user, current_workspace_id: company.id, password: "testing!") }
 
-  context "when using valid credentials" do
-    it "logs user in" do
-      visit "/users/sign_in"
-      within("#new_user") do
-        fill_in "Email", with: user.email
-        fill_in "Password", with: "password"
+  context "when user is an admin, owner, employee" do
+    before do
+      create(:employment, company:, user:)
+    end
+
+    context "when using valid credentials" do
+      it "logs user in" do
+        visit "/users/sign_in"
+        within("#new_user") do
+          fill_in "Email", with: user.email
+          fill_in "Password", with: "testing!"
+        end
+
+        click_button "SIGN IN"
+
+        expect(page).to have_content "Success"
       end
-
-      click_button "SIGN IN"
-
-      expect(page).to have_content "Success"
     end
   end
 end
