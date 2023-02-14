83684cf19e92479b066944910ad33d968a019a01
onkar <onkar@saeloun.com>
2023-02-14 10:24:59 +0530

    added conditional components

--- app/javascript/src/components/Team/Details/PersonalDetails/index.tsx
+++ app/javascript/src/components/Team/Details/PersonalDetails/index.tsx
@@ -4,13 +4,17 @@ import { Outlet, useParams, useNavigate } from "react-router-dom";
 
 import teamsApi from "apis/teams";
 import Loader from "common/Loader/index";
+import { MobileEditHeader } from "common/Mobile/MobileEditHeader";
 import { useTeamDetails } from "context/TeamDetailsContext";
+import { useUserContext } from "context/UserContext";
 import { teamsMapper } from "mapper/teams.mapper";
 
+import MobilePersonalDetails from "./MobilePersonalDetails";
 import StaticPage from "./StaticPage";
 
 const PersonalDetails = () => {
   const { memberId } = useParams();
+  const { isDesktop } = useUserContext();
   const {
     updateDetails,
     details: { personalDetails },
@@ -33,22 +37,44 @@ const PersonalDetails = () => {
 
   return (
     <Fragment>
-      <div className="flex items-center justify-between bg-miru-han-purple-1000 px-10 py-4">
-        <h1 className="text-2xl font-bold text-white">Personal Details</h1>
-        <button
-          className="cursor-pointer rounded-md border border-white bg-miru-han-purple-1000 px-6 py-2 font-bold text-white"
-          data-cy="update-profile"
-          onClick={() => navigate(`/team/${memberId}/edit`, { replace: true })}
-        >
-          Edit
-        </button>
-      </div>
-      {isLoading ? (
-        <div className="flex min-h-70v items-center justify-center">
-          <Loader />
-        </div>
-      ) : (
-        <StaticPage personalDetails={personalDetails} />
+      {isDesktop && (
+        <Fragment>
+          <div className="flex items-center justify-between bg-miru-han-purple-1000 px-10 py-4">
+            <h1 className="text-2xl font-bold text-white">Personal Details</h1>
+            <button
+              className="cursor-pointer rounded-md border border-white bg-miru-han-purple-1000 px-6 py-2 font-bold text-white"
+              data-cy="update-profile"
+              onClick={() =>
+                navigate(`/team/${memberId}/edit`, { replace: true })
+              }
+            >
+              Edit
+            </button>
+          </div>
+          {isLoading ? (
+            <div className="flex min-h-70v items-center justify-center">
+              <Loader />
+            </div>
+          ) : (
+            <StaticPage personalDetails={personalDetails} />
+          )}
+        </Fragment>
+      )}
+      {!isDesktop && (
+        <Fragment>
+          <MobileEditHeader
+            backHref={`/team/${memberId}/options`}
+            href={`/team/${memberId}/edit`}
+            title="Personal Details"
+          />
+          {isLoading ? (
+            <div className="flex min-h-70v items-center justify-center">
+              <Loader />
+            </div>
+          ) : (
+            <MobilePersonalDetails personalDetails={personalDetails} />
+          )}
+        </Fragment>
       )}
       <Outlet />
     </Fragment>
