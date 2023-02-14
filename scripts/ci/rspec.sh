d922e36eafb120f8c2cad57eb9910817d4044706
Gowsik <gowsik@saeloun.com>
2023-02-14 12:37:47 +0530

    Updated the rspec sh file

--- scripts/ci/rspec.sh
+++ scripts/ci/rspec.sh
@@ -5,7 +5,6 @@ set -eu
 export RAILS_ENV=test
 bundle install --with test
 bin/yarn install
-bin/rails db:environment:set RAILS_ENV=test
 rake db:create
 rake db:migrate
 bundle exec rspec --color spec
