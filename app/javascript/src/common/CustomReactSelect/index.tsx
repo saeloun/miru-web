bc871b855cccd9c3907a2c8aaa1cd068d086014a
onkar <onkar@saeloun.com>
2023-02-14 09:29:26 +0530

    added getstyle method

--- app/javascript/src/common/CustomReactSelect/index.tsx
+++ app/javascript/src/common/CustomReactSelect/index.tsx
@@ -15,23 +15,34 @@ export const CustomReactSelect = ({
   name,
   value,
   isErr,
-}) => (
-  <div className="outline relative">
-    <Select
-      classNamePrefix={classNamePrefix}
-      name={name}
-      options={options}
-      placeholder={label}
-      styles={isErr ? customErrStyles : customStyles}
-      value={value}
-      components={{
-        ValueContainer: CustomValueContainer,
-        IndicatorSeparator: () => null,
-      }}
-      onChange={handleOnChange}
-    />
-  </div>
-);
+  isDesktopView,
+}) => {
+  const getStyle = () => {
+    if (isErr) {
+      return customErrStyles(isDesktopView);
+    }
+
+    return customStyles(isDesktopView);
+  };
+
+  return (
+    <div className="outline relative">
+      <Select
+        classNamePrefix={classNamePrefix}
+        name={name}
+        options={options}
+        placeholder={label}
+        styles={getStyle()}
+        value={value}
+        components={{
+          ValueContainer: CustomValueContainer,
+          IndicatorSeparator: () => null,
+        }}
+        onChange={handleOnChange}
+      />
+    </div>
+  );
+};
 
 CustomReactSelect.defaultProps = {
   classNamePrefix: "react-select-filter",
@@ -39,4 +50,5 @@ CustomReactSelect.defaultProps = {
   placeholder: "Please select...",
   handleOnChange: () => {}, // eslint-disable-line  @typescript-eslint/no-empty-function
   isErr: false,
+  isDesktopView: true,
 };
