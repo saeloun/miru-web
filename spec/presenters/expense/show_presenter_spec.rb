f1407898a3b1e13b4438e82a2e01152fba1624b5
Shalaka Patil <shalu214patil@gmail.com>
2023-02-15 11:43:05 +0530

    Add spec for expense show presenter

---
+++ spec/presenters/expense/show_presenter_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Expense::ShowPresenter do
+  let(:company) { create :company }
+  let(:expense_category) { create(:expense_category, company:) }
+  let(:vendor) { create(:vendor, company:) }
+  let(:expense) { create(:expense, :with_receipts, company:, expense_category:, vendor:) }
+
+  describe "show_data" do
+    before do
+      @data = Expense::ShowPresenter.new(expense).process
+    end
+
+    it "returns required data of expense" do
+      expect(@data).to have_key(:id)
+      expect(@data).to have_key(:vendor_name)
+      expect(@data).to have_key(:category_name)
+      expect(@data).to have_key(:amount)
+      expect(@data).to have_key(:date)
+      expect(@data).to have_key(:description)
+      expect(@data).to have_key(:type)
+      expect(@data).to have_key(:receipts)
+    end
+  end
+end
