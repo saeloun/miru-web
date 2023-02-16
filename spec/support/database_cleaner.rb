ce6a621e29b1e766cd4cc7ae017eb19a297772b7
Gowsik <gowsik@saeloun.com>
2023-02-16 16:23:16 +0530

    Updated database cleaner and rubocop yml

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -13,16 +13,10 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :system) do
-    # :rack_test driver's Rack app under test shares database connection
-    # with the specs, so continue to use transaction strategy for speed.
-    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
-
-    unless driver_shares_db_connection_with_specs
-      # Driver is probably for an external browser with an app
-      # under test that does *not* share a database connection with the
-      # specs, so use truncation strategy.
-      DatabaseCleaner.strategy = :truncation
-    end
+    # Driver is probably for an external browser with an app
+    # under test that does *not* share a database connection with the
+    # specs, so use truncation strategy.
+    DatabaseCleaner.strategy = :truncation
   end
 
   config.before do
