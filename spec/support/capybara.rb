9ff147fdf1257b2f1c4425e2b79d3b60f5a9b9ca
Gowsik <gowsik@saeloun.com>
2023-02-16 12:01:42 +0530

    Add capybara support file

---
+++ spec/support/capybara.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "capybara/rails"
+require "capybara/rspec"
+
+Capybara.server = :puma
+
+if ENV["CI"].present?
+  Capybara.register_driver :chrome_headless do |app|
+    options = Selenium::WebDriver::Chrome::Options.new(args: %w[headless window-size=1400,1000])
+
+    if ENV["HUB_URL"]
+      Capybara::Selenium::Driver.new(
+        app,
+        browser: :remote,
+        url: ENV["HUB_URL"],
+        capabilities: options)
+    end
+  end
+
+  Capybara.default_driver = :chrome_headless
+  Capybara.javascript_driver = :chrome_headless
+else
+  Capybara.default_driver = :selenium_chrome
+  Capybara.javascript_driver = :selenium_chrome
+end
