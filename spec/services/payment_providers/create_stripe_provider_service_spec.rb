c810aaca08f447c7d10966c93c2e8c912530af43
Vipul A M <vipulnsward@gmail.com>
2023-02-21 23:45:24 +0530

    x

--- spec/services/payment_providers/create_stripe_provider_service_spec.rb
+++ spec/services/payment_providers/create_stripe_provider_service_spec.rb
@@ -1,23 +1,20 @@
 # frozen_string_literal: true
 
 require "rails_helper"
-require "zip"
 
 RSpec.describe PaymentProviders::CreateStripeProviderService do
   let(:company) { create(:company) }
 
   describe "#process" do
     context "when stripe account is connected" do
-      before do
-        # While running specs, we don't want url and to access url, we need to set ActiveStorage url
-        allow_any_instance_of(Company).to receive(:stripe_connected_account).and_return({ a: 1 })
+      it "payment provide is created" do
+        stripe_connected_account = StripeConnectedAccount.new
+        allow(stripe_connected_account).to receive(:details_submitted).and_return(true)
+
+        allow_any_instance_of(Company).to receive(:stripe_connected_account).and_return(stripe_connected_account)
         allow_any_instance_of(StripeConnectedAccount).to receive(:details_submitted).and_return(true)
 
-        described_class.new(company).process
-      end
-
-      it "checks if" do
-        expect(PaymentsProvider.count).to eq(1)
+        expect { described_class.new(company).process }.to change(PaymentsProvider, :count).by(1)
       end
     end
   end
