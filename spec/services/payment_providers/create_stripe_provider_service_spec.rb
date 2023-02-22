4a4002d854be11f98d0ce3b770062b1ff7d2fd92
Shalaka Patil <shalu214patil@gmail.com>
2023-02-22 09:24:51 +0530

    Add more spec

--- spec/services/payment_providers/create_stripe_provider_service_spec.rb
+++ spec/services/payment_providers/create_stripe_provider_service_spec.rb
@@ -11,9 +11,16 @@ RSpec.describe PaymentProviders::CreateStripeProviderService do
         stripe_connected_account = StripeConnectedAccount.new
         allow(stripe_connected_account).to receive(:details_submitted).and_return(true)
         allow_any_instance_of(Company).to receive(:stripe_connected_account).and_return(stripe_connected_account)
-        allow_any_instance_of(StripeConnectedAccount).to receive(:details_submitted).and_return(true)
-
         expect { described_class.new(company).process }.to change(PaymentsProvider, :count).by(1)
+      end
+    end
+
+    context "when stripe account is not connected" do
+      it "payment provide is created" do
+        stripe_connected_account = StripeConnectedAccount.new
+        allow(stripe_connected_account).to receive(:details_submitted).and_return(true)
+        allow_any_instance_of(Company).to receive(:stripe_connected_account).and_return(nil)
+        expect { described_class.new(company).process }.to change(PaymentsProvider, :count).by(0)
       end
     end
   end
