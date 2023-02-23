4e37d2a6c5fac05545448e20266db3357d22da59
Onkar Hasabe <104751221+onkar-saeloun@users.noreply.github.com>
2023-02-23 11:43:07 +0530

    Time entry report filter new design (#1053)
    * created a component for custom date range picker with input
    
    * added client reports component
    
    * added date range filter
    
    * added time entry report common filter sidebar
    
    * added group by filter
    
    * added status filter
    
    * added team member filter
    
    * updated time entry filter wrapper
    
    * added filter sidebar options
    
    * added date options
    
    * Time entry filter mobile responsive design (#1060)
    
    * added custom date range custom date range with input
    
    * added mobile popup
    
    * added open date calendar function
    
    * added show calendar params
    
    * added handle component
    
    * resolved PR comments
    
    ---------
    
    Co-authored-by: Vipul A M <vipulnsward@gmail.com>

---
+++ app/javascript/src/components/Reports/Filters/DateRangeFilter.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+import { MinusIcon, PlusIcon } from "miruIcons";
+
+import CustomDateRangeWithInput from "common/CustomDateRangeWIthInput";
+import CustomRadioButton from "common/CustomRadio";
+
+const DateRangeFilter = ({
+  setIsClientOpen,
+  setIsStatusOpen,
+  setIsDateRangeOpen,
+  isDateRangeOpen,
+  filters,
+  dateRangeList,
+  showCustomFilter,
+  dateRange,
+  handleSelectDate,
+  selectedInput,
+  onClickInput,
+  submitCustomDatePicker,
+  handleSelectFilter,
+  wrapperRef,
+  showCustomCalendar,
+  handleOpenDateCalendar,
+  setShowCustomCalendar,
+}) => (
+  <div className="relative cursor-pointer border-b border-miru-gray-200 pb-5 pt-6 text-miru-dark-purple-1000 hover:text-miru-han-purple-1000">
+    <div
+      className="flex items-center justify-between px-5"
+      onClick={() => {
+        setIsStatusOpen(false);
+        setIsClientOpen(false);
+        setIsDateRangeOpen(!isDateRangeOpen);
+      }}
+    >
+      <h5 className="text-xs font-bold leading-4 tracking-wider">DATE RANGE</h5>
+      <div className="flex items-center">
+        {filters.dateRange.value != "all" && (
+          <span className="mr-7 flex h-5 w-5 items-center justify-center rounded-full bg-miru-han-purple-1000 text-xs font-semibold text-white">
+            {1}
+          </span>
+        )}
+        {isDateRangeOpen ? <MinusIcon size={16} /> : <PlusIcon size={16} />}
+      </div>
+    </div>
+    {isDateRangeOpen && (
+      <div className="lg:mt-7">
+        {dateRangeList.map(dateRange => (
+          <CustomRadioButton
+            classNameWrapper="px-5 py-2.5"
+            defaultCheck={dateRange.value == filters.dateRange.value}
+            groupName="dateRange"
+            id={dateRange.value}
+            key={dateRange.value}
+            label={dateRange.label}
+            value={dateRange.value}
+            handleOnChange={event =>
+              handleSelectFilter(dateRange, event.target)
+            }
+          />
+        ))}
+      </div>
+    )}
+    {isDateRangeOpen && showCustomFilter && (
+      <CustomDateRangeWithInput
+        dateRange={dateRange}
+        handleOpenDateCalendar={handleOpenDateCalendar}
+        handleSelectDate={handleSelectDate}
+        selectedInput={selectedInput}
+        setShowCustomCalendar={setShowCustomCalendar}
+        showCustomCalendar={showCustomCalendar}
+        submitCustomDatePicker={submitCustomDatePicker}
+        wrapperRef={wrapperRef}
+        onClickInput={onClickInput}
+      />
+    )}
+  </div>
+);
+
+export default DateRangeFilter;
