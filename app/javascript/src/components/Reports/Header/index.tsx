d1adc92351714e9a06256fcdb28dfa80467455ea
Onkar Hasabe <104751221+onkar-saeloun@users.noreply.github.com>
2023-02-23 11:43:07 +0530

    Time entry report filter new design (#1053)
    * created a component for custom date range picker with input
    
    * added client reports component
    
    * added date range filter
    
    * added time entry report common filter sidebar
    
    * added group by filter
    
    * added status filter
    
    * added team member filter
    
    * updated time entry filter wrapper
    
    * added filter sidebar options
    
    * added date options
    
    * Time entry filter mobile responsive design (#1060)
    
    * added custom date range custom date range with input
    
    * added mobile popup
    
    * added open date calendar function
    
    * added show calendar params
    
    * added handle component
    
    * resolved PR comments
    
    ---------
    
    Co-authored-by: Vipul A M <vipulnsward@gmail.com>

--- app/javascript/src/components/Reports/Header/index.tsx
+++ app/javascript/src/components/Reports/Header/index.tsx
@@ -106,7 +106,7 @@ const Header = ({
           {showMoreOptions && (
             <MobileMoreOptions setVisibilty={setShowMoreOptions}>
               <li
-                className="flex items-center p-2 text-miru-han-purple-1000"
+                className="flex items-center p-2 text-sm text-miru-han-purple-1000"
                 onClick={() => {
                   setIsFilterVisible(!isFilterVisible);
                 }}
@@ -114,9 +114,30 @@ const Header = ({
                 <FilterIcon className="mr-4" color="#7C5DEE" size={16} />{" "}
                 Filters
               </li>
-              {/* <li className="flex items-center p-2 text-miru-dark-purple-400">
-                <PaperPlaneTiltIcon className="mr-4" size={16} /> Share
-              </li> */}
+              <li>
+                <button
+                  className="menuButton__list-item pl-2"
+                  onClick={() => {
+                    setShowExportOptions(false);
+                    handleDownload("csv");
+                  }}
+                >
+                  <FileCsvIcon color="#5B34EA" size={16} weight="bold" />
+                  <span className="ml-3 text-sm">Export as CSV</span>
+                </button>
+              </li>
+              <li>
+                <button
+                  className="menuButton__list-item pl-2"
+                  onClick={() => {
+                    setShowExportOptions(false);
+                    handleDownload("pdf");
+                  }}
+                >
+                  <FilePdfIcon color="#5B34EA" size={16} weight="bold" />
+                  <span className="ml-3 text-sm">Export as PDF</span>
+                </button>
+              </li>
             </MobileMoreOptions>
           )}
         </div>
