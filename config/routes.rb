844a9c34e5486883159d2c5e9e131174e81a8b38
Vipul A M <vipulnsward@gmail.com>
2023-02-23 19:30:24 +0530

    Init GraphQL

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ class ActionDispatch::Routing::Mapper
 end
 
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
   devise_for :users, controllers: {
     registrations: "users/registrations",
     sessions: "users/sessions",
