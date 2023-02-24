63588925e4c103955bd167ced2a105ed5c0df507
Prasanth Chaduvula <chaduvulaprasanth1998@gmail.com>
2023-02-24 16:12:35 +0530

    Refactored Main component to handle all the client side view (#1080)
    * Added Dashboard component to handle all authenticated routes
    
    * Made Main.tsx aa main component to handle all public and private routing in future
    
    * Deleted Display view as all the logic handled by Main component
    
    * Fixed mobile responsive

--- app/javascript/src/components/Main.tsx
+++ app/javascript/src/components/Main.tsx
@@ -1,70 +1,10 @@
 import React from "react";
 
-import { Routes, Route, Outlet, Navigate } from "react-router-dom";
+import { Roles } from "constants/index";
 
-import ErrorPage from "common/Error";
-import { Roles, Paths } from "constants/index";
-import { ROUTES } from "constants/routes";
+import Dashboard from "./Dashboard";
 
-const RestrictedRoute = ({ user, role, authorisedRoles }) => {
-  if (!user) {
-    window.location.href = Paths.SIGN_IN;
-
-    return;
-  }
-
-  if (authorisedRoles.includes(role)) {
-    return <Outlet />;
-  }
-
-  const url =
-    role === Roles.BOOK_KEEPER
-      ? Paths.PAYMENTS
-      : role === Roles.OWNER
-      ? Paths.INVOICES
-      : Paths.TIME_TRACKING;
-
-  return <Navigate to={url} />;
-};
-
-const RootElement = ({ role }) => {
-  const url =
-    role === Roles.OWNER
-      ? Paths.INVOICES
-      : role === Roles.BOOK_KEEPER
-      ? Paths.PAYMENTS
-      : Paths.TIME_TRACKING;
-
-  return <Navigate to={url} />;
-};
-
-const Main: React.FC<Iprops> = props => (
-  <div className="h-full overflow-x-scroll p-0 font-manrope lg:absolute lg:top-0 lg:bottom-0 lg:right-0 lg:w-5/6 lg:px-20 lg:py-3">
-    <Routes>
-      <Route element={<RootElement role={props.companyRole} />} path="/" />
-      {ROUTES.map(parentRoute => (
-        <Route
-          key={parentRoute.path}
-          path={parentRoute.path}
-          element={
-            <RestrictedRoute
-              authorisedRoles={parentRoute.authorisedRoles}
-              role={props.companyRole}
-              user={props.user}
-            />
-          }
-        >
-          {parentRoute.subRoutes.map(({ path, Component }) => (
-            <Route element={<Component {...props} />} key={path} path={path} /> //TODO: Move user data to context
-          ))}
-        </Route>
-      ))}
-      <Route path={Paths.AUTHORIZATION} />
-      <Route element={<ErrorPage />} path="*" />
-    </Routes>
-  </div>
-);
-
+const Main = (props: Iprops) => <Dashboard {...props} />;
 interface Iprops {
   user: object;
   companyRole: Roles;
