8ed8b8a3f57110683318a8443b7cff1c51bfdb33
onkar <onkar@saeloun.com>
2023-02-24 08:40:29 +0530

    added custom styles. import

--- app/javascript/src/common/CustomAsyncSelect/index.tsx
+++ app/javascript/src/common/CustomAsyncSelect/index.tsx
@@ -4,9 +4,9 @@ import React from "react";
 
 import AsyncSelect from "react-select/async";
 
-import { customErrStyles } from "common/CustomReactSelect/CustomErrStyle";
-import { customStyles } from "common/CustomReactSelect/CustomStyle";
 import { CustomValueContainer } from "common/CustomReactSelect/CustomValueContainer";
+import { customErrStyles } from "common/CustomReactSelectErrStyles";
+import { customStyles } from "common/CustomReactSelectStyles";
 
 export const CustomAsyncSelect = ({
   classNamePrefix,
@@ -16,29 +16,41 @@ export const CustomAsyncSelect = ({
   handleOnChange,
   value,
   isErr,
-}) => (
-  <div className="outline relative">
-    <AsyncSelect
-      cacheOptions
-      defaultOptions
-      classNamePrefix={classNamePrefix}
-      loadOptions={loadOptions}
-      name={name}
-      placeholder={label}
-      styles={isErr ? customErrStyles : customStyles}
-      value={value || null}
-      components={{
-        ValueContainer: CustomValueContainer,
-        IndicatorSeparator: () => null,
-      }}
-      onChange={handleOnChange}
-    />
-  </div>
-);
+  isDesktopView,
+}) => {
+  const getStyle = () => {
+    if (isErr) {
+      return customErrStyles(isDesktopView);
+    }
+
+    return customStyles(isDesktopView);
+  };
+
+  return (
+    <div className="outline relative">
+      <AsyncSelect
+        cacheOptions
+        defaultOptions
+        classNamePrefix={classNamePrefix}
+        loadOptions={loadOptions}
+        name={name}
+        placeholder={label}
+        styles={getStyle()}
+        value={value || null}
+        components={{
+          ValueContainer: CustomValueContainer,
+          IndicatorSeparator: () => null,
+        }}
+        onChange={handleOnChange}
+      />
+    </div>
+  );
+};
 
 CustomAsyncSelect.defaultProps = {
   classNamePrefix: "react-select-filter",
   label: "Select",
   placeholder: "Date Format",
   handleOnChange: () => {}, // eslint-disable-line  @typescript-eslint/no-empty-function
+  isDesktopView: false,
 };
