68343507caa8747ea07c65ba0aa130e4644b086f
onkar <onkar@saeloun.com>
2023-02-24 08:41:52 +0530

    added common styles

---
+++ app/javascript/src/common/CustomReactSelectErrStyles/index.ts
@@ -0,0 +1,33 @@
+export const customErrStyles = isDesktopView => ({
+  control: provided => ({
+    ...provided,
+    backgroundColor: "#FFFFFF",
+    minHeight: 48,
+    padding: "0",
+    border: "1px solid #E04646",
+    "border-color": "E04646",
+  }),
+  menu: provided => ({
+    ...provided,
+    fontSize: "12px",
+    letterSpacing: "2px",
+  }),
+  valueContainer: provided => ({
+    ...provided,
+    overflow: "visible",
+  }),
+  placeholder: base => ({
+    ...base,
+    position: "absolute",
+    top: "-40%",
+    transition: "top 0.2s, font-size 0.2s",
+    fontSize: 9,
+    backgroundColor: "#FFFFFF",
+    color: "#E04646",
+  }),
+  singleValue: base => ({
+    ...base,
+    fontWeight: 500,
+    fontSize: isDesktopView ? 16 : 14,
+  }),
+});
