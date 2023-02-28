85a8cd9a63e58997e45fc178ff547249ed40f543
Apoorv Tiwari <tiwari.apoorv1316@gmail.com>
2023-02-28 18:51:40 +0530

    Create client spec (#1064)

---
+++ spec/system/clients/create_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Create client", type: :system do
+  let(:company) { create(:company) }
+  let(:user) { create(:user, current_workspace_id: company.id) }
+
+  context "when user is admin" do
+    before do
+      create(:employment, company:, user:)
+      user.add_role :admin, company
+      sign_in(user)
+    end
+
+    it "creates the client successfully" do
+      with_forgery_protection do
+        visit "time-tracking"
+        expect(page).to have_content "MONTH"
+      end
+    end
+  end
+end
