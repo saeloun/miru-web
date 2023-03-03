3a02009b9b71301c1349d44fc969a8f5f67a9ca2
Shalaka Patil <shalu214patil@gmail.com>
2023-03-03 11:42:00 +0530

    Refactor Reports::TimeEntries::Result class

--- app/services/reports/time_entries/result.rb
+++ app/services/reports/time_entries/result.rb
@@ -1,67 +1,44 @@
 # frozen_string_literal: true
 
-module Reports::TimeEntries
-  class Result < ApplicationService
-    attr_reader :es_response, :group_by
+class Reports::TimeEntries::Result < ApplicationService
+  attr_reader :es_response, :group_by
 
-    def initialize(es_response, group_by)
-      @es_response = es_response
-      @group_by = group_by
-    end
+  GROUP_BY_INPUT_TO_ES_FIELD = {
+    "team_member" => :user_id,
+    "client" => :client_id,
+    "project" => :project_id
+  }
 
-    def process
-      if group_by.blank?
-        # client_logo = fetch_client_logo(es_response.first)
-        [{ label: "", entries: es_response }]
-      else
-        process_response_by_group_by
-      end
+  GROUP_BY_INPUT_TO_GROUP_LABEL_FIELD = {
+    "team_member" => :user_name,
+    "client" => :client_name,
+    "project" => :project_name
+  }
+
+  def initialize(es_response, group_by)
+    @es_response = es_response
+    @group_by = group_by
+  end
+
+  def process
+    if group_by.blank?
+      # client_logo = fetch_client_logo(es_response.first)
+      [{ label: "", entries: es_response }]
+    else
+      process_response_by_group_by
     end
+  end
+
+  private
 
     def process_response_by_group_by
-      public_send("group_by_#{group_by}")
-    end
-
-    # TODO: Add client logo in each entry
-    def group_by_team_member
-      grouped_data = es_response.group_by(&:user_id)
+      grouped_data = es_response.group_by(&GROUP_BY_INPUT_TO_ES_FIELD[group_by])
       grouped_data.map do | user_id, entries |
-        { label: group_label(entries.first), entries: }
-      end
-    end
-
-    def group_by_client
-      grouped_data = es_response.group_by(&:client_id)
-      grouped_data.map do | client_id, entries |
-        { label: group_label(entries.first), entries: }
-      end
-    end
-
-    def group_by_project
-      grouped_data = es_response.group_by(&:project_id)
-      grouped_data.map do | project_id, entries |
-        { label: group_label(entries.first), entries: }
-      end
-    end
-
-    def group_label(timesheet_entry, bucket_name = nil)
-      case group_by
-      when "team_member"
-        timesheet_entry.user_name
-      when "client"
-        timesheet_entry.client_name
-      when "project"
-        timesheet_entry.project_name
-      when "week"
-        start_date = DateTime.parse(bucket_name)
-        end_date = start_date + 6.days
-        date_format = "%d %b %Y"
-        "#{start_date.strftime(date_format)} - #{end_date.strftime(date_format)}"
+        { label: entries.first[GROUP_BY_INPUT_TO_GROUP_LABEL_FIELD[group_by]], entries: }
       end
     end
 
     def fetch_client_logo(timesheet_entry)
       timesheet_entry.project.client.logo_url
     end
-  end
 end
