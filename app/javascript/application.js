a0be4c27250b57a1ae81a3093065a45a2f4a77a0
Vipul A M <vipulnsward@gmail.com>
2023-03-04 16:21:43 +0530

    Get rid of toastr

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -11,26 +11,6 @@ require("jquery");
 
 require("settings");
 
-global.toastr = require("toastr");
-
-global.toastr.options = {
-  closeButton: true,
-  closeHtml: "<button></button>",
-  debug: false,
-  newestOnTop: false,
-  progressBar: false,
-  positionClass: "toast-bottom-center",
-  preventDuplicates: false,
-  onclick: null,
-  showDuration: "300",
-  hideDuration: "1000",
-  timeOut: "5000",
-  extendedTimeOut: "1000",
-  showEasing: "swing",
-  hideEasing: "linear",
-  showMethod: "fadeIn",
-  hideMethod: "fadeOut",
-};
 Rails.start();
 ActiveStorage.start();
 
