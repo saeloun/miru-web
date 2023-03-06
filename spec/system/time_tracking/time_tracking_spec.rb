540b0c9de8d25ab832a57fe3a4321bebaca346de
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2023-03-06 13:02:58 +0530

    add system create, read and delete for employee

--- spec/system/time_tracking/time_tracking_spec.rb
+++ spec/system/time_tracking/time_tracking_spec.rb
@@ -68,4 +68,54 @@ RSpec.describe "Time Tracking", type: :system do
       end
     end
   end
+
+  context "when user is employee" do
+    before do
+      @company = create(:company)
+      @user = create(:user, current_workspace_id: @company.id)
+      @employment = create(:employment, company: @company, user: @user)
+      @client = create(:client, company: @company)
+      @project = create(:project, client: @client)
+      @project_member = create(:project_member, user: @user, project: @project)
+      @user.add_role :employee, @company
+      sign_in(@user)
+    end
+
+    it "can add time entry" do
+      with_forgery_protection do
+        visit "time-tracking"
+
+        click_button "NEW ENTRY"
+        select @client.name, from: "client"
+        fill_in "notes",	with: "Testing note!"
+        fill_in "timeInput", with: "8"
+        click_button "SAVE"
+        expect(page).to have_content("Timesheet created", wait: 3)
+      end
+    end
+
+    it "can edit time entry" do
+      create(:timesheet_entry, user: @user, project: @project)
+      with_forgery_protection do
+        visit "time-tracking"
+
+        el = find(:css, "#editIcon", visible: false).hover
+        el.click
+        fill_in "notes",	with: "Testing note!"
+        click_button "UPDATE"
+        expect(page).to have_content("Timesheet updated", wait: 3)
+      end
+    end
+
+    it "can delete time entry" do
+      create(:timesheet_entry, user: @user, project: @project)
+      with_forgery_protection do
+        visit "time-tracking"
+
+        el = find(:css, "#deleteIcon", visible: false).hover
+        el.click
+        expect(page).to have_content("Timesheet deleted", wait: 3)
+      end
+    end
+  end
 end
