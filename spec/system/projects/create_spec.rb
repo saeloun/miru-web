4c1ddf824b70b86c7efa85bf9d370bc5165f6b57
“Apoorv <tiwari.apoorv1316@gmail.com>
2023-03-06 17:20:57 +0530

    specs for crud on projects

---
+++ spec/system/projects/create_spec.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Creating Project", type: :system do
+  let(:company) { create(:company) }
+  let(:user) { create(:user, current_workspace_id: company.id) }
+  let!(:client) { create(:client, company:) }
+
+  context "when user is an admin" do
+    before do
+      create(:employment, company:, user:)
+      user.add_role :admin, company
+      login_as(user)
+    end
+
+    context "when creating a billable project" do
+      it "creates a billable project successfully" do
+        with_forgery_protection do
+          visit "/projects"
+
+          click_button "NEW PROJECT"
+          select client.name, from: "select-client"
+          fill_in "project-name", with: "Test Project"
+          choose "Billable"
+          click_button "ADD PROJECT"
+
+          expect(page).to have_content("Test Project", wait: 3)
+          expect(page).to have_content(client.name)
+          expect(page).to have_content("Billable")
+        end
+      end
+    end
+
+    context "when creating a non-billable" do
+      it "creates a non-billable project successfully" do
+        with_forgery_protection do
+          visit "/projects"
+
+          click_button "NEW PROJECT"
+          select client.name, from: "select-client"
+          fill_in "project-name", with: "Non Billable Project"
+          choose "Non-billable"
+          click_button "ADD PROJECT"
+
+          expect(page).to have_content("Non Billable Project", wait: 3)
+          expect(page).to have_content(client.name)
+        end
+      end
+    end
+
+    context "when creating a project without a name" do
+      it "add project button is disabled" do
+        with_forgery_protection do
+          visit "/projects"
+
+          click_button "NEW PROJECT"
+          select client.name, from: "select-client"
+          fill_in "project-name", with: ""
+          choose "Non-billable"
+          expect(page).to have_button("ADD PROJECT", disabled: true)
+        end
+      end
+    end
+  end
+
+  context "when user is an employee" do
+    before do
+      create(:employment, company:, user:)
+      user.add_role :employee, company
+      login_as(user)
+    end
+
+    it "new project button should not be visible for employees" do
+      with_forgery_protection do
+        visit "/projects"
+
+        expect(page).to have_no_button("NEW PROJECT")
+      end
+    end
+  end
+end
