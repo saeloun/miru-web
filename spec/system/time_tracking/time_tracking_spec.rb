2c585e345e1c49214dcf3f6dd5db3d18c8606f3b
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2023-03-06 12:51:36 +0530

    add spec for viewing other users entry

--- spec/system/time_tracking/time_tracking_spec.rb
+++ spec/system/time_tracking/time_tracking_spec.rb
@@ -21,7 +21,6 @@ RSpec.describe "Time Tracking", type: :system do
 
         click_button "NEW ENTRY"
         select @client.name, from: "client"
-        # debugger
         fill_in "notes",	with: "Testing note!"
         fill_in "timeInput", with: "8"
         click_button "SAVE"
@@ -50,6 +49,22 @@ RSpec.describe "Time Tracking", type: :system do
         el = find(:css, "#deleteIcon", visible: false).hover
         el.click
         expect(page).to have_content("Timesheet deleted", wait: 3)
+      end
+    end
+
+    it "can view other users entry" do
+      user_two = create(:user, current_workspace_id: @company.id)
+      create(:employment, company: @company, user: user_two)
+      create(:project_member, user: user_two, project: @project)
+      time_entry = create(:timesheet_entry, user: user_two, project: @project)
+      with_forgery_protection do
+        visit "time-tracking"
+
+        within(".css-6j8wv5-Input") do
+          find("input#react-select-8-input").set(" ").set(user_two.full_name).send_keys(:tab)
+        end
+
+        expect(page).to have_content(time_entry.note)
       end
     end
   end
