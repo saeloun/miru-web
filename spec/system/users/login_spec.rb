83c8bec939becf116a1765082fc5992e14bea160
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2023-03-09 21:12:25 +0530

    Added empty line after visit in system specs

--- spec/system/users/login_spec.rb
+++ spec/system/users/login_spec.rb
@@ -18,6 +18,7 @@ RSpec.describe "Sign-in", type: :system do
 
         fill_in "email", with: user.email
         fill_in "password", with: user.password
+
         click_on "Sign In"
 
         expect(page).to have_current_path("/time-tracking")
@@ -30,6 +31,7 @@ RSpec.describe "Sign-in", type: :system do
 
         fill_in "email", with: "invalid@example.com"
         fill_in "password", with: "password"
+
         click_on "Sign In"
 
         expect(page).to have_current_path("/login")
@@ -45,6 +47,7 @@ RSpec.describe "Sign-in", type: :system do
 
         fill_in "email", with: user.email
         fill_in "password", with: user.password
+
         click_on "Sign In"
 
         expect(page).to have_current_path("/email_confirmation?email=#{user.email}")
