151760989bd95d75c43db4c8ce5521f088c8a2e3
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2023-03-09 20:28:21 +0530

    Removed validate_company from application controller as we are handling the routing on react

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!, :validate_company!
+  skip_before_action :authenticate_user!
   skip_after_action :verify_authorized
 
   def index
