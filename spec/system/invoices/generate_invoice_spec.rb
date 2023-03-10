10da67d85fc73683589f68f7caea5e3e2c99f3bb
mohinid <mohinidahiya4@gmail.com>
2023-03-10 14:27:16 +0530

    use shared_examples for common tests

--- spec/system/invoices/generate_invoice_spec.rb
+++ spec/system/invoices/generate_invoice_spec.rb
@@ -2,6 +2,49 @@
 
 require "rails_helper"
 
+RSpec.shared_examples "Generate Invoice" do
+  it "is able to view generate invoice page elements" do
+      with_forgery_protection do
+       visit "invoices"
+
+       expect(page).to have_text "Invoices"
+       click_button("Create New Invoice")
+
+       # Check labels
+       expect(page).to have_text "Generate Invoice"
+       expect(page).to have_text company.name
+       expect(page).to have_text company.business_phone
+     end
+    end
+
+  it "is able to generates Invoice successfully for an employee of his organisation" do
+    with_forgery_protection do
+      visit "invoices"
+
+      expect(page).to have_text "Invoices"
+      click_button("Create New Invoice")
+
+      # Add client
+      click_button("+ ADD CLIENT")
+      find(:xpath, '//*[@id="client-list"]/div[2]/div').click()
+
+      # Add invoice number
+      find(:field, placeholder: "Enter invoice number").set("test-invoice-1")
+
+      # Add user from line items
+      click_button("+ NEW LINE ITEM")
+      find(:field, placeholder: "Name").click()
+      find(:xpath, "//*[@id='entries-list']/span[contains(text(), '#{employee.first_name}')]").click()
+      click_button("SAVE")
+
+      # Verify closing edit window and loading invoice lists page with the same invoice number
+      expect(page).to have_current_path "/invoices?invoices_per_page=20&page=1"
+      expect(page).to have_xpath "//h1[text()='All Invoices']"
+      expect(page).to have_xpath "//h3[text()='test-invoice-1']"
+    end
+  end
+end
+
 RSpec.describe "Generate Invoice", type: :system do
   let(:company) { create(:company, base_currency: "USD") }
   let(:admin) { create(:user, current_workspace_id: company.id) }
@@ -35,49 +78,10 @@ RSpec.describe "Generate Invoice", type: :system do
 
   context "when logged-in user is Admin" do
     before do
-      login_as admin
+      login_as owner
     end
 
-    it "is able to view generate invoice page elements" do
-      with_forgery_protection do
-       visit "invoices"
-
-       expect(page).to have_text "Invoices"
-       click_button("Create New Invoice")
-
-       # Check labels
-       expect(page).to have_text "Generate Invoice"
-       expect(page).to have_text company.name
-       expect(page).to have_text company.business_phone
-     end
-    end
-
-    it "is able to generates Invoice successfully for an employee of his organisation" do
-      with_forgery_protection do
-        visit "invoices"
-
-        expect(page).to have_text "Invoices"
-        click_button("Create New Invoice")
-
-        # Add client
-        click_button("+ ADD CLIENT")
-        find(:xpath, '//*[@id="client-list"]/div[2]/div').click()
-
-        # Add invoice number
-        find(:field, placeholder: "Enter invoice number").set("test-invoice-1")
-
-        # Add user from line items
-        click_button("+ NEW LINE ITEM")
-        find(:field, placeholder: "Name").click()
-        find(:xpath, "//*[@id='entries-list']/span[contains(text(), '#{employee.first_name}')]").click()
-        click_button("SAVE")
-
-        # Verify closing edit window and loading invoice lists page with the same invoice number
-        expect(page).to have_current_path "/invoices?invoices_per_page=20&page=1"
-        expect(page).to have_xpath "//h1[text()='All Invoices']"
-        expect(page).to have_xpath "//h3[text()='test-invoice-1']"
-      end
-    end
+    include_examples "Generate Invoice"
   end
 
   context "when logged-in user is an Owner" do
@@ -85,33 +89,7 @@ RSpec.describe "Generate Invoice", type: :system do
       login_as owner
     end
 
-    it "is able to generates Invoice successfully for an employee of his organisation" do
-      with_forgery_protection do
-        visit "invoices"
-
-        expect(page).to have_text "Invoices"
-        click_button("Create New Invoice")
-        expect(page).to have_current_path("/invoices/generate")
-
-        # Add client
-        click_button("+ ADD CLIENT")
-        find(:xpath, '//*[@id="client-list"]/div[2]/div').click()
-
-        # Add invoice number
-        find(:field, placeholder: "Enter invoice number").set("test-invoice-1")
-
-        # Add user from line items
-        click_button("+ NEW LINE ITEM")
-        find(:field, placeholder: "Name").click()
-        find(:xpath, "//*[@id='entries-list']/span[contains(text(), '#{employee.first_name}')]").click()
-        click_button("SAVE")
-
-        # Verify closing edit window and loading invoice lists page with the same invoice number
-        expect(page).to have_current_path "/invoices?invoices_per_page=20&page=1"
-        expect(page).to have_xpath "//h1[text()='All Invoices']"
-        expect(page).to have_xpath "//h3[text()='test-invoice-1']"
-      end
-    end
+    include_examples "Generate Invoice"
   end
 
   context "when logged-in user is an Employee" do
