30037cd8f135674fde291d36cfdf73bbc92205cb
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2023-03-10 22:25:21 +0530

    add additional spec for employee

--- spec/system/time_tracking/time_tracking_spec.rb
+++ spec/system/time_tracking/time_tracking_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe "Time Tracking", type: :system do
   let!(:client) { create(:client, company:) }
   let!(:project) { create(:project, client:) }
   let(:admin) { create(:user, current_workspace_id: company.id) }
+  let(:employee) { create(:user, current_workspace_id: company.id) }
 
   context "when user is admin" do
     it_behaves_like "Time tracking", is_admin: true
@@ -35,5 +36,44 @@ RSpec.describe "Time Tracking", type: :system do
 
   context "when user is employee" do
     it_behaves_like "Time tracking", is_admin: false
+
+    it "can only see the projects assigned to them" do
+      employee.add_role :employee, company
+      create(:employment, company:, user: employee)
+      create(:project_member, user: employee, project:)
+      login_as(employee)
+
+      user_two = create(:user, current_workspace_id: company.id)
+      client_two = create(:client, company:)
+      project_two = create(:project, client: client_two, name: "Project Two")
+      create(:project_member, user: user_two, project: project_two)
+
+      with_forgery_protection do
+        visit "time-tracking"
+
+        click_button "NEW ENTRY"
+        select = find(:css, "#client")
+        expect(page).to have_select("client", options: ["Client", client.name])
+      end
+    end
+
+    it "cannot edit or delete billed projects" do
+      employee.add_role :employee, company
+      create(:employment, company:, user: employee)
+      create(:project_member, user: employee, project:)
+      login_as(employee)
+
+      project_two = create(:project, client:, name: "Project Two", billable: true)
+      create(:project_member, user: employee, project: project_two)
+      time_entry = create(:timesheet_entry, user: employee, project: project_two)
+      time_entry.update!(bill_status: "billed")
+
+      with_forgery_protection do
+        visit "time-tracking"
+
+        expect(page).not_to have_selector("#editIcon")
+        expect(page).not_to have_selector("#deleteIcon")
+      end
+    end
   end
 end
