31ecbcbadcbec7ab849ce560f020c5f27dd1e289
“Deepti <deepti@saeloun.com>
2023-03-13 17:20:07 +0530

    Fixed "Admin & owner users are unable to access personal details page of a team member "

--- app/policies/team_members/detail_policy.rb
+++ app/policies/team_members/detail_policy.rb
@@ -2,12 +2,14 @@
 
 class TeamMembers::DetailPolicy < ApplicationPolicy
   def show?
+    return false unless record.present?
     user.has_any_role?(
       { name: :admin, resource: record.company },
       { name: :owner, resource: record.company }) || user == record.user
   end
 
   def update?
+    return false unless record.present?
     user.has_any_role?(
       { name: :admin, resource: record.company },
       { name: :owner, resource: record.company }) || user == record.user
