16e472d3ae3aca01f67c19e35135ec5cfd751056
Vipul A M <vipulnsward@gmail.com>
2023-03-14 18:36:14 +0530

    Refactor stripe checkout service (#1109)
    * Move Stripe Handle Event to service
    
    * x
    
    ---------
    
    Co-authored-by: Shalaka Patil <shalu214patil@gmail.com>

--- app/controllers/webhooks/stripe_controller.rb
+++ app/controllers/webhooks/stripe_controller.rb
@@ -1,14 +1,21 @@
 # frozen_string_literal: true
 
 class Webhooks::StripeController < ApplicationController
-  include StripeCheckout
-
   skip_before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
   skip_after_action :verify_authorized
   protect_from_forgery except: :fulfill_stripe_checkout
 
   def fulfill_stripe_checkout
-    handle_stripe_checkout_event(request)
+    stripe_event_service = HandleStripeCheckoutEventService.new(
+      payload: request.body.read,
+      stripe_signature: request.env["HTTP_STRIPE_SIGNATURE"])
+    stripe_event_service.process
+
+    if stripe_event_service.status
+      render json: stripe_event_service.json, status: stripe_event_service.status
+    else
+      render json: {}, status: :ok
+    end
   end
 end
