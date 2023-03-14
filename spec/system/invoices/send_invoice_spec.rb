91b0be87702a5d177724267d5e034bcc3a390094
mohinid <mohinidahiya4@gmail.com>
2023-03-14 12:32:50 +0530

    created shared_examples/invoices folder

--- spec/system/invoices/send_invoice_spec.rb
+++ spec/system/invoices/send_invoice_spec.rb
@@ -2,40 +2,6 @@
 
 require "rails_helper"
 
-RSpec.shared_examples "Send Invoice" do
-  it "is able to send invoice from Invoices List page" do
-        with_forgery_protection do
-          visit "invoices"
-
-          expect(page).to have_text "Invoices"
-          expect(page).to have_xpath "//h1[text()='All Invoices']"
-
-          # Verify the page must have the new invoice
-          within_table("invoices-list-table") do
-            tr = find(:xpath, "//*[@id='invoices-list-table-row'][1]")
-            expect(tr).to have_xpath "//h3[text()='#{invoice.invoice_number}']"
-            expect(tr).to have_xpath "//span[text()='#{company.name}']"
-            tr.hover
-
-            # Send invoice
-            find_by_id("send-invoice-button", visible: false).click()
-            click_button("Send Invoice")
-          end
-        end
-      end
-
-  it "is able to send invoice from Edit Invoice page" do
-    with_forgery_protection do
-      visit "invoices/#{invoice.id}/edit"
-
-      find_by_id("send-invoice-button").click()
-      click_button("Send Invoice")
-      expect(page).to have_content(/PROCESSING.../, wait: 3)
-      # expect(page).to have_content("Invoice will be sent!") - Need to verify.
-    end
-  end
-end
-
 RSpec.describe "Send Invoice", type: :system do
   let(:invoice) { create :invoice_with_invoice_line_items, status: :draft }
   let(:client) { invoice.client }
---
+++ spec/system/shared_examples/invoices/generate_invoice.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.shared_examples "Generate Invoice" do
+  it "is able to view generate invoice page elements" do
+      with_forgery_protection do
+       visit "invoices"
+
+       expect(page).to have_text "Invoices"
+       click_button("Create New Invoice")
+
+       # Check labels
+       expect(page).to have_text "Generate Invoice"
+       expect(page).to have_text company.name
+       expect(page).to have_text company.business_phone
+     end
+    end
+
+  it "is able to generates Invoice successfully for an employee of his organisation" do
+    with_forgery_protection do
+      visit "invoices"
+
+      expect(page).to have_text "Invoices"
+      click_button("Create New Invoice")
+
+      # Add client
+      click_button("+ ADD CLIENT")
+      find(:xpath, '//*[@id="client-list"]/div[2]/div').click()
+
+      # Add invoice number
+      find(:field, placeholder: "Enter invoice number").set("test-invoice-1")
+
+      # Add user from line items
+      click_button("+ NEW LINE ITEM")
+      find(:field, placeholder: "Name").click()
+      find(:xpath, "//*[@id='entries-list']/span[contains(text(), '#{employee.first_name}')]").click()
+      click_button("SAVE")
+
+      # Verify closing edit window and loading invoice lists page with the same invoice number
+      expect(page).to have_current_path "/invoices?invoices_per_page=20&page=1"
+      expect(page).to have_xpath "//h1[text()='All Invoices']"
+      expect(page).to have_xpath "//h3[text()='test-invoice-1']"
+    end
+  end
+end
