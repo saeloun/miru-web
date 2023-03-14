51ef704c775aa28caf11cfeaf781f6db0e04ce63
Ankita <Ankita012@users.noreply.github.com>
2023-03-14 13:37:44 +0530

    Revenue report: UI changes as per new design (#1127)
    * revenue report UI chnages as per new design
    
    * PR comments resolved

--- app/javascript/src/components/Reports/RevenueByClientReport/Container/TableRow.tsx
+++ app/javascript/src/components/Reports/RevenueByClientReport/Container/TableRow.tsx
@@ -26,18 +26,18 @@ const TableRow = ({ currency, report }) => {
           </p>
         </span>
       </td>
-      <td className="w-2/5 whitespace-pre-wrap px-6 py-4 text-left text-base font-normal text-miru-dark-purple-1000">
+      <td className="w-2/5 whitespace-pre-wrap px-0 py-4 text-right text-base font-normal text-miru-dark-purple-1000">
         {currencyFormat(currency, overdueAmount)}
       </td>
-      <td className="w-2/5 whitespace-pre-wrap px-6 py-4 text-left text-base font-normal text-miru-dark-purple-1000">
+      <td className="w-2/5 whitespace-pre-wrap px-0 py-4 text-right text-base font-normal text-miru-dark-purple-1000">
         {currencyFormat(currency, outstandingAmount)}
       </td>
-      <td className="w-1/5 whitespace-nowrap px-6 py-4 text-left">
+      <td className="w-1/5 whitespace-nowrap px-6 py-4 text-right">
         <p className="text-base	 font-normal text-miru-dark-purple-1000">
           {currencyFormat(currency, paidAmount)}
         </p>
       </td>
-      <td className="w-1/5 whitespace-nowrap py-4 pl-6 text-right text-xl font-bold text-miru-dark-purple-1000">
+      <td className="w-1/5 whitespace-nowrap py-4 pl-0 text-right text-xl font-bold text-miru-dark-purple-1000">
         {currencyFormat(currency, totalAmount)}
       </td>
     </tr>
