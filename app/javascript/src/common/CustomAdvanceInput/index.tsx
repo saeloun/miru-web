dacdd8106e0cfdc7eabb3f17e9f0ee064fd6209b
Shruti Apte <shrutiapte20@gmail.com>
2023-03-15 11:59:36 +0530

    resolved comments

--- app/javascript/src/common/CustomAdvanceInput/index.tsx
+++ app/javascript/src/common/CustomAdvanceInput/index.tsx
@@ -6,24 +6,37 @@ import classNames from "classnames";
 
 import { useOutsideClick } from "../../helpers/outsideClick";
 
+type CustomAdvanceInputProps = {
+  id?: string;
+  value: any;
+  label?: string;
+  wrapperClassName?: string;
+  inputBoxClassName?: string;
+  labelClassName?: string;
+};
+
+const getDefaultInputBoxClassName = focused =>
+  `form__input block w-full h-full appearance-none p-4 text-base bg-white ${
+    focused ? "border-miru-han-purple-1000" : "border-miru-gray-1000"
+  }`;
+
+const getDefaultLabelClassName = (focused, value) =>
+  focused || value
+    ? "absolute duration-300 -top-2 -z-1 left-4 origin-0 text-xs font-normal text-miru-dark-purple-400 bg-white"
+    : "absolute duration-300 -z-1 origin-0 top-3 left-4 text-miru-dark-purple-200 bg-white text-sm lg:text-base font-medium";
+
 export const CustomAdvanceInput = ({
   id,
   value,
   label,
-  wrapperClassName = "",
-  inputBoxClassName = "",
-}) => {
+  wrapperClassName,
+  inputBoxClassName,
+  labelClassName,
+}: CustomAdvanceInputProps) => {
   const inputRef = useRef(null);
   const [focused, setFocused] = useState<boolean>(false);
-  const defaultInputBoxClassName = `form__input block w-full h-full appearance-none p-4 text-base bg-white ${
-      focused ? "border-miru-han-purple-1000" : "border-miru-gray-1000"
-    }`,
-    defaultWrapperClassName = "outline relative";
-
-  const labelClassName =
-    focused || value
-      ? "absolute duration-300 -top-2 -z-1 left-4 origin-0 text-xs font-normal text-miru-dark-purple-400 bg-white"
-      : "absolute duration-300 -z-1 origin-0 top-3 left-4 text-miru-dark-purple-200 bg-white text-sm lg:text-base font-medium";
+  const defaultInputBoxClassName = getDefaultInputBoxClassName(focused);
+  const defaultLabelClassName = getDefaultLabelClassName(focused, value);
 
   useEffect(() => {
     focused
@@ -35,7 +48,7 @@ export const CustomAdvanceInput = ({
 
   return (
     <div
-      className={classNames(defaultWrapperClassName, wrapperClassName)}
+      className={classNames("outline relative", wrapperClassName)}
       ref={inputRef}
       onClick={() => setFocused(true)}
     >
@@ -45,7 +58,9 @@ export const CustomAdvanceInput = ({
       >
         {value}
       </div>
-      <span className={labelClassName}>{label}</span>
+      <span className={classNames(defaultLabelClassName, labelClassName)}>
+        {label}
+      </span>
     </div>
   );
 };
--- app/javascript/src/common/CustomInputText/index.tsx
+++ app/javascript/src/common/CustomInputText/index.tsx
@@ -4,51 +4,64 @@ import React from "react";
 
 import classNames from "classnames";
 
+const defaultInputBoxClassName =
+  "form__input block w-full appearance-none bg-white p-4 text-base h-12 border-miru-gray-1000";
+const defaultWrapperClassName = "outline relative h-12";
+const defaultLabelClassname =
+  "absolute top-0.5 left-1 h-6 z-1 origin-0 bg-white p-2 text-sm lg:text-base font-medium text-miru-dark-purple-200 duration-300";
+
+type customInputTextProps = {
+  id?: string;
+  inputBoxClassName?: string;
+  dataCy?: string;
+  disabled?: boolean;
+  name?: string;
+  type?: string;
+  value: any;
+  onChange: any;
+  labelClassName?: string;
+  label?: string;
+  wrapperClassName?: string;
+  readOnly?: boolean;
+};
+
 export const CustomInputText = ({
   id,
-  inputBoxClassName = "",
+  inputBoxClassName,
   dataCy,
   disabled,
   name,
   type,
   value,
   onChange,
-  labelClassName = "",
+  labelClassName,
   label,
-  wrapperClassName = "",
+  wrapperClassName,
   readOnly,
-}) => {
-  const defaultInputBoxClassName =
-    "form__input block w-full appearance-none bg-white p-4 text-base h-12 border-miru-gray-1000";
-  const defaultWrapperClassName = "outline relative h-12";
-  const defaultLabelClassname =
-    "absolute top-0.5 left-1 h-6 z-1 origin-0 bg-white p-2 text-sm lg:text-base font-medium text-miru-dark-purple-200 duration-300";
-
-  return (
-    <div className="field relative">
-      <div className={classNames(defaultWrapperClassName, wrapperClassName)}>
-        <input
-          className={classNames(defaultInputBoxClassName, inputBoxClassName)}
-          data-cy={dataCy}
-          disabled={disabled}
-          id={id}
-          name={name}
-          placeholder=" "
-          readOnly={readOnly}
-          type={type}
-          value={value}
-          onChange={onChange}
-        />
-        <label
-          className={classNames(defaultLabelClassname, labelClassName)}
-          htmlFor={name}
-        >
-          {label}
-        </label>
-      </div>
+}: customInputTextProps) => (
+  <div className="field relative">
+    <div className={classNames(defaultWrapperClassName, wrapperClassName)}>
+      <input
+        className={classNames(defaultInputBoxClassName, inputBoxClassName)}
+        data-cy={dataCy}
+        disabled={disabled}
+        id={id}
+        name={name}
+        placeholder=" "
+        readOnly={readOnly}
+        type={type}
+        value={value}
+        onChange={onChange}
+      />
+      <label
+        className={classNames(defaultLabelClassname, labelClassName)}
+        htmlFor={name}
+      >
+        {label}
+      </label>
     </div>
-  );
-};
+  </div>
+);
 
 CustomInputText.defaultProps = {
   type: "text",
