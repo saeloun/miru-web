a86a44d44ffae94e21e61ab0ec5a7332798381c2
“Deepti <deepti@saeloun.com>
2023-03-20 16:42:42 +0530

    Fixed failed spec.

--- app/policies/team_members/detail_policy.rb
+++ app/policies/team_members/detail_policy.rb
@@ -4,13 +4,13 @@ class TeamMembers::DetailPolicy < ApplicationPolicy
   def show?
     return false unless record.present?
 
-    user_is_admin_or_owner?
+    user_is_admin_or_owner? || user == record.user
   end
 
   def update?
     return false unless record.present?
 
-    user_is_admin_or_owner?
+    user_is_admin_or_owner? || user == record.user
   end
 
   private
