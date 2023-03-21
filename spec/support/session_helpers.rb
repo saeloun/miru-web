d4398b4d8cc71cb81499b5d6fae29364845ed15a
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2023-03-21 13:00:05 +0530

    Add timesheet entry system spec (#1103)
    * Add spec for creating timesheet entry
    
    * Add specs for edit and delete of time entries
    
    * create instance variables
    
    * add spec for viewing other users entry
    
    * add system create, read and delete for employee
    
    * add system spec for viewing time entries for employee and admin
    
    * use let, create roles, remove extra spacing
    
    * add weekly_view spec for adding time entry
    
    * view other users weekly view entries
    
    * view timesheet entry and employee create and view entries
    
    * crud for day view for employee and admin
    
    * change
    
    * use shared_examples_for
    
    * move time tracking commons to separate file
    
    * add additional spec for employee
    
    * add spec for editing the date
    
    * fix tests
    
    * shows correct total hours logged
    
    * revert
    
    * fix sessions helper
    
    * edit time entry on week view
    
    * fix test cases
    
    * refactor
    
    * remove
    
    * move to separate folders
    
    * fix
    
    * test fix
    
    * fix: tests

--- spec/support/session_helpers.rb
+++ spec/support/session_helpers.rb
@@ -4,10 +4,10 @@ module SessionHelpers
   def sign_in(user)
     with_forgery_protection do
       visit "/login"
-      fill_in "Email", with: user.email
-      fill_in "Password", with: user.password
+      fill_in "email", with: user.email
+      fill_in "password", with: user.password
       click_button "Sign In"
-      sleep 5
+      sleep 2
     end
   end
 end
