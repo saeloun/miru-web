38bdbe51931a5f558e1ce4cc3668be4dcd6e93c5
MohiniRDdahiya <mohini@saeloun.com>
2023-03-21 12:30:02 +0530

    Generate, Edit & Send Invoice -  System Specs (#1107)
    * generate invoice specs
    
    * revert Gemfile.lock and spec_helper.rb changes
    
    * generate invoice specs
    
    * remove Gemfile.lock changes
    
    * send-invoice-specs
    
    * Discard changes to Gemfile.lock
    
    * edit invoice specs
    
    * Discard changes to Gemfile.lock
    
    * review comments
    -change login from signup
    -update role definitions
    -add spec for updating invoice number
    
    * use shared_examples for common tests
    
    * created shared_examples/invoices folder
    
    * change ids to camelCase
    
    * update sign_in method
    
    * fix failing tests
    
    * comment out code for amounts in factory
    
    * fix review comments
    
    ---------
    
    Co-authored-by: “Apoorv <“tiwari.apoorv1316@gmail.com”>
    Co-authored-by: Prasanth Chaduvula <chaduvulaprasanth1998@gmail.com>

--- app/javascript/src/components/Invoices/common/NewLineItemTable/index.tsx
+++ app/javascript/src/components/Invoices/common/NewLineItemTable/index.tsx
@@ -58,6 +58,7 @@ const NewLineItemTable = ({
               <div
                 className="flex cursor-pointer justify-between py-2 px-3 hover:bg-miru-gray-100"
                 data-cy="entries-list"
+                id="entriesList"
                 key={index}
                 onClick={() => selectRowId(item)}
               >
--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -8,12 +8,12 @@ FactoryBot.define do
     due_date { Faker::Date.between(from: self.issue_date, to: Date.today) }
     invoice_number { Faker::Alphanumeric.unique.alpha(number: 4) }
     reference { Faker::Invoice.reference[1..12] }
-    amount { Faker::Number.decimal(r_digits: 2) }
-    outstanding_amount { Faker::Number.decimal(r_digits: 2) }
-    tax { Faker::Number.decimal(r_digits: 2) }
-    amount_paid { Faker::Number.decimal(r_digits: 2) }
-    amount_due { Faker::Number.decimal(r_digits: 2) }
-    discount { Faker::Number.decimal(r_digits: 2) }
+    # amount { Faker::Number.decimal(r_digits: 2) }
+    # outstanding_amount { Faker::Number.decimal(r_digits: 2) }
+    # tax { Faker::Number.decimal(r_digits: 2) }
+    # amount_paid { Faker::Number.decimal(r_digits: 2) }
+    # amount_due { Faker::Number.decimal(r_digits: 2) }
+    # discount { Faker::Number.decimal(r_digits: 2) }
     status { [:draft, :paid, :overdue].sample }
     external_view_key { "#{SecureRandom.hex}" }
     factory :invoice_with_invoice_line_items do
