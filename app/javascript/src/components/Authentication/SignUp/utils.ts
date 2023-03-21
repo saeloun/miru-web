7c47e375c283875ba462c8caab62a4dcd60b5baf
sanjibroy360 <sanjibroy0098@gmail.com>
2023-03-21 13:23:32 +0530

    Update the password validation regex

--- app/javascript/src/components/Authentication/SignUp/utils.ts
+++ app/javascript/src/components/Authentication/SignUp/utils.ts
@@ -17,13 +17,13 @@ export const signUpFormValidationSchema = Yup.object().shape({
     .required("Email ID cannot be blank"),
   password: Yup.string()
     .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/, // eslint-disable-line
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s\x00-\x1F\x7F])[^\s\x00-\x1F\x7F]{8,}$/, // eslint-disable-line
       "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Character"
     )
     .required("Password can not be blank"),
   confirm_password: Yup.string()
     .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/, // eslint-disable-line
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s\x00-\x1F\x7F])[^\s\x00-\x1F\x7F]{8,}$/, // eslint-disable-line
       "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Character"
     )
     .oneOf([Yup.ref("password"), null], "Passwords must match")
