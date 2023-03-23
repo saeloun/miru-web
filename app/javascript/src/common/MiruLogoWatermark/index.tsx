bf85d19d30c95a1b64ab06f7e8d730f3717c0c3e
sanjibroy360 <sanjibroy0098@gmail.com>
2023-03-23 15:21:34 +0530

    Ensure that Miru logo watermark will be displayed only in desktop view

--- app/javascript/src/common/MiruLogoWatermark/index.tsx
+++ app/javascript/src/common/MiruLogoWatermark/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { MiruLogoWatermarkSVG } from "miruIcons";
 
 const MiruLogoWatermark = () => (
-  <div className="absolute bottom-0 left-0 z-negative-1 w-2/5 lg:w-1/6">
+  <div className="absolute bottom-0 left-0 z-negative-1 hidden w-2/5 lg:block lg:w-1/6">
     <img className="w-full object-contain" src={MiruLogoWatermarkSVG} />
   </div>
 );
