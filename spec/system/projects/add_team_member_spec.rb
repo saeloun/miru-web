a2b0c2493e0b9a87f37636dfdcb9212d29074b62
Apoorv Tiwari <tiwari.apoorv1316@gmail.com>
2023-03-24 15:08:14 +0530

    System specs projects (#1161)
    * specs for crud on projects
    
    * add specs for projects
    
    * add spec for adding team memebr
    
    * add spec for removing & edit project member
    
    * fix failing specs
    
    ---------
    
    Co-authored-by: “Apoorv <“tiwari.apoorv1316@gmail.com”>

---
+++ spec/system/projects/add_team_member_spec.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Add team memebr to project", type: :system do
+  let(:company) { create(:company) }
+  let!(:user) { create(:user, current_workspace_id: company.id) }
+  let!(:user_2) { create(:user, current_workspace_id: company.id) }
+  let(:client) { create(:client, company:) }
+  let!(:project) { create(:project, client:) }
+
+  context "when adding a project member for a project" do
+    before do
+      create(:employment, company:, user:)
+      user.add_role :admin, company
+      sign_in(user)
+    end
+
+    it "can add project member to a project" do
+      with_forgery_protection do
+        visit "/projects"
+
+        find("tbody").hover.click
+        find("#kebabMenu").click
+        click_button "Add/Remove Team Members"
+        sleep 1
+        click_button "Add another team member"
+        find(".react-select-filter__control.css-1ysvbq8-control").click
+        find("#react-select-2-option-0").click
+        fill_in "Rate", with: "100"
+        click_button "Add team members to project"
+
+        expect(page).to have_content(user.first_name)
+        expect(page).to have_content("100")
+        expect(page).to have_content(project.name)
+      end
+    end
+  end
+
+  context "when project member is already added to a project" do
+    before do
+      create(:employment, company:, user:)
+      create(:project_member, user: user_2, project:)
+      user.add_role :admin, company
+      sign_in(user)
+    end
+
+    it "can remove member from a project" do
+      with_forgery_protection do
+        visit "/projects"
+
+        find("tbody").hover.click
+        find("#kebabMenu").click
+        click_button "Add/Remove Team Members"
+        sleep 1
+        click_button "removeMember"
+        click_button "Add team members to project"
+
+        expect(page).not_to have_content(user_2.first_name)
+        expect(page).to have_content(project.name)
+      end
+    end
+
+    it "can edit the rate for a project member" do
+      with_forgery_protection do
+        visit "/projects"
+
+        find("tbody").hover.click
+        find("#kebabMenu").click
+        click_button "Add/Remove Team Members"
+        sleep 1
+        fill_in "Rate", with: "500"
+        click_button "Add team members to project"
+
+        expect(page).to have_content(user.first_name)
+        expect(page).to have_content("500")
+        expect(page).to have_content(project.name)
+      end
+    end
+  end
+end
