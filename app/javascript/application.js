e174291715f36c917f47f1fcb93cd2029be262be
Vipul A M <vipulnsward@gmail.com>
2023-03-28 14:31:28 +0530

    Upgrade webpacker to rc6 (#1085)
    * Upgrade webpacker to rc6
    
    * Add postcss
    
    * Use node lts
    
    * f
    
    * x
    
    * x
    
    * Get rid of toastr
    
    ---------
    
    Co-authored-by: Prasanth Chaduvula <chaduvulaprasanth1998@gmail.com>

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,16 +4,17 @@ import Rails from "@rails/ujs";
 import "alpine-turbo-drive-adapter";
 import * as ReactRailsUJS from "react_ujs";
 
+import "./stylesheets/application.scss";
+
 require("alpinejs");
 require("jquery");
 
-import "../stylesheets/application.scss";
+require("settings");
 
 Rails.start();
 ActiveStorage.start();
 
 // Support component names relative to this directory:
 const componentRequireContext = require.context("src/components", true);
+// eslint-disable-next-line react-hooks/rules-of-hooks
 ReactRailsUJS.useContext(componentRequireContext);
-
-require("packs/settings");
