f31c289a1c0516e6656272d00f92ff67550f5b04
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2023-03-30 17:02:59 +0530

    Fixed invitation mailer system spec

--- spec/system/teams/create_spec.rb
+++ spec/system/teams/create_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "Inviting team memeber", type: :system do
     end
 
     context "when inviting team memeber" do
-      it "with valid inputs" do
+      before do
         with_forgery_protection do
           visit "/teams"
 
@@ -24,31 +24,21 @@ RSpec.describe "Inviting team memeber", type: :system do
           fill_in "email", with: "john@example.com"
           choose "Employee"
           click_button "SEND INVITE"
-
-          expect(page).to have_content("John Doe")
-          expect(page).to have_content("Pending Invitation")
         end
       end
 
+      it "with valid inputs" do
+        expect(page).to have_content("John Doe")
+        expect(page).to have_content("Pending Invitation")
+      end
+
       it "sends an invitation mail to the user" do
-        with_forgery_protection do
-          visit "/teams"
-
-          click_on "NEW USER"
-          fill_in "First Name", with: "John"
-          fill_in "Last Name", with: "Doe"
-          fill_in "email", with: "john@example.com"
-          choose "Employee"
-          click_button "SEND INVITE"
-
-          perform_enqueued_jobs
-
-          expect(ActiveJob::Base.queue_adapter.performed_jobs.last["job_class"]).to eq("ActionMailer::MailDeliveryJob")
-          expect(ActionMailer::Base.deliveries.last.to).to include("john@example.com")
-          expect(ActionMailer::Base.deliveries.last.subject).to eq("Welcome to Miru!")
-          expect(ActionMailer::Base.deliveries.last.body).to include("click here")
-          expect(ActionMailer::Base.deliveries.last.body).to include("to accept the invitation")
-        end
+        perform_enqueued_jobs
+        expect(ActiveJob::Base.queue_adapter.performed_jobs.last["job_class"]).to eq("ActionMailer::MailDeliveryJob")
+        expect(ActionMailer::Base.deliveries.last.to).to include("john@example.com")
+        expect(ActionMailer::Base.deliveries.last.subject).to eq("Welcome to Miru!")
+        expect(ActionMailer::Base.deliveries.last.body).to include("click here")
+        expect(ActionMailer::Base.deliveries.last.body).to include("to accept the invitation")
       end
     end
   end
