9e0bd1e33b4428ab14346deec655fbf0e4a7af3f
Shruti-Apte <72149587+Shruti-Apte@users.noreply.github.com>
2023-03-30 19:44:58 +0530

    Generate Invoice section 3: mobile view  (#1193)
    * invoice details section
    
    * added formik
    
    * Line Item section
    
    * merge error fixed
    
    * validation issues fixed
    
    * invoice preview section and billing section
    
    * comments resolved
    
    ---------
    
    Co-authored-by: Prasanth Chaduvula <chaduvulaprasanth1998@gmail.com>

--- app/javascript/src/components/Invoices/common/CompanyInfo/index.tsx
+++ app/javascript/src/components/Invoices/common/CompanyInfo/index.tsx
@@ -4,25 +4,24 @@ const CompanyInfo = ({ company, logo = "" }) => {
   const phoneNumber = company?.phoneNumber || company?.phone_number;
 
   return (
-    <div className="flex flex-col justify-between border-b border-miru-gray-400 md:h-40 md:flex-row md:p-10">
-      <div className="flex flex-col md:flex-row">
+    <div className="flex h-24 items-center justify-between border-b border-miru-gray-400 p-4 pb-2 lg:h-40 lg:p-10">
+      <div className="flex h-full items-center">
         <img
-          className="m-auto h-20 w-20 rounded-full md:m-0 md:mr-5"
+          className="mr-2 h-10 w-10 rounded-full lg:mr-5 lg:h-20 lg:w-20"
           src={company.logo ? company.logo : logo}
         />
-        <div className="mt-2 text-center md:text-left">
-          <p className="text-xl font-bold text-miru-dark-purple-1000 md:text-3xl">
+        <div className="text-center md:text-left lg:mt-2">
+          <p className="text-xl font-bold leading-7 text-miru-dark-purple-1000 lg:text-3xl">
             {company.name}
           </p>
-          <p className="mt-1 text-base font-normal text-miru-dark-purple-1000">
+          <p className="mt-1 text-left text-xs font-normal text-miru-dark-purple-1000 lg:text-base">
             {phoneNumber || company.business_phone}
           </p>
         </div>
       </div>
-      <div className="overflow-y-auto text-center text-base font-normal text-miru-dark-purple-1000 md:w-72 md:text-right">
+      <div className="overflow-y-auto text-right text-right text-xs font-normal text-miru-dark-purple-1000 lg:h-auto lg:w-72 lg:text-base">
         <p className="whitespace-pre">
-          {" "}
-          {company.address.split(",").join(",\n")}{" "}
+          {company.address.split(",").join(",\n")}
         </p>
         <p>{company.country}</p>
       </div>
