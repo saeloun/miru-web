7073eb5ebb4c527fade09fad557a021ecd91f95d
“Deepti <deepti@saeloun.com>
2023-04-06 14:51:47 +0530

    fixed rubocop offenses.

--- spec/system/clients/create_spec.rb
+++ spec/system/clients/create_spec.rb
@@ -7,7 +7,25 @@ RSpec.describe "Create client", type: :system do
   let(:user) { create(:user, current_workspace_id: company.id) }
   let(:existing_client) { create(:client_with_phone_number_without_country_code, company:) }
   let(:client) { build(:client_with_phone_number_without_country_code, company:) }
-  let(:address) { build(:address, :with_company, country: "US", state: "Alabama", city: "Brooklyn" ) }
+  let(:address) { build(:address, :with_company) }
+
+  def select_values_from_select_box
+    within("div#country") do
+      find(".react-select-filter__control.css-digfch-control").click
+      find("#react-select-2-option-232").click
+    end
+
+    within("div#state") do
+      find(".react-select-filter__control.css-digfch-control").click
+      find("#react-select-3-option-1").click
+    end
+
+    within("div#city") do
+      find(".react-select-filter__control.css-digfch-control").click
+      fill_in "react-select-4-input", with: "Skita"
+      find("#react-select-4-option-0").click
+    end
+  end
 
   context "when user is an admin" do
     before do
@@ -20,37 +38,18 @@ RSpec.describe "Create client", type: :system do
       it "creates the client successfully" do
         with_forgery_protection do
           visit "/clients"
-
           click_button "NEW CLIENT"
           find(
-            'form input[type="file"]',
-            visible: false).set(Rails.root.join("spec", "support", "fixtures", "test-image.png"))
+            "form input[type='file']",
+            visible: false).set(Rails.root.join("spec", "support", "fixtures", "test-image.png")
+          )
           fill_in "name", with: client.name
           fill_in "email", with: client.email
           fill_in "phone", with: client.phone
           fill_in "address1", with: address.address_line_1
-
-          within('div#country') do
-            find(".react-select-filter__control.css-digfch-control").click
-            find("#react-select-2-option-232").click
-          end
-
-          within('div#state') do
-            find(".react-select-filter__control.css-digfch-control").click
-            find("#react-select-3-option-1").click
-          end
-
-          within('div#city') do
-            find(".react-select-filter__control.css-digfch-control").click
-            fill_in "react-select-4-input", with: "Skita"
-            find("#react-select-4-option-0").click
-          end
-
+          select_values_from_select_box
           fill_in "zipcode", with: address.pin
-
           click_button "SAVE CHANGES"
-
-          expect(page).to have_css('img#logo[src*="test-image.png"]')
           expect(page).to have_content(client.name)
         end
       end
@@ -65,21 +64,7 @@ RSpec.describe "Create client", type: :system do
           fill_in "phone", with: client.phone
           fill_in "address1", with: address.address_line_1
 
-          within('div#country') do
-            find(".react-select-filter__control.css-digfch-control").click
-            find("#react-select-2-option-232").click
-          end
-
-          within('div#state') do
-            find(".react-select-filter__control.css-digfch-control").click
-            find("#react-select-3-option-1").click
-          end
-
-          within('div#city') do
-            find(".react-select-filter__control.css-digfch-control").click
-            fill_in "react-select-4-input", with: "Skita"
-            find("#react-select-4-option-0").click
-          end
+          select_values_from_select_box
 
           fill_in "zipcode", with: address.pin
           click_button "SAVE CHANGES"
@@ -99,21 +84,7 @@ RSpec.describe "Create client", type: :system do
           fill_in "phone", with: client.phone
           fill_in "address1", with: address.address_line_1
 
-          within('div#country') do
-            find(".react-select-filter__control.css-digfch-control").click
-            find("#react-select-2-option-232").click
-          end
-
-          within('div#state') do
-            find(".react-select-filter__control.css-digfch-control").click
-            find("#react-select-3-option-1").click
-          end
-
-          within('div#city') do
-            find(".react-select-filter__control.css-digfch-control").click
-            fill_in "react-select-4-input", with: "Skita"
-            find("#react-select-4-option-0").click
-          end
+          select_values_from_select_box
 
           fill_in "zipcode", with: address.pin
           click_button "SAVE CHANGES"
@@ -129,8 +100,9 @@ RSpec.describe "Create client", type: :system do
 
           click_button "NEW CLIENT"
           find(
-            'form input[type="file"]',
-            visible: false).set(Rails.root.join("spec", "support", "fixtures", "pdf-file.pdf"))
+            "form input[type='file']",
+            visible: false).set(Rails.root.join("spec", "support", "fixtures", "pdf-file.pdf")
+          )
 
           expect(page).to have_content("Incorrect file format. Please upload an image of type PNG or JPG.")
           expect(page).to have_content("Max size (30kb)")
@@ -143,8 +115,9 @@ RSpec.describe "Create client", type: :system do
 
           click_button "NEW CLIENT"
           find(
-            'form input[type="file"]',
-            visible: false).set(Rails.root.join("spec", "support", "fixtures", "invalid-file.png"))
+            "form input[type='file']",
+            visible: false).set(Rails.root.join("spec", "support", "fixtures", "invalid-file.png")
+          )
 
           expect(page).to have_content("File size exceeded the max limit of 30KB.")
         end
--- spec/system/clients/edit_spec.rb
+++ spec/system/clients/edit_spec.rb
@@ -7,6 +7,24 @@ RSpec.describe "Edit client", type: :system do
   let(:user) { create(:user, current_workspace_id: company.id) }
   let!(:client) { create(:client_with_phone_number_without_country_code, company:) }
 
+  def select_values_from_select_box
+    within("div#country") do
+      find(".react-select-filter__control.css-digfch-control").click
+      find("#react-select-2-option-232").click
+    end
+
+    within("div#state") do
+      find(".react-select-filter__control.css-digfch-control").click
+      find("#react-select-3-option-1").click
+    end
+
+    within("div#city") do
+      find(".react-select-filter__control.css-digfch-control").click
+      fill_in "react-select-4-input", with: "Skita"
+      find("#react-select-4-option-0").click
+    end
+  end
+
   before do
     create(:employment, company:, user:)
     user.add_role :admin, company
@@ -17,40 +35,17 @@ RSpec.describe "Edit client", type: :system do
     it "edit the client successfully" do
       with_forgery_protection do
         visit "/clients"
-
         find(".hoverIcon").hover.click
         find("#kebabMenu").click()
-
         within("ul.menuButton__wrapper") do
           find("li:nth-child(2) > button").click
         end
-
-        fill_in "name", with: "test client", wait: true
-        fill_in "email", with: "client@test.com", wait: true
-        fill_in "phone", with: "9123456789",wait: true
-
-        within('div#country') do
-          find(".react-select-filter__control.css-digfch-control").click
-          find("#react-select-2-option-232").click
-        end
-
-        within('div#state') do
-          find(".react-select-filter__control.css-digfch-control").click
-          find("#react-select-3-option-1").click
-        end
-
-        within('div#city') do
-          find(".react-select-filter__control.css-digfch-control").click
-          fill_in "react-select-4-input", with: "Skita"
-          find("#react-select-4-option-0").click
-        end
-
+        fill_in "name", with: "test client"
+        fill_in "email", with: "client@test.com"
+        fill_in "phone", with: "9123456789"
+        select_values_from_select_box
         click_button "SAVE CHANGES"
-
-        expect(page).to have_content("test client")
-
         client.reload
-
         expect(client.name).to eq("test client")
         expect(client.email).to eq("client@test.com")
       end
