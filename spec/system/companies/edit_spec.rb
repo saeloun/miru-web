323b8d4a036043a2b1ac2f3add51297b73ceba80
“Deepti <deepti@saeloun.com>
2023-04-06 14:58:12 +0530

    Fixed rubocop offenses.

--- spec/system/companies/edit_spec.rb
+++ spec/system/companies/edit_spec.rb
@@ -6,6 +6,18 @@ RSpec.describe "Edit company", type: :system do
   let(:company) { create(:company) }
   let(:user) { create(:user, current_workspace_id: company.id) }
 
+  def select_values_from_select_box
+    within("div#countrySelect") do
+      find(".react-select-filter__control.css-digfch-control").click
+      find("#react-select-9-option-232").click
+    end
+
+    within("div#stateSelect") do
+      find(".react-select-filter__control.css-digfch-control").click
+      find("#react-select-10-option-41").click
+    end
+  end
+
   before do
     create(:employment, company:, user:)
     user.add_role :admin, company
@@ -23,25 +35,17 @@ RSpec.describe "Edit company", type: :system do
 
         find("a[href='/profile/edit/organization-details']").click
         click_button "Edit"
+
         fill_in "companyName", with: "test company"
         fill_in "addressLine1", with: "Test address"
 
-        within("div#countrySelect") do
-          find(".react-select-filter__control.css-digfch-control").click
-          find("#react-select-9-option-232").click
-        end
-        within("div#stateSelect") do
-          find(".react-select-filter__control.css-digfch-control").click
-          find("#react-select-10-option-41").click
-        end
+        select_values_from_select_box
 
         fill_in "zipcode", with: "12238"
         click_button "Save"
 
         expect(page).to have_content("Changes saved successfully")
-
         company.reload
-
         expect(company.name).to eq("test company")
       end
     end
@@ -51,15 +55,15 @@ RSpec.describe "Edit company", type: :system do
     it "throws error when removing address1 value" do
       with_forgery_protection do
         visit "/profile/edit"
-        
+
         within("ul.tracking-wider") do
-            find('div:nth-child(2) > button').click
+          find("div:nth-child(2) > button").click
         end
 
-        find("a[href='/profile/edit/organization-details']").click
+        find('a[href="/profile/edit/organization-details"]').click
         click_button "Edit"
         fill_in "addressLine1", with: ""
-        
+
         click_button "Save"
 
         expect(page).to have_content("Address Line 1 cannot be blank")
