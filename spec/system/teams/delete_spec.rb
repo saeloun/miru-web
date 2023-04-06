fe7ac37d659a59fdc32aea82198b9b82c1781fb5
Apoorv Tiwari <tiwari.apoorv1316@gmail.com>
2023-04-06 15:39:34 +0530

    System specs for teams (#1180)
    * add spec for invtie team memebr
    
    * Fixed invitation mailer system spec
    
    * Fixed invitation mailer system spec
    
    * add spec for edit and delete
    
    * fix tests
    
    * add spec for employee
    
    ---------
    
    Co-authored-by: “Apoorv <“tiwari.apoorv1316@gmail.com”>
    Co-authored-by: prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
    Co-authored-by: Mayank Agnihotri <mayankagnihotri7@gmail.com>

---
+++ spec/system/teams/delete_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Delete team member", type: :system do
+  let(:company) { create(:company) }
+  let(:user) { create(:user, current_workspace_id: company.id) }
+  let(:employee_user) { create(:user, current_workspace_id: company.id) }
+
+  context "when user is an admin" do
+    before do
+      create(:employment, company:, user:)
+      create(:employment, company:, user: employee_user)
+      user.add_role :admin, company
+      employee_user.add_role :employee, company
+      sign_in(user)
+    end
+
+    context "when deleting team memeber" do
+      it "can delete a team member" do
+        with_forgery_protection do
+          visit "/teams"
+
+          expect(page).to have_content(employee_user.first_name)
+
+          el = all(:css, "#deleteMember", visible: false).last.hover
+          el.click
+          click_button "DELETE"
+          expect(page).not_to have_content(employee_user.first_name)
+        end
+      end
+    end
+  end
+end
