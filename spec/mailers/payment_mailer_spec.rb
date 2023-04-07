a93ee20fa3224e8c1850a61793a54bdcf1a9942a
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2023-04-07 11:18:53 +0530

    fix mailer test case

--- spec/mailers/payment_mailer_spec.rb
+++ spec/mailers/payment_mailer_spec.rb
@@ -7,13 +7,14 @@ RSpec.describe PaymentMailer, type: :mailer do
     let(:company) { create :company, :with_logo }
     let(:client) { create :client, company: }
     let(:invoice) { create :invoice, client: }
-    let(:recipients) { [invoice.client.email, "miru@example.com"] }
+    let(:user) { create(:user, current_workspace_id: company.id, companies: [company]) }
     let(:subject) { "Payment details by #{invoice.client.name}" }
-    let(:mail) { PaymentMailer.with(invoice:, subject:, recipients:).payment }
+    let(:mail) { PaymentMailer.with(invoice:, subject:).payment }
 
     it "renders the headers" do
+      user.add_role :admin, company
       expect(mail.subject).to eq(subject)
-      expect(mail.to).to eq(recipients)
+      expect(mail.to).to eq([user.email])
     end
 
     it "renders the body" do
