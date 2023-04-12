c5c0143f940949f3b78e8a71e7e415d148bdd895
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2023-04-12 14:23:06 +0530

    remove onblur

--- app/javascript/src/components/Clients/Modals/MobileMenu.tsx
+++ app/javascript/src/components/Clients/Modals/MobileMenu.tsx
@@ -13,7 +13,6 @@ const MobileMenu = ({ setShowDeleteDialog, setShowMobileModal }: IProps) => (
     style={{
       backgroundColor: "rgba(29, 26, 49, 0.6)",
     }}
-    onMouseDown={() => setShowMobileModal(false)}
   >
     <div className="relative flex h-full w-full flex-col items-center justify-center p-8 px-4 md:flex md:items-center md:justify-center">
       <div className="modal-width w-full min-w-0 transform rounded-lg bg-white px-6 pb-6 shadow-xl transition-all sm:max-w-md sm:align-middle">
