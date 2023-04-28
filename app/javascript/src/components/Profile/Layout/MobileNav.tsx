dd4716e1f7b94f6a42c33fdac45ebbd522c2bf60
Sanjib Roy <sanjibroy0098@gmail.com>
2023-04-28 21:05:04 +0530

    Make organization settings pages mobile responsive (#1268)
    * Design the header section
    
    * Make the static organization details page and settings nav bar mobile responsive
    
    * Handle the back button click
    
    * Make the edit organization details form page responsive
    
    * Ensure that user will be able to upload or delete profile image
    
    * Fix minor UI issues
    
    * Fix minor UI issues
    
    * Ensure that for ipad view the cancel button of header will not be disabled
    
    * Make the suggested changes and ensure that Loader will show in the center of the page

--- app/javascript/src/components/Profile/Layout/MobileNav.tsx
+++ app/javascript/src/components/Profile/Layout/MobileNav.tsx
@@ -1,33 +1,91 @@
 /* eslint-disable no-unused-vars */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { Outlet, useParams } from "react-router-dom";
+import { ClientsIcon as BuildingsIcon, UserIcon } from "miruIcons";
+import { Outlet, useNavigate, useParams } from "react-router-dom";
 
+import WorkspaceApi from "apis/workspaces";
+import Loader from "common/Loader/index";
 import withLayout from "common/Mobile/HOC/withLayout";
+import { useUserContext } from "context/UserContext";
 
 import { TeamUrl } from "./TeamUrl";
-import { UserInformation } from "./UserInformation";
+import { UserDetails } from "./UserDetails";
 
-const getTeamUrls = memberId => [
+const getSettingsNavUrls = memberId => [
   {
-    url: `/profile/edit`,
-    text: "PERSONAL DETAILS",
+    groupName: "Personal",
+    navItems: [
+      {
+        url: "/profile/edit",
+        text: "PERSONAL DETAILS",
+        icon: <UserIcon size={16} />,
+      },
+    ],
+  },
+  {
+    isCompanyDetails: true,
+    navItems: [
+      {
+        url: "/profile/edit/organization-details",
+        text: "ORG. SETTINGS",
+        icon: <BuildingsIcon size={16} />,
+      },
+    ],
   },
 ];
 
 const MobileNav = () => {
+  const { isDesktop, user } = useUserContext();
   const { memberId } = useParams();
-  const urlList = getTeamUrls(memberId);
+  const urlList = getSettingsNavUrls(memberId);
+  const navigate = useNavigate();
+  const [currentWorkspace, setCurrentWorkspace] = useState<any>({});
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const getCurrentWorkspace = async () => {
+    const res = await WorkspaceApi.get();
+    const { workspaces } = res.data;
+    workspaces.find(wrk => {
+      if (wrk.id == user.current_workspace_id) {
+        setCurrentWorkspace(wrk);
+      }
+    });
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    getCurrentWorkspace();
+  }, []);
+
+  useEffect(() => {
+    if (isDesktop) {
+      navigate("/profile/edit");
+    }
+  }, [isDesktop]);
+
   const mobileView = () => (
     <div>
-      <UserInformation />
-      <TeamUrl urlList={urlList} />
+      <UserDetails />
+      <TeamUrl
+        currentWorkspaceName={currentWorkspace?.name || ""}
+        urlList={urlList}
+      />
       <Outlet />
     </div>
   );
 
   const DisplayView = withLayout(mobileView, true, true);
+
+  if (isLoading) {
+    return (
+      <div className="flex h-80v w-full flex-col justify-center">
+        <Loader />
+      </div>
+    );
+  }
 
   return <DisplayView />;
 };
