9b8a2eee56fc16d477d3b575b5ba969ff87893b3
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2023-05-04 14:03:17 +0530

    Updated formik input box font size

--- app/javascript/src/common/FormikFields/InputField/index.tsx
+++ app/javascript/src/common/FormikFields/InputField/index.tsx
@@ -7,7 +7,7 @@ import { Field } from "formik";
 import { PasswordIconSVG, PasswordIconTextSVG } from "miruIcons";
 
 const defaultInputBoxClassName =
-  "form__input block w-full appearance-none bg-white p-3.75 text-sm lg:text-base h-12 border-miru-gray-1000";
+  "form__input block w-full appearance-none bg-white p-3.75 text-base lg:text-base h-12 border-miru-gray-1000";
 
 const defaultLabelClassname =
   "absolute top-0.5 left-1 h-6 z-1 origin-0 bg-white p-2 text-sm lg:text-base font-medium text-miru-dark-purple-200 duration-300";
