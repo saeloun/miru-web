27ac454c7017305a57151d5d5d82dd21176be711
Prasanth Chaduvula <chaduvulaprasanth1998@gmail.com>
2023-05-04 10:44:22 +0530

    Added empty state message to projects list page (#1281)
    * Added empty state message to projects list page
    
    * Removed search projects option empty porjects list page

--- app/javascript/src/components/Projects/List/Header.tsx
+++ app/javascript/src/components/Projects/List/Header.tsx
@@ -5,10 +5,13 @@ import { PlusIcon } from "miruIcons";
 
 import projectApi from "apis/projects";
 import AutoSearch from "common/AutoSearch";
+import { useUserContext } from "context/UserContext";
 
 import SearchDataRow from "./SearchDataRow";
 
-const Header = ({ setShowProjectModal, isAdminUser }) => {
+const Header = ({ projectDataPresent, setShowProjectModal, isAdminUser }) => {
+  const { isDesktop } = useUserContext();
+
   const fetchProjects = async searchString => {
     try {
       const res = await projectApi.search(searchString);
@@ -21,17 +24,20 @@ const Header = ({ setShowProjectModal, isAdminUser }) => {
 
   return (
     <div
-      className={`m-4 flex items-center lg:mx-0 lg:mt-6 lg:mb-3 ${
-        isAdminUser ? "justify-between" : ""
+      className={`m-4 flex items-center  lg:mx-0 lg:mt-6 lg:mb-3 ${
+        isAdminUser &&
+        `${isDesktop || projectDataPresent ? "justify-between" : "justify-end"}`
       }`}
     >
       <h2 className="header__title hidden lg:inline">Projects</h2>
       {isAdminUser && (
         <>
-          <AutoSearch
-            SearchDataRow={SearchDataRow}
-            searchAction={fetchProjects}
-          />
+          {projectDataPresent && (
+            <AutoSearch
+              SearchDataRow={SearchDataRow}
+              searchAction={fetchProjects}
+            />
+          )}
           <button
             className="header__button flex"
             onClick={() => setShowProjectModal(true)}
