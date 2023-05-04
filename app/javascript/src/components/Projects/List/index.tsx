605fb5fc728475f6a2b3c8baf9860b9438214a12
Prasanth Chaduvula <chaduvulaprasanth1998@gmail.com>
2023-05-04 10:44:22 +0530

    Added empty state message to projects list page (#1281)
    * Added empty state message to projects list page
    
    * Removed search projects option empty porjects list page

--- app/javascript/src/components/Projects/List/index.tsx
+++ app/javascript/src/components/Projects/List/index.tsx
@@ -4,6 +4,7 @@ import Logger from "js-logger";
 import { ToastContainer } from "react-toastify";
 
 import projectApi from "apis/projects";
+import EmptyStates from "common/EmptyStates";
 import withLayout from "common/Mobile/HOC/withLayout";
 import { TOASTER_DURATION } from "constants/index";
 import { useUserContext } from "context/UserContext";
@@ -47,55 +48,65 @@ const ProjectList = ({ isAdminUser }) => {
       <ToastContainer autoClose={TOASTER_DURATION} />
       <Header
         isAdminUser={isAdminUser}
+        projectDataPresent={Boolean(projects.length)}
         setShowProjectModal={setShowProjectModal}
       />
       <div className="flex flex-col">
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
             <div className="overflow-hidden">
-              <table className="mt-4 min-w-full divide-y divide-gray-200">
-                <thead>
-                  <tr>
-                    <th className="table__header" scope="col">
-                      PROJECT/CLIENT
-                    </th>
-                    <th
-                      className="table__header hidden lg:table-cell"
-                      scope="col"
-                    />
-                    <th
-                      className="table__header hidden text-right lg:table-cell"
-                      scope="col"
-                    >
-                      HOURS LOGGED
-                    </th>
-                    <th
-                      className="table__header text-right lg:hidden"
-                      scope="col"
-                    >
-                      HOURS
-                    </th>
-                    <th
-                      className="table__header hidden lg:table-cell"
-                      scope="col"
-                    />
-                    <th className="table__header" scope="col" />
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-200 bg-white">
-                  {projects.map((project, index) => (
-                    <Project
-                      key={index}
-                      {...project}
-                      isAdminUser={isAdminUser}
-                      setDeleteProjectData={setDeleteProjectData}
-                      setEditProjectData={setEditProjectData}
-                      setShowDeleteDialog={setShowDeleteDialog}
-                      setShowProjectModal={setShowProjectModal}
-                    />
-                  ))}
-                </tbody>
-              </table>
+              {projects && projects.length > 0 ? (
+                <table className="mt-4 min-w-full divide-y divide-gray-200">
+                  <thead>
+                    <tr>
+                      <th className="table__header" scope="col">
+                        PROJECT/CLIENT
+                      </th>
+                      <th
+                        className="table__header hidden lg:table-cell"
+                        scope="col"
+                      />
+                      <th
+                        className="table__header hidden text-right lg:table-cell"
+                        scope="col"
+                      >
+                        HOURS LOGGED
+                      </th>
+                      <th
+                        className="table__header text-right lg:hidden"
+                        scope="col"
+                      >
+                        HOURS
+                      </th>
+                      <th
+                        className="table__header hidden lg:table-cell"
+                        scope="col"
+                      />
+                      <th className="table__header" scope="col" />
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-200 bg-white">
+                    {projects.map((project, index) => (
+                      <Project
+                        key={index}
+                        {...project}
+                        isAdminUser={isAdminUser}
+                        setDeleteProjectData={setDeleteProjectData}
+                        setEditProjectData={setEditProjectData}
+                        setShowDeleteDialog={setShowDeleteDialog}
+                        setShowProjectModal={setShowProjectModal}
+                      />
+                    ))}
+                  </tbody>
+                </table>
+              ) : (
+                <EmptyStates
+                  Message="Looks like there aren’t any projects added yet."
+                  messageClassName="w-full lg:mt-5"
+                  showNoSearchResultState={false}
+                  wrapperClassName="mt-5"
+                />
+              )}
             </div>
           </div>
         </div>
