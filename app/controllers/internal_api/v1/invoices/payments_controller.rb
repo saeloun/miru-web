6f5efce2f0ea2d745385592836a99e232db38331
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2023-05-10 13:44:11 +0530

    send email if payment succeeds.

--- app/controllers/internal_api/v1/invoices/payments_controller.rb
+++ app/controllers/internal_api/v1/invoices/payments_controller.rb
@@ -8,16 +8,20 @@ class InternalApi::V1::Invoices::PaymentsController < InternalApi::V1::Applicati
 
   def success
     if InvoicePayment::StripePaymentIntent.new(@invoice).process
-      PaymentMailer.with(
-        invoice: @invoice,
-        subject: "Payment details by #{@invoice.client.name}").payment.deliver_later
+      if @invoice.paid?
+        PaymentMailer.with(
+          invoice: @invoice,
+          subject: "Payment details by #{@invoice.client.name}").payment.deliver_later
 
-      @invoice.send_to_client_email(
-        invoice: @invoice,
-        subject: "Payment Receipt of Invoice #{@invoice.invoice_number} from #{@invoice.company.name}"
-      )
+        @invoice.send_to_client_email(
+          invoice: @invoice,
+          subject: "Payment Receipt of Invoice #{@invoice.invoice_number} from #{@invoice.company.name}"
+        )
 
-      render json: { invoice: @invoice, notice: I18n.t("invoices.payments.success.success") }, status: :ok
+        render json: { invoice: @invoice, notice: I18n.t("invoices.payments.success.success") }, status: :ok
+      else
+        render json: { invoice: @invoice, notice: I18n.t("invoices.payments.success.success") }, status: :ok
+      end
     else
       render json: { error: I18n.t("invoices.payments.success.failure") }, status: :unprocessable_entity
     end
