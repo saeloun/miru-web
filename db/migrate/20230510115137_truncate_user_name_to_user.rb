1b80e13fc47446cc71c61c87064c4b2e1ce61370
Apoorv Tiwari <tiwari.apoorv1316@gmail.com>
2023-05-11 18:36:27 +0530

    Add length validation for first and last name for user model (#1305)
    add 20 length validation for first and last name
    
    Co-authored-by: “Apoorv <“tiwari.apoorv1316@gmail.com”>

---
+++ db/migrate/20230510115137_truncate_user_name_to_user.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class TruncateUserNameToUser < ActiveRecord::Migration[7.0]
+  def change
+    User.find_each do |user|
+      user.update(first_name: user.first_name[0..19], last_name: user.last_name[0..19])
+    end
+  end
+end
