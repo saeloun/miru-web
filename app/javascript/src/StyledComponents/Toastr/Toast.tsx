f6b4254ada8e7e52ce34abe478ef4e224a34711b
Prasanth Chaduvula <chaduvulaprasanth1998@gmail.com>
2023-05-15 13:24:04 +0530

    Removed react linkify (#1319)

--- app/javascript/src/StyledComponents/Toastr/Toast.tsx
+++ app/javascript/src/StyledComponents/Toastr/Toast.tsx
@@ -1,18 +1,8 @@
 import React from "react";
 
-import Linkify from "react-linkify";
-
 const Toast = ({ message, buttonLabel = "" }) => (
   <div>
-    <Linkify
-      componentDecorator={(decoratedHref, decoratedText, key) => (
-        <a href={decoratedHref} key={key} rel="noreferrer" target="_blank">
-          {decoratedText}
-        </a>
-      )}
-    >
-      <p>{message}</p>
-    </Linkify>
+    <p>{message}</p>
     {buttonLabel && (
       <div className="toastr-message-container__btn-wrapper">
         <button
