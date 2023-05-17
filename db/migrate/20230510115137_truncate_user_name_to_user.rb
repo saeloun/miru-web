84c655b5a563ae5fb1b49558e9bfcea5de54701b
“Apoorv <tiwari.apoorv1316@gmail.com>
2023-05-17 20:17:53 +0530

    add safe operator in migrations

--- db/migrate/20230510115137_truncate_user_name_to_user.rb
+++ db/migrate/20230510115137_truncate_user_name_to_user.rb
@@ -3,7 +3,7 @@
 class TruncateUserNameToUser < ActiveRecord::Migration[7.0]
   def change
     User.find_each do |user|
-      user.update(first_name: user.first_name[0..19], last_name: user.last_name[0..19])
+      user.update(first_name: user.first_name&.[0..19], last_name: user.last_name&.[0..19])
     end
   end
 end
