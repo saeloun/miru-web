6b784602c967c0f6a21d8324ab35e2adca8d81ac
Apoorv Tiwari <tiwari.apoorv1316@gmail.com>
2023-05-17 20:31:50 +0530

    Add safe operator in migrations (#1326)
    * add safe operator in migrations
    
    * fix migartion
    
    ---------
    
    Co-authored-by: “Apoorv <“tiwari.apoorv1316@gmail.com”>

--- db/migrate/20230511104809_truncate_project_name_to_project.rb
+++ db/migrate/20230511104809_truncate_project_name_to_project.rb
@@ -3,7 +3,7 @@
 class TruncateProjectNameToProject < ActiveRecord::Migration[7.0]
   def change
     Project.find_each do |project|
-      project.update(name: project.name.slice(0, 30))
+      project.update(name: project.name&.slice(0, 30))
     end
   end
 end
