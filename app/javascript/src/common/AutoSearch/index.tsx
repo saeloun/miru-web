637a935bece654607338b8bbda9e1120c1db5e73
Shruti-Apte <72149587+Shruti-Apte@users.noreply.github.com>
2023-05-23 07:42:12 +0530

    Consistent  Searchbar UI  (#1330)
    searchbar is now consistent on all pages

--- app/javascript/src/common/AutoSearch/index.tsx
+++ app/javascript/src/common/AutoSearch/index.tsx
@@ -31,31 +31,27 @@ const AutoSearch = ({ searchAction, SearchDataRow }: Iprops) => {
   };
 
   return (
-    <div className="header__searchWrap w-10/12 rounded-full border py-2 md:w-1/2 lg:w-1/3">
-      <div className="relative mx-auto w-11/12 rounded">
-        <div>
-          <input
-            className="outline-none w-full px-2 text-sm font-medium leading-5"
-            id="searchInput"
-            placeholder="Search"
-            type="text"
-            value={searchQuery}
-            onChange={e => setSearchQuery(e.target.value)}
-          />
-          <button className=" absolute inset-y-0 right-0 flex cursor-pointer items-center pr-3 ">
-            {searchQuery ? (
-              <XIcon size={12} onClick={onSearchClear} />
-            ) : (
-              <SearchIcon size={12} />
-            )}
-          </button>
-          <SearchDropdown
-            SearchedDataRow={SearchDataRow}
-            list={searchResult}
-            loading={loading}
-          />
-        </div>
-      </div>
+    <div className="relative mx-auto w-11/12 rounded-full md:w-1/2 lg:w-1/3">
+      <input
+        className="outline-none w-full rounded-full border-miru-gray-1000 bg-miru-gray-100 py-2 px-3 text-sm font-medium leading-5 focus:border focus:ring-1 focus:ring-miru-gray-1000"
+        id="searchInput"
+        placeholder="Search"
+        type="text"
+        value={searchQuery}
+        onChange={e => setSearchQuery(e.target.value)}
+      />
+      <button className=" absolute inset-y-0 right-3 flex cursor-pointer items-center pr-3 ">
+        {searchQuery ? (
+          <XIcon size={12} onClick={onSearchClear} />
+        ) : (
+          <SearchIcon className="text-miru-gray-1000" size={16} weight="bold" />
+        )}
+      </button>
+      <SearchDropdown
+        SearchedDataRow={SearchDataRow}
+        list={searchResult}
+        loading={loading}
+      />
     </div>
   );
 };
