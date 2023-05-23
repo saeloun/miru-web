7a85b64e4a9c704248cf16a324b1926d808f2f13
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2023-05-23 14:50:44 +0530

    fixed client info view on invoices

--- app/javascript/src/components/Invoices/Invoice/ClientInfo.tsx
+++ app/javascript/src/components/Invoices/Invoice/ClientInfo.tsx
@@ -9,11 +9,11 @@ const ClientInfo = ({ client }) => {
       <p className="absolute left-2 -top-2 bg-miru-gray-100 px-2 text-xs font-medium text-miru-dark-purple-1000">
         Billed to
       </p>
-      <div>
+      <div className="h-full overflow-y-scroll">
         <p className="text-xl font-bold leading-7 text-miru-dark-purple-1000">
           {client.name}
         </p>
-        <p className="truncateOverflowText w-52 text-sm font-normal text-miru-dark-purple-600">
+        <p className="w-52 text-sm font-normal text-miru-dark-purple-600">
           {`${address_line_1}${address_line_2 ? `, ${address_line_2}` : ""}${
             address_line_2 ? "," : ""
           }\n ${city}, ${state}, ${country},\n ${pin}`}
