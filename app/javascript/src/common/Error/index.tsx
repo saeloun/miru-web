b4a9788cc5c1d7f9b333dcabf3c871e17a369310
Shruti-Apte <72149587+Shruti-Apte@users.noreply.github.com>
2023-05-23 07:34:53 +0530

    Error page: 404 (#1327)
    error page updated according to designs

--- app/javascript/src/common/Error/index.tsx
+++ app/javascript/src/common/Error/index.tsx
@@ -1,5 +1,39 @@
 import React from "react";
 
-const ErrorPage = () => <h1>There is some Error on the page.</h1>;
+import { Animation_404 } from "miruIcons";
+import { useNavigate } from "react-router-dom";
+import { Button } from "StyledComponents";
 
+import withLayout from "common/Mobile/HOC/withLayout";
+import { useUserContext } from "context/UserContext";
+
+const ErrorPage = () => {
+  const navigate = useNavigate();
+  const { isDesktop } = useUserContext();
+
+  const ErrorPageLayout = () => (
+    <div className="flex h-90v w-full items-center justify-center lg:h-full">
+      <div className="flex h-full w-full flex-col items-center justify-center bg-miru-gray-100 px-4 lg:h-5/6">
+        <img className="h-auto w-5/6 lg:w-1/4" src={Animation_404} />
+        <span className="py-15/100 text-center text-base font-semibold leading-5 text-miru-dark-purple-200 lg:pt-5/100 lg:pb-2/100 lg:text-lg lg:leading-7">
+          Page not Found
+        </span>
+        <Button
+          className="w-full py-2 lg:w-1/4 lg:py-4"
+          style="primary"
+          onClick={() => {
+            navigate("/time-tracking");
+          }}
+        >
+          <span className="text-base font-bold leading-5 text-white lg:text-lg lg:leading-7">
+            Go to Home Page
+          </span>
+        </Button>
+      </div>
+    </div>
+  );
+  const Main = withLayout(ErrorPageLayout, !isDesktop, !isDesktop);
+
+  return isDesktop ? ErrorPageLayout() : <Main />;
+};
 export default ErrorPage;
