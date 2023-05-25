1b58f3500a1dc29ed3ac61127891503fd5a34024
“Apoorv <tiwari.apoorv1316@gmail.com>
2023-05-25 16:41:42 +0530

    context for user avatar

--- app/javascript/src/components/Navbar/UserInfo.tsx
+++ app/javascript/src/components/Navbar/UserInfo.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 
-import { NavAvatarSVG } from "miruIcons";
+import { useUserContext } from "context/UserContext";
 
-const UserInfo = ({ user }) => (
-  <div className="overflow-XIcon-auto flex h-16 w-full items-center bg-miru-gray-100 p-4 lg:mt-6">
-    <img alt="avatar" className="mr-2" src={NavAvatarSVG} />
-    <div className="overflow-XIcon-auto flex flex-col">
-      <span className="pt-1 text-base font-bold leading-5">{`${user.first_name} ${user.last_name}`}</span>
-      <span className="text-xs font-normal leading-4">{user.email}</span>
+import Avatar from "../../StyledComponents/Avatar";
+
+const UserInfo = ({ user }) => {
+  const { avatarUrl } = useUserContext();
+
+  return (
+    <div className="overflow-XIcon-auto flex h-16 w-full items-center bg-miru-gray-100 p-4 lg:mt-6">
+      <Avatar classNameImg="mr-2" url={avatarUrl} />
+      <div className="overflow-XIcon-auto flex flex-col">
+        <span className="pt-1 text-base font-bold leading-5">{`${user.first_name} ${user.last_name}`}</span>
+        <span className="text-xs font-normal leading-4">{user.email}</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default UserInfo;
