9bb0856bbb6954b2f01e03ab7816c8c95c1a9998
Shruti Apte <shrutiapte20@gmail.com>
2023-05-25 16:34:17 +0530

    scrolllbar added to sidenavigation

--- app/javascript/src/components/Navbar/index.tsx
+++ app/javascript/src/components/Navbar/index.tsx
@@ -7,14 +7,12 @@ import UserInfo from "./UserInfo";
 
 const Navbar = ({ user, companyRole }) => (
   <div className="fixed top-0 bottom-0 left-0 flex h-full w-1/6 flex-col justify-between shadow-2xl">
-    <div>
-      <Header />
+    <Header />
+    <div className="flex h-full flex-col justify-between overflow-y-auto">
       <Options companyRole={companyRole} />
-    </div>
-    <div>
       <UserActions />
-      <UserInfo user={user} />
     </div>
+    <UserInfo user={user} />
   </div>
 );
 
