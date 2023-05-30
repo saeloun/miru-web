5e27d3e55f1f91620dad8c8cef9060cd9f0d9704
Gowsik <gowsik@saeloun.com>
2023-05-30 14:58:04 +0530

    Removed unused files

--- spec/requests/internal_api/v1/invoices/waived_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "rails_helper"
-
-RSpec.describe "InternalApi::V1::Invoices::Waiveds", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
-end
