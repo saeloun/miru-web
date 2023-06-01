f8d08394a6079389c33e6fd378461ebd9971d916
Gowsik Vivekanandan <52308930+gowsik-ragunath@users.noreply.github.com>
2023-06-01 11:45:00 +0530

    Mark Invoice as waived (#1349)
    * Created controller and policy
    
    * Added waived feature
    
    * Fixed typo and updatede mobile view
    
    * Updated invoice email component
    
    * Removed unused files
    
    * Added waived to filter
    
    * Changed the method name
    
    * Changed the method name
    
    * Fixed invoice spec
    
    ---------
    
    Co-authored-by: Gowsik <gowsik@saeloun.com>

--- app/javascript/src/components/Invoices/Invoice/InvoiceActions.tsx
+++ app/javascript/src/components/Invoices/Invoice/InvoiceActions.tsx
@@ -14,6 +14,7 @@ const InvoiceActions = ({
   editInvoiceLink,
   sendInvoice,
   deleteInvoice,
+  wavieInvoice,
   invoice,
 }) => {
   const [isMoreOptionsVisible, setIsMoreOptionsVisible] =
@@ -47,6 +48,7 @@ const InvoiceActions = ({
             downloadInvoice={handleDownloadInvoice}
             invoice={invoice}
             markInvoiceAsPaid={markInvoiceAsPaid}
+            wavieInvoice={wavieInvoice}
           />
         )}
       </div>
