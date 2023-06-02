2c47ff0a07d66f09598c6c4eacca1c4784aa6ebb
Prasanth Chaduvula <chaduvulaprasanth1998@gmail.com>
2023-06-02 11:38:04 +0530

    Refactor client forms (#1352)
    * Split client forms code base into different components
    
    * Added disable client form btn functionality to utils
    
    * Disable form save btn if it fails validation
    
    * Moved to try catch & no reloading any more to update the client data
    
    * Extracted project list on client details to table component
    
    * Added TotalHoursChart component to show client total hours
    
    * Extracted project table headers to constants
    
    * Fixed client form system specs
    
    * Removed unnecessry api error from client forms
    
    * No more reloading on mobile client form update action

---
+++ app/javascript/src/components/Clients/Details/TotalHoursChart.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+import { cashFormatter, currencySymbol } from "helpers";
+
+import AmountBoxContainer from "common/AmountBox";
+import ChartBar from "common/ChartBar";
+
+const TotalHoursChart = ({
+  projectDetails,
+  totalMinutes,
+  overdueOutstandingAmount,
+  handleSelectChange,
+}) => {
+  const currencySymb = currencySymbol(overdueOutstandingAmount?.currency);
+
+  const amountBox = [
+    {
+      title: "OVERDUE",
+      amount:
+        currencySymb + cashFormatter(overdueOutstandingAmount?.overdue_amount),
+    },
+    {
+      title: "OUTSTANDING",
+      amount:
+        currencySymb +
+        cashFormatter(overdueOutstandingAmount?.outstanding_amount),
+    },
+  ];
+
+  return (
+    <div className="bg-miru-gray-100 py-10 px-10">
+      <div className="flex justify-end">
+        <select
+          className="focus:outline-none
+          m-0
+          border-none
+          bg-transparent
+          bg-clip-padding bg-no-repeat px-3
+          py-1.5
+          text-base
+          font-normal
+          text-miru-han-purple-1000
+          transition
+          ease-in-out"
+          onChange={handleSelectChange}
+        >
+          <option className="text-miru-dark-purple-600" value="week">
+            THIS WEEK
+          </option>
+          <option className="text-miru-dark-purple-600" value="month">
+            THIS MONTH
+          </option>
+          <option className="text-miru-dark-purple-600" value="year">
+            THIS YEAR
+          </option>
+        </select>
+      </div>
+      {projectDetails && (
+        <ChartBar data={projectDetails} totalMinutes={totalMinutes} />
+      )}
+      <AmountBoxContainer amountBox={amountBox} />
+    </div>
+  );
+};
+
+export default TotalHoursChart;
