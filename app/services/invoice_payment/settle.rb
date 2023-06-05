5762ea571604d982b126b607d997933ff7e29adf
“Apoorv <tiwari.apoorv1316@gmail.com>
2023-06-05 18:48:34 +0530

    add event for mark as paid invoice

--- app/services/invoice_payment/settle.rb
+++ app/services/invoice_payment/settle.rb
@@ -2,12 +2,12 @@
 
 module InvoicePayment
   class InvoicePayment::Settle < ApplicationService
-    attr_reader :payment_params
-    attr_accessor :payment, :invoice
+    attr_reader :payment_params, :payment, :invoice, :stripe_payment_data
 
-    def initialize(payment_params, invoice)
+    def initialize(payment_params, invoice, stripe_payment_data = {})
       @invoice = invoice
       @payment_params = payment_params
+      @stripe_payment_data = stripe_payment_data
     end
 
     def process
