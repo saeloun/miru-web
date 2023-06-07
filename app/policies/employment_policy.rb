e25093848873d59f5bd586dcb68c0df883da0b97
Apoorv Tiwari <tiwari.apoorv1316@gmail.com>
2023-06-07 14:36:07 +0530

    Add code to show and update employment (#1358)
    * add code to show and update employment
    
    * add request specs
    
    * add policy specs
    
    * fix policy
    
    ---------
    
    Co-authored-by: “Apoorv <“tiwari.apoorv1316@gmail.com”>

--- app/policies/employment_policy.rb
+++ app/policies/employment_policy.rb
@@ -4,4 +4,21 @@ class EmploymentPolicy < ApplicationPolicy
   def index?
     user_owner_role? || user_admin_role?
   end
+
+  def show?
+    authorize_current_user && (user_owner_role? || user_admin_role?)
+  end
+
+  def update?
+    authorize_current_user && (user_owner_role? || user_admin_role?)
+  end
+
+  def authorize_current_user
+    if user.current_workspace_id == record.company_id
+      true
+    else
+      @error_message_key = :different_workspace
+      false
+    end
+  end
 end
