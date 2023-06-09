385fb3197ef9b2887e517d5b63ef3c1861dd3559
Gowsik <v.gowsik@gmail.com>
2023-06-09 16:21:18 +0530

    Removed git at the end of message value

--- app/controllers/internal_api/v1/invoices_controller.rb
+++ app/controllers/internal_api/v1/invoices_controller.rb
@@ -63,7 +63,7 @@ class InternalApi::V1::InvoicesController < InternalApi::V1::ApplicationControll
       invoice.sending! unless invoice.paid? || invoice.overdue?
       invoice.send_to_email(
         subject: invoice_email_params[:subject],
-        message: invoice_email_params[:message],git 
+        message: invoice_email_params[:message],
         recipients: invoice_email_params[:recipients]
       )
 
