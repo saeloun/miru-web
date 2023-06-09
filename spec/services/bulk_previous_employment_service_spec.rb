3ee067e945795c342e947d15cbea681dc5c2c547
“Apoorv <tiwari.apoorv1316@gmail.com>
2023-06-09 17:41:42 +0530

    fix-service-spec

--- spec/services/bulk_previous_employment_service_spec.rb
+++ spec/services/bulk_previous_employment_service_spec.rb
@@ -5,8 +5,11 @@ require "rails_helper"
 RSpec.describe BulkPreviousEmploymentService do
   describe "#process" do
     let(:user) { create(:user) }
+    let(:previous_employment) { create(:previous_employment, user:) }
     let(:added_employments) { [{ "company_name" => "ABC Company", "role" => "Software Engineer" }] }
-    let(:updated_employments) { [{ "id" => 1, "company_name" => "Updated Company", "role" => "Updated Role" }] }
+    let(:updated_employments) {
+  [{ "id" => previous_employment.id, "company_name" => "Updated Company", "role" => "Updated Role" }]
+}
     let(:removed_employment_ids) { [2, 3] }
     let(:employments) do
       {
@@ -24,7 +27,9 @@ RSpec.describe BulkPreviousEmploymentService do
         service.process
       }.to change { user.previous_employments.count }.by(1)
 
-      updated_employment = user.previous_employments.find_by(id: 1)
+      p previous_employment
+      updated_employment = user.previous_employments.find_by!(id: previous_employment.id)
+      # updated_employment = PreviousEmployments.find_by(user_id: user.id, id:1)
       expect(updated_employment.company_name).to eq("Updated Company")
       expect(updated_employment.role).to eq("Updated Role")
 
