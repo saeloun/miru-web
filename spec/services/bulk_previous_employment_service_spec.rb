a9d8166ffbc4cf38767caf9c2105c6c01d09e092
“Apoorv <tiwari.apoorv1316@gmail.com>
2023-06-09 11:14:27 +0530

    add code for bulk opertaions on previous employments

---
+++ spec/services/bulk_previous_employment_service_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe BulkPreviousEmploymentService do
+  describe "#process" do
+    let(:user) { create(:user) }
+    let(:added_employments) { [{ "company_name" => "ABC Company", "role" => "Software Engineer" }] }
+    let(:updated_employments) { [{ "id" => 1, "company_name" => "Updated Company", "role" => "Updated Role" }] }
+    let(:removed_employment_ids) { [2, 3] }
+    let(:employments) do
+      {
+        user:,
+        added_employments:,
+        updated_employments:,
+        removed_employment_ids:
+      }
+    end
+
+    it "updates previous employments" do
+      service = BulkPreviousEmploymentService.new(employments)
+
+      expect {
+        service.process
+      }.to change { user.previous_employments.count }.by(1)
+
+      updated_employment = user.previous_employments.find_by(id: 1)
+      expect(updated_employment.company_name).to eq("Updated Company")
+      expect(updated_employment.role).to eq("Updated Role")
+
+      expect(user.previous_employments.pluck(:id)).not_to include(2, 3)
+    end
+  end
+end
