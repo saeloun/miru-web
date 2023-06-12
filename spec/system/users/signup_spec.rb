5e64e973a2cb89676cdad454e8acd4f70dfe1376
Gowsik Vivekanandan <52308930+gowsik-ragunath@users.noreply.github.com>
2023-06-12 14:52:09 +0530

    Disable signup (#1367)
    * change version
    
    * update version to 0.10.0
    
    * Added validation
    
    * Discarded invoice cant be viewed and only 5 recipient can be added
    
    * Discarded invoice cant be viewed and only 5 recipient can be added
    
    * Removed git at the end of message value
    
    * Disabled sign up
    
    * Comments test related to signup
    
    * removed sign up system spec
    
    ---------
    
    Co-authored-by: “Apoorv <“tiwari.apoorv1316@gmail.com”>
    Co-authored-by: Gowsik <gowsik@saeloun.com>

--- spec/system/users/signup_spec.rb
+++ spec/system/users/signup_spec.rb
@@ -1,263 +1,263 @@
 # frozen_string_literal: true
 
-require "rails_helper"
+# require "rails_helper"
 
-RSpec.describe "User Signup", type: :system do
-  let(:user) { build(:user, password: "Welcome@123") }
-  let(:existing_user) { create(:user) }
+# RSpec.describe "User Signup", type: :system do
+#   let(:user) { build(:user, password: "Welcome@123") }
+#   let(:existing_user) { create(:user) }
 
-  context "when signing up with a new email" do
-    it "allows a user to sign up for the website" do
-      with_forgery_protection do
-        visit "/signup"
+#   context "when signing up with a new email" do
+#     it "allows a user to sign up for the website" do
+#       with_forgery_protection do
+#         visit "/signup"
 
-        fill_in "first_name", with: user.first_name
-        fill_in "last_name", with: user.last_name
-        fill_in "email", with: user.email
-        fill_in "password", with: user.password
-        fill_in "confirm_password", with: user.password
-        find(:css, "#termsOfService", visible: false).set(true)
+#         fill_in "first_name", with: user.first_name
+#         fill_in "last_name", with: user.last_name
+#         fill_in "email", with: user.email
+#         fill_in "password", with: user.password
+#         fill_in "confirm_password", with: user.password
+#         find(:css, "#termsOfService", visible: false).set(true)
 
-        click_on "Sign Up"
+#         click_on "Sign Up"
 
-        expect(page).to have_content("Email Verification")
-        expect(page).to have_content(user.email)
-      end
-    end
+#         expect(page).to have_content("Email Verification")
+#         expect(page).to have_content(user.email)
+#       end
+#     end
 
-    it "allows a user to sign up with a password containing hyphens as special characters" do
-      with_forgery_protection do
-        visit "/signup"
+#     it "allows a user to sign up with a password containing hyphens as special characters" do
+#       with_forgery_protection do
+#         visit "/signup"
 
-        fill_in "first_name", with: user.first_name
-        fill_in "last_name", with: user.last_name
-        fill_in "email", with: user.email
-        fill_in "password", with: "wohXyq-cusgoz-fexde0"
-        fill_in "confirm_password", with: "wohXyq-cusgoz-fexde0"
-        find(:css, "#termsOfService", visible: false).set(true)
+#         fill_in "first_name", with: user.first_name
+#         fill_in "last_name", with: user.last_name
+#         fill_in "email", with: user.email
+#         fill_in "password", with: "wohXyq-cusgoz-fexde0"
+#         fill_in "confirm_password", with: "wohXyq-cusgoz-fexde0"
+#         find(:css, "#termsOfService", visible: false).set(true)
 
-        click_on "Sign Up"
+#         click_on "Sign Up"
 
-        expect(page).to have_content("Email Verification")
-        expect(page).to have_content(user.email)
-      end
-    end
+#         expect(page).to have_content("Email Verification")
+#         expect(page).to have_content(user.email)
+#       end
+#     end
 
-    it "allows a user to sign up with a password containing blank spaces in between" do
-      with_forgery_protection do
-        visit "/signup"
+#     it "allows a user to sign up with a password containing blank spaces in between" do
+#       with_forgery_protection do
+#         visit "/signup"
 
-        fill_in "first_name", with: user.first_name
-        fill_in "last_name", with: user.last_name
-        fill_in "email", with: user.email
-        fill_in "password", with: "My password with spaces@2023"
-        fill_in "confirm_password", with: "My password with spaces@2023"
-        find(:css, "#termsOfService", visible: false).set(true)
+#         fill_in "first_name", with: user.first_name
+#         fill_in "last_name", with: user.last_name
+#         fill_in "email", with: user.email
+#         fill_in "password", with: "My password with spaces@2023"
+#         fill_in "confirm_password", with: "My password with spaces@2023"
+#         find(:css, "#termsOfService", visible: false).set(true)
 
-        click_on "Sign Up"
+#         click_on "Sign Up"
 
-        expect(page).to have_content("Email Verification")
-        expect(page).to have_content(user.email)
-      end
-    end
+#         expect(page).to have_content("Email Verification")
+#         expect(page).to have_content(user.email)
+#       end
+#     end
 
-    it "allows to verify their email" do
-      with_forgery_protection do
-        visit "/signup"
+#     it "allows to verify their email" do
+#       with_forgery_protection do
+#         visit "/signup"
 
-        fill_in "first_name", with: user.first_name
-        fill_in "last_name", with: user.last_name
-        fill_in "email", with: user.email
-        fill_in "password", with: user.password
-        fill_in "confirm_password", with: user.password
-        find(:css, "#termsOfService", visible: false).set(true)
+#         fill_in "first_name", with: user.first_name
+#         fill_in "last_name", with: user.last_name
+#         fill_in "email", with: user.email
+#         fill_in "password", with: user.password
+#         fill_in "confirm_password", with: user.password
+#         find(:css, "#termsOfService", visible: false).set(true)
 
-        click_on "Sign Up"
+#         click_on "Sign Up"
 
-        expect(page).to have_content("Email Verification")
-        expect(page).to have_content(user.email)
+#         expect(page).to have_content("Email Verification")
+#         expect(page).to have_content(user.email)
 
-        email_body = ActionMailer::Base.deliveries.first.body.to_s
-        confirmation_token = email_body.match(/confirmation_token=([\w-]+)/)[1]
+#         email_body = ActionMailer::Base.deliveries.first.body.to_s
+#         confirmation_token = email_body.match(/confirmation_token=([\w-]+)/)[1]
 
-        visit user_confirmation_path(confirmation_token:)
+#         visit user_confirmation_path(confirmation_token:)
 
-        expect(page).to have_content "Thanks for verifying your email."
-      end
-    end
-  end
+#         expect(page).to have_content "Thanks for verifying your email."
+#       end
+#     end
+#   end
 
-  context "when attempting to sign up with an existing email" do
-    it "throws an error when using already registered email" do
-      with_forgery_protection do
-        visit "/signup"
+#   context "when attempting to sign up with an existing email" do
+#     it "throws an error when using already registered email" do
+#       with_forgery_protection do
+#         visit "/signup"
 
-        fill_in "first_name", with: user.first_name
-        fill_in "last_name", with: user.last_name
-        fill_in "email", with: existing_user.email
-        fill_in "password", with: user.password
-        fill_in "confirm_password", with: user.password
-        find(:css, "#termsOfService", visible: false).set(true)
+#         fill_in "first_name", with: user.first_name
+#         fill_in "last_name", with: user.last_name
+#         fill_in "email", with: existing_user.email
+#         fill_in "password", with: user.password
+#         fill_in "confirm_password", with: user.password
+#         find(:css, "#termsOfService", visible: false).set(true)
 
-        click_on "Sign Up"
-        sleep 1
-        expect(page).to have_content "Email ID already exists"
-      end
-    end
-  end
+#         click_on "Sign Up"
+#         sleep 1
+#         expect(page).to have_content "Email ID already exists"
+#       end
+#     end
+#   end
 
-  context "when attempting to sign up with invalid password" do
-    it "throws an error when using password with less than 8 characters" do
-      with_forgery_protection do
-        visit "/signup"
+#   context "when attempting to sign up with invalid password" do
+#     it "throws an error when using password with less than 8 characters" do
+#       with_forgery_protection do
+#         visit "/signup"
 
-        fill_in "first_name", with: user.first_name
-        fill_in "last_name", with: user.last_name
-        fill_in "email", with: existing_user.email
-        fill_in "password", with: "Pass@1"
-        fill_in "confirm_password", with: "Pass@1"
-        find(:css, "#termsOfService", visible: false).set(true)
+#         fill_in "first_name", with: user.first_name
+#         fill_in "last_name", with: user.last_name
+#         fill_in "email", with: existing_user.email
+#         fill_in "password", with: "Pass@1"
+#         fill_in "confirm_password", with: "Pass@1"
+#         find(:css, "#termsOfService", visible: false).set(true)
 
-        click_on "Sign Up"
+#         click_on "Sign Up"
 
-        expect(page).to have_content "Must Contain at least 8 Characters, One Uppercase, One Lowercase, One Number and One Special Character"
-      end
-    end
+#         expect(page).to have_content "Must Contain at least 8 Characters, One Uppercase, One Lowercase, One Number and One Special Character"
+#       end
+#     end
 
-    it "throws an error when using password without at least one uppercase letter" do
-      with_forgery_protection do
-        visit "/signup"
+#     it "throws an error when using password without at least one uppercase letter" do
+#       with_forgery_protection do
+#         visit "/signup"
 
-        fill_in "first_name", with: user.first_name
-        fill_in "last_name", with: user.last_name
-        fill_in "email", with: existing_user.email
-        fill_in "password", with: "password@2023"
-        fill_in "confirm_password", with: "password@2023"
-        find(:css, "#termsOfService", visible: false).set(true)
+#         fill_in "first_name", with: user.first_name
+#         fill_in "last_name", with: user.last_name
+#         fill_in "email", with: existing_user.email
+#         fill_in "password", with: "password@2023"
+#         fill_in "confirm_password", with: "password@2023"
+#         find(:css, "#termsOfService", visible: false).set(true)
 
-        click_on "Sign Up"
+#         click_on "Sign Up"
 
-        expect(page).to have_content "Must Contain at least 8 Characters, One Uppercase, One Lowercase, One Number and One Special Character"
-      end
-    end
+#         expect(page).to have_content "Must Contain at least 8 Characters, One Uppercase, One Lowercase, One Number and One Special Character"
+#       end
+#     end
 
-    it "throws an error when using password without at least one lowercase letter" do
-      with_forgery_protection do
-        visit "/signup"
+#     it "throws an error when using password without at least one lowercase letter" do
+#       with_forgery_protection do
+#         visit "/signup"
 
-        fill_in "first_name", with: user.first_name
-        fill_in "last_name", with: user.last_name
-        fill_in "email", with: existing_user.email
-        fill_in "password", with: "PASSWORD@2023"
-        fill_in "confirm_password", with: "PASSWORD@2023"
-        find(:css, "#termsOfService", visible: false).set(true)
+#         fill_in "first_name", with: user.first_name
+#         fill_in "last_name", with: user.last_name
+#         fill_in "email", with: existing_user.email
+#         fill_in "password", with: "PASSWORD@2023"
+#         fill_in "confirm_password", with: "PASSWORD@2023"
+#         find(:css, "#termsOfService", visible: false).set(true)
 
-        click_on "Sign Up"
+#         click_on "Sign Up"
 
-        expect(page).to have_content "Must Contain at least 8 Characters, One Uppercase, One Lowercase, One Number and One Special Character"
-      end
-    end
+#         expect(page).to have_content "Must Contain at least 8 Characters, One Uppercase, One Lowercase, One Number and One Special Character"
+#       end
+#     end
 
-    it "throws an error when using password without at least one number" do
-      with_forgery_protection do
-        visit "/signup"
+#     it "throws an error when using password without at least one number" do
+#       with_forgery_protection do
+#         visit "/signup"
 
-        fill_in "first_name", with: user.first_name
-        fill_in "last_name", with: user.last_name
-        fill_in "email", with: existing_user.email
-        fill_in "password", with: "@Password"
-        fill_in "confirm_password", with: "@Password"
-        find(:css, "#termsOfService", visible: false).set(true)
+#         fill_in "first_name", with: user.first_name
+#         fill_in "last_name", with: user.last_name
+#         fill_in "email", with: existing_user.email
+#         fill_in "password", with: "@Password"
+#         fill_in "confirm_password", with: "@Password"
+#         find(:css, "#termsOfService", visible: false).set(true)
 
-        click_on "Sign Up"
+#         click_on "Sign Up"
 
-        expect(page).to have_content "Must Contain at least 8 Characters, One Uppercase, One Lowercase, One Number and One Special Character"
-      end
-    end
+#         expect(page).to have_content "Must Contain at least 8 Characters, One Uppercase, One Lowercase, One Number and One Special Character"
+#       end
+#     end
 
-    it "throws an error when using password without at least one special character" do
-      with_forgery_protection do
-        visit "/signup"
+#     it "throws an error when using password without at least one special character" do
+#       with_forgery_protection do
+#         visit "/signup"
 
-        fill_in "first_name", with: user.first_name
-        fill_in "last_name", with: user.last_name
-        fill_in "email", with: existing_user.email
-        fill_in "password", with: "Password2023"
-        fill_in "confirm_password", with: "Password2023"
-        find(:css, "#termsOfService", visible: false).set(true)
+#         fill_in "first_name", with: user.first_name
+#         fill_in "last_name", with: user.last_name
+#         fill_in "email", with: existing_user.email
+#         fill_in "password", with: "Password2023"
+#         fill_in "confirm_password", with: "Password2023"
+#         find(:css, "#termsOfService", visible: false).set(true)
 
-        click_on "Sign Up"
+#         click_on "Sign Up"
 
-        expect(page).to have_content "Must Contain at least 8 Characters, One Uppercase, One Lowercase, One Number and One Special Character"
-      end
-    end
+#         expect(page).to have_content "Must Contain at least 8 Characters, One Uppercase, One Lowercase, One Number and One Special Character"
+#       end
+#     end
 
-    it "throws an error when using a password with spaces as special characters" do
-      with_forgery_protection do
-        visit "/signup"
+#     it "throws an error when using a password with spaces as special characters" do
+#       with_forgery_protection do
+#         visit "/signup"
 
-        fill_in "first_name", with: user.first_name
-        fill_in "last_name", with: user.last_name
-        fill_in "email", with: existing_user.email
-        fill_in "password", with: "Password 2023"
-        fill_in "confirm_password", with: "Password 2023"
-        find(:css, "#termsOfService", visible: false).set(true)
+#         fill_in "first_name", with: user.first_name
+#         fill_in "last_name", with: user.last_name
+#         fill_in "email", with: existing_user.email
+#         fill_in "password", with: "Password 2023"
+#         fill_in "confirm_password", with: "Password 2023"
+#         find(:css, "#termsOfService", visible: false).set(true)
 
-        click_on "Sign Up"
+#         click_on "Sign Up"
 
-        expect(page).to have_content "Must Contain at least 8 Characters, One Uppercase, One Lowercase, One Number and One Special Character"
-      end
-    end
+#         expect(page).to have_content "Must Contain at least 8 Characters, One Uppercase, One Lowercase, One Number and One Special Character"
+#       end
+#     end
 
-    it "throws an error when using a password that starts with blank space" do
-      with_forgery_protection do
-        visit "/signup"
+#     it "throws an error when using a password that starts with blank space" do
+#       with_forgery_protection do
+#         visit "/signup"
 
-        fill_in "first_name", with: user.first_name
-        fill_in "last_name", with: user.last_name
-        fill_in "email", with: existing_user.email
-        fill_in "password", with: " Password@2023"
-        fill_in "confirm_password", with: " Password@2023"
-        find(:css, "#termsOfService", visible: false).set(true)
+#         fill_in "first_name", with: user.first_name
+#         fill_in "last_name", with: user.last_name
+#         fill_in "email", with: existing_user.email
+#         fill_in "password", with: " Password@2023"
+#         fill_in "confirm_password", with: " Password@2023"
+#         find(:css, "#termsOfService", visible: false).set(true)
 
-        click_on "Sign Up"
+#         click_on "Sign Up"
 
-        expect(page).to have_content "Password cannot start or end with a blank space"
-      end
-    end
+#         expect(page).to have_content "Password cannot start or end with a blank space"
+#       end
+#     end
 
-    it "throws an error when using a password that ends with blank space" do
-      with_forgery_protection do
-        visit "/signup"
+#     it "throws an error when using a password that ends with blank space" do
+#       with_forgery_protection do
+#         visit "/signup"
 
-        fill_in "first_name", with: user.first_name
-        fill_in "last_name", with: user.last_name
-        fill_in "email", with: existing_user.email
-        fill_in "password", with: "Password@2023 "
-        fill_in "confirm_password", with: "Password@2023 "
-        find(:css, "#termsOfService", visible: false).set(true)
+#         fill_in "first_name", with: user.first_name
+#         fill_in "last_name", with: user.last_name
+#         fill_in "email", with: existing_user.email
+#         fill_in "password", with: "Password@2023 "
+#         fill_in "confirm_password", with: "Password@2023 "
+#         find(:css, "#termsOfService", visible: false).set(true)
 
-        click_on "Sign Up"
+#         click_on "Sign Up"
 
-        expect(page).to have_content "Password cannot start or end with a blank space"
-      end
-    end
+#         expect(page).to have_content "Password cannot start or end with a blank space"
+#       end
+#     end
 
-    it "throws an error when password and confirm password do not match" do
-      with_forgery_protection do
-        visit "/signup"
+#     it "throws an error when password and confirm password do not match" do
+#       with_forgery_protection do
+#         visit "/signup"
 
-        fill_in "first_name", with: user.first_name
-        fill_in "last_name", with: user.last_name
-        fill_in "email", with: existing_user.email
-        fill_in "password", with: "Welcome@123"
-        fill_in "confirm_password", with: "HelloWorld@123"
-        find(:css, "#termsOfService", visible: false).set(true)
+#         fill_in "first_name", with: user.first_name
+#         fill_in "last_name", with: user.last_name
+#         fill_in "email", with: existing_user.email
+#         fill_in "password", with: "Welcome@123"
+#         fill_in "confirm_password", with: "HelloWorld@123"
+#         find(:css, "#termsOfService", visible: false).set(true)
 
-        click_on "Sign Up"
+#         click_on "Sign Up"
 
-        expect(page).to have_content "Passwords must match"
-      end
-    end
-  end
-end
+#         expect(page).to have_content "Passwords must match"
+#       end
+#     end
+#   end
+# end
