c18b8ad6b2db9338e207a2cfef811ad3fc92843f
Gowsik Vivekanandan <52308930+gowsik-ragunath@users.noreply.github.com>
2023-06-12 14:52:09 +0530

    Disable signup (#1367)
    * change version
    
    * update version to 0.10.0
    
    * Added validation
    
    * Discarded invoice cant be viewed and only 5 recipient can be added
    
    * Discarded invoice cant be viewed and only 5 recipient can be added
    
    * Removed git at the end of message value
    
    * Disabled sign up
    
    * Comments test related to signup
    
    * removed sign up system spec
    
    ---------
    
    Co-authored-by: “Apoorv <“tiwari.apoorv1316@gmail.com”>
    Co-authored-by: Gowsik <gowsik@saeloun.com>

--- app/javascript/src/components/Authentication/SignUp/index.tsx
+++ app/javascript/src/components/Authentication/SignUp/index.tsx
@@ -2,13 +2,11 @@ import React from "react";
 
 import MiruLogoWatermark from "common/MiruLogoWatermark";
 
-import SignUpForm from "./SignUpForm";
-
 import FeaturePreviews from "../FeaturePreviews";
 
 const SignUp = () => (
   <div className="relative flex min-h-screen">
-    <SignUpForm />
+    {/* <SignUpForm /> */}
     <FeaturePreviews />
     <MiruLogoWatermark />
   </div>
