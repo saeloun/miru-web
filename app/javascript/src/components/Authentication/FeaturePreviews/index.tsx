0defff6416ffa1d7fe577aace02b6af144638e8a
Shruti Apte <shrutiapte20@gmail.com>
2023-06-19 17:13:31 +0530

    overlapping issue and alignment issue on responsive screen fixed

--- app/javascript/src/components/Authentication/FeaturePreviews/index.tsx
+++ app/javascript/src/components/Authentication/FeaturePreviews/index.tsx
@@ -9,47 +9,53 @@ import { MIRU_APP_URL } from "constants/index";
 import { carouselItems } from "./utils";
 
 const FeaturePreviews = () => (
-  <div className="hidden min-h-full bg-miru-han-purple-1000 md:w-1/2 md:pt-40 lg:block">
+  <div className="hidden min-h-full bg-miru-han-purple-1000 md:w-1/2 lg:block">
     <a href={MIRU_APP_URL} rel="noreferrer noopener">
       <div className="absolute right-10 top-10 z-10">
         <img alt="" height="64" src={BlurredMiruLogo} width="64" />
       </div>
     </a>
-    <div className="mx-auto w-8/12 md:pt-20">
+    <div
+      className="mx-auto h-full w-8/12 items-center justify-center"
+      id="feature-carousel"
+    >
       <Carousel
         autoPlay
         infiniteLoop
+        dynamicHeight={false}
         interval={5000}
         showArrows={false}
         showStatus={false}
         showThumbs={false}
       >
         {carouselItems?.map((feature, i) => (
-          <div key={i}>
+          <div className="flex h-full w-full flex-col justify-between" key={i}>
             {feature.type == "image" ? (
-              <img
-                className="h-300 mb-4 w-480 object-contain"
-                src={feature.image}
-                style={{ boxShadow: "0px 0px 32px rgba(0, 0, 0, 0.1)" }}
-              />
+              <div className="player-wrapper mb-4 h-3/4 w-full">
+                <img
+                  className="h-full w-full"
+                  src={feature.image}
+                  style={{ boxShadow: "0px 0px 32px rgba(0, 0, 0, 0.1)" }}
+                />
+              </div>
             ) : (
-              <div className="player-wrapper">
+              <div className="player-wrapper mb-4 h-full w-full">
                 <ReactPlayer
                   loop
                   muted
                   playing
-                  className="react-player"
+                  className="react-player object-contain"
                   height="100%"
                   url={feature.url}
                   width="100%"
                 />
               </div>
             )}
-            <div>
-              <h2 className="mb-3 font-manrope text-2xl font-semibold not-italic text-miru-white-1000">
+            <div className="h-1/5">
+              <h2 className="mb-auto font-manrope text-2vh font-semibold not-italic text-miru-white-1000">
                 {feature.texts.title}
               </h2>
-              <p className="mb-10 font-manrope text-xs font-normal not-italic text-miru-white-1000">
+              <p className="mb-auto font-manrope text-xs font-normal not-italic text-miru-white-1000">
                 {feature.texts.description}
               </p>
             </div>
