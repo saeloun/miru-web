18c967da5927cb6be4f90e6e666669c1f5ecdcf0
Prasanth Chaduvula <chaduvulaprasanth1998@gmail.com>
2023-06-26 12:36:03 +0530

    Client invoices dashboard mobile responsive (#1390)
    * made client invoices list page mobile responsive
    
    * made client invoice details responsive
    
    * fixed navigation url on miru icon

--- app/javascript/src/components/Navbar/Mobile/Header.tsx
+++ app/javascript/src/components/Navbar/Mobile/Header.tsx
@@ -4,15 +4,45 @@ import React, { Fragment } from "react";
 import { MiruLogoSVG, PurpleMiruLogoWithTextSVG } from "miruIcons";
 import { Link } from "react-router-dom";
 
-import { Paths } from "constants/index";
+import { Roles, Paths } from "constants/index";
+import { useUserContext } from "context/UserContext";
 
 const Header = ({ selectedTab }) => {
+  const { user, companyRole } = useUserContext();
+
+  const rootPath = () => {
+    if (!user) {
+      window.location.href = Paths.SIGN_IN;
+
+      return null;
+    }
+
+    let url;
+
+    switch (companyRole) {
+      case Roles.BOOK_KEEPER:
+        url = Paths.PAYMENTS;
+        break;
+      case Roles.OWNER:
+        url = "invoices";
+        break;
+      case Roles.CLIENT:
+        url = "invoices";
+        break;
+      default:
+        url = Paths.TIME_TRACKING;
+        break;
+    }
+
+    return `/${url}`;
+  };
+
   const getHeaderContent = () => {
     if (selectedTab == "More" || selectedTab == null) {
       return (
         <Link
           className="flex w-full items-center justify-center"
-          to={Paths.TIME_TRACKING}
+          to={rootPath()}
         >
           <img
             alt="miru-logo"
@@ -25,10 +55,7 @@ const Header = ({ selectedTab }) => {
 
     return (
       <Fragment>
-        <Link
-          className="flex items-center justify-center"
-          to={Paths.TIME_TRACKING}
-        >
+        <Link className="flex items-center justify-center" to={rootPath()}>
           <img alt="miru-logo" className="h-6 w-6" src={MiruLogoSVG} />
         </Link>
         <span className="z-40 w-full pr-3 text-center text-base font-bold leading-5 text-miru-han-purple-1000">
