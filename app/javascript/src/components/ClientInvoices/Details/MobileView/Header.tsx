3cb30498d7386b0a6824f212a3594decc5798f8c
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2023-06-26 17:57:47 +0530

    made invoices email view mobile responsive

--- app/javascript/src/components/ClientInvoices/Details/MobileView/Header.tsx
+++ app/javascript/src/components/ClientInvoices/Details/MobileView/Header.tsx
@@ -1,29 +1,34 @@
 import React from "react";
 
 import { ArrowLeftIcon } from "miruIcons";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Badge, Button } from "StyledComponents";
 
 import getStatusCssClass from "utils/getBadgeStatus";
 
 const Header = ({ invoice }) => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const { pathname } = location;
 
   return (
     <div className="sticky top-0 left-0 right-0 z-50 flex h-12 items-center justify-between bg-white px-4 shadow-c1">
       <div className="flex items-center">
-        <Button
-          style="ternary"
-          onClick={() => {
-            navigate("/invoices");
-          }}
-        >
-          <ArrowLeftIcon
-            className="mr-4 text-miru-dark-purple-1000"
-            size={16}
-            weight="bold"
-          />
-        </Button>
+        {!pathname.includes("view") && (
+          <Button
+            style="ternary"
+            onClick={() => {
+              navigate("/invoices");
+            }}
+          >
+            <ArrowLeftIcon
+              className="mr-4 text-miru-dark-purple-1000"
+              size={16}
+              weight="bold"
+            />
+          </Button>
+        )}
         <span>Invoice #{invoice.invoice_number}</span>
       </div>
       <div>
