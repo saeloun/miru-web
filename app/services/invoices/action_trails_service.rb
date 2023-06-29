205b5e341493ba1ab42ac1f628bc263db2908d61
Gowsik Vivekanandan <52308930+gowsik-ragunath@users.noreply.github.com>
2023-06-29 10:26:11 +0530

    Added payment trails (#1394)
    * Added payment trails
    
    * Removed pay_invoice event from event trail
    
    * Added user name in manual payment

--- app/services/invoices/action_trails_service.rb
+++ app/services/invoices/action_trails_service.rb
@@ -1,19 +1,23 @@
 # frozen_string_literal: true
 
 class Invoices::ActionTrailsService < ApplicationService
-  attr_reader :invoice_id, :events, :users, :trails
+  attr_reader :invoice_id, :events, :users, :trails, :payment_events, :payment_trails
 
   def initialize(invoice_id)
     @invoice_id = invoice_id
     @events = nil
+    @payment_events = nil
     @users = {}
     @trails = []
+    @payment_trails = []
   end
 
   def process
     fetch_events
+    fetch_payment_events
     fetch_users
     generate_trails_from_events
+    generate_trails_from_payment_events
   end
 
   private
@@ -22,9 +26,22 @@ class Invoices::ActionTrailsService < ApplicationService
       @events = Ahoy::Event.where_properties({ type: :invoice, id: invoice_id.to_i }).order({ time: :desc })
     end
 
+    def fetch_payment_events
+      invoice = Invoice.find_by(id: invoice_id)
+      return [] if invoice.nil?
+
+      @payment_events = invoice.payments.order(created_at: :desc)
+    end
+
     def generate_trails_from_events
       events.each do |event|
-        @trails << generate_trail_from_event(event)
+        trails << generate_trail_from_event(event)
+      end
+    end
+
+    def generate_trails_from_payment_events
+      payment_events.each do |event|
+        @payment_trails << generate_trail_from_payment_event(event)
       end
     end
 
@@ -34,9 +51,24 @@ class Invoices::ActionTrailsService < ApplicationService
         generic_trail_data(event)
       when "send_invoice"
         send_invoice_trail_data(event)
-      when "pay_invoice"
-        pay_invoice_trail_data(event)
       end
+    end
+
+    def generate_trail_from_payment_event(event)
+      {
+        type: event.status,
+        name: event.name,
+        note: event.note,
+        amount: event.amount,
+        created_at: event.created_at
+      }
+    end
+
+    def generate_trail_hash
+      [
+        trails: @trails,
+        payment_trails: @payment_trails
+      ]
     end
 
     def fetch_users
