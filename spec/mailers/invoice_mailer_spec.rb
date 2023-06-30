2d48838756813fdb2b3b4c0dcd1ab9f257e4b89d
gowsik <v.gowsik@gmail.com>
2023-06-30 17:26:12 +0530

    Fixed failed test cases

--- spec/mailers/invoice_mailer_spec.rb
+++ spec/mailers/invoice_mailer_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe InvoiceMailer, type: :mailer do
   describe "invoice" do
     let(:company) { create :company, :with_logo }
     let(:client) { create :client, company: }
-    let(:invoice) { create :invoice, client:, company: }
+    let(:invoice) { create :invoice, client:, company:, status: :sending }
     let(:recipients) { [invoice.client.email, "miru@example.com"] }
     let(:subject) { "Invoice (#{invoice.invoice_number}) due on #{invoice.due_date}" }
     let(:mail) { InvoiceMailer.with(invoice_id: invoice.id, subject:, recipients:).invoice }
