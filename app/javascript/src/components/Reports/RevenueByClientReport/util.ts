5a02b69032b28f2d5e28f6379af2101b61126700
Shruti-Apte <72149587+Shruti-Apte@users.noreply.github.com>
2023-07-04 18:07:44 +0530

    Amount notation updated revenue report (#1403)
    * amount notation updated
    
    * logic moved to utils

--- app/javascript/src/components/Reports/RevenueByClientReport/util.ts
+++ app/javascript/src/components/Reports/RevenueByClientReport/util.ts
@@ -1,1 +1,35 @@
 export const REVENUE_REPORT_PAGE = "Revenue Report";
+
+export const summaryList = (revenueByClientReport, isDesktop) => {
+  if (isDesktop) {
+    return [
+      {
+        label: "TOTAL PENDING AMOUNT",
+        value: revenueByClientReport.summary.totalOutstandingAmount,
+      },
+      {
+        label: "TOTAL PAID AMOUNT",
+        value: revenueByClientReport.summary.totalPaidAmount,
+      },
+      {
+        label: "TOTAL REVENUE",
+        value: revenueByClientReport.summary.totalRevenue,
+      },
+    ];
+  }
+
+  return [
+    {
+      label: "PENDING",
+      value: revenueByClientReport.summary.totalOutstandingAmount,
+    },
+    {
+      label: "PAID",
+      value: revenueByClientReport.summary.totalPaidAmount,
+    },
+    {
+      label: "REVENUE",
+      value: revenueByClientReport.summary.totalRevenue,
+    },
+  ];
+};
