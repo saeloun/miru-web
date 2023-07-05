68703c3471eba0c235e6e3cf91816ea14c691860
Gowsik Vivekanandan <52308930+gowsik-ragunath@users.noreply.github.com>
2023-07-05 21:16:06 +0530

    Added ability to disconnect stripe (#1405)
    * Added ability to disconnect stripe
    
    * Fixed invoice payment updation logic
    
    * Added request spec
    
    * Changed the return url

---
+++ app/javascript/src/components/Profile/Organization/Popups/DisconnectPayment.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+import { Modal, Button } from "StyledComponents";
+
+import paymentSettings from "apis/payment-settings";
+
+interface IProps {
+  setShowDisconnectDialog: any;
+  showDisconnectDialog: boolean;
+  paymentMode?: any;
+  fetchPaymentSettings: any;
+}
+
+const DisconnectPayment = ({
+  setShowDisconnectDialog,
+  showDisconnectDialog,
+  paymentMode = "Stripe",
+  fetchPaymentSettings,
+}: IProps) => {
+  const disconnectPayment = async () => {
+    await paymentSettings.disconnectStripe();
+    setShowDisconnectDialog(false);
+    fetchPaymentSettings();
+  };
+
+  return (
+    <Modal
+      customStyle="sm:my-8 sm:w-full sm:max-w-lg sm:align-middle"
+      isOpen={showDisconnectDialog}
+      onClose={() => setShowDisconnectDialog(false)}
+    >
+      <div className="mb-8 mt-4 flex-col">
+        <h6 className="mb-2 text-2xl font-bold">Disconnect Stripe</h6>
+        <p className="mt-2 font-normal">
+          Are you sure you want to disconnect{" "}
+          <b className="font-bold">{paymentMode}</b> payment gateway? You won’t
+          be able to receive payments through {paymentMode} until you connect a{" "}
+          {paymentMode} account.
+        </p>
+      </div>
+      <div className="flex justify-between">
+        <Button
+          className="mr-2 w-1/2"
+          size="medium"
+          style="secondary"
+          onClick={() => {
+            setShowDisconnectDialog(false);
+          }}
+        >
+          CANCEL
+        </Button>
+        <Button
+          className="ml-2 w-1/2 bg-miru-red-400 text-white"
+          size="medium"
+          onClick={() => {
+            disconnectPayment();
+          }}
+        >
+          Disconnect
+        </Button>
+      </div>
+    </Modal>
+  );
+};
+
+export default DisconnectPayment;
