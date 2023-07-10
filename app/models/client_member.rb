e92e68ff610ea14e4762e2b561ed02faeb4af027
Prasanth Chaduvula <chaduvulaprasanth1998@gmail.com>
2023-07-10 11:48:50 +0530

    Fixed Client Portal: No invoices after switching the workspace (#1412)
    * Fixed the client no invoices found after switching workspace
    
    * Fixed the client member list on client form
    
    * Fixed client and client member creation with current company
    
    * refactored client invoices controller
    
    * Updated client members seed data with company association
    
    * Fixed client members on client forms

--- app/models/client_member.rb
+++ app/models/client_member.rb
@@ -8,19 +8,23 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  client_id  :bigint           not null
+#  company_id :bigint           not null
 #  user_id    :bigint           not null
 #
 # Indexes
 #
-#  index_client_members_on_client_id  (client_id)
-#  index_client_members_on_user_id    (user_id)
+#  index_client_members_on_client_id   (client_id)
+#  index_client_members_on_company_id  (company_id)
+#  index_client_members_on_user_id     (user_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (client_id => clients.id)
+#  fk_rails_...  (company_id => companies.id)
 #  fk_rails_...  (user_id => users.id)
 #
 class ClientMember < ApplicationRecord
   belongs_to :client
   belongs_to :user
+  belongs_to :company
 end
