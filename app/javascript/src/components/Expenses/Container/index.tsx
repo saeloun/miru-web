667ffd8fac3588f12410dde37323460e078cfcfb
Shruti Apte <shrutiapte20@gmail.com>
2023-07-24 11:58:59 +0530

    Table structure added

--- app/javascript/src/components/Expenses/Container/index.tsx
+++ app/javascript/src/components/Expenses/Container/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import ExpensesSummary from "./ExpensesSummary";
+import Table from "./Table";
 
 const Container = () => (
   <div className="mt-6">
     <ExpensesSummary />
+    <Table />
   </div>
 );
 
