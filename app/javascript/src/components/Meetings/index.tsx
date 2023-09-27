539f6c046de49f9c68119e43fe8166fd5f399756
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2023-09-27 13:07:40 +0530

    add conditional display of connect meeting app button

--- app/javascript/src/components/Meetings/index.tsx
+++ app/javascript/src/components/Meetings/index.tsx
@@ -1,13 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 
+import { CalendarIcon } from "miruIcons";
+import { useNavigate } from "react-router-dom";
+
 import EmptyStates from "common/EmptyStates";
+import { useUserContext } from "context/UserContext";
 
 import Header from "./Header";
 import MeetingList from "./MeetingList";
 
+import Button from "../../StyledComponents/Button";
+
 const Meetings = () => {
-  const [meetings, setMeetings] = useState<any[]>(["a", "v", "c"]);
+  const [meetings, setMeetings] = useState<any[]>([]);
+  const { calendarConnected } = useUserContext();
+  const navigate = useNavigate();
 
   return (
     <>
@@ -16,9 +24,25 @@ const Meetings = () => {
         <MeetingList />
       ) : (
         <EmptyStates
-          Message="You don't have any pending meetings that can be added as time entries"
+          containerClassName="h-1/2"
           showNoSearchResultState={false}
-        />
+          Message={
+            calendarConnected
+              ? "You don't have any pending meetings that can be added as time entries"
+              : "You have not connected any meeting app to track your meetings"
+          }
+        >
+          {!calendarConnected && (
+            <Button
+              className="flex h-14 w-96 items-center justify-center text-xl font-bold"
+              style="primary"
+              onClick={() => navigate("/profile/edit/integrations")}
+            >
+              <CalendarIcon className="mr-2" />
+              <span>Connect Meeting apps</span>
+            </Button>
+          )}
+        </EmptyStates>
       )}
     </>
   );
