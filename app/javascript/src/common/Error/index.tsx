14c2a9309b0b32e0b202ec47b33b60682ea2411f
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2023-10-04 09:38:12 +0530

    fixed typo on badge component docs

--- app/javascript/src/common/Error/index.tsx
+++ app/javascript/src/common/Error/index.tsx
@@ -14,21 +14,18 @@ const ErrorPage = () => {
 
   const ErrorPageLayout = () => (
     <div className="flex h-90v w-full items-center justify-center lg:h-full">
-      <div className="flex h-full w-full flex-col items-center justify-center bg-miru-gray-100 px-4 lg:h-5/6">
+      <div className="flex h-full w-full flex-col items-center justify-center bg-white px-4 lg:h-5/6">
         <img className="h-auto w-5/6 lg:w-1/4" src={Animation_404} />
         <span className="py-15/100 text-center text-base font-semibold leading-5 text-miru-dark-purple-200 lg:pt-5/100 lg:pb-2/100 lg:text-lg lg:leading-7">
           Page not Found
         </span>
         <Button
-          className="w-full py-2 lg:w-1/4 lg:py-4"
-          style="primary"
+          style="ternary"
           onClick={() => {
             navigate(dashboardUrl(companyRole));
           }}
         >
-          <span className="text-base font-bold leading-5 text-white lg:text-lg lg:leading-7">
-            Go to Home Page
-          </span>
+          Click here
         </Button>
       </div>
     </div>
