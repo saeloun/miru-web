92dd173933a0cba22e5c17feae984958fd59a586
sainikhil1605 <sainikhilvatti1605@gmail.com>
2023-10-26 09:13:26 -0500

    Fixed username issue in side nav when user refreshes page

--- app/javascript/src/components/Profile/RouteConfig.tsx
+++ app/javascript/src/components/Profile/RouteConfig.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { Routes, Route, Navigate } from "react-router-dom";
 
+import profileApi from "apis/profile";
 import ErrorPage from "common/Error";
 import { useUserContext } from "context/UserContext";
+import { teamsMapper } from "mapper/teams.mapper";
 
+import { useProfile } from "./context/EntryContext";
 import { SETTINGS_ROUTES } from "./routes";
+
+import Toastr from "../../StyledComponents/Toastr";
 
 const ProtectedRoute = ({ role, authorisedRoles, children }) => {
   if (authorisedRoles.includes(role)) {
@@ -17,6 +22,30 @@ const ProtectedRoute = ({ role, authorisedRoles, children }) => {
 
 const RouteConfig = () => {
   const { companyRole } = useUserContext();
+  const {
+    setUserState,
+    profileSettings: { first_name, last_name },
+  } = useProfile();
+
+  const getData = async () => {
+    if (!first_name && !last_name) {
+      const data = await profileApi.index();
+      if (data.status && data.status == 200) {
+        const addressData = await profileApi.getAddress(data.data.user.id);
+        const userObj = teamsMapper(
+          data.data.user,
+          addressData.data.addresses[0]
+        );
+        setUserState("profileSettings", userObj);
+      } else {
+        Toastr.error("Something went wrong.");
+      }
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <Routes>
