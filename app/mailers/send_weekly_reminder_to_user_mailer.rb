9554e26d1edfe2ca0ffff07f9354507258c981f2
“Apoorv <tiwari.apoorv1316@gmail.com>
2023-10-30 11:36:22 +0530

    disbale weekly timesheet reminders

--- app/mailers/send_weekly_reminder_to_user_mailer.rb
+++ app/mailers/send_weekly_reminder_to_user_mailer.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
-class SendWeeklyReminderToUserMailer < ApplicationMailer
-  def notify_user_about_missed_entries
-    recipients = params[:recipients]
-    @company = params[:company_name]
-    @name = params[:name]
+# class SendWeeklyReminderToUserMailer < ApplicationMailer
+#   def notify_user_about_missed_entries
+#     recipients = params[:recipients]
+#     @company = params[:company_name]
+#     @name = params[:name]
 
-    @starting_date = params[:start_date]
-    @starting_date = @starting_date.ordinalize(year: false)
+#     @starting_date = params[:start_date]
+#     @starting_date = @starting_date.ordinalize(year: false)
 
-    @ending_date = params[:end_date]
-    @ending_date = @ending_date.ordinalize(year: false)
+#     @ending_date = params[:end_date]
+#     @ending_date = @ending_date.ordinalize(year: false)
 
-    subject = "Reminder to Update your Timesheet on Miru"
+#     subject = "Reminder to Update your Timesheet on Miru"
 
-    mail(to: recipients, subject:, reply_to: ENV["REPLY_TO_EMAIL"])
-  end
-end
+#     mail(to: recipients, subject:, reply_to: ENV["REPLY_TO_EMAIL"])
+#   end
+# end
