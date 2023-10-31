610d9e02bde7a28fdd96a47a1de277518273719e
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2023-10-31 15:49:11 +0530

    add test cases for calendars controller

--- spec/requests/internal_api/v1/calendars_controller_spec.rb
+++ spec/requests/internal_api/v1/calendars_controller_spec.rb
@@ -6,20 +6,11 @@ RSpec.describe InternalApi::V1::CalendarsController, type: :request do
   let(:company) { create(:company) }
   let(:user) { create(:user, current_workspace_id: company.id, calendar_id: "primary") }
   let(:client) { Signet::OAuth2::Client.new(client_options) }
-  let(:access_token) do
-    allow(Signet::OAuth2::Client).to receive(:new).and_return(client)
-    allow(client).to receive(:fetch_access_token!).and_return("access_token")
-
-    get internal_api_v1_callback_path, params: { code: "1234567890" }
-
-    session[:authorization]
-  end
 
   before do
     create(:employment, company:, user:)
     user.add_role :owner, company
     sign_in user
-    access_token
   end
 
   describe "#redirect" do
@@ -33,43 +24,51 @@ RSpec.describe InternalApi::V1::CalendarsController, type: :request do
   end
 
   describe "#callback" do
-    it "fetches access token from Google Calendar" do
-      # request.headers["Authorization"] = "Bearer #{access_token}"
-      expect(client).to have_received(:fetch_access_token!)
-      expect(session[:authorization]).to eq("access_token")
+    it "handles a valid authorization code" do
+      allow_any_instance_of(Signet::OAuth2::Client).to receive(:fetch_access_token!).and_return(SecureRandom.hex(64))
+
+      get internal_api_v1_callback_path, params: { code: SecureRandom.hex(73) }
+      expect(response).to redirect_to(internal_api_v1_calendars_path)
+      expect(session[:authorization]).not_to be_nil
+      expect(user.reload.calendar_connected).to be_truthy
     end
 
-    it "redirects to the calendars page if authorization code is present" do
-      # request.headers["Authorization"] = "Bearer #{access_token}"
-      # get internal_api_v1_callback_path, params: { code: "1234567890" }
-      expect(response).to redirect_to(internal_api_v1_calendars_path)
+    it "handles an empty authorization code" do
+      allow_any_instance_of(Signet::OAuth2::Client).to receive(:fetch_access_token!).and_return(SecureRandom.hex(64))
+
+      get internal_api_v1_callback_path, params: { code: "" }
+      expect(response).to have_http_status(:success)
+      expect(user.reload.calendar_connected).to be_falsey
     end
   end
 
   describe "#calendars" do
-    it "updates user calendar connected status to true when redirected to the calendars method" do
-      expect(user.calendar_connected).to eq(true)
-    end
+    it "fetches calendars and updates user information" do
+      allow_any_instance_of(Signet::OAuth2::Client).to receive(:fetch_access_token!).and_return(SecureRandom.hex(64))
+      allow_any_instance_of(Google::Apis::CalendarV3::CalendarService).to receive(:list_calendar_lists).and_return(
+        double(
+          items: [double(
+            primary?: true, id: "primary")]))
 
-    it "redirects to the integrations page" do
-      update_client!(access_token)
       get internal_api_v1_calendars_path
-
       expect(response).to redirect_to("/settings/integrations")
+      expect(user.reload.calendar_connected).to be_truthy
+      expect(user.reload.calendar_id).to eq("primary")
     end
   end
 
   describe "#events" do
     it "gets events for the current month" do
+      allow_any_instance_of(Signet::OAuth2::Client).to receive(:fetch_access_token!).and_return(SecureRandom.hex(64))
+      allow_any_instance_of(
+        Calendars::MonthlyCalendarEventsService
+      ).to receive(:get_events_for_month).and_return(event_list)
+
       get internal_api_v1_events_path, params: { month: "10", year: "2023" }
-      current_month = Time.now.month
-      current_year = Time.now.year
 
-      service = Calendars::MonthlyCalendarEventsService.new(
-        user.calendar_id, current_month, current_year,
-        client).get_events_for_month
-
-      expect(service).to have_received(:get_events_for_month).with("10", "2023")
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:events)
+      expect(assigns(:meetings)).to be_nil
     end
   end
 
@@ -84,7 +83,20 @@ RSpec.describe InternalApi::V1::CalendarsController, type: :request do
     }
   end
 
-  def update_client!(access_token)
-    client.update!("access_token" => { key: "access_token", value: access_token })
+  def event_list
+    events = [
+        OpenStruct.new(
+          summary: "Meeting with Client A", start: OpenStruct.new(date_time: DateTime.new(2023, 10, 15, 10, 0)),
+          end: OpenStruct.new(date_time: DateTime.new(2023, 10, 15, 11, 0)), description: "meeting about launch"
+        ),
+        OpenStruct.new(
+          summary: "Team Standup", start: OpenStruct.new(date_time: DateTime.new(2023, 10, 16, 9, 0)),
+          end: OpenStruct.new(date_time: DateTime.new(2023, 10, 16, 9, 30)), description: "discuss about work done during day time"
+        ),
+        OpenStruct.new(
+          summary: "Meeting with Client B", start: OpenStruct.new(date_time: DateTime.new(2023, 10, 17, 12, 30)),
+          end: OpenStruct.new(date_time: DateTime.new(2023, 10, 17, 13, 30)), description: "Meeting about urgent production bug"
+        )
+      ]
   end
 end
