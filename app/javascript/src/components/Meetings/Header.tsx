2ef1857e7c235e93601c6bba14ec40dd818fdd85
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2023-10-31 16:57:06 +0530

    return from the components

--- app/javascript/src/components/Meetings/Header.tsx
+++ app/javascript/src/components/Meetings/Header.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 import { MeetingIconSVG } from "miruIcons";
 
-// import BackButton from "components/Invoices/Invoice/BackButton";
+import BackButton from "components/Invoices/Invoice/BackButton";
 
 const Header = () => (
   <>
     <div className="flex items-center text-xl font-bold">
-      {/* <BackButton href="/time-tracking" /> */}
+      <BackButton href="/time-tracking" />
       Meetings
     </div>
     <div className="flex h-30 items-center rounded-xl bg-miru-han-purple-1000 text-white">
