de02b36a6a556f42dc3f06b06b58f2b501f0c3bd
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2023-11-07 13:29:49 +0530

    check whether current user has flag to virtually verify contacts

--- app/controllers/internal_api/v1/application_controller.rb
+++ app/controllers/internal_api/v1/application_controller.rb
@@ -19,6 +19,6 @@ class InternalApi::V1::ApplicationController < ActionController::API
   end
 
   def set_virtual_verified_invitations_allowed
-    @virtual_verified_invitations_allowed = ENV["VIRTUAL_VERIFIED_ADMIN_EMAILS"].include?(current_user.email)
+    @virtual_verified_invitations_allowed = ENV["VIRTUAL_VERIFIED_ADMIN_EMAILS"]&.include?(current_user.email)
   end
 end
