962533eb9da725ed769e5eb9bcac74c2bfad5a08
Gowsik Vivekanandan <52308930+gowsik-ragunath@users.noreply.github.com>
2023-11-16 16:20:55 +0530

    Store bounced and compliant email address (#1593)
    Added controller to store bouncing and compliant emails

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -3,5 +3,6 @@
   namespace :api, defaults: { format: "json" } do
     namespace :v1 do
       resources :timesheet_entry, only: [:create]
+      resources :sns_subscriptions, only: [:create]
     end
   end
