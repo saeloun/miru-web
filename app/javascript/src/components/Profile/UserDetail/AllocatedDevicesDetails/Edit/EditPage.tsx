821a3310874bd4ff54a8c2af6f78ed3ad1b0989b
Shruti Apte <shrutiapte20@gmail.com>
2023-11-24 15:46:41 +0530

    worked on review comments

--- app/javascript/src/components/Profile/UserDetail/AllocatedDevicesDetails/Edit/EditPage.tsx
+++ app/javascript/src/components/Profile/UserDetail/AllocatedDevicesDetails/Edit/EditPage.tsx
@@ -35,7 +35,7 @@ const EditPage = ({ devices, setDevices }) => {
 
   return (
     <div className="mt-4 h-full bg-miru-gray-100 px-10">
-      <div className="flex border-b border-b-miru-gray-400 py-10">
+      <div className="flex py-10">
         <div className="flex w-1/5 pr-4">
           <MobileIcon
             className="mr-2 mt-1 text-miru-dark-purple-1000"
@@ -47,87 +47,93 @@ const EditPage = ({ devices, setDevices }) => {
           </span>
         </div>
         <div className="w-10/12">
-          {devices.map(device => (
-            <React.Fragment key={device.id}>
-              <div className="flex w-full flex-row items-start">
-                <div className="w-11/12">
-                  <div className="flex flex-row py-3">
-                    <div className="flex w-1/2 flex-col px-2">
-                      <CustomReactSelect
-                        id="device_type"
-                        label="Device Type"
-                        name="device_type"
-                        options={deviceTypes}
-                        value={setDeviceType(device.device_type)}
-                      />
-                    </div>
-                    <div className="flex w-1/2 flex-col px-2">
-                      <CustomInputText
-                        id="model"
-                        label="Model"
-                        name="model"
-                        type="text"
-                        value={device.name}
-                      />
-                    </div>
-                  </div>
-                  <div className="flex flex-row py-3">
-                    <div className="flex w-1/2 flex-col px-2">
-                      <CustomInputText
-                        id="serial_number"
-                        label="Serial Number"
-                        name="serial_number"
-                        type="text"
-                        value={device.serial_number}
-                      />
-                    </div>
-                    <div className="flex w-1/2 flex-col px-2">
-                      <CustomInputText
-                        id="memory"
-                        label="Memory"
-                        name="memory"
-                        type="text"
-                        value={device.specifications.ram}
-                      />
-                    </div>
-                  </div>
-                  <div className="flex flex-row py-3">
-                    <div className="flex w-1/2 flex-col">
-                      <div className="field relative flex w-full flex-col px-2">
+          {devices.map(device => {
+            const { id, device_type, name, serial_number, specifications } =
+              device;
+
+            return (
+              <div key={id}>
+                <div className="flex w-full flex-row items-start">
+                  <div className="w-11/12">
+                    <div className="flex flex-row py-3">
+                      <div className="flex w-1/2 flex-col px-2">
+                        <CustomReactSelect
+                          id="device_type"
+                          label="Device Type"
+                          name="device_type"
+                          options={deviceTypes}
+                          value={setDeviceType(device_type)}
+                        />
+                      </div>
+                      <div className="flex w-1/2 flex-col px-2">
                         <CustomInputText
-                          id="graphics"
-                          label="Graphics"
-                          name="graphics"
+                          id="model"
+                          label="Model"
+                          name="model"
                           type="text"
-                          value={device.specifications.graphics}
+                          value={name}
                         />
                       </div>
                     </div>
-                    <div className="flex w-1/2 flex-col">
-                      <div className="field relative flex w-full flex-col px-2">
+                    <div className="flex flex-row py-3">
+                      <div className="flex w-1/2 flex-col px-2">
                         <CustomInputText
-                          id="processor"
-                          label="Processor"
-                          name="processor"
+                          id="serial_number"
+                          label="Serial Number"
+                          name="serial_number"
                           type="text"
-                          value={device.specifications.processor}
+                          value={serial_number}
+                        />
+                      </div>
+                      <div className="flex w-1/2 flex-col px-2">
+                        <CustomInputText
+                          id="memory"
+                          label="Memory"
+                          name="memory"
+                          type="text"
+                          value={specifications.ram}
                         />
                       </div>
                     </div>
+                    <div className="flex flex-row py-3">
+                      <div className="flex w-1/2 flex-col">
+                        <div className="field relative flex w-full flex-col px-2">
+                          <CustomInputText
+                            id="graphics"
+                            label="Graphics"
+                            name="graphics"
+                            type="text"
+                            value={specifications.graphics}
+                          />
+                        </div>
+                      </div>
+                      <div className="flex w-1/2 flex-col">
+                        <div className="field relative flex w-full flex-col px-2">
+                          <CustomInputText
+                            id="processor"
+                            label="Processor"
+                            name="processor"
+                            type="text"
+                            value={specifications.processor}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex w-1/12 items-center justify-center py-5">
+                    <Button className="p-2 text-center" style="ternary">
+                      <DeleteIcon />
+                    </Button>
                   </div>
                 </div>
-                <div className="flex w-1/12 items-center justify-center py-5">
-                  <Button className="rounded border-0 bg-transparent p-2 text-center text-miru-han-purple-1000 hover:bg-miru-dark-purple-100 hover:text-miru-han-purple-600">
-                    <DeleteIcon />
-                  </Button>
-                </div>
+                <Divider CustomStyle="my-5 w-11/12" />
               </div>
-              <Divider CustomStyle="my-5 w-11/12" />
-            </React.Fragment>
-          ))}
-          <div className="mt-10 flex w-full items-center justify-between">
+            );
+          })}
+          <div className="mt-10 flex w-11/12 items-center justify-between">
             <Button
-              className="mx-2 w-full rounded border border-dashed  border-miru-dark-purple-200 bg-white py-3 text-center text-base font-bold tracking-widest text-miru-dark-purple-200"
+              className="mx-2 w-full py-3"
+              style="dashed"
               onClick={addAnotherDevice}
             >
               + Add Another Device
