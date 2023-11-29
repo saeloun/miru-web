96f19aadf7ab631de9c643da205c3cf25211f8b6
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2023-11-29 15:58:45 +0530

    refactored session controller to support desktop

--- app/controllers/internal_api/v1/users/sessions_controller.rb
+++ app/controllers/internal_api/v1/users/sessions_controller.rb
@@ -9,27 +9,11 @@ class InternalApi::V1::Users::SessionsController < Devise::SessionsController
     user = User.find_for_database_authentication(email: user_params[:email])
 
     if invalid_password?(user)
-      render json: { error: I18n.t("sessions.failure.invalid") }, status: :unprocessable_entity
+      render_invalid_password_error
     elsif !user.confirmed?
-      render json: { error: I18n.t("devise.failure.unconfirmed"), unconfirmed: !user.confirmed? },
-        status: :unprocessable_entity
+      render_unconfirmed_user_error(user)
     else
-      sign_in(user)
-      app = params[:app] || ""
-
-      if app == "miru-desktop"
-        initial_props = {
-          user:,
-          avatar_url: current_user && current_user.avatar_url,
-          company_role: current_user && current_user.roles.find_by(resource: current_company)&.name,
-          confirmed_user: current_user && current_user.confirmed?,
-          company: current_company,
-          google_oauth_success: @google_oauth_success.present?
-        }
-        render json: { notice: I18n.t("devise.sessions.signed_in"), **initial_props }, status: :ok
-      else
-        render json: { notice: I18n.t("devise.sessions.signed_in"), user: }, status: :ok
-      end
+      handle_successful_sign_in(user)
     end
   end
 
@@ -47,5 +31,45 @@ class InternalApi::V1::Users::SessionsController < Devise::SessionsController
 
     def invalid_password?(user)
       user.blank? || !user.valid_password?(user_params[:password])
+    end
+
+    def render_invalid_password_error
+      render json: { error: I18n.t("sessions.failure.invalid") }, status: :unprocessable_entity
+    end
+
+    def render_unconfirmed_user_error(user)
+      render json: {
+        error: I18n.t("devise.failure.unconfirmed"),
+        unconfirmed: !user.confirmed?
+      }, status: :unprocessable_entity
+    end
+
+    def handle_successful_sign_in(user)
+      sign_in(user)
+
+      app = params[:app] || ""
+
+      if app == "miru-desktop"
+        render_sign_in_response_for_desktop(user)
+      else
+        render_sign_in_response(user)
+      end
+    end
+
+    def render_sign_in_response(user)
+      render json: { notice: I18n.t("devise.sessions.signed_in"), user: }, status: :ok
+    end
+
+    def render_sign_in_response_for_desktop(user)
+      initial_props = {
+        user:,
+        avatar_url: current_user && current_user.avatar_url,
+        company_role: current_user && current_user.roles.find_by(resource: current_company)&.name,
+        confirmed_user: current_user && current_user.confirmed?,
+        company: current_company,
+        google_oauth_success: @google_oauth_success.present?
+      }
+
+      render json: { notice: I18n.t("devise.sessions.signed_in"), **initial_props }, status: :ok
     end
 end
