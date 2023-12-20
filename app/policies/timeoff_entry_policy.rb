82f477c2b540c6d21654fcd4eecba9d897231cb0
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2023-12-20 09:18:00 +0530

    added timeoff entry policy

---
+++ app/policies/timeoff_entry_policy.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class TimeoffEntryPolicy < ApplicationPolicy
+  def create?
+    user_owner_role? || user_admin_role? || user_employee_role?
+  end
+
+  def update?
+    authorize_current_user
+  end
+
+  def destroy?
+    authorize_current_user
+  end
+
+  def permitted_attributes
+    [
+      :duration, :note, :leave_date, :user_id, :leave_type_id
+    ]
+  end
+
+  def authorize_current_user
+    unless user.current_workspace_id == record.company.id
+      @error_message_key = :different_workspace
+      return false
+    end
+
+    user_owner_role? || user_admin_role? || user_employee_role?
+  end
+end
