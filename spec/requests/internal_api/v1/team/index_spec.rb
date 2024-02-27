22808b70711d1a0de2382ec6ac786efed0b2c6b2
“Apoorv <tiwari.apoorv1316@gmail.com>
2024-02-27 13:38:25 +0530

    fix error in spec

--- spec/requests/internal_api/v1/team/index_spec.rb
+++ spec/requests/internal_api/v1/team/index_spec.rb
@@ -62,7 +62,7 @@ RSpec.describe "InternalApi::V1::Team#index", type: :request do
       send_request :get, internal_api_v1_team_index_path(
         q: { first_name_or_last_name_or_email_cont: user.first_name }
       ), headers: auth_headers(user)
-      employment = user.employments.find_by!(company_id: current_company.id)
+      employment = user.employments.find_by!(company_id: company.id)
 
       service_arr = [{
         "id" => user.id, "firstName" => user.first_name, "lastName" => user.last_name, "name" => "#{user.first_name} #{user.last_name}",
