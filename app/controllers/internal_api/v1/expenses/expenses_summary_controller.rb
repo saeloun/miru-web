84693286ce4a92e85fc9d6aea86b67c67aa9ca62
“Apoorv <tiwari.apoorv1316@gmail.com>
2024-03-08 08:53:17 +0530

    refactor code

--- app/controllers/internal_api/v1/expenses/expenses_summary_controller.rb
+++ app/controllers/internal_api/v1/expenses/expenses_summary_controller.rb
@@ -5,8 +5,8 @@ class InternalApi::V1::Expenses::ExpensesSummaryController < InternalApi::V1::Ap
     authorize :index, policy_class: Expenses::ExpensesSummaryPolicy
     render json: {
       expenses_summary: {
-        default_categories_sum: default_category_expenses_sum,
-        company_categories_sum: company_expenses_categories_sum,
+        default_categories_sum: formatted_expenses_sum(default_category_expenses),
+        company_categories_sum: formatted_expenses_sum(company_expenses_categories),
         total_sum: total_expenses_sum
       }
     }
@@ -19,23 +19,24 @@ class InternalApi::V1::Expenses::ExpensesSummaryController < InternalApi::V1::Ap
       FormatAmountService.new(current_company.base_currency, total_expenses).process
     end
 
-    def default_category_expenses_sum
-      default_category_expenses = Expense.where(
-        expense_category: ExpenseCategory.default_categories, company_id: current_company.id)
-      category_expense_sum = Hash.new(0)
-      default_category_expenses.each do |expense|
-        category_expense_sum[expense.expense_category.name] += expense.amount
-      end
-      category_expense_sum
+    def default_category_expenses
+      Expense.where(expense_category: ExpenseCategory.default_categories, company_id: current_company.id)
     end
 
-    def company_expenses_categories_sum
-      company_expenses_categories = Expense.where(
-        expense_category: current_company.expense_categories, company_id: current_company.id)
-      expenses_sum = Hash.new(0)
-      company_expenses_categories.each do |expense|
-        expenses_sum[expense.expense_category.name] += expense.amount
+    def company_expenses_categories
+      Expense.where(expense_category: current_company.expense_categories)
+    end
+
+    def formatted_expenses_sum(expenses)
+      category_expense_sum = Hash.new(0)
+      expenses.each do |expense|
+        category_expense_sum[expense.expense_category.name] += expense.amount
       end
-      expenses_sum
+
+      formatted_sum = {}
+      category_expense_sum.each do |category, amount|
+        formatted_sum[category] = FormatAmountService.new(current_company.base_currency, amount).process
+      end
+      formatted_sum
     end
 end
