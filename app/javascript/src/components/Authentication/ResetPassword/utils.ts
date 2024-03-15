36cc51708d6d48ee9e11bfa4a72317a9abec7448
Saeloun <saeloun@Saelouns-MacBook-Pro.local>
2024-03-15 16:12:15 +0530

    password field bugs

--- app/javascript/src/components/Authentication/ResetPassword/utils.ts
+++ app/javascript/src/components/Authentication/ResetPassword/utils.ts
@@ -9,7 +9,7 @@ export const resetPasswordFormValidationSchema = Yup.object().shape({
   password: Yup.string()
     .matches(/^\S.*\S$/, "Password cannot start or end with a blank space")
     .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s\x00-\x1F\x7F])[\S\s]{8,}$/, // eslint-disable-line
+      /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[_\W\s\x00-\x1F\x7F])[\S\s]{8,}$/, // eslint-disable-line
       "Must Contain at least 8 Characters, One Uppercase, One Lowercase, One Number and One Special Character"
     )
     .required("Password cannot be blank"),
