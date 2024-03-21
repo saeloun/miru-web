223f2f6b5dc60c021deba1830f35df6e2a939193
Shruti Apte <shruti@saeloun.com>
2024-03-21 10:07:12 +0530

    whitespace character removed

--- app/javascript/src/components/Authentication/SignUp/utils.ts
+++ app/javascript/src/components/Authentication/SignUp/utils.ts
@@ -31,7 +31,7 @@ export const signUpFormValidationSchema = Yup.object().shape({
   confirm_password: Yup.string()
     .matches(/^\S.*\S$/, "Password cannot start or end with a blank space")
     .matches(
-      /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[_\W\s\x00-\x1F\x7F])[\S\s]{8,}$/, // eslint-disable-line
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[_\w\s\x00-\x1F\x7F])[\S]{8,}$/, // eslint-disable-line
       "Must Contain at least 8 Characters, One Uppercase, One Lowercase, One Number and One Special Character"
     )
     .oneOf([Yup.ref("password"), null], "Passwords must match")
