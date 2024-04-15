7c9ba8e6fa9de43ed498361bb571fbcdeabb8d62
apoorv1316 <tiwari.apoorv1316@gmail.com>
2024-04-15 13:44:14 +0530

    fix bug in group by team

--- app/services/reports/time_entries/report_service.rb
+++ app/services/reports/time_entries/report_service.rb
@@ -71,8 +71,9 @@ class Reports::TimeEntries::ReportService
       group_by = params[:group_by]&.to_sym
       return unless [:client, :project, :team_member].include?(group_by)
 
-      filter_service = TimeEntries::Filters.new(params.slice(:date_range, :from, :to))
-      where_conditions = filter_service.date_range_filter
+      filter_service = TimeEntries::Filters.new(params)
+      where_conditions = filter_service.process
+      where_conditions.delete(:client_id) if where_conditions.key?(:client_id)
 
       joins_clause, group_field = case group_by
                                   when :client
@@ -85,8 +86,8 @@ class Reports::TimeEntries::ReportService
                                     raise ArgumentError, "Unsupported group_by: #{group_by}"
       end
 
-      grouped_durations = TimesheetEntry.where(where_conditions)
-        .joins(joins_clause)
+      grouped_durations = TimesheetEntry.joins(joins_clause)
+        .where(where_conditions)
         .reorder("")
         .group(group_field)
         .sum(:duration)
