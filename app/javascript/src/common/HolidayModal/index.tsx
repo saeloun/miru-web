7214b25ce6f06837a6edab048c3d770f64d0b883
Shruti Apte <shruti@saeloun.com>
2024-04-17 11:34:21 +0530

    calendar component added on leave management

---
+++ app/javascript/src/common/HolidayModal/index.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+
+import { XIcon } from "miruIcons";
+import { Button, Modal } from "StyledComponents";
+
+import CustomYearPicker from "common/CustomYearPicker";
+import { MobileEditHeader } from "common/Mobile/MobileEditHeader";
+import { yearCalendar } from "constants/leaveType";
+import { useUserContext } from "context/UserContext";
+
+import CalendarComponent from "./CalendarComponent";
+
+const HolidayModal = ({
+  tileContent,
+  toggleCalendarModal,
+  showCalendar,
+  currentYear,
+  setCurrentYear,
+}) => {
+  const { isDesktop } = useUserContext();
+
+  const CalendarForm = () => (
+    <div className="modal__form flex-col bg-white px-4">
+      <div className="mt-3 flex w-full items-center justify-between lg:justify-end">
+        <div className="flex flex-row items-center">
+          <div className="mr-4 flex flex-row items-center text-xs font-medium lg:text-sm">
+            <div className="mr-2 h-4 w-4 rounded-2xl bg-miru-chart-pink-600" />
+            Holidays
+          </div>
+          <div className="flex flex-row items-center text-xs font-medium lg:text-sm">
+            <div className="mr-2 h-4 w-4 rounded-2xl bg-miru-gray-800" />
+            Optional Holidays
+          </div>
+        </div>
+        <CustomYearPicker
+          currentYear={currentYear}
+          setCurrentYear={setCurrentYear}
+          wrapperClassName="text-miru-han-purple-1000 flex items-center justify-end lg:hidden text-sm"
+        />
+      </div>
+      {Object.keys(yearCalendar).map((quarters, key) => (
+        <div className="flex flex-col gap-4 pt-4 lg:flex-row" key={key}>
+          {yearCalendar[quarters].quarter.map((month, key) => (
+            <CalendarComponent
+              id={month.id}
+              key={key}
+              name={month.name}
+              tileContent={tileContent}
+              year={currentYear}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+
+  if (isDesktop) {
+    return (
+      <Modal
+        customStyle="min-w-60v lg:p-0 lg:pb-6 hidden lg:block"
+        isOpen={showCalendar}
+        onClose={toggleCalendarModal}
+      >
+        <div className="modal__position mx-0 h-10 w-full items-center bg-miru-han-purple-1000 px-4 text-white">
+          <span className="flex-1 pl-4 text-base">
+            Public and optional holidays
+          </span>
+          <CustomYearPicker
+            currentYear={currentYear}
+            setCurrentYear={setCurrentYear}
+          />
+          <div className="modal__close flex-1 text-right">
+            <Button className="modal__button" onClick={toggleCalendarModal}>
+              <XIcon color="#CDD6DF" size={15} />
+            </Button>
+          </div>
+        </div>
+        <CalendarForm />
+      </Modal>
+    );
+  }
+
+  if (showCalendar) {
+    return (
+      <div className="absolute inset-0 z-50 bg-white">
+        <MobileEditHeader
+          backHref="/leave-management/"
+          href=""
+          showEdit={false}
+          title="Public & Optional Holidays"
+        />
+        <div className="my-12">
+          <CalendarForm />
+        </div>
+      </div>
+    );
+  }
+};
+
+export default HolidayModal;
--- app/javascript/src/components/LeaveManagement/Container/index.tsx
+++ app/javascript/src/components/LeaveManagement/Container/index.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 
-import { minToHHMM } from "helpers";
-import { XIcon } from "miruIcons";
+import { getYear } from "date-fns";
+import dayjs from "dayjs";
+import { minToHHMM, companyDateFormater } from "helpers";
+import { XIcon, CalendarIcon } from "miruIcons";
+import { Button, Tooltip } from "StyledComponents";
 
 import { Divider } from "common/Divider";
+import HolidayModal from "common/HolidayModal";
+import { useUserContext } from "context/UserContext";
 
 import LeaveBlock from "./LeaveBlock";
 import Table from "./Table";
@@ -15,51 +20,116 @@ const Container = ({
   totalTimeoffEntriesDuration,
   selectedLeaveType,
   setSelectedLeaveType,
-}) => (
-  <div className="mx-4 my-6 h-full lg:mx-0">
-    <p className="text-base font-normal text-miru-dark-purple-1000 lg:text-2xl">
-      {getLeaveBalanaceDateText()}
-    </p>
-    <div className="mt-6 grid w-full gap-4 lg:grid-cols-3">
-      {leaveBalance.map((leaveType, index) => (
-        <LeaveBlock
-          key={index}
-          leaveType={leaveType}
-          selectedLeaveType={selectedLeaveType}
-          setSelectedLeaveType={setSelectedLeaveType}
+}) => {
+  const [showCalendar, setShowCalendar] = useState<boolean>(false);
+  const [currentYear, setCurrentYear] = useState<number>(getYear(new Date()));
+  const { company } = useUserContext();
+  const dateFormat = companyDateFormater(company?.date_format);
+  const toggleCalendarModal = () => setShowCalendar(!showCalendar);
+
+  const HolidayButton = ({ content, date, className }) => (
+    <div className={`holiday-wrapper ${className}`}>
+      <Tooltip className="tooltip" content={content}>
+        <span>{date.getDate()}</span>
+      </Tooltip>
+    </div>
+  );
+
+  const tileContent = ({ date }) => {
+    const currentDate = dayjs(date).format(dateFormat);
+    const holiday = timeoffEntries?.find(
+      entry => entry.type == "holiday" && entry.leaveDate === currentDate
+    );
+
+    if (holiday) {
+      return (
+        <HolidayButton
+          content={holiday?.holidayInfo?.name}
+          date={date}
+          className={
+            holiday?.holidayInfo?.category === "national"
+              ? "holiday"
+              : "optional-holiday"
+          }
         />
-      ))}
-    </div>
-    <div className="mt-10">
-      <div className="flex items-center justify-between py-3 lg:py-5">
-        <div className="flex items-center">
-          <span className="mr-4 text-lg font-bold text-miru-dark-purple-1000 lg:text-xl">
-            Leave Details
-          </span>
-          {selectedLeaveType && (
-            <div className="flex items-center justify-center rounded-xl bg-miru-gray-400 px-3 py-1 lg:mx-2 lg:my-0">
-              <span className="tracking-wide text-base font-normal capitalize text-miru-dark-purple-1000">
-                {selectedLeaveType.name}
-              </span>
-              <XIcon
-                className="ml-2 cursor-pointer"
-                size={14}
-                onClick={() => setSelectedLeaveType(null)}
-              />
-            </div>
-          )}
+      );
+    }
+
+    return <button>{date.getDate()}</button>;
+  };
+
+  return (
+    <Fragment>
+      <div className="mx-4 my-6 h-full lg:mx-0">
+        <div className="flex w-full items-center justify-between">
+          <p className="text-base font-normal text-miru-dark-purple-1000 lg:text-2xl">
+            {getLeaveBalanaceDateText()}
+          </p>
+          <Button
+            className="flex items-center text-center text-xs font-bold lg:px-4"
+            style="secondary"
+            onClick={toggleCalendarModal}
+          >
+            <CalendarIcon
+              className="mr-2 text-miru-han-purple-1000 lg:hidden"
+              size={16}
+              weight="bold"
+            />
+            <span className="lg:hidden">Holiday</span>
+            <span className="hidden lg:block">View Holiday Calendar</span>
+          </Button>
         </div>
-        <p className="text-sm font-medium text-miru-dark-purple-1000 lg:text-base">
-          Total :{" "}
-          <span className="font-bold">
-            {minToHHMM(totalTimeoffEntriesDuration)}
-          </span>
-        </p>
+        <div className="mt-6 grid w-full gap-4 lg:grid-cols-3">
+          {leaveBalance.map((leaveType, index) => (
+            <LeaveBlock
+              key={index}
+              leaveType={leaveType}
+              selectedLeaveType={selectedLeaveType}
+              setSelectedLeaveType={setSelectedLeaveType}
+            />
+          ))}
+        </div>
+        <div className="mt-10">
+          <div className="flex items-center justify-between py-3 lg:py-5">
+            <div className="flex items-center">
+              <span className="mr-4 text-lg font-bold text-miru-dark-purple-1000 lg:text-xl">
+                Leave Details
+              </span>
+              {selectedLeaveType && (
+                <div className="flex items-center justify-center rounded-xl bg-miru-gray-400 px-3 py-1 lg:mx-2 lg:my-0">
+                  <span className="tracking-wide text-base font-normal capitalize text-miru-dark-purple-1000">
+                    {selectedLeaveType.name}
+                  </span>
+                  <XIcon
+                    className="ml-2 cursor-pointer"
+                    size={14}
+                    onClick={() => setSelectedLeaveType(null)}
+                  />
+                </div>
+              )}
+            </div>
+            <p className="text-sm font-medium text-miru-dark-purple-1000 lg:text-base">
+              Total :
+              <span className="font-bold">
+                {minToHHMM(totalTimeoffEntriesDuration)}
+              </span>
+            </p>
+          </div>
+          <Divider />
+          <Table timeoffEntries={timeoffEntries} />
+        </div>
       </div>
-      <Divider />
-      <Table timeoffEntries={timeoffEntries} />
-    </div>
-  </div>
-);
+      {showCalendar && (
+        <HolidayModal
+          currentYear={currentYear}
+          setCurrentYear={setCurrentYear}
+          showCalendar={showCalendar}
+          tileContent={tileContent}
+          toggleCalendarModal={toggleCalendarModal}
+        />
+      )}
+    </Fragment>
+  );
+};
 
 export default Container;
