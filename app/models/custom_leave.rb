ed0237ce27d565fcc67bced420c71f4c1aa3e014
apoorv1316 <tiwari.apoorv1316@gmail.com>
2024-04-29 12:48:41 +0530

    crud custom leaves

---
+++ app/models/custom_leave.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: custom_leaves
+#
+#  id                :bigint           not null, primary key
+#  allocation_period :integer          not null
+#  allocation_value  :integer          not null
+#  name              :string           not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  leave_id          :bigint           not null
+#
+# Indexes
+#
+#  index_custom_leaves_on_leave_id  (leave_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (leave_id => leaves.id)
+#
+class CustomLeave < ApplicationRecord
+  belongs_to :leave
+  has_many :custom_leave_users, dependent: :destroy
+  has_many :users, through: :custom_leave_users
+
+  enum allocation_period: {
+    days: 0,
+    weeks: 1,
+    months: 2
+  }
+
+  validates :name, :allocation_value, :allocation_period, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_11_174949) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_26_053100) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -143,6 +143,25 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_11_174949) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "calendar_enabled", default: true
+  end
+
+  create_table "custom_leave_users", force: :cascade do |t|
+    t.bigint "custom_leave_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["custom_leave_id"], name: "index_custom_leave_users_on_custom_leave_id"
+    t.index ["user_id"], name: "index_custom_leave_users_on_user_id"
+  end
+
+  create_table "custom_leaves", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "allocation_value", null: false
+    t.integer "allocation_period", null: false
+    t.bigint "leave_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["leave_id"], name: "index_custom_leaves_on_leave_id"
   end
 
   create_table "data_migrations", primary_key: "version", id: :string, force: :cascade do |t|
@@ -536,6 +555,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_11_174949) do
   add_foreign_key "client_members", "companies"
   add_foreign_key "client_members", "users"
   add_foreign_key "clients", "companies"
+  add_foreign_key "custom_leave_users", "custom_leaves", column: "custom_leave_id"
+  add_foreign_key "custom_leave_users", "users"
+  add_foreign_key "custom_leaves", "leaves", column: "leave_id"
   add_foreign_key "devices", "companies"
   add_foreign_key "devices", "users"
   add_foreign_key "employments", "companies"
