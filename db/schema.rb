6b38247606f213e320aa21595330094d4400e3ec
apoorv1316 <tiwari.apoorv1316@gmail.com>
2024-05-16 13:08:40 +0530

    prevent editing locked entries

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_29_142938) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_16_054849) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -475,6 +475,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_29_142938) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "discarded_at"
+    t.boolean "locked", default: false
     t.index ["bill_status"], name: "index_timesheet_entries_on_bill_status"
     t.index ["discarded_at"], name: "index_timesheet_entries_on_discarded_at"
     t.index ["project_id"], name: "index_timesheet_entries_on_project_id"
