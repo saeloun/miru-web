127cdbdf4a87d6b2c932e0491c1d661aca0a89de
Shruti Apte <shruti@saeloun.com>
2024-05-20 13:38:09 +0530

    Insurance details UI and API integration

--- app/javascript/src/components/Profile/UserDetail/AllocatedDevicesDetails/Edit/EditPage.tsx
+++ app/javascript/src/components/Profile/UserDetail/AllocatedDevicesDetails/Edit/EditPage.tsx
@@ -11,8 +11,18 @@ const EditPage = ({
   handleDeviceChange,
   handleDeleteDevice,
   errDetails,
+  DOARef,
+  DOERef,
+  dateFormat,
+  showDOAPicker,
+  showDOEPicker,
+  setShowDOAPicker,
+  setShowDOEPicker,
+  handleDOAChage,
+  handleDOEChage,
+  handleIsInsured,
 }) => (
-  <div className="mt-4 flex w-full flex-1 bg-miru-gray-100 px-4 lg:px-10">
+  <div className="mt-4 flex h-full w-full flex-1 bg-miru-gray-100 px-4 lg:px-10">
     <div className="flex w-full py-10">
       <div className="flex w-1/5 pr-4">
         <MobileIcon
@@ -25,11 +35,21 @@ const EditPage = ({
         </span>
       </div>
       <DeviceForm
+        DOARef={DOARef}
+        DOERef={DOERef}
         addAnotherDevice={addAnotherDevice}
+        dateFormat={dateFormat}
         devices={devices}
         errDetails={errDetails}
+        handleDOAChage={handleDOAChage}
+        handleDOEChage={handleDOEChage}
         handleDeleteDevice={handleDeleteDevice}
         handleDeviceChange={handleDeviceChange}
+        handleIsInsured={handleIsInsured}
+        setShowDOAPicker={setShowDOAPicker}
+        setShowDOEPicker={setShowDOEPicker}
+        showDOAPicker={showDOAPicker}
+        showDOEPicker={showDOEPicker}
       />
     </div>
   </div>
