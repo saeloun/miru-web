38384b3c12d98dbb8c55f13fa72260c0d4dd9aae
apoorv1316 <tiwari.apoorv1316@gmail.com>
2024-05-29 11:18:08 +0530

    review comments

--- app/javascript/src/components/Reports/RevenueByClientReport/index.tsx
+++ app/javascript/src/components/Reports/RevenueByClientReport/index.tsx
@@ -141,7 +141,6 @@ const RevenueByClientReport = () => {
     const response = await clientRevenueApi.download(type, `?${queryParams}`);
     const url = window.URL.createObjectURL(new Blob([response.data]));
     const link = document.createElement("a");
-    // const filename = `${selectedFilter.dateRange.label}.${type}`;
     const filename = `report.${type}`;
     link.href = url;
     link.setAttribute("download", filename);
