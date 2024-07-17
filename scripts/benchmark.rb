7ce307a5a501a6cf3a40e3b51b927ff640e59278
apoorv1316 <tiwari.apoorv1316@gmail.com>
2024-07-17 10:45:10 +0530

    modify config and script

--- scripts/benchmark.rb
+++ scripts/benchmark.rb
@@ -7,36 +7,16 @@ invoice_ids = Invoice.limit(10).pluck(:id)
 company_logo = "saeloun_logo.png"
 download_id = SecureRandom.uuid
 root_url = "https://staging.miru.so/"
-# current_url_options = { protocol: 'http', host: 'localhost', port: 3000 }
-
-def set_cable_adapter(adapter)
-  Rails.application.config.action_cable.adapter = adapter
-  Rails.application.config.action_cable.cable({ adapter: })
-  ActionCable.server.config.cable = Rails.application.config.action_cable.cable
-end
+current_url_options = { protocol: "https", host: "staging.miru.so", port: 443 }
 
 Benchmark.ips do |x|
-  # Test with Redis adapter
-  set_cable_adapter("redis")
-  x.report("BulkInvoiceDownloadJob with Redis") do
-    BulkInvoiceDownloadJob.perform_now(
-      invoice_ids,
-      company_logo,
-      download_id,
-      root_url,
-      # current_url_options
-    )
-  end
-
-  # Test with SolidCable adapter
-  set_cable_adapter("solid_cable")
   x.report("BulkInvoiceDownloadJob with SolidCable") do
     BulkInvoiceDownloadJob.perform_now(
       invoice_ids,
       company_logo,
       download_id,
       root_url,
-      # current_url_options
+      current_url_options
     )
   end
 end
