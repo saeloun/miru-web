29adfb2429b5a6cbc6d403e1e2f29bccd1ee4c82
apoorv1316 <tiwari.apoorv1316@gmail.com>
2024-09-19 14:36:43 +0530

    refactor

--- app/policies/timeoff_entry_policy.rb
+++ app/policies/timeoff_entry_policy.rb
@@ -1,67 +1,44 @@
 # frozen_string_literal: true
 
 class TimeoffEntryPolicy < ApplicationPolicy
-  ALLOWED_ROLES = [:admin, :owner, :employee].freeze
-
   def index?
-    user.has_any_role?(
-      { name: :admin, resource: user.current_workspace },
-      { name: :owner, resource: user.current_workspace },
-      { name: :employee, resource: user.current_workspace })
+    user_has_any_workspace_role?
   end
 
   def create?
-    user.has_any_role?(
-      { name: :admin, resource: user.current_workspace },
-      { name: :owner, resource: user.current_workspace },
-      { name: :employee, resource: user.current_workspace })
+    user_has_any_workspace_role?
   end
 
   def update?
-    authorize_update_destroy
+    week_over? ? user_has_admin_or_owner_role? : user_has_any_workspace_role?
   end
 
   def destroy?
-    authorize_update_destroy
+    update?
   end
 
   def permitted_attributes
-    [
-      :duration, :note, :leave_date, :user_id, :leave_type_id, :holiday_info_id
-    ]
+    [:duration, :note, :leave_date, :user_id, :leave_type_id, :holiday_info_id]
   end
 
   private
 
-    def authorize_update_destroy
-      return false unless authorize_current_user
-
-      if week_over?
-        user.has_any_role?(
-          { name: :admin, resource: user.current_workspace },
-          { name: :owner, resource: user.current_workspace })
-      else
-        user.has_any_role?(
-          { name: :admin, resource: user.current_workspace },
-          { name: :owner, resource: user.current_workspace },
-          { name: :employee, resource: user.current_workspace })
-      end
-    end
-
     def week_over?
-      return false unless record.leave_date.present?
-
-      leave_date_end_of_week = record.leave_date.end_of_week(:sunday)
-
-      Time.current > leave_date_end_of_week
+      record.leave_date.present? && Time.current > record.leave_date.end_of_week(:sunday)
     end
 
-    def authorize_current_user
-      unless user.current_workspace_id == record.company.id
-        @error_message_key = :different_workspace
-        return false
-      end
+    def user_has_any_workspace_role?
+      user.has_any_role?(
+        { name: :admin, resource: user.current_workspace },
+        { name: :owner, resource: user.current_workspace },
+        { name: :employee, resource: user.current_workspace }
+      )
+    end
 
-      user_owner_role? || user_admin_role? || user_employee_role?
+    def user_has_admin_or_owner_role?
+      user.has_any_role?(
+        { name: :admin, resource: user.current_workspace },
+        { name: :owner, resource: user.current_workspace }
+      )
     end
 end
