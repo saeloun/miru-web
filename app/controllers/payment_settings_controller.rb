df4402091c1189605611e9498a558e67b23caa2d
apoorv1316 <tiwari.apoorv1316@gmail.com>
2024-09-23 20:31:33 +0530

    fix specs

--- app/controllers/payment_settings_controller.rb
+++ app/controllers/payment_settings_controller.rb
@@ -2,11 +2,11 @@
 
 class PaymentSettingsController < ApplicationController
   def index
-    authorize :index, policy_class: PaymentSettingsPolicy
+    authorize current_company, policy_class: PaymentSettingsPolicy
   end
 
   def refresh_stripe_connect
-    authorize :refresh_stripe_connect, policy_class: PaymentSettingsPolicy
+    authorize current_company, policy_class: PaymentSettingsPolicy
 
     redirect_to stripe_connected_account.url, allow_other_host: true
   end
