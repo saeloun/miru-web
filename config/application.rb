51ab8f49175c742af52345722db672d2aea45a45
gowsik <v.gowsik@gmail.com>
2024-11-20 19:48:18 +0530

    Reverted application.rb changes

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module MiruWeb
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-    config.autoload_paths = config.autoload_paths.dup + %W(
+    config.autoload_paths += %W(
       #{config.root}/app/services/concerns
       #{config.root}/app/mailers/concerns
     )
