aff7cecd2bc89ac4a556b1ec52fec14faead8810
gowsik <v.gowsik@gmail.com>
2024-11-20 18:39:35 +0530

    Updated factory

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,10 @@ module MiruWeb
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-    config.autoload_paths += %W(#{config.root}/app/services/concerns #{config.root}/app/mailers/concerns)
+    config.autoload_paths = config.autoload_paths.dup + %W(
+      #{config.root}/app/services/concerns
+      #{config.root}/app/mailers/concerns
+    )
 
     # Configuration for the application, engines, and railties goes here.
     #
