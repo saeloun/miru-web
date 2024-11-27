b2dcc3e97f6dc8c4ee6ee23fd528ebe15bb73589
Shruti-Apte <72149587+Shruti-Apte@users.noreply.github.com>
2024-11-27 12:13:16 +0530

    Improve email score (#1935)
    Co-authored-by: Shruti Apte <shruti@saeloun.com>

--- app/mailers/send_payment_reminder_mailer.rb
+++ app/mailers/send_payment_reminder_mailer.rb
@@ -12,9 +12,6 @@ class SendPaymentReminderMailer < ApplicationMailer
 
     # pdf = InvoicePayment::PdfGeneration.process(@invoice, @company_logo, root_url)
     # attachments["invoice_#{@invoice.invoice_number}.pdf"] = pdf
-    attachments.inline["miruLogoWithText.png"] = File.read("public/miruLogoWithText.png")
-    attachments.inline["Instagram.png"] = File.read("public/Instagram.png")
-    attachments.inline["Twitter.png"] = File.read("public/Twitter.png")
 
     mail(to: recipients, subject:, reply_to: ENV["REPLY_TO_EMAIL"])
   end
