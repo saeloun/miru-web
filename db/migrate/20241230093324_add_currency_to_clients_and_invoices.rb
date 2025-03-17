f78be2e711f89214f7f9661f56dd4fa7c9301c8e
Jijo Bose <bosejijo@gmail.com>
2025-03-17 18:18:11 +0530

    Fixed rubocop

--- db/migrate/20241230093324_add_currency_to_clients_and_invoices.rb
+++ db/migrate/20241230093324_add_currency_to_clients_and_invoices.rb
@@ -2,7 +2,7 @@
 
 class AddCurrencyToClientsAndInvoices < ActiveRecord::Migration[7.1]
   def change
-    add_column :clients, :currency, :string, default: 'USD', null: false
-    add_column :invoices, :currency, :string, default: 'USD', null: false
+    add_column :clients, :currency, :string, default: "USD", null: false
+    add_column :invoices, :currency, :string, default: "USD", null: false
   end
 end
