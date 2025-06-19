e448a6e47a9c91ca22e95fa8059a9871b75a60c2
Minakshi Zambre <minanajardhane@gmail.com>
2025-06-19 14:21:53 +0530

    Change in grover gem init config and pptruser changes

--- config/initializers/grover.rb
+++ config/initializers/grover.rb
@@ -10,12 +10,40 @@ Grover.configure do |config|
     prefer_css_page_size: true,
     emulate_media: "screen",
     cache: false,
-    timeout: 0, # Timeout in ms. A value of `0` means 'no timeout'
+    timeout: 60000, # Timeout in ms. A value of `0` means 'no timeout'
     wait_until: "domcontentloaded",
-    launch_args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    launch_args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-gpu",
+      "--disable-dev-shm-usage", # Critical for low memory
+      "--disable-background-timer-throttling",
+      "--disable-backgrounding-occluded-windows",
+      "--disable-renderer-backgrounding",
+      "--disable-extensions",
+      "--disable-plugins",
+      "--disable-default-apps",
+      "--disable-sync",
+      "--disable-translate",
+      "--hide-scrollbars",
+      "--mute-audio",
+      "--no-first-run",
+      "--safebrowsing-disable-auto-update",
+      "--disable-ipc-flooding-protection",
+      # Memory limits
+      "--memory-pressure-off",
+      "--max_old_space_size=256", # Limit V8 heap to 256MB
+      "--aggressive-cache-discard",
+      # Remove these problematic flags:
+      # "--single-process"  - Actually uses MORE memory
+      # "--no-zygote"      - Slower startup
+    ],
+    javascript_enabled: false,
+    print_background: false, # Reduce memory for backgrounds
+    display_header_footer: false #
   }
 
   if !(Rails.env.development? || Rails.env.test?)
-    config.options[:executable_path] = "google-chrome"
+    config.options[:executable_path] = "/usr/bin/google-chrome"
   end
 end
