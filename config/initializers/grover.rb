d99f76048803d4c93a4b850bd23a19fb22601623
Minakshi Zambre <minanajardhane@gmail.com>
2025-06-20 12:57:36 +0530

    Grover gem add Browserless for testing purpose

--- config/initializers/grover.rb
+++ config/initializers/grover.rb
@@ -10,40 +10,19 @@ Grover.configure do |config|
     prefer_css_page_size: true,
     emulate_media: "screen",
     cache: false,
-    timeout: 60000, # Timeout in ms. A value of `0` means 'no timeout'
+    timeout: 0, # Timeout in ms. A value of `0` means 'no timeout'
     wait_until: "domcontentloaded",
-    launch_args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-gpu",
-      "--disable-dev-shm-usage", # Critical for low memory
-      "--disable-background-timer-throttling",
-      "--disable-backgrounding-occluded-windows",
-      "--disable-renderer-backgrounding",
-      "--disable-extensions",
-      "--disable-plugins",
-      "--disable-default-apps",
-      "--disable-sync",
-      "--disable-translate",
-      "--hide-scrollbars",
-      "--mute-audio",
-      "--no-first-run",
-      "--safebrowsing-disable-auto-update",
-      "--disable-ipc-flooding-protection",
-      # Memory limits
-      "--memory-pressure-off",
-      "--max_old_space_size=256", # Limit V8 heap to 256MB
-      "--aggressive-cache-discard",
-      # Remove these problematic flags:
-      # "--single-process"  - Actually uses MORE memory
-      # "--no-zygote"      - Slower startup
-    ],
-    javascript_enabled: false,
-    print_background: false, # Reduce memory for backgrounds
-    display_header_footer: false #
+    launch_args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    # Use browserless.io
+    host: "chrome.browserless.io",
+    port: 443,
+    protocol: "https",
+    # path: '/pdf',
+    # Add your API token
+    query: { token: "2SWqkp0G9CeF4tDbf7cba252f94922e9cf1449b4852b6cb86" }
   }
 
-  if !(Rails.env.development? || Rails.env.test?)
-    config.options[:executable_path] = "/usr/bin/google-chrome"
-  end
+  # if !(Rails.env.development? || Rails.env.test?)
+  #   config.options[:executable_path] = "google-chrome"
+  # end
 end
