b36803b68f8fadfc24df4aa2c64fb4aa0617697b
Vipul A M <vipulnsward@gmail.com>
2025-08-09 19:03:37 +0530

    Fix missing TIME_INTERVAL constant in Admin::SettingsController
    The TIME_INTERVAL constant was being referenced but not defined in the controller.
    Added the constant definition to fix the NameError.

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -2,6 +2,7 @@
 
 module Admin
   class SettingsController < Admin::ApplicationController
+    TIME_INTERVAL = Setting::TIME_INTERVAL
     # Overwrite any of the RESTful controller actions to implement custom behavior
     # For example, you may want to send an email after a foo is updated.
     #
