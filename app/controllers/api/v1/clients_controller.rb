241ecf7b22e871746c795e1cf569d0268d54e47b
Vipul A M <vipulnsward@gmail.com>
2025-08-25 23:44:23 +0200

    fix: rubocop violations autocorrected
    - Fix trailing whitespace
    - Fix missing newline at end of file
    - Use numeric HTTP status code 201 instead of :created

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -32,7 +32,7 @@ class Api::V1::ClientsController < Api::V1::ApplicationController
   def create
     authorize Client
     client = Client.create!(client_params)
-    render :create, locals: { client:, address: client.current_address }, status: :created
+    render :create, locals: { client:, address: client.current_address }, status: 201
   end
 
   def add_client_contact
