e13b8e0c0ca2f88b26fe506ea5a4207bb88e6acd
Vipul A M <vipulnsward@gmail.com>
2025-09-06 11:25:25 +0200

    fix: resolve JavaScript linting issues and verify build

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def app_name
-    @app_name ||= Rails.configuration.x.app_name || 'Miru Agency OS'
+    @app_name ||= Rails.configuration.x.app_name || "Miru Agency OS"
   end
 end
