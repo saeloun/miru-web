f31f66d226fd0ec162905b95077a8f8a400f12f1
Vipul A M <vipulnsward@gmail.com>
2026-03-06 21:39:07 +0530

    fix(cable): migrate solid cable to single database

---
+++ db/migrate/20260306160730_create_solid_cable_messages.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class CreateSolidCableMessages < ActiveRecord::Migration[8.0]
+  def change
+    create_table :solid_cable_messages do |t|
+      t.binary :channel, limit: 1024, null: false
+      t.binary :payload, limit: 536_870_912, null: false
+      t.datetime :created_at, null: false
+      t.bigint :channel_hash, null: false
+
+      t.index :channel
+      t.index :channel_hash
+      t.index :created_at
+    end
+  end
+end
