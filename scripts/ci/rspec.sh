7d84e4953650c92104fc83768221608653a4bde1
Vipul A M <vipulnsward@gmail.com>
2026-03-20 18:36:17 +0530

    fix(ci): align test setup with refreshed schema

--- scripts/ci/rspec.sh
+++ scripts/ci/rspec.sh
@@ -6,6 +6,6 @@ bundle config set --local with 'ci'
 bundle config set --local without 'development production test'
 bin/yarn install
 bin/rails db:create
-bin/rails db:migrate
+bin/rails runner 'ActiveRecord::Base.connection_pool.migration_context.migrate'
 bundle exec rspec --color --exclude-pattern "spec/system/**/*.rb"
 bundle config set --local without ''
--- scripts/ci/system_spec.sh
+++ scripts/ci/system_spec.sh
@@ -6,7 +6,7 @@ bundle config set --local with 'ci'
 bundle config set --local without 'development production test'
 bin/yarn install
 bin/rails db:create
-bin/rails db:migrate
+bin/rails runner 'ActiveRecord::Base.connection_pool.migration_context.migrate'
 bin/rails assets:precompile
 
 bundle exec rspec --color spec/system/
