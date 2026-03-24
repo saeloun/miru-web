5c43a3c2978001ad0a89deabf5d1c9b35f9aecfc
Vipul A M <vipulnsward@gmail.com>
2026-03-24 16:00:34 +0530

    fix(auth): stabilize root formats and oauth host

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,6 +9,13 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  canonical_base_url = ENV["APP_BASE_URL"].presence
+  if canonical_base_url
+    OmniAuth.config.full_host = lambda do |_env|
+      canonical_base_url
+    end
+  end
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
