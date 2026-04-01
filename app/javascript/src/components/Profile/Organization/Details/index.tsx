bf9b102c6de968c73febb65bbcf1022dcd9c609f
Vipul A M <vipulnsward@gmail.com>
2026-04-01 17:52:23 +0530

    refactor(profile): trim duplicate organization wrapper logic

--- app/javascript/src/components/Profile/Organization/Details/index.tsx
+++ app/javascript/src/components/Profile/Organization/Details/index.tsx
@@ -1,104 +1,17 @@
-import React, { useEffect, useState } from "react";
-
-import { companiesApi } from "apis/api";
+import React, { useEffect } from "react";
 import { useUserContext } from "context/UserContext";
 import { useNavigate } from "react-router-dom";
 import { sendGAPageView } from "utils/googleAnalytics";
-import worldCountries from "world-countries";
 
 import OrganizationSettingsPage from "./ModernOrganizationSettings";
-
-const initialState = {
-  id: null,
-  logoUrl: "",
-  companyName: "",
-  companyAddr: "",
-  companyPhone: "",
-  countryName: "",
-  companyCurrency: "",
-  companyRate: 0.0,
-  companyFiscalYear: "",
-  companyDateFormat: "",
-  companyTimezone: "",
-  logo: null,
-  companyWorkingHours: "",
-  companyWorkingDays: "",
-};
 
 const OrgDetails = () => {
   const navigate = useNavigate();
   const { isDesktop } = useUserContext();
-  const [orgDetails, setOrgDetails] = useState(initialState);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const getData = async () => {
-    setIsLoading(true);
-    const res = await companiesApi.index();
-    const companyDetails = { ...res.data.company_details };
-    const {
-      logo,
-      name,
-      address,
-      business_phone,
-      currency,
-      standard_price,
-      fiscal_year_end,
-      date_format,
-      timezone,
-      id,
-      working_hours,
-      working_days,
-    } = companyDetails;
-
-    const { address_line_1, address_line_2, city, state, pin, country } =
-      address;
-
-    const {
-      name: { common: CountryName },
-    } = country
-      ? worldCountries.find(worldCountry => worldCountry["cca2"] === country)
-      : { name: { common: "" } };
-
-    const companyAddrParts = [
-      address_line_1,
-      address_line_2,
-      city,
-      state,
-      pin,
-      CountryName,
-    ];
-
-    const companyAddr = companyAddrParts
-      .filter(part => part !== null && part !== undefined && part !== "")
-      .join(", ");
-
-    setOrgDetails({
-      logoUrl: logo,
-      companyName: name,
-      companyAddr,
-      companyPhone: business_phone,
-      countryName: address.country,
-      companyCurrency: currency,
-      companyRate: parseFloat(standard_price),
-      companyFiscalYear: fiscal_year_end,
-      companyDateFormat: date_format,
-      companyTimezone: timezone,
-      id,
-      logo: null,
-      companyWorkingHours: working_hours,
-      companyWorkingDays: working_days,
-    });
-    setIsLoading(false);
-  };
 
   useEffect(() => {
     sendGAPageView();
-    getData();
   }, []);
-
-  const handleEditClick = () => {
-    navigate(`/settings/profile/organization/edit`, { replace: true });
-  };
 
   const handleBackBtnClick = () => {
     navigate(isDesktop ? `/settings/organization` : "/settings", {
--- app/javascript/src/components/Profile/Organization/Payment/index.tsx
+++ app/javascript/src/components/Profile/Organization/Payment/index.tsx
@@ -1,8 +1,4 @@
-import { ApiStatus as PaymentSettingsStatus } from "constants/index";
-
-import React, { useState, useEffect } from "react";
-
-import { paymentSettings } from "apis/api";
+import React, { useEffect } from "react";
 import { useUserContext } from "context/UserContext";
 import { useNavigate } from "react-router-dom";
 import { sendGAPageView } from "utils/googleAnalytics";
@@ -10,43 +6,10 @@ import { sendGAPageView } from "utils/googleAnalytics";
 import OrganizationPaymentSettingsPage from "./ModernPaymentSettings";
 
 const PaymentSettings = () => {
-  const [status, setStatus] = React.useState<PaymentSettingsStatus>(
-    PaymentSettingsStatus.IDLE
-  );
   const navigate = useNavigate();
   const { isDesktop } = useUserContext();
-  const [accountLink, setAccountLink] = useState<string>("");
-  const [isStripeConnected, setIsStripeConnected] = useState<boolean>(null);
-  const [showDisconnectDialog, setShowDisconnectDialog] =
-    useState<boolean>(false);
-
-  const connectStripe = async () => {
-    const res = await paymentSettings.connectStripe();
-    setAccountLink(res.data.accountLink);
-  };
-
-  const fetchPaymentSettings = async () => {
-    try {
-      setStatus(PaymentSettingsStatus.LOADING);
-      const res = await paymentSettings.get();
-      setIsStripeConnected(res.data.providers.stripe.connected);
-      setStatus(PaymentSettingsStatus.SUCCESS);
-    } catch {
-      setStatus(PaymentSettingsStatus.ERROR);
-    }
-  };
 
   useEffect(() => sendGAPageView(), []);
-
-  useEffect(() => {
-    if (accountLink) {
-      window.location.href = accountLink;
-    }
-  }, [accountLink]);
-
-  useEffect(() => {
-    fetchPaymentSettings();
-  }, [isStripeConnected]);
 
   const handleBackBtnClick = () => {
     navigate(isDesktop ? "/settings/profile" : "/settings", { replace: true });
