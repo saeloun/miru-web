f7d5192aebbf237ea0c0ed4eb98e9e4b2de5e135
Vipul A M <vipulnsward@gmail.com>
2026-07-30 02:17:20 +0530

    fix: keep demo passwords out of direct storage

--- spec/scripts/production_demo/ensure_matrix_spec.rb
+++ spec/scripts/production_demo/ensure_matrix_spec.rb
@@ -12,4 +12,20 @@ RSpec.describe "production demo matrix" do
   ensure
     ENV["DEMO_MATRIX_PASSWORD"] = password if password
   end
+
+  it "hashes the demo password before saving users" do
+    original_password = ENV["DEMO_MATRIX_PASSWORD"]
+    ENV["DEMO_MATRIX_PASSWORD"] = "Demo password 123!"
+    create(:company, name: "Saeloun Inc")
+
+    expect {
+      load Rails.root.join("script/production_demo/ensure_matrix.rb")
+    }.to output.to_stdout
+
+    user = User.find_by!(email: "vipul@saeloun.com")
+    expect(user).to be_valid_password("Demo password 123!")
+    expect(user.encrypted_password).not_to include("Demo password 123!")
+  ensure
+    ENV["DEMO_MATRIX_PASSWORD"] = original_password
+  end
 end
